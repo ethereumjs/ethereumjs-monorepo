e1dca17b36e5dc510e74e6c3daba96d7a90c28fc
wanderer <mjbecze@gmail.com>
2015-04-20 18:16:46 -0400

    clean up streams

--- readStream.js
+++ readStream.js
@@ -16,19 +16,20 @@ util.inherits(internals.ReadStream, Readable);
 
 internals.ReadStream.prototype._read = function () {
   var self = this;
-  if (this.next) {
+  if (this.next && !this.started) {
+    //unpause stream
     this.next();
   } else if (!this.started) {
     this.started = true;
     this.trie._findAll(this.trie.root, [], function (val, key, onDone) {
       key = TrieNode.nibblesToBuffer(key);
       self.next = onDone;
-      if (val) {
-        self.push({
-          key: key,
-          value: val.value
-        });
-      }
+      self.started = self.push({
+        key: key,
+        value: val.value
+      });
+
+      if(self.started) onDone();
     }, function () {
       self.push(null);
     });
