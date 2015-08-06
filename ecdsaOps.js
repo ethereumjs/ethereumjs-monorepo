e458114c72a7f67d2b8561e30302fd4ff9eb12e8
wanderer <mjbecze@gmail.com>
2015-08-06 23:33:06 +0000

    bump secp256k1 version

--- ecdsaOps.js
+++ ecdsaOps.js
@@ -8,9 +8,17 @@ const utils = require('ethereumjs-util')
  */
 exports.txVerifySignature = function() {
   var msgHash = this.hash(false)
-  var sig = Buffer.concat([utils.pad(this.r, 32), utils.pad(this.s, 32)], 64)
+  var sig = {
+    signature: Buffer.concat([utils.pad(this.r, 32), utils.pad(this.s, 32)], 64),
+    recovery: utils.bufferToInt(this.v) - 27
+  }
 
-  this._senderPubKey = ecdsa.recoverCompact(msgHash, sig, utils.bufferToInt(this.v) - 27)
+  try{
+    this._senderPubKey = ecdsa.recover(msgHash, sig, false)
+  }catch(e){
+    return false 
+  }
+
   if (this._senderPubKey && this._senderPubKey.toString('hex') !== '') 
     return true
   else 
@@ -23,12 +31,12 @@ exports.txVerifySignature = function() {
  * @param {Buffer} privateKey
  */
 exports.txSign = function(privateKey) {
-  var msgHash = this.hash(false),
-    sig = ecdsa.signCompact(privateKey, msgHash)
+  var msgHash = this.hash(false)
+  var sig = ecdsa.sign(msgHash, privateKey)
 
-  this.r = sig.r
-  this.s = sig.s
-  this.v = sig.recoveryId + 27
+  this.r = sig.signature.slice(0, 32)
+  this.s = sig.signature.slice(32, 64)
+  this.v = sig.recovery + 27
 }
 
 /**
@@ -37,7 +45,6 @@ exports.txSign = function(privateKey) {
  * @return {Buffer}
  */
 exports.txGetSenderPublicKey = function() {
-
   if (!this._senderPubKey || !this._senderPubKey.length)
     this.verifySignature()
 
@@ -53,9 +60,14 @@ exports.txGetSenderPublicKey = function() {
  * @return {Buffer}         public key otherwise null
  */
 exports.ecrecover = function(msgHash, v, r, s) {
-  var sig = Buffer.concat([utils.pad(r, 32), utils.pad(s, 32)], 64)
-  var recid = utils.bufferToInt(v) - 27 
-  var senderPubKey = ecdsa.recoverCompact(msgHash, sig, recid)
+  var sig = { 
+    signature: Buffer.concat([utils.pad(r, 32), utils.pad(s, 32)], 64),
+    recovery: utils.bufferToInt(v) - 27 
+  }
+
+  console.log('here!');
+  var senderPubKey = ecdsa.recover(msgHash, sig, true)
+  console.log(senderPubKey)
 
   if (senderPubKey && senderPubKey.toString('hex') !== '')
     return senderPubKey
