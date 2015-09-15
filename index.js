55673aad64bc775f997c00fca8d05afed396f65e
wanderer <mjbecze@gmail.com>
2015-09-15 17:52:00 +0000

    added standard js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const assert = require('assert')
 const rlp = require('rlp')
 const BN = require('bn.js')
 
-//the max interger that this VM can handle
+// the max interger that this VM can handle
 exports.MAX_INTEGER = new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 16)
 exports.TWO_POW256 = new BN('115792089237316195423570985008687907853269984665640564039457584007913129639936')
 
-//hex string of SHA3-256 hash of `null`
+// hex string of SHA3-256 hash of `null`
 exports.SHA3_NULL = 'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
 
-//SHA3-256 hash of the rlp of []
+// SHA3-256 hash of the rlp of []
 exports.SHA3_RLP_ARRAY = '1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347'
 
-//SHA3-256 hash of the rlp of `null`
+// SHA3-256 hash of the rlp of `null`
 exports.SHA3_RLP = '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421'
 
 exports.BN = BN
@@ -26,10 +26,10 @@ exports.rlp = rlp
  * @param {Integer} bytes  the number of bytes the buffer should be
  * @return {Buffer}
  */
-exports.zeros = function(bytes) {
-  var buf = new Buffer(bytes);
-  buf.fill(0);
-  return buf;
+exports.zeros = function (bytes) {
+  var buf = new Buffer(bytes)
+  buf.fill(0)
+  return buf
 }
 
 /**
@@ -39,12 +39,12 @@ exports.zeros = function(bytes) {
  * @param {Integer}  length the number of bytes the output should be
  * @return {Buffer|Array}
  */
-exports.pad = function(msg, length) {
+exports.pad = function (msg, length) {
   if (msg.length < length) {
     var buf = exports.zeros(length)
     msg.copy(buf, length - msg.length)
     return buf
-  } 
+  }
   return msg.slice(-length)
 }
 
@@ -54,7 +54,7 @@ exports.pad = function(msg, length) {
  * @param {Buffer|Array|String}
  * @return {Buffer|Array|String}
  */
-exports.unpad = function(a) {
+exports.unpad = function (a) {
   var first = a[0]
   while (a.length > 0 && first.toString() === '0') {
     a = a.slice(1)
@@ -69,12 +69,13 @@ exports.unpad = function(a) {
  * @param {Number}
  * @return {String}
  */
-exports.intToHex = function(i) {
+exports.intToHex = function (i) {
   assert(i % 1 === 0, 'number is not a interger')
   assert(i >= 0, 'number must be positive')
   var hex = i.toString(16)
-  if (hex.length % 2)
+  if (hex.length % 2) {
     hex = '0' + hex
+  }
 
   return hex
 }
@@ -85,7 +86,7 @@ exports.intToHex = function(i) {
  * @param {Number}
  * @return {Buffer}
  */
-exports.intToBuffer = function(i) {
+exports.intToBuffer = function (i) {
   var hex = exports.intToHex(i)
   return new Buffer(hex, 'hex')
 }
@@ -96,10 +97,11 @@ exports.intToBuffer = function(i) {
  * @par[MaÅam {B[M`Êuffer}
  * @return {Number}
  */
-exports.bufferToInt = function(buf) {
-  if (buf.length === 0)
+exports.bufferToInt = function (buf) {
+  if (buf.length === 0) {
     return 0
-  
+  }
+
   return parseInt(buf.toString('hex'), 16)
 }
 
@@ -109,9 +111,10 @@ exports.bufferToInt = function(buf) {
  * @param {Buffer} num
  * @return {BN}
  */
-exports.fromSigned = function(num) {
-  if (num.length === 32 && num[0] >= 128)
+exports.fromSigned = function (num) {
+  if (num.length === 32 && num[0] >= 128) {
     return new BN(num).sub(exports.TWO_POW256)
+  }
 
   return new BN(num)
 }
@@ -122,20 +125,21 @@ exports.fromSigned = function(num) {
  * @param {Bignum} num
  * @return {Buffer}
  */
-exports.toUnsigned = function(num) {
-  if (num.cmpn(0) === -1) 
+exports.toUnsigned = function (num) {
+  if (num.cmpn(0) === -1) {
     return new Buffer(num.add(exports.TWO_POW256).toArray())
-  
+  }
+
   return new Buffer(num.toArray())
 }
 
-exports.sha3 = function(a, bytes) {
-  if(!bytes) bytes = 256
+exports.sha3 = function (a, bytes) {
+  if (!bytes) bytes = 256
 
   var h = new SHA3.SHA3Hash(bytes)
-  if (a)
+  if (a) {
     h.update(a)
-
+  }
   return new Buffer(h.digest('hex'), 'hex')
 }
 
@@ -145,7 +149,7 @@ exports.sha3 = function(a, bytes) {
  * @param {Buffer}
  * @return {Buffer}
  */
-exports.pubToAddress = exports.publicToAddress = function(pubKey) {
+exports.pubToAddress = exports.publicToAddress = function (pubKey) {
   var hash = new SHA3.SHA3Hash(256)
   hash.update(pubKey.slice(-64))
   return new Buffer(hash.digest('hex').slice(-40), 'hex')
@@ -157,7 +161,7 @@ exports.pubToAddress = exports.publicToAddress = function(pubKey) {
  * @param {Buffer} privateKey
  * @return {Buffer}
  */
-var privateToPublic = exports.privateToPublic = function(privateKey){
+var privateToPublic = exports.privateToPublic = function (privateKey) {
   privateKey = new BN(privateKey)
   var key = ec.keyFromPrivate(privateKey).getPublic().toJSON()
   return new Buffer(key[0].toArray().concat(key[1].toArray()))
@@ -169,7 +173,7 @@ var privateToPublic = exports.privateToPublic = function(privateKey){
  * @param {Buffer} privateKey
  * @return {Buffer}
  */
-exports.privateToAddress = function(privateKey){
+exports.privateToAddress = function (privateKey) {
   return exports.publicToAddress(privateToPublic(privateKey))
 }
 
@@ -179,11 +183,12 @@ exports.privateToAddress = function(privateKey){
  * @param {Buffer} from the address which is creating this new address
  * @param {Buffer} nonce the nonce of the from account
  */
-exports.generateAddress = function(from, nonce) {
+exports.generateAddress = function (from, nonce) {
   nonce = new Buffer(new BN(nonce).subn(1).toArray())
 
-  if (nonce.toString('hex') === '00') 
-    nonce = 0;
+  if (nonce.toString('hex') === '00') {
+    nonce = 0
+  }
 
   var hash = exports.sha3(rlp.encode([new Buffer(from, 'hex'), nonce]))
   return hash.slice(12)
@@ -195,18 +200,18 @@ exports.generateAddress = function(from, nonce) {
  * @para[M`Êm {Object} self the `Object` to define properties on
  * @param {Array} fields an array fields to define
  */
-exports.defineProperties = function(self, fields, data) {
-
+exports.defineProperties = function (self, fields, data) {
   self.raw = []
   self._fields = []
 
-  self.toJSON = function(label){
+  self.toJSON = function (label) {
     if (label) {
       var obj = {}
 
       for (var prop in this) {
-        if (typeof this[prop] !== 'function' && prop !== 'raw' && prop !== '_fields')
+        if (typeof this[prop] !== 'function' && prop !== 'raw' && prop !== '_fields') {
           obj[prop] = this[prop].toString('hex')
+        }
       }
       return obj
     }
@@ -214,72 +219,82 @@ exports.defineProperties = function(self, fields, data) {
     return exports.baToJSON(this.raw)
   }
 
-  fields.forEach(function(field, i) {
+  fields.forEach(function (field, i) {
     self._fields.push(field.name)
     Object.defineProperty(self, field.name, {
       enumerable: true,
       configurable: true,
-      get: function() {
+      get: function () {
         return this.raw[i]
       },
-      set: function(v) {
+      set: function (v) {
         if (!Buffer.isBuffer(v)) {
-          if (typeof v === 'string')
+          if (typeof v === 'string') {
             v = new Buffer(padToEven(exports.stripHexPrefix(v)), 'hex')
-          else if (typeof v === 'number')
+          } else if (typeof v === 'number') {
             v = exports.intToBuffer(v)
-          else if (v === null || v === undefined)
+          } else if (v === null || v === undefined) {
             v = new Buffer([])
-          else if (v.toArray) 
-            //converts a BN to a Buffer
+          } else if (v.toArray) {
+            // converts a BN to a Buffer
             v = new Buffer(v.toArray())
-          else
+          } else {
             throw new Error('invalid type')
+          }
         }
 
-        if(v.toString('hex') === '00' &&  field.noZero)
+        if (v.toString('hex') === '00' && field.noZero) {
           v = new Buffer([])
+        }
 
-        if(field.word && new BN(v).cmp(exports.MAX_INTEGER) === 1)
-          throw(new Error('to large of value'))
+        if (field.word && new BN(v).cmp(exports.MAX_INTEGER) === 1) {
+          throw new Error('to large of value')
+        }
 
-        if(!(field.empty && v.length === 0) && field.pad && v.length < field.length)
+        if (!(field.empty && v.length === 0) && field.pad && v.length < field.length) {
           v = exports.pad(v, field.length)
+        }
 
-        if(field.allowLess && field.length){
+        if (field.allowLess && field.length) {
           assert(field.length >= v.length)
-        } else if (!(field.empty && v.length === 0) && field.length)
+        } else if (!(field.empty && v.length === 0) && field.length) {
           assert(field.length === v.length, 'The field ' + field.name + 'must have byte length of ' + field.length)
+        }
 
         this.raw[i] = v
       }
     })
 
-    if(field.default)
+    if (field.default) {
       self[field.name] = field.default
+    }
   })
 
   if (data) {
-    if(typeof data === 'string')
+    if (typeof data === 'string') {
       data = new Buffer(exports.stripHexPrefix(data), 'hex')
+    }
 
-    if (Buffer.isBuffer(data))
+    if (Buffer.isBuffer(data)) {
       data = rlp.decode(data)
+    }
 
     if (Array.isArray(data)) {
-      if(data.length > self._fields.length) 
-        throw(new Error('wrong number of fields in data'))
-
-      //make sure all the items are buffers
-      data.forEach(function(d, i) {
-         self[self._fields[i]] = typeof d === 'string' ? new Buffer(d, 'hex') : d
-      })
-    } else if(typeof data === 'object') {
-      for (var prop in data) {
-        if (self._fields.indexOf(prop) !== -1)
-          self[prop] = data[prop]
+      if (data.length > self._fields.length) {
+        throw (new Error('wrong number of fields in data'))
       }
-    } else{
+
+      // make sure all the items are buffers
+      data.forEach(function (d, i) {
+        self[self._fields[i]] = typeof d === 'string' ? new Buffer(d, 'hex') : d
+      })
+    } else if (typeof data === 'object') {
+      for (var prop in data) {
+        if (self._fields.indexOf(prop) !== -1) {
+          self[prop] = data[prop]
+        }
+      }
+    } else {
       throw new Error('invalid data')
     }
   }
@@ -290,12 +305,13 @@ exports.defineProperties = function(self, fields, data) {
  * @method printBA
  * @param {Buffer|Array}
  */
-exports.printBA = function(ba) {
+exports.printBA = function (ba) {
   if (Buffer.isBuffer(ba)) {
-    if (ba.length === 0) 
+    if (ba.length === 0) {
       console.log('new Buffer(0)')
-    else 
-      console.log('new Buffer(\'' + ba.toString('hex') + '\', \'hex\')')
+    } else {
+      console.log("new Buffer('" + ba.toString('hex') + "', 'hex')")
+    }
   } else if (ba instanceof Array) {
     console.log('[')
     for (var i = 0; i < ba.length; i++) {
@@ -303,8 +319,9 @@ exports.printBA = function(ba) {
       console.log(',')
     }
     console.log(']')
-  } else 
+  } else {
     console.log(ba)
+  }
 }
 
 /**
@@ -312,7 +329,7 @@ exports.printBA = function(ba) {
  * @method BAToJSON
  * @param {Buffer|Array}
  */
-exports.baToJSON = function(ba) {
+exports.baToJSON = function (ba) {
   if (Buffer.isBuffer(ba)) {
     return ba.toString('hex')
   } else if (ba instanceof Array) {
@@ -324,26 +341,28 @@ exports.baToJSON = function(ba) {
   }
 }
 
-//Returns a Boolean on whether or not the a sting starts with 0x
-exports.isHexPrefixed = function(str){
+// Returns a Boolean on whether or not the a sting starts with 0x
+exports.isHexPrefixed = function (str) {
   return str.slice(0, 2) === '0x'
 }
 
-//Removes 0x from a given String
-exports.stripHexPrefix = function(str){
-  if (typeof str !== 'string')
-     return str
-  return exports.isHexPrefixed(str) ?  str.slice(2) : str
+// Removes 0x from a given String
+exports.stripHexPrefix = function (str) {
+  if (typeof str !== 'string') {
+    return str
+  }
+  return exports.isHexPrefixed(str) ? str.slice(2) : str
 }
 
-//Adds 0x to a given string if it does not already start with 0x
-exports.addHexPrefix = function(str){
-  if (typeof str !== 'string')
-     return str
+// Adds 0x to a given string if it does not already start with 0x
+exports.addHexPrefix = function (str) {
+  if (typeof str !== 'string') {
+    return str
+  }
   return exports.isHexPrefixed(str) ? '0x' + str : str
 }
 
-function padToEven(a){
-  if (a.length % 2) a = '0' + a;
+function padToEven (a) {
+  if (a.length % 2) a = '0' + a
   return a
 }
