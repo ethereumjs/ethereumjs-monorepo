a1fb8c79f6702b4e623d5ac0046f2047bfd27ad5
wanderer <mjbecze@gmail.com>
2015-09-15 05:26:07 +0000

    updated deps

--- test/transactions.js
+++ test/transactions.js
@@ -1,75 +1,85 @@
-const tape = require('tape');
-const utils = require('ethereumjs-util');
+const tape = require('tape')
+const utils = require('ethereumjs-util')
 const rlp = utils.rlp
-const Transaction = require('../index.js');
-const txFixtures = require('./txs.json');
+const Transaction = require('../index.js')
+const txFixtures = require('./txs.json')
 
 tape('[Transaction]: Basic functions', function(t) {
-  var transactions = [];
+  var transactions = []
+
   t.test('should decode transactions', function(st) {
     txFixtures.forEach(function(tx) {
-      var pt = new Transaction(tx.raw);
-      st.equal(pt.nonce.toString('hex'), tx.raw[0]);
-      st.equal(pt.gasPrice.toString('hex'), tx.raw[1]);
-      st.equal(pt.gasLimit.toString('hex'), tx.raw[2]);
-      st.equal(pt.to.toString('hex'), tx.raw[3]);
-      st.equal(pt.value.toString('hex'), tx.raw[4]);
-      st.equal(pt.v.toString('hex'), tx.raw[6]);
-      st.equal(pt.r.toString('hex'), tx.raw[7]);
-      st.equal(pt.s.toString('hex'), tx.raw[8]);
-      st.equal(pt.data.toString('hex'), tx.raw[5]);
-      transactions.push(pt);
-    });
-    st.end();
-  });
+      var pt = new Transaction(tx.raw)
+      st.equal(pt.nonce.toString('hex'), tx.raw[0])
+      st.equal(pt.gasPrice.toString('hex'), tx.raw[1])
+      st.equal(pt.gasLimit.toString('hex'), tx.raw[2])
+      st.equal(pt.to.toString('hex'), tx.raw[3])
+      st.equal(pt.value.toString('hex'), tx.raw[4])
+      st.equal(pt.v.toString('hex'), tx.raw[6])
+      st.equal(pt.r.toString('hex'), tx.raw[7])
+      st.equal(pt.s.toString('hex'), tx.raw[8])
+      st.equal(pt.data.toString('hex'), tx.raw[5])
+      transactions.push(pt)
+    })
+    st.end()
+  })
 
   t.test('should serialize', function(st) {
     transactions.forEach(function(tx) {
-      st.deepEqual(tx.serialize(), rlp.encode(tx.raw));
-    });
-    st.end();
-  });
+      st.deepEqual(tx.serialize(), rlp.encode(tx.raw))
+    })
+    st.end()
+  })
 
   t.test('should verify Signatures', function(st) {
     transactions.forEach(function(tx) {
-      st.equals(tx.verifySignature(), true);
-    });
-    st.end();
-  });
+      st.equals(tx.verifySignature(), true)
+    })
+    st.end()
+  })
 
   t.test('should  not verify Signatures', function(st) {
     transactions.forEach(function(tx) {
-      tx.s = utils.zeros(32);
-      st.equals(tx.verifySignature(), false);
-    });
-    st.end();
-  });
+      tx.s = utils.zeros(32)
+      st.equals(tx.verifySignature(), false)
+    })
+    st.end()
+  })
 
   t.test('should sign tx', function(st) {
     transactions.forEach(function(tx, i) {
       if (txFixtures[i].privateKey) {
-        var privKey = new Buffer(txFixtures[i].privateKey, 'hex');
-        tx.sign(privKey);
+        var privKey = new Buffer(txFixtures[i].privateKey, 'hex')
+        tx.sign(privKey)
       }
-    });
-    st.end();
-  });
+    })
+    st.end()
+  })
 
   t.test('should get sender\'s address after signing it', function(st) {
     transactions.forEach(function(tx, i) {
       if (txFixtures[i].privateKey) {
-        st.equals(tx.getSenderAddress().toString('hex'), txFixtures[i].sendersAddress);
+        st.equals(tx.getSenderAddress().toString('hex'), txFixtures[i].sendersAddress)
       }
-    });
-    st.end();
-  });
+    })
+    st.end()
+  })
 
   t.test('should verify signing it', function(st) {
     transactions.forEach(function(tx, i) {
       if (txFixtures[i].privateKey) {
-        st.equals(tx.verifySignature(), true);
+        st.equals(tx.verifySignature(), true)
       }
-    });
-    st.end();
-  });
-});
+    })
+    st.end()
+  })
+
+  // t.test('should decode a ', function(st) {
+  //   transactions.forEach(function(tx, i) {
+  //     if (txFixtures[i].privateKey) {
+  //       st.equals(tx.verifySignature(), true)
+  //     }
+  //   })
+  //   st.end()
+  // })
+})
