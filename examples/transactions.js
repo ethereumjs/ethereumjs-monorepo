7c26c55e3b5ada745d60212aaa779cbd95eff6ab
wanderer <mjbecze@gmail.com>
2015-11-26 15:31:48 +0000

    updated example

--- examples/transactions.js
+++ examples/transactions.js
@@ -42,17 +42,16 @@ console.log('--------------------')
 // If you have a transaction that you want to verify you can parse it. If you got
 // it directly from the network it will be rlp encoded. You can decode you the rlp
 // module. After that you should have something like
-
 var rawTx = [
-  '00',
-  '09184e72a000',
-  '2710',
-  '0000000000000000000000000000000000000000',
-  '00',
-  '7f7465737432000000000000000000000000000000000000000000000000000000600057',
-  '1c',
-  '5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab',
-  '5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13'
+  '0x00',
+  '0x09184e72a000',
+  '0x2710',
+  '0x0000000000000000000000000000000000000000',
+  '0x00',
+  '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
+  '0x1c',
+  '0x5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab',
+  '0x5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13'
 ]
 
 var tx2 = new Transaction(rawTx)
