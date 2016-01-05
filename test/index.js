39c66fdd3b081712c7ac0474aa68a21a9c878cda
Alex Beregszaszi <alex@rtfs.hu>
2016-01-05 21:31:09 +0000

    Fix test description

--- test/index.js
+++ test/index.js
@@ -175,7 +175,7 @@ describe('pubToAddress 0x', function () {
 })
 
 describe('privateToAddress', function () {
-  it('should produce an address given a public key', function () {
+  it('should produce an address given a private key', function () {
     var address = '2f015c60e0be116b1f0cd534704db9c92118fb6a'
     // Our private key
     var privateKey = new Buffer([234, 84, 189, 197, 45, 22, 63, 136, 201, 58, 176, 97, 87, 130, 207, 113, 138, 46, 251, 158, 81, 167, 152, 154, 171, 27, 8, 6, 126, 156, 28, 95])
