00e2f6a0585bfed2e072a16feb6745767c18c70a
Alex Beregszaszi <alex@rtfs.hu>
2016-02-23 21:28:07 +0000

    Remove test for fromV1() as it contains an invalid password

--- test/index.js
+++ test/index.js
@@ -23,6 +23,7 @@ describe('.toV3()', function () {
   })
 })
 
+/*
 describe('.fromV1()', function () {
   it('should work', function () {
     var sample = '{"Address":"d4584b5f6229b7be90727b0fc8c6b91bb427821f","Crypto":{"CipherText":"07533e172414bfa50e99dba4a0ce603f654ebfa1ff46277c3e0c577fdc87f6bb4e4fe16c5a94ce6ce14cfa069821ef9b","IV":"16d67ba0ce5a339ff2f07951253e6ba8","KeyHeader":{"Kdf":"scrypt","KdfParams":{"DkLen":32,"N":262144,"P":1,"R":8,"SaltLen":32},"Version":"1"},"MAC":"8ccded24da2e99a11d48cda146f9cc8213eb423e2ea0d8427f41c3be414424dd","Salt":"06870e5e6a24e183a5c807bd1c43afd86d573f7db303ff4853d135cd0fd3fe91"},"Id":"0498f19a-59db-4d54-ac95-33901b4f1870","Version":"1"}'
@@ -30,6 +31,7 @@ describe('.fromV1()', function () {
     assert.equal(wallet.getAddressString(), '0xd4584b5f6229b7be90727b0fc8c6b91bb427821f')
   })
 })
+*/
 
 describe('.fromV3()', function () {
   it('should work with PBKDF2', function () {
