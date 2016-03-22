ea562f92238b4f3153ae907e369ee03b00e3fe21
Alex Beregszaszi <alex@rtfs.hu>
2016-03-22 01:09:46 +0000

    Fix 'v' check in ecrecover ('v' can be a Buffer)

--- index.js
+++ index.js
@@ -488,8 +488,8 @@ exports.ecsign = function (msgHash, privateKey) {
 exports.ecrecover = function (msgHash, v, r, s) {
   var signature = Buffer.concat([exports.setLength(r, 32), exports.setLength(s, 32)], 64)
   var recovery = exports.bufferToInt(v) - 27
-  if (v > 28 || v < 27) {
-    throw (new Error('Invalid v value'))
+  if (recovery !== 0 && recovery !== 1) {
+    throw new Error('Invalid signature v value')
   }
   var senderPubKey = secp256k1.recover(msgHash, signature, recovery)
   return secp256k1.publicKeyConvert(senderPubKey, false).slice(1)
