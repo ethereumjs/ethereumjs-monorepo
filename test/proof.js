f5390ece38ba4ea407070dd206594409791efd91
Jordi Baylina <jordi@baylina.cat>
2016-11-02 01:32:12 +0100

    lint

--- test/proof.js
+++ test/proof.js
@@ -1,10 +1,6 @@
-/*jslint node: true */
-"use strict";
-
 const Trie = require('../index.js')
 const async = require('async')
 const tape = require('tape')
-const ethUtil = require('ethereumjs-util')
 
 tape('simple merkle proofs generation and verification', function (tester) {
   var it = tester.test
@@ -12,37 +8,37 @@ tape('simple merkle proofs generation and verification', function (tester) {
     var trie = new Trie()
 
     async.series([
-        function(cb) {
-            trie.put("key1aa", "0123456789012345678901234567890123456789xx",cb);
-        },
-        function(cb) {
-            trie.put("key2bb", "aval2",cb);
-        },
-        function(cb) {
-            trie.put("key3cc", "aval3",cb);
-        },
-        function(cb) {
-            Trie.prove(trie, "key2bb", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key2bb", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "aval2");
-                    cb();
-                });
-            });
-        },
-        function(cb) {
-            Trie.prove(trie, "key1aa", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key1aa", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "0123456789012345678901234567890123456789xx");
-                    cb();
-                });
-            });
-        }
-    ], function(err) {
-        t.end(err);
+      function (cb) {
+        trie.put('key1aa', '0123456789012345678901234567890123456789xx', cb)
+      },
+      function (cb) {
+        trie.put('key2bb', 'aval2', cb)
+      },
+      function (cb) {
+        trie.put('key3cc', 'aval3', cb)
+      },
+      function (cb) {
+        Trie.prove(trie, 'key2bb', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key2bb', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), 'aval2')
+            cb()
+          })
+        })
+      },
+      function (cb) {
+        Trie.prove(trie, 'key1aa', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key1aa', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), '0123456789012345678901234567890123456789xx')
+            cb()
+          })
+        })
+      }
+    ], function (err) {
+      t.end(err)
     })
   })
 
@@ -50,21 +46,21 @@ tape('simple merkle proofs generation and verification', function (tester) {
     var trie = new Trie()
 
     async.series([
-        function(cb) {
-            trie.put("key1aa", "0123456789012345678901234567890123456789xx",cb);
-        },
-        function(cb) {
-            Trie.prove(trie, "key1aa", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key1aa", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "0123456789012345678901234567890123456789xx");
-                    cb();
-                });
-            });
-        }
-    ], function(err) {
-        t.end(err);
+      function (cb) {
+        trie.put('key1aa', '0123456789012345678901234567890123456789xx', cb)
+      },
+      function (cb) {
+        Trie.prove(trie, 'key1aa', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key1aa', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), '0123456789012345678901234567890123456789xx')
+            cb()
+          })
+        })
+      }
+    ], function (err) {
+      t.end(err)
     })
   })
 
@@ -72,81 +68,78 @@ tape('simple merkle proofs generation and verification', function (tester) {
     var trie = new Trie()
 
     async.series([
-        function(cb) {
-            trie.put("key1aa", "01234",cb);
-        },
-        function(cb) {
-            Trie.prove(trie, "key1aa", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key1aa", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "01234");
-                    cb();
-                });
-            });
-        }
-    ], function(err) {
-        t.end(err);
+      function (cb) {
+        trie.put('key1aa', '01234', cb)
+      },
+      function (cb) {
+        Trie.prove(trie, 'key1aa', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key1aa', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), '01234')
+            cb()
+          })
+        })
+      }
+    ], function (err) {
+      t.end(err)
     })
   })
-
 
   it('create a merkle proof and verify it whit keys in the midle', function (t) {
     var trie = new Trie()
 
     async.series([
-        function(cb) {
-            trie.put("key1aa", "0123456789012345678901234567890123456789xxx",cb);
-        },
-        function(cb) {
-            trie.put("key1", "0123456789012345678901234567890123456789Very_Long",cb);
-        },
-        function(cb) {
-            trie.put("key2bb", "aval3",cb);
-        },
-        function(cb) {
-            trie.put("key2", "short",cb);
-        },
-        function(cb) {
-            trie.put("key3cc", "aval3",cb);
-        },
-        function(cb) {
-            trie.put("key3", "1234567890123456789012345678901",cb);
-        },
-        function(cb) {
-            Trie.prove(trie, "key1", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key1", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "0123456789012345678901234567890123456789Very_Long");
-                    cb();
-                });
-            });
-        },
-        function(cb) {
-            Trie.prove(trie, "key2", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key2", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "short");
-                    cb();
-                });
-            });
-        },
-        function(cb) {
-            Trie.prove(trie, "key3", function(err, prove) {
-                if (err) return cb(err);
-                Trie.verifyProof(trie.root, "key3", prove, function(err, val) {
-                    if (err) return cb(err);
-                    t.equal(val.toString('utf8'), "1234567890123456789012345678901");
-                    cb();
-                });
-            });
-        }
-    ], function(err) {
-        t.end(err);
+      function (cb) {
+        trie.put('key1aa', '0123456789012345678901234567890123456789xxx', cb)
+      },
+      function (cb) {
+        trie.put('key1', '0123456789012345678901234567890123456789Very_Long', cb)
+      },
+      function (cb) {
+        trie.put('key2bb', 'aval3', cb)
+      },
+      function (cb) {
+        trie.put('key2', 'short', cb)
+      },
+      function (cb) {
+        trie.put('key3cc', 'aval3', cb)
+      },
+      function (cb) {
+        trie.put('key3', '1234567890123456789012345678901', cb)
+      },
+      function (cb) {
+        Trie.prove(trie, 'key1', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key1', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), '0123456789012345678901234567890123456789Very_Long')
+            cb()
+          })
+        })
+      },
+      function (cb) {
+        Trie.prove(trie, 'key2', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key2', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), 'short')
+            cb()
+          })
+        })
+      },
+      function (cb) {
+        Trie.prove(trie, 'key3', function (err, prove) {
+          if (err) return cb(err)
+          Trie.verifyProof(trie.root, 'key3', prove, function (err, val) {
+            if (err) return cb(err)
+            t.equal(val.toString('utf8'), '1234567890123456789012345678901')
+            cb()
+          })
+        })
+      }
+    ], function (err) {
+      t.end(err)
     })
   })
-
-
 })
