1d0551a0f82c0f056aacc7dfa7097cf3c6e3f62e
kumavis <kumavis@users.noreply.github.com>
2017-01-31 01:21:32 -0800

    require ethereum-common params directly

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 const utils = require('ethereumjs-util')
-const params = require('ethereum-common')
+const params = require('ethereum-common/params.json')
 const BN = utils.BN
   /**
    * An object that repersents the block header
