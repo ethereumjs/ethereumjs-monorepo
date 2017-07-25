377eaffa8b28278711bc6de9ae8044715394535e
Jared Wasinger <j-wasinger@hotmail.com>
2017-07-25 15:23:16 -0700

    fix lint

--- tests/tester.js
+++ tests/tester.js
@@ -135,11 +135,11 @@ function runTests (name, runnerArgs, cb) {
   testGetterArgs.test = argv.test
 
   runnerArgs.forkConfig = FORK_CONFIG
-  runnerArgs.debugging = true; // for BlockchainTests
+  // runnerArgs.debugging = true; // for BlockchainTests
   // runnerArgs.vmtrace = true; // for VMTests
 
-  testsStatus = {}
- 
+  var testsStatus = {}
+
   tape(name, t => {
     const runner = require(`./${name}Runner.js`)
     testing.getTestsFromArgs(name, (fileName, testName, test) => {
@@ -154,13 +154,13 @@ function runTests (name, runnerArgs, cb) {
           t.comment(`file: ${fileName} test: ${testName}`)
           runner(fileName, testsStatus, runnerArgs, test, t, resolve)
         }
-      }).catch(function(err) {
+      }).catch(function (err) {
         console.log(err)
       })
     }, testGetterArgs).then(() => {
       if (Object.keys(testsStatus).length > 0) {
-        console.log("Failed tests:")
-        for (k in testsStatus) {
+        console.log('Failed tests:')
+        for (var k in testsStatus) {
           console.log(k)
         }
       }
