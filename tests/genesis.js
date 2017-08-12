79ab903fb3bfada2b06dde3be31edd401df5b1f4
holgerd77 <Holger.Drewes@gmail.com>
2017-08-12 11:19:50 +0200

    Update the tests to run with 1.0 version of ethereumjs-testing

--- tests/genesis.js
+++ tests/genesis.js
@@ -1,14 +1,17 @@
-const genesisData = require('ethereumjs-testing').tests.basicTests.genesishashestest
+const testing = require('ethereumjs-testing')
 const tape = require('tape')
 const Block = require('../')
 
-tape('[Common]: genesis hashes tests', function (t) {
-  t.test('should generete the genesis correctly', function (st) {
-    var blockGenesis = new Block()
-    blockGenesis.setGenesisParams()
-    var rlpGenesis = blockGenesis.serialize()
-    st.strictEqual(rlpGenesis.toString('hex'), genesisData.genesis_rlp_hex, 'rlp hex mismatch')
-    st.strictEqual(blockGenesis.hash().toString('hex'), genesisData.genesis_hash)
-    st.end()
-  })
+
+
+tape('[Common]: genesis hashes tests', t => {
+  let args = {}
+  args.file = /genesishashestest/
+  const genesisData = testing.getSingleFile('BasicTests/genesishashestest.json');
+  var blockGenesis = new Block()
+  blockGenesis.setGenesisParams()
+  var rlpGenesis = blockGenesis.serialize()
+  t.strictEqual(rlpGenesis.toString('hex'), genesisData.genesis_rlp_hex, 'rlp hex mismatch')
+  t.strictEqual(blockGenesis.hash().toString('hex'), genesisData.genesis_hash)
+  t.end()
 })
