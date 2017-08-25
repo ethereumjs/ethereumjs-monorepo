61d786957f33d73eea276ff6607fef614ea41b84
holgerd77 <Holger.Drewes@gmail.com>
2017-08-25 11:40:10 +0200

    Move genesis hashes test to block test file

--- tests/block.js
+++ tests/block.js
@@ -1,21 +1,35 @@
 const tape = require('tape')
 const utils = require('ethereumjs-util')
-const Block = require('../index.js')
 const params = require('ethereum-common')
-const testData = require('./testdata.json')
+const testing = require('ethereumjs-testing')
+const Block = require('../index.js')
 
-tape('[Block]: Block functions', function (t) {
-  t.test('should be true for isGenesis', function (st) {
+
+tape('[Block]: block functions', function (t) {
+  const testData = require('./testdata.json')
+
+  t.test('should test validateTransactions', function (st) {
+    var block = new Block(Buffer.from(testData.blocks[0].rlp.slice(2), 'hex'))
+    st.equal(block.validateTransactions(), true)
+    st.end()
+  })
+  
+  t.test('should test isGenesis', function (st) {
     var block = new Block()
-    st.equal(block.isGenesis(), false)
+    st.notEqual(block.isGenesis(), true)
     block.header.number = new Buffer([])
     st.equal(block.isGenesis(), true)
     st.end()
   })
-
-  t.test('should be true for transaction validation', function (st) {
-    var block = new Block(Buffer.from(testData.blocks[0].rlp.slice(2), 'hex'))
-    st.equal(block.validateTransactions(), true, 'test validateTransactions()')
-    st.end()
+  
+  const testDataGenesis = testing.getSingleFile('BasicTests/genesishashestest.json')
+  t.test('should test genesis hashes', function(st) {
+    var genesisBlock = new Block()
+    genesisBlock.setGenesisParams()
+    var rlp = genesisBlock.serialize()
+    t.strictEqual(rlp.toString('hex'), testDataGenesis.genesis_rlp_hex, 'rlp hex match')
+    t.strictEqual(genesisBlock.hash().toString('hex'), testDataGenesis.genesis_hash, 'genesis hash match')
+    t.end()
   })
 })
+
