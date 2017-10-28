hugo-dc/ethereumjs-vm

Change fakeBlockchain.getBlock to match ethereumjs-blockchain

lib/fakeBlockChain.js
@@ -2,9 +2,16 @@ const Buffer = require('safe-buffer').Buffer
 const utils = require('ethereumjs-util')
 
 module.exports = {
-  getBlock: function (n, cb) {
-    // FIXME: this will fail on block numbers >53 bits
-    var hash = utils.sha3(Buffer.from(utils.bufferToInt(n).toString(), 'utf8'))
+  getBlock: function (blockTag, cb) {
+    var hash
+
+    if (Buffer.isBuffer(blockTag)) {
+      hash = utils.sha3(blockTag)
+    } else if (Number.isInteger(blockTag)) {
+      hash = utils.sha3(utils.toBuffer(hash).toStirng('hex'))
+    } else {
+      cb(new Error('Unknown blockTag type'))
+    }
 
     var block = {
       hash: function () {

ethereumjs/ethereumjs-monorepo

lib/fakeBlockChain.js
@@ -2,9 +2,16 @@ const Buffer = require('safe-buffer').Buffer
 const utils = require('ethereumjs-util')
 
 module.exports = {
-  getBlock: function (n, cb) {
-    // FIXME: this will fail on block numbers >53 bits
-    var hash = utils.sha3(Buffer.from(utils.bufferToInt(n).toString(), 'utf8'))
+  getBlock: function (blockTag, cb) {
+    var hash
+
+    if (Buffer.isBuffer(blockTag)) {
+      hash = utils.sha3(blockTag)
+    } else if (Number.isInteger(blockTag)) {
+      hash = utils.sha3(utils.toBuffer(blockTag).toString('hex'))
+    } else {
+      cb(new Error('Unknown blockTag type'))
+    }
 
     var block = {
       hash: function () {