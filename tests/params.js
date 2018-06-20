19f94f250d229e835d2222ee9250b657e1fe3463
holgerd77 <Holger.Drewes@gmail.com>
2018-06-20 21:44:39 +0200

    Remove leftover *Gas postfix for some gas prices

--- tests/params.js
+++ tests/params.js
@@ -4,22 +4,22 @@ const Common = require('../index.js')
 tape('[Common]: Parameter access', function (t) {
   t.test('Basic usage', function (st) {
     let c = new Common('mainnet')
-    st.equal(c.param('gasPrices', 'ecAddGas', 'byzantium'), 500, 'Should return correct value when HF directly provided')
+    st.equal(c.param('gasPrices', 'ecAdd', 'byzantium'), 500, 'Should return correct value when HF directly provided')
 
     c.setHardfork('byzantium')
-    st.equal(c.param('gasPrices', 'ecAddGas'), 500, 'Should return correct value for HF set in class')
+    st.equal(c.param('gasPrices', 'ecAdd'), 500, 'Should return correct value for HF set in class')
 
     st.end()
   })
 
   t.test('Error cases', function (st) {
     let c = new Common('mainnet')
-    st.throws(function () { c.param('gasPrices', 'ecAddGas') }, /neither a hardfork set nor provided by param$/, 'Should throw when no hardfork set or provided')
-    st.throws(function () { c.param('gasPrizes', 'ecAddGas', 'byzantium') }, /Topic gasPrizes not defined$/, 'Should throw when called with non-existing topic')
+    st.throws(function () { c.param('gasPrices', 'ecAdd') }, /neither a hardfork set nor provided by param$/, 'Should throw when no hardfork set or provided')
+    st.throws(function () { c.param('gasPrizes', 'ecAdd', 'byzantium') }, /Topic gasPrizes not defined$/, 'Should throw when called with non-existing topic')
     st.throws(function () { c.param('gasPrices', 'notexistingvalue', 'byzantium') }, /value for notexistingvalue not found$/, 'Should throw when called with non-existing value')
 
     c.setHardfork('byzantium')
-    st.equal(c.param('gasPrices', 'ecAddGas'), 500, 'Should return correct value for HF set in class')
+    st.equal(c.param('gasPrices', 'ecAdd'), 500, 'Should return correct value for HF set in class')
 
     c = new Common('mainnet', 'byzantium', ['byzantium', 'constantinople'])
     st.throws(function () { c.param('gasPrices', 'expByte', 'spuriousDragon') }, /supportedHardforks$/, 'Should throw when calling param() with an unsupported hardfork')
@@ -30,7 +30,7 @@ tape('[Common]: Parameter access', function (t) {
 
   t.test('Parameter updates', function (st) {
     let c = new Common('mainnet')
-    st.throws(function () { c.param('gasPrices', 'ecAddGas', 'spuriousDragon') }, /value for ecAddGas not found$/, 'Should throw for a value set on a later HF')
+    st.throws(function () { c.param('gasPrices', 'ecAdd', 'spuriousDragon') }, /value for ecAdd not found$/, 'Should throw for a value set on a later HF')
 
     st.equal(c.param('pow', 'minerReward', 'chainstart'), '5000000000000000000', 'Should return correct value for chain start')
     st.equal(c.param('pow', 'minerReward', 'byzantium'), '3000000000000000000', 'Should reflect HF update changes')
