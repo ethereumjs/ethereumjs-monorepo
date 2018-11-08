c1be507678b7703a48e03b8c759b7a743b4fde24
Gerald Nash⚡️ <g.nash.dev@gmail.com>
2018-11-08 15:24:12 -0500

    Add web3_* RPC endpoints (#65)
    Add web3_* rpc endpoints

---
+++ test/rpc/web3/clientVersion.js
@@ -0,0 +1,69 @@
+const test = require('tape')
+
+const { platform } = require('os')
+const request = require('supertest')
+const { startRPC, closeRPC, createManager } = require('../helpers')
+const blockChain = require('../blockChainStub.js')
+const Chain = require('../../../lib/blockchain/chain.js')
+
+function createNode () {
+  let chain = new Chain({ blockchain: blockChain({}) })
+  chain.opened = true
+  return {
+    services: [{ name: 'eth', chain: chain }]
+  }
+}
+
+test('call web3_clientVersion', t => {
+  const manager = createManager(createNode())
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'web3_clientVersion',
+    params: [],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { result } = res.body
+      const { version } = require('../../../package.json')
+      const expectedClientTitle = 'EthereumJS'
+      const expectedPackageVersion = version
+      const expectedPlatform = platform()
+      const expectedNodeVersion = `node${process.version.substring(1)}`
+
+      if (result.length === 0) {
+        throw new Error('Empty result string')
+      }
+
+      const [
+        actualClientTitle,
+        actualPackageVersion,
+        actualPlatform,
+        actualNodeVersion
+      ] = result.split('/')
+
+      if (actualClientTitle !== expectedClientTitle) {
+        throw new Error('Incorrect client title')
+      }
+      if (actualPackageVersion !== expectedPackageVersion) {
+        throw new Error('Incorrect package version')
+      }
+      if (actualPlatform !== expectedPlatform) {
+        throw new Error('Incorrect platform')
+      }
+      if (actualNodeVersion !== expectedNodeVersion) {
+        throw new Error('Incorrect Node.js version')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
---
+++ test/rpc/web3/sha3.js
@@ -0,0 +1,117 @@
+const test = require('tape')
+
+const request = require('supertest')
+const { startRPC, closeRPC, createManager } = require('../helpers')
+const blockChain = require('../blockChainStub.js')
+const Chain = require('../../../lib/blockchain/chain.js')
+
+function createNode () {
+  let chain = new Chain({ blockchain: blockChain({}) })
+  chain.opened = true
+  return {
+    services: [{ name: 'eth', chain: chain }]
+  }
+}
+
+test('call web3_sha3 with one valid parameter', t => {
+  const manager = createManager(createNode())
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'web3_sha3',
+    params: ['0x68656c6c6f20776f726c64'],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { result } = res.body
+      if (result.length === 0) {
+        throw new Error('Empty result string')
+      }
+
+      if (
+        result !==
+        '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad'
+      ) {
+        throw new Error('Hash returns incorrect value')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
+
+test('call web3_sha3 with one non-hex parameter', t => {
+  const manager = createManager(createNode())
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'web3_sha3',
+    params: ['hello world'],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { error } = res.body
+
+      if (error.code !== -32602) {
+        throw new Error('Incorrect error code')
+      }
+
+      if (
+        error.message !== 'invalid argument 0: hex string without 0x prefix'
+      ) {
+        throw new Error('Incorrect error message')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
+
+test('call web3_sha3 with no parameters', t => {
+  const manager = createManager(createNode())
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'web3_sha3',
+    params: [],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { error } = res.body
+
+      if (error.code !== -32602) {
+        throw new Error('Incorrect error code')
+      }
+
+      if (error.message !== 'missing value for required argument 0') {
+        throw new Error('Incorrect error message')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
