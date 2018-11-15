c420f30885f831826b6ce508808685011892c786
Gerald Nash <g.nash.dev@gmail.com>
2018-11-15 01:00:38 -0500

    Add net_version and net_listening RPC methods

---
+++ test/rpc/net/listening.js
@@ -0,0 +1,81 @@
+const test = require('tape')
+
+const request = require('supertest')
+const Common = require('ethereumjs-common')
+const { startRPC, closeRPC, createManager } = require('../helpers')
+const blockChain = require('../blockChainStub.js')
+const Chain = require('../../../lib/blockchain/chain.js')
+
+function createNode (opened, commonChain = new Common('mainnet')) {
+  let chain = new Chain({ blockchain: blockChain({}) })
+  chain.opened = true
+  return {
+    services: [{ name: 'eth', chain: chain }],
+    common: commonChain,
+    opened
+  }
+}
+
+test('call net_listening while listening', t => {
+  const manager = createManager(createNode(true))
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'net_listening',
+    params: [],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { result } = res.body
+      if (typeof result !== 'boolean') {
+        throw new Error('Result should be a boolean, but is not')
+      }
+
+      if (result !== true) {
+        throw new Error('Not listening, when it should be')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
+
+test('call net_listening while not listening', t => {
+  const manager = createManager(createNode(false))
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'net_listening',
+    params: [],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { result } = res.body
+      if (typeof result !== 'boolean') {
+        throw new Error('Result should be a boolean, but is not')
+      }
+
+      if (result !== false) {
+        throw new Error('Listening, when it not should be')
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
