4846ef5bef650e7eed0a9ac8355253c697c9535d
holgerd77 <Holger.Drewes@gmail.com>
2018-11-24 06:57:35 +0100

    Added support for goerli testnet

--- genesisStates/index.js
+++ genesisStates/index.js
@@ -5,10 +5,12 @@ genesisStates['names'][1] = 'mainnet'
 genesisStates['names'][3] = 'ropsten'
 genesisStates['names'][4] = 'rinkeby'
 genesisStates['names'][42] = 'kovan'
+genesisStates['names'][6284] = 'goerli'
 
 genesisStates['mainnet'] = require('./mainnet.json')
 genesisStates['ropsten'] = require('./ropsten.json')
 genesisStates['rinkeby'] = require('./rinkeby.json')
 genesisStates['kovan'] = require('./kovan.json')
+genesisStates['goerli'] = require('./goerli.json')
 
 module.exports = genesisStates
