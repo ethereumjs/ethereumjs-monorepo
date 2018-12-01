92c458f58fde23ee98a97b2c4052382bdf08d511
Dan Miller <danjm.com@gmail.com>
2018-11-30 20:58:53 -0330

    Allow specification of test suite via command line arg.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,13 @@
-require('./fake.js')
-require('./api.js')
-require('./transactionRunner.js')
+const argv = require('minimist')(process.argv.slice(2))
+
+if (argv.f) {
+  require('./fake.js')
+} else if (argv.a) {
+  require('./api.js')
+} else if (argv.t) {
+  require('./transactionRunner.js')
+} else {
+  require('./fake.js')
+  require('./api.js')
+  require('./transactionRunner.js')
+}
