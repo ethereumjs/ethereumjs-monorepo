043ff9b34313bfc0dcc3f4bcc7a1129b03d607c8
Vinay Pulim <v@pulim.com>
2018-12-02 20:36:33 -0500

    Fix bug with sender status

--- test/net/protocol/sender.js
+++ test/net/protocol/sender.js
@@ -4,7 +4,7 @@ const { Sender } = require('../../../lib/net/protocol')
 tape('[Sender]', t => {
   t.test('should get/set status', t => {
     const sender = new Sender()
-    t.deepEquals(sender.status, {}, 'empty status')
+    t.deepEquals(sender.status, null, 'empty status')
     sender.status = {id: 1}
     t.deepEquals(sender.status, {id: 1}, 'status correct')
     t.end()
