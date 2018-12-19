5db9bbe45b2ebe5f2dfcf9ab019fd99eade1ffd6
Simon Warta <simon@kullo.net>
2018-12-19 16:53:34 +0100

    Add comment on List type

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,8 @@ import BN = require('bn.js')
 
 export type Input = Buffer | string | number | Uint8Array | BN | Dictionary | List | null
 
+// Use interface extension instead of type alias to
+// make circular declaration possible.
 export interface List extends Array<Input> {}
 
 export interface Dictionary {
