989ee64faf0ce47d150de3104c81aebfbc26ba08
Sina Mahmoodi <itz.s1na@gmail.com>
2019-01-15 10:28:33 +0100

    Merge checkpoint & secure interfaces with es6 classes
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/secure.js
+++ src/secure.js
@@ -1,8 +1,10 @@
-const CheckpointTrie = require('./index')
-const secureInterface = require('./secure-interface')
+const ethUtil = require('ethereumjs-util')
+const CheckpointTrie = require('./checkpoint-trie')
 
 /**
- * You can create a secure Trie where the keys are automatically hashed using **SHA3** by using `require('merkle-patricia-tree/secure')`. It has the same methods and constructor as `Trie`.
+ * You can create a secure Trie where the keys are automatically hashed
+ * using **keccak256** by using `require('merkle-patricia-tree/secure')`.
+ * It has the same methods and constructor as `Trie`.
  * @class SecureTrie
  * @extends Trie
  * @public
@@ -10,6 +12,32 @@ const secureInterface = require('./secure-interface')
 module.exports = class SecureTrie extends CheckpointTrie {
   constructor (...args) {
     super(...args)
-    secureInterface(this)
+  }
+
+  copy () {
+    return new SecureTrie(this.db, this.root)
+  }
+
+  get (key, cb) {
+    const hash = ethUtil.keccak256(key)
+    super.get(hash, cb)
+  }
+
+  /**
+   * For a falsey value, use the original key
+   * to avoid double hashing the key.
+   */
+  put (key, val, cb) {
+    if (!val) {
+      this.del(key, cb)
+    } else {
+      const hash = ethUtil.keccak256(key)
+      super.put(hash, val, cb)
+    }
+  }
+
+  del (key, cb) {
+    const hash = ethUtil.keccak256(key)
+    super.del(hash, cb)
   }
 }
