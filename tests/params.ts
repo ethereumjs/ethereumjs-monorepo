eb42f46feee7f45c78556de7b743d5479659106c
holgerd77 <Holger.Drewes@gmail.com>
2019-01-31 14:14:39 +0100

    Extracted msg string variables on tests to get more readable formatting results

--- tests/params.ts
+++ tests/params.ts
@@ -4,111 +4,85 @@ import Common from '../src/'
 tape('[Common]: Parameter access', function(t: tape.Test) {
   t.test('Basic usage', function(st: tape.Test) {
     const c = new Common('mainnet')
-    st.equal(
-      c.param('gasPrices', 'ecAdd', 'byzantium'),
-      500,
-      'Should return correct value when HF directly provided',
-    )
+    let msg = 'Should return correct value when HF directly provided'
+    st.equal(c.param('gasPrices', 'ecAdd', 'byzantium'), 500, msg)
 
     c.setHardfork('byzantium')
-    st.equal(c.param('gasPrices', 'ecAdd'), 500, 'Should return correct value for HF set in class')
+    msg = 'Should return correct value for HF set in class'
+    st.equal(c.param('gasPrices', 'ecAdd'), 500, msg)
 
     st.end()
   })
 
   t.test('Error cases', function(st: tape.Test) {
     let c = new Common('mainnet')
-    st.throws(
-      function() {
-        c.param('gasPrices', 'ecAdd')
-      },
-      /neither a hardfork set nor provided by param$/,
-      'Should throw when no hardfork set or provided',
-    )
-    st.throws(
-      function() {
-        c.param('gasPrizes', 'ecAdd', 'byzantium')
-      },
-      /Topic gasPrizes not defined$/,
-      'Should throw when called with non-existing topic',
-    )
-    st.throws(
-      function() {
-        c.param('gasPrices', 'notexistingvalue', 'byzantium')
-      },
-      /value for notexistingvalue not found$/,
-      'Should throw when called with non-existing value',
-    )
+    let f = function() {
+      c.param('gasPrices', 'ecAdd')
+    }
+    let msg = 'Should throw when no hardfork set or provided'
+    st.throws(f, /neither a hardfork set nor provided by param$/, msg)
+
+    f = function() {
+      c.param('gasPrizes', 'ecAdd', 'byzantium')
+    }
+    msg = 'Should throw when called with non-existing topic'
+    st.throws(f, /Topic gasPrizes not defined$/, msg)
+
+    f = function() {
+      c.param('gasPrices', 'notexistingvalue', 'byzantium')
+    }
+    msg = 'Should throw when called with non-existing value'
+    st.throws(f, /value for notexistingvalue not found$/, msg)
 
     c.setHardfork('byzantium')
     st.equal(c.param('gasPrices', 'ecAdd'), 500, 'Should return correct value for HF set in class')
 
     c = new Common('mainnet', 'byzantium', ['byzantium', 'constantinople'])
-    st.throws(
-      function() {
-        c.param('gasPrices', 'expByte', 'spuriousDragon')
-      },
-      /supportedHardforks$/,
-      'Should throw when calling param() with an unsupported hardfork',
-    )
-    st.throws(
-      function() {
-        c.paramByBlock('gasPrices', 'expByte', 0)
-      },
-      /supportedHardforks$/,
-      'Should throw when calling paramByBlock() with an unsupported hardfork',
-    )
+    f = function() {
+      c.param('gasPrices', 'expByte', 'spuriousDragon')
+    }
+    msg = 'Should throw when calling param() with an unsupported hardfork'
+    st.throws(f, /supportedHardforks$/, msg)
+
+    f = function() {
+      c.paramByBlock('gasPrices', 'expByte', 0)
+    }
+    msg = 'Should throw when calling paramByBlock() with an unsupported hardfork'
+    st.throws(f, /supportedHardforks$/, msg)
 
     st.end()
   })
 
   t.test('Parameter updates', function(st: tape.Test) {
     const c = new Common('mainnet')
-    st.throws(
-      function() {
-        c.param('gasPrices', 'ecAdd', 'spuriousDragon')
-      },
-      /value for ecAdd not found$/,
-      'Should throw for a value set on a later HF',
-    )
+    let f = function() {
+      c.param('gasPrices', 'ecAdd', 'spuriousDragon')
+    }
+    let msg = 'Should throw for a value set on a later HF'
+    st.throws(f, /value for ecAdd not found$/, msg)
 
-    st.equal(
-      c.param('pow', 'minerReward', 'chainstart'),
-      '5000000000000000000',
-      'Should return correct value for chain start',
-    )
-    st.equal(
-      c.param('pow', 'minerReward', 'byzantium'),
-      '3000000000000000000',
-      'Should reflect HF update changes',
-    )
-    st.equal(
-      c.param('gasPrices', 'netSstoreNoopGas', 'constantinople'),
-      200,
-      'Should return updated sstore gas prices for constantinople',
-    )
-    st.equal(
-      c.param('gasPrices', 'netSstoreNoopGas', 'petersburg'),
-      null,
-      'Should nullify SSTORE related values for petersburg',
-    )
+    msg = 'Should return correct value for chain start'
+    st.equal(c.param('pow', 'minerReward', 'chainstart'), '5000000000000000000', msg)
+
+    msg = 'Should reflect HF update changes'
+    st.equal(c.param('pow', 'minerReward', 'byzantium'), '3000000000000000000', msg)
+
+    msg = 'Should return updated sstore gas prices for constantinople'
+    st.equal(c.param('gasPrices', 'netSstoreNoopGas', 'constantinople'), 200, msg)
+
+    msg = 'Should nullify SSTORE related values for petersburg'
+    st.equal(c.param('gasPrices', 'netSstoreNoopGas', 'petersburg'), null, msg)
 
     st.end()
   })
 
   t.test('Access by block number, paramByBlock()', function(st: tape.Test) {
     const c = new Common('mainnet', 'byzantium')
+    let msg = 'Should correctly translate block numbers into HF states (updated value)'
+    st.equal(c.paramByBlock('pow', 'minerReward', 4370000), '3000000000000000000', msg)
 
-    st.equal(
-      c.paramByBlock('pow', 'minerReward', 4370000),
-      '3000000000000000000',
-      'Should correctly translate block numbers into HF states (updated value)',
-    )
-    st.equal(
-      c.paramByBlock('pow', 'minerReward', 4369999),
-      '5000000000000000000',
-      'Should correctly translate block numbers into HF states (original value)',
-    )
+    msg = 'Should correctly translate block numbers into HF states (original value)'
+    st.equal(c.paramByBlock('pow', 'minerReward', 4369999), '5000000000000000000', msg)
 
     st.comment('-----------------------------------------------------------------')
     st.end()
