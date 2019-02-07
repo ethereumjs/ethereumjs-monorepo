6bd7d03db213e70d3c2f26a9426a2f61d75fd596
holgerd77 <Holger.Drewes@gmail.com>
2019-02-07 14:01:56 +0100

    Updated common, block and blockchain dependencies to Goerli ready versions, adjusted Common import, additional Goerli RPC test

--- test/rpc/net/version.js
+++ test/rpc/net/version.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 
 const request = require('supertest')
-const Common = require('ethereumjs-common')
+const Common = require('ethereumjs-common').default
 const { startRPC, closeRPC, createManager, createNode } = require('../helpers')
 
 test('call net_version on ropsten', t => {
@@ -145,6 +145,45 @@ test('call net_version on kovan', t => {
       if (result !== '42') {
         throw new Error(
           `Incorrect chain ID. Expected: 42, Received: ${result}`
+        )
+      }
+    })
+    .end((err, res) => {
+      closeRPC(server)
+      t.end(err)
+    })
+})
+
+test('call net_version on goerli', t => {
+  const manager = createManager(createNode({ opened: true, commonChain: new Common('goerli') }))
+  const server = startRPC(manager.getMethods())
+
+  const req = {
+    jsonrpc: '2.0',
+    method: 'net_version',
+    params: [],
+    id: 1
+  }
+
+  request(server)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(req)
+    .expect(200)
+    .expect(res => {
+      const { result } = res.body
+
+      if (typeof result !== 'string') {
+        throw new Error('Result should be a string, but is not')
+      }
+
+      if (result.length === 0) {
+        throw new Error('Empty result string')
+      }
+
+      if (result !== '5') {
+        throw new Error(
+          `Incorrect chain ID. Expected: 5, Received: ${result}`
         )
       }
     })
