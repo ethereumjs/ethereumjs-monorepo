288b5ee1f18a5f6dc582da69fb682d31882d6690
Chi Kei Chan <chikeichan@gmail.com>
2019-02-18 23:02:00 -0800

    Add tests for constants value

--- test/index.js
+++ test/index.js
@@ -3,6 +3,50 @@ const ethUtils = require('../dist/index.js')
 const BN = require('bn.js')
 const eip1014Testdata = require('./testdata/eip1014Examples.json')
 
+describe('constants', function() {
+  it('should match constants', function () {
+    assert.equal(
+      ethUtils.MAX_INTEGER.toString('hex'),
+      'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+    )
+
+    assert.equal(
+      ethUtils.TWO_POW256.toString('hex'),
+      '10000000000000000000000000000000000000000000000000000000000000000'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_NULL_S,
+      'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_NULL.toString('hex'),
+      'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_RLP_ARRAY_S,
+      '1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_RLP_ARRAY.toString('hex'),
+      '1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_RLP_S,
+      '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421'
+    )
+
+    assert.equal(
+      ethUtils.KECCAK256_RLP.toString('hex'),
+      '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421'
+    )
+  })
+})
+
 describe('zeros function', function () {
   it('should produce lots of 0s', function () {
     const z60 = ethUtils.zeros(30)
