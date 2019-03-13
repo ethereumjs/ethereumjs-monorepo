f176c246f74ccd1bee06dee5e49fc5c56b0c1d26
Sina Mahmoodi <itz.s1na@gmail.com>
2019-03-13 11:16:25 +0100

    test: run slow tests

--- tests/tester.js
+++ tests/tester.js
@@ -20,40 +20,7 @@ const skipPermanent = [
   'UncleFromSideChain' // Only BlockchainTest, same as ForkUncle, the TD is the same for two diffent branches so its not clear which one should be the finally chain
 ]
 // tests running slow (run from time to time)
-const skipSlow = [
-  'Call50000', // slow
-  'Call50000_ecrec', // slow
-  'Call50000_identity', // slow
-  'Call50000_identity2', // slow
-  'Call50000_sha256', // slow
-  'Call50000_rip160', // slow
-  'Call50000bytesContract50_1', // slow
-  'Call50000bytesContract50_2',
-  'Call1MB1024Calldepth', // slow
-  'static_Call1MB1024Calldepth', // slow
-  'static_Call50000', // slow
-  'static_Call50000_ecrec',
-  'static_Call50000_identity',
-  'static_Call50000_identity2',
-  'static_Call50000_rip160',
-  'static_Return50000_2',
-  'Callcode50000', // slow
-  'Return50000', // slow
-  'Return50000_2', // slow
-  'static_Call50000', // slow
-  'static_Call50000_ecrec', // slow
-  'static_Call50000_identity', // slow
-  'static_Call50000_identity2', // slow
-  'static_Call50000_sha256', // slow
-  'static_Call50000_rip160', // slow
-  'static_Call50000bytesContract50_1', // slow
-  'static_Call50000bytesContract50_2',
-  'static_Call1MB1024Calldepth', // slow
-  'static_Callcode50000', // slow
-  'static_Return50000', // slow
-  'static_Return50000_2', // slow
-  'QuadraticComplexitySolidity_CallDataCopy'
-]
+const skipSlow = []
 
 /*
 NOTE: VM tests have been disabled since they are generated using Frontier gas costs, and ethereumjs-vm doesn't support historical fork rules
@@ -69,39 +36,7 @@ ok 38985 valid gas usage
 # file: vmLogTest test: log0_nonEmptyMem
 */
 
-const skipVM = [
-  // slow performance tests
-  'loop-mul',
-  'loop-add-10M',
-  'loop-divadd-10M',
-  'loop-divadd-unr100-10M',
-  'loop-exp-16b-100k',
-  'loop-exp-1b-1M',
-  'loop-exp-2b-100k',
-  'loop-exp-32b-100k',
-  'loop-exp-4b-100k',
-  'loop-exp-8b-100k',
-  'loop-exp-nop-1M',
-  'loop-mulmod-2M',
-  // some VM tests fail because the js runner executes CALLs
-  // see https://github.com/ethereum/tests/wiki/VM-Tests  > Since these tests are meant only as a basic test of VM operation, the CALL and CREATE instructions are not actually executed.
-  'ABAcalls0',
-  'ABAcallsSuicide0',
-  'ABAcallsSuicide1',
-  'sha3_bigSize',
-  'CallRecursiveBomb0',
-  'CallToNameRegistrator0',
-  'CallToPrecompiledContract',
-  'CallToReturn1',
-  'PostToNameRegistrator0',
-  'PostToReturn1',
-  'callcodeToNameRegistrator0',
-  'callcodeToReturn1',
-  'callstatelessToNameRegistrator0',
-  'callstatelessToReturn1',
-  'createNameRegistrator',
-  'randomTest643' // TODO fix this
-]
+const skipVM = []
 
 if (argv.r) {
   randomized(argv.r, argv.v)
