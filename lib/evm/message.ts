3c7b58d7b2d8fae3b74bed53e7a0495305afc757
Sina Mahmoodi <itz.s1na@gmail.com>
2019-04-18 15:14:24 +0200

    Migrate message to ts

--- lib/evm/message.ts
+++ lib/evm/message.ts
@@ -1,7 +1,21 @@
-const BN = require('bn.js')
+import BN = require('bn.js')
 
-module.exports = class Message {
-  constructor (opts) {
+export default class Message {
+  to: Buffer
+  value: BN
+  caller: Buffer
+  gasLimit: BN
+  data: Buffer
+  depth: number
+  code: Buffer
+  _codeAddress: Buffer
+  isStatic: boolean
+  isCompiled: boolean
+  salt: Buffer
+  selfdestruct: any
+  delegatecall: boolean
+
+  constructor (opts: any) {
     this.to = opts.to
     this.value = opts.value ? new BN(opts.value) : new BN(0)
     this.caller = opts.caller
@@ -17,7 +31,7 @@ module.exports = class Message {
     this.delegatecall = opts.delegatecall || false
   }
 
-  get codeAddress () {
+  get codeAddress (): Buffer {
     return this._codeAddress ? this._codeAddress : this.to
   }
 }
