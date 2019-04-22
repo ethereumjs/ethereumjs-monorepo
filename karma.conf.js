8e26dd4316dee9f207366392af80c5d8d2262b99
Patricio Palladino <email@patriciopalladino.com>
2019-04-22 13:13:16 -0300

    Add comment to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
   config.set({
     browserNoActivityTimeout: 60000,
     frameworks: ['browserify', 'detectBrowsers', 'tap'],
+    // the official transaction's test suite is disabled for now, see https://github.com/ethereumjs/ethereumjs-testing/issues/40
     files: ['./test-build/test/api.js', './test-build/test/fake.js'],
     preprocessors: {
       './test-build/**/*.js': ['browserify']
