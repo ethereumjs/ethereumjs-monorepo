f87e78a5665576a382e351f7b99b277e150dd7f5
Patricio Palladino <email@patriciopalladino.com>
2019-06-18 17:09:48 -0300

    Add types for Chain params

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,41 @@ export interface chainsType {
   }
   [key: string]: {}
 }
+
+export interface Chain {
+  name: string
+  chainId: number
+  networkId: number
+  comment: string
+  url: string
+  genesis: GenesisBlock
+  hardforks: Hardfork[]
+  bootstrapNodes: BootstrapNode[]
+}
+
+export interface GenesisBlock {
+  hash: string
+  timestamp: string | null
+  gasLimit: number
+  difficulty: number
+  nonce: string
+  extraData: string
+  stateRoot: string
+}
+
+export interface Hardfork {
+  name: string
+  block: number | null
+  consensus: string
+  finality: any
+}
+
+export interface BootstrapNode {
+  ip: string
+  port: number | string
+  network?: string
+  chainId?: number
+  id: string
+  location: string
+  comment: string
+}
