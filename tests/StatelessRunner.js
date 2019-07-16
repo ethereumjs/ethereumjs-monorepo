bb85e0688a1411ab3f9799632512c6dfdae0dd09
Sina Mahmoodi <itz.s1na@gmail.com>
2019-07-16 18:39:34 +0200

    Add initial stateless protoype tester

---
+++ tests/StatelessRunner.js
@@ -0,0 +1,170 @@
+const testUtil = require('./util')
+const { promisify } = require('util')
+const ethUtil = require('ethereumjs-util')
+const Account = require('ethereumjs-account').default
+const BN = ethUtil.BN
+const { getRequiredForkConfigAlias } = require('./util')
+const { HookedStateManager, stateFromProofs } = require('./stateless')
+
+const VM = require('../dist/index.js').default
+const PStateManager = require('../dist/state/promisified').default
+
+async function runTestCase (options, testData, t) {
+  let expectedPostStateRoot = testData.postStateRoot
+  if (expectedPostStateRoot.substr(0, 2) === '0x') {
+    expectedPostStateRoot = expectedPostStateRoot.substr(2)
+  }
+
+  // Prepare tx and block
+  let tx = testUtil.makeTx(testData.transaction)
+  let block = testUtil.makeBlockFromEnv(testData.env)
+  tx._homestead = true
+  tx.enableHomestead = true
+  block.isHomestead = function () {
+    return true
+  }
+  if (!tx.validate()) {
+    return
+  }
+
+  // Hooked state manager proves necessary trie nodes
+  // for a stateless execution
+  let stateManager = new HookedStateManager()
+  // Set up VM
+  let vm = new VM({
+    stateManager: stateManager,
+    hardfork: options.forkConfig.toLowerCase()
+  })
+  if (options.jsontrace) {
+    hookVM(vm, t)
+  }
+  await promisify(testUtil.setupPreConditions)(stateManager._trie, testData)
+
+  const preStateRoot = stateManager._trie.root
+  stateManager.preStateRoot = preStateRoot
+
+  try {
+    await vm.runTx({ tx: tx, block: block })
+  } catch (err) {
+    await deleteCoinbase(new PStateManager(stateManager), block.header.coinbase)
+  }
+  t.equal(stateManager._trie.root.toString('hex'), expectedPostStateRoot, 'the state roots should match')
+
+  const proofData = {
+    accountProofs: stateManager.proofs,
+    codes: stateManager.codes,
+    storageProofs: stateManager.storageProofs
+  }
+  // Now use proofs to construct stateManager
+  // and run tx again to make sure it works
+  stateManager = await stateFromProofs(preStateRoot, proofData)
+  vm = new VM({
+    stateManager: stateManager,
+    hardfork: options.forkConfig.toLowerCase()
+  })
+  if (options.jsontrace) {
+    hookVM(vm, t)
+  }
+  try {
+    await vm.runTx({ tx: tx, block: block })
+  } catch (err) {
+    await deleteCoinbase(new PStateManager(stateManager), block.header.coinbase)
+  }
+  t.equal(stateManager._trie.root.toString('hex'), expectedPostStateRoot, 'the state roots should match')
+}
+
+/*
+ * If tx is invalid and coinbase is empty, the test harness
+ * expects the coinbase account to be deleted from state.
+ * Without this ecmul_0-3_5616_28000_96 would fail.
+ */
+async function deleteCoinbase (pstate, coinbaseAddr) {
+  const account = await pstate.getAccount(coinbaseAddr)
+  if (new BN(account.balance).isZero()) {
+    await pstate.putAccount(coinbaseAddr, new Account())
+    await pstate.cleanupTouchedAccounts()
+    await promisify(pstate._wrapped._cache.flush.bind(pstate._wrapped._cache))()
+  }
+}
+
+function hookVM (vm, t) {
+  vm.on('step', function (e) {
+    let hexStack = []
+    hexStack = e.stack.map(item => {
+      return '0x' + new BN(item).toString(16, 0)
+    })
+
+    var opTrace = {
+      'pc': e.pc,
+      'op': e.opcode.opcode,
+      'gas': '0x' + e.gasLeft.toString('hex'),
+      'gasCost': '0x' + e.opcode.fee.toString(16),
+      'stack': hexStack,
+      'depth': e.depth,
+      'opName': e.opcode.name
+    }
+
+    t.comment(JSON.stringify(opTrace))
+  })
+  vm.on('afterTx', function (results) {
+    let stateRoot = {
+      'stateRoot': vm.stateManager._trie.root.toString('hex')
+    }
+    t.comment(JSON.stringify(stateRoot))
+  })
+}
+
+function parseTestCases (forkConfig, testData, data, gasLimit, value) {
+  let testCases = []
+  if (testData['post'][forkConfig]) {
+    testCases = testData['post'][forkConfig].map(testCase => {
+      let testIndexes = testCase['indexes']
+      let tx = { ...testData.transaction }
+      if (data !== undefined && testIndexes['data'] !== data) {
+        return null
+      }
+
+      if (value !== undefined && testIndexes['value'] !== value) {
+        return null
+      }
+
+      if (gasLimit !== undefined && testIndexes['gas'] !== gasLimit) {
+        return null
+      }
+
+      tx.data = testData.transaction.data[testIndexes['data']]
+      tx.gasLimit = testData.transaction.gasLimit[testIndexes['gas']]
+      tx.value = testData.transaction.value[testIndexes['value']]
+      return {
+        'transaction': tx,
+        'postStateRoot': testCase['hash'],
+        'env': testData['env'],
+        'pre': testData['pre']
+      }
+    })
+  }
+
+  testCases = testCases.filter(testCase => {
+    return testCase != null
+  })
+
+  return testCases
+}
+
+module.exports = async function runStateTest (options, testData, t) {
+  const forkConfig = getRequiredForkConfigAlias(options.forkConfig)
+  try {
+    const testCases = parseTestCases(forkConfig, testData, options.data, options.gasLimit, options.value)
+    if (testCases.length > 0) {
+      for (const testCase of testCases) {
+        await runTestCase(options, testCase, t)
+      }
+    } else {
+      t.comment(`No ${forkConfig} post state defined, skip test`)
+      return
+    }
+  } catch (e) {
+    t.fail('error running test case for fork: ' + forkConfig)
+    console.log('error:', e)
+  }
+}
---
+++ tests/tester.js
@@ -0,0 +1,258 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2))
+const tape = require('tape')
+const testing = require('ethereumjs-testing')
+const FORK_CONFIG = argv.fork || 'Petersburg'
+const {
+  getRequiredForkConfigAlias
+} = require('./util')
+// tests which should be fixed
+const skipBroken = [
+  'dynamicAccountOverwriteEmpty' // temporary till fixed (2019-01-30), skipped along constantinopleFix work time constraints
+]
+// tests skipped due to system specifics / design considerations
+const skipPermanent = [
+  'SuicidesMixingCoinbase', // sucides to the coinbase, since we run a blockLevel we create coinbase account.
+  'static_SuicidesMixingCoinbase', // sucides to the coinbase, since we run a blockLevel we create coinbase account.
+  'ForkUncle', // Only BlockchainTest, correct behaviour unspecified (?)
+  'UncleFromSideChain' // Only BlockchainTest, same as ForkUncle, the TD is the same for two diffent branches so its not clear which one should be the finally chain
+]
+// tests running slow (run from time to time)
+const skipSlow = [
+  'Call50000', // slow
+  'Call50000_ecrec', // slow
+  'Call50000_identity', // slow
+  'Call50000_identity2', // slow
+  'Call50000_sha256', // slow
+  'Call50000_rip160', // slow
+  'Call50000bytesContract50_1', // slow
+  'Call50000bytesContract50_2',
+  'Call1MB1024Calldepth', // slow
+  'static_Call1MB1024Calldepth', // slow
+  'static_Call50000', // slow
+  'static_Call50000_ecrec',
+  'static_Call50000_identity',
+  'static_Call50000_identity2',
+  'static_Call50000_rip160',
+  'static_Return50000_2',
+  'Callcode50000', // slow
+  'Return50000', // slow
+  'Return50000_2', // slow
+  'static_Call50000', // slow
+  'static_Call50000_ecrec', // slow
+  'static_Call50000_identity', // slow
+  'static_Call50000_identity2', // slow
+  'static_Call50000_sha256', // slow
+  'static_Call50000_rip160', // slow
+  'static_Call50000bytesContract50_1', // slow
+  'static_Call50000bytesContract50_2',
+  'static_Call1MB1024Calldepth', // slow
+  'static_Callcode50000', // slow
+  'static_Return50000', // slow
+  'static_Return50000_2', // slow
+  'QuadraticComplexitySolidity_CallDataCopy'
+]
+
+/*
+NOTE: VM tests have been disabled since they are generated using Frontier gas costs, and ethereumjs-vm doesn't support historical fork rules
+
+TODO: some VM tests do not appear to be executing (don't print an "ok" statement):
+...
+# file: vmLogTest test: log0_emptyMem
+ok 38984 valid gas usage
+# file: vmLogTest test: log0_logMemStartTooHigh
+# file: vmLogTest test: log0_logMemsizeTooHigh
+# file: vmLogTest test: log0_logMemsizeZero
+ok 38985 valid gas usage
+# file: vmLogTest test: log0_nonEmptyMem
+*/
+
+const skipVM = [
+  // slow performance tests
+  'loop-mul',
+  'loop-add-10M',
+  'loop-divadd-10M',
+  'loop-divadd-unr100-10M',
+  'loop-exp-16b-100k',
+  'loop-exp-1b-1M',
+  'loop-exp-2b-100k',
+  'loop-exp-32b-100k',
+  'loop-exp-4b-100k',
+  'loop-exp-8b-100k',
+  'loop-exp-nop-1M',
+  'loop-mulmod-2M',
+  // some VM tests fail because the js runner executes CALLs
+  // see https://github.com/ethereum/tests/wiki/VM-Tests  > Since these tests are meant only as a basic test of VM operation, the CALL and CREATE instructions are not actually executed.
+  'ABAcalls0',
+  'ABAcallsSuicide0',
+  'ABAcallsSuicide1',
+  'sha3_bigSize',
+  'CallRecursiveBomb0',
+  'CallToNameRegistrator0',
+  'CallToPrecompiledContract',
+  'CallToReturn1',
+  'PostToNameRegistrator0',
+  'PostToReturn1',
+  'callcodeToNameRegistrator0',
+  'callcodeToReturn1',
+  'callstatelessToNameRegistrator0',
+  'callstatelessToReturn1',
+  'createNameRegistrator',
+  'randomTest643' // TODO fix this
+]
+
+if (argv.r) {
+  randomized(argv.r, argv.v)
+} else if (argv.s) {
+  runTests('GeneralStateTests', argv)
+} else if (argv.v) {
+  runTests('VMTests', argv)
+} else if (argv.b) {
+  runTests('BlockchainTests', argv)
+} else if (argv.stateless) {
+  runTests('Stateless', argv)
+}
+
+// randomized tests
+// returns 1 if the tests fails
+// returns 0 if the tests succeds
+function randomized (stateTest) {
+  const stateRunner = require('./stateRunner.js')
+  let errored = false
+
+  tape.createStream({
+    objectMode: true
+  }).on('data', function (row) {
+    if (row.ok === false && !errored) {
+      errored = true
+      process.stdout.write('1')
+      process.exit()
+    }
+  }).on('end', function () {
+    process.stdout.write('0')
+  })
+
+  try {
+    stateTest = JSON.parse(stateTest)
+  } catch (e) {
+    console.error('invalid json')
+    process.exit()
+  }
+
+  var keys = Object.keys(stateTest)
+  stateTest = stateTest[keys[0]]
+
+  tape('', t => {
+    stateRunner({}, stateTest, t, t.end)
+  })
+}
+
+function getSkipTests (choices, defaultChoice) {
+  let skipTests = []
+  if (!choices) {
+    choices = defaultChoice
+  }
+  choices = choices.toLowerCase()
+  if (choices !== 'none') {
+    let choicesList = choices.split(',')
+    let all = choicesList.includes('all')
+    if (all || choicesList.includes('broken')) {
+      skipTests = skipTests.concat(skipBroken)
+    }
+    if (all || choicesList.includes('permanent')) {
+      skipTests = skipTests.concat(skipPermanent)
+    }
+    if (all || choicesList.includes('slow')) {
+      skipTests = skipTests.concat(skipSlow)
+    }
+  }
+  return skipTests
+}
+
+function runTests (name, runnerArgs, cb) {
+  let testGetterArgs = {}
+
+  testGetterArgs.skipTests = getSkipTests(argv.skip, argv.runSkipped ? 'NONE' : 'ALL')
+  testGetterArgs.runSkipped = getSkipTests(argv.runSkipped, 'NONE')
+  testGetterArgs.skipVM = skipVM
+  testGetterArgs.forkConfig = getRequiredForkConfigAlias(FORK_CONFIG)
+  testGetterArgs.file = argv.file
+  testGetterArgs.test = argv.test
+  testGetterArgs.dir = argv.dir
+  testGetterArgs.excludeDir = argv.excludeDir
+  testGetterArgs.testsPath = argv.testsPath
+
+  testGetterArgs.customStateTest = argv.customStateTest
+
+  runnerArgs.forkConfig = FORK_CONFIG
+  runnerArgs.jsontrace = argv.jsontrace
+  runnerArgs.debug = argv.debug // for BlockchainTests
+
+  // for GeneralStateTests
+  runnerArgs.data = argv.data
+  runnerArgs.gasLimit = argv.gas
+  runnerArgs.value = argv.value
+
+  // runnerArgs.vmtrace = true; // for VMTests
+
+  if (name === 'Stateless') {
+    tape(name, t => {
+      const stateTestRunner = require('./StatelessRunner.js')
+      testing.getTestsFromArgs('GeneralStateTests', async (fileName, testName, test) => {
+        let runSkipped = testGetterArgs.runSkipped
+        let inRunSkipped = runSkipped.includes(fileName)
+        if (runSkipped.length === 0 || inRunSkipped) {
+          t.comment(`file: ${fileName} test: ${testName}`)
+          return stateTestRunner(runnerArgs, test, t)
+        }
+      }, testGetterArgs).then(() => {
+        t.end()
+      }).catch((err) => {
+        console.log(err)
+        t.end()
+      })
+    })
+  } else if (argv.customStateTest) {
+    const stateTestRunner = require('./GeneralStateTestsRunner.js')
+    let fileName = argv.customStateTest
+    tape(name, t => {
+      testing.getTestFromSource(fileName, (err, test) => {
+        if (err) {
+          return t.fail(err)
+        }
+
+        t.comment(`file: ${fileName} test: ${test.testName}`)
+        stateTestRunner(runnerArgs, test, t, () => {
+          t.end()
+        })
+      })
+    })
+  } else {
+    tape(name, t => {
+      const runner = require(`./${name}Runner.js`)
+      testing.getTestsFromArgs(name, (fileName, testName, test) => {
+        return new Promise((resolve, reject) => {
+          if (name === 'VMTests') {
+            // suppress some output of VMTests
+            // t.comment(`file: ${fileName} test: ${testName}`)
+            test.fileName = fileName
+            test.testName = testName
+            runner(runnerArgs, test, t, resolve)
+          } else {
+            let runSkipped = testGetterArgs.runSkipped
+            let inRunSkipped = runSkipped.includes(fileName)
+            if (runSkipped.length === 0 || inRunSkipped) {
+              t.comment(`file: ${fileName} test: ${testName}`)
+              runner(runnerArgs, test, t, resolve)
+            } else {
+              resolve()
+            }
+          }
+        }).catch(err => console.log(err))
+      }, testGetterArgs).then(() => {
+        t.end()
+      })
+    })
+  }
+}
