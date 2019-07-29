4769f90900238b33f058b808c715dca721434afd
Patricio Palladino <email@patriciopalladino.com>
2019-07-29 15:32:22 -0300

    Migrate RPC related functions

--- src/header-from-rpc.ts
+++ src/header-from-rpc.ts
@@ -1,36 +1,38 @@
-// 'use strict'
-// const BlockHeader = require('./header')
-// const ethUtil = require('ethereumjs-util')
-//
-// module.exports = blockHeaderFromRpc
-//
-// /**
-//  * Creates a new block header object from Ethereum JSON RPC.
-//  * @param {Object} blockParams - Ethereum JSON RPC of block (eth_getBlockByNumber)
-//  */
-// function blockHeaderFromRpc(blockParams) {
-//   const blockHeader = new BlockHeader({
-//     parentHash: blockParams.parentHash,
-//     uncleHash: blockParams.sha3Uncles,
-//     coinbase: blockParams.miner,
-//     stateRoot: blockParams.stateRoot,
-//     transactionsTrie: blockParams.transactionsRoot,
-//     receiptTrie: blockParams.receiptRoot || blockParams.receiptsRoot || ethUtil.SHA3_NULL,
-//     bloom: blockParams.logsBloom,
-//     difficulty: blockParams.difficulty,
-//     number: blockParams.number,
-//     gasLimit: blockParams.gasLimit,
-//     gasUsed: blockParams.gasUsed,
-//     timestamp: blockParams.timestamp,
-//     extraData: blockParams.extraData,
-//     mixHash: blockParams.mixHash,
-//     nonce: blockParams.nonce,
-//   })
-//
-//   // override hash incase something was missing
-//   blockHeader.hash = function() {
-//     return ethUtil.toBuffer(blockParams.hash)
-//   }
-//
-//   return blockHeader
-// }
+import { BlockHeader } from './header'
+import * as ethUtil from 'ethereumjs-util'
+import { NetworkOptions } from './types'
+
+/**
+ * Creates a new block header object from Ethereum JSON RPC.
+ * @param blockParams - Ethereum JSON RPC of block (eth_getBlockByNumber)
+ * @param networkOptions - An object describing the network
+ */
+export default function blockHeaderFromRpc(blockParams: any, networkOptions?: NetworkOptions) {
+  const blockHeader = new BlockHeader(
+    {
+      parentHash: blockParams.parentHash,
+      uncleHash: blockParams.sha3Uncles,
+      coinbase: blockParams.miner,
+      stateRoot: blockParams.stateRoot,
+      transactionsTrie: blockParams.transactionsRoot,
+      receiptTrie: blockParams.receiptRoot || blockParams.receiptsRoot || ethUtil.KECCAK256_NULL,
+      bloom: blockParams.logsBloom,
+      difficulty: blockParams.difficulty,
+      number: blockParams.number,
+      gasLimit: blockParams.gasLimit,
+      gasUsed: blockParams.gasUsed,
+      timestamp: blockParams.timestamp,
+      extraData: blockParams.extraData,
+      mixHash: blockParams.mixHash,
+      nonce: blockParams.nonce,
+    },
+    networkOptions,
+  )
+
+  // override hash in case something was missing
+  blockHeader.hash = function() {
+    return ethUtil.toBuffer(blockParams.hash)
+  }
+
+  return blockHeader
+}
