8bf127f896ceb1d3376f25a7998b2bae752c4e0d
Ryan Ghods <ryan@ryanio.com>
2019-12-06 13:13:28 -0800

    use firefox headless

--- karma.conf.js
+++ karma.conf.js
@@ -7,15 +7,15 @@ module.exports = function(config) {
     // the official transaction's test suite is disabled for now, see https://github.com/ethereumjs/ethereumjs-testing/issues/40
     files: ['./test-build/test/api.js', './test-build/test/fake.js'],
     preprocessors: {
-      './test-build/**/*.js': ['browserify']
+      './test-build/**/*.js': ['browserify'],
     },
     singleRun: true,
     detectBrowsers: {
       enabled: true,
       usePhantomJS: false,
       postDetection: function(availableBrowsers) {
-        return ['Firefox']
+        return ['FirefoxHeadless']
       },
-    }
+    },
   })
 }
