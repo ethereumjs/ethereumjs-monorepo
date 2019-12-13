6aeed945f16bcb69f3aa1466ef198c0d555bec2d
Everton Fraga <ev@ethereum.org>
2019-12-13 10:54:12 -0500

    Adding difficulty tests from ethereum/tests

--- test/difficulty.ts
+++ test/difficulty.ts
@@ -29,6 +29,11 @@ tape('[Header]: difficulty tests', t => {
     homestead: require('./difficultyHomestead.json').tests,
     byzantium: require('./difficultyByzantium.json').tests,
     constantinople: require('./difficultyConstantinople.json').tests,
+    muirGlacier: Object.assign(
+      require('./difficultyEIP2384.json').tests,
+      require('./difficultyEIP2384_random.json').tests,
+      require('./difficultyEIP2384_random_to20M.json').tests,
+    ),
   }
   for (const hardfork in hardforkTestData) {
     const testData = hardforkTestData[hardfork]
@@ -44,7 +49,12 @@ tape('[Header]: difficulty tests', t => {
       block.header.difficulty = test.currentDifficulty
       block.header.number = test.currentBlockNumber
 
-      runDifficultyTests(test, parentBlock, block, 'fork determination by hardfork param')
+      runDifficultyTests(
+        test,
+        parentBlock,
+        block,
+        `fork determination by hardfork param (${hardfork})`,
+      )
     }
   }
 
@@ -66,7 +76,12 @@ tape('[Header]: difficulty tests', t => {
       block.header.difficulty = test.currentDifficulty
       block.header.number = test.currentBlockNumber
 
-      runDifficultyTests(test, parentBlock, block, 'fork determination by block number')
+      runDifficultyTests(
+        test,
+        parentBlock,
+        block,
+        `fork determination by block number (${test.currentBlockNumber})`,
+      )
     }
   }
 
