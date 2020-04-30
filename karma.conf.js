5e05e32c32456ad87a15fb8f991db5b8e1de9722
Ryan Ghods <ryan@ryanio.com>
2020-04-30 11:51:04 -0700

    Add karma test runner

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'karma-typescript'],
+    files: ['src/**/*.ts', 'test/**/!(integration)*.ts'],
+    preprocessors: {
+      '**/*.ts': ['karma-typescript'],
+    },
+    plugins: ['karma-mocha', 'karma-typescript', 'karma-chrome-launcher', 'karma-firefox-launcher'],
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+      },
+    },
+    colors: true,
+    reporters: ['progress', 'karma-typescript'],
+    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
+    singleRun: true,
+    concurrency: Infinity,
+    // Fail after timeout
+    browserDisconnectTimeout: 100000,
+    browserNoActivityTimeout: 100000,
+  })
+}
