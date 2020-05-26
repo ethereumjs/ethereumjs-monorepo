aee38f442e58949200cbd71679ac009ed5ac659a
Ryan Ghods <ryan@ryanio.com>
2020-05-26 11:05:56 -0700

    upgrade ci provider to github actions upgrade dev deps (ethereumjs-block, nyc, standard) remove ethereumjs-testing and move ethash_tests.json into test dir use single imports for ethereumjs-util, upgrade to v7.0.2

--- tests/ethash.js
+++ tests/ethash.js
@@ -1,9 +1,9 @@
 const Ethash = require('../index.js')
 const ethHashUtil = require('../util.js')
-const ethUtil = require('ethereumjs-util')
+const { bufferToInt } = require('ethereumjs-util')
 const Header = require('ethereumjs-block/header.js')
 const tape = require('tape')
-const powTests = require('ethereumjs-testing').tests.powTests.ethash_tests
+const powTests = require('./ethash_tests.json')
 
 var ethash = new Ethash()
 var tests = Object.keys(powTests)
@@ -11,21 +11,41 @@ var tests = Object.keys(powTests)
 tape('POW tests', function (t) {
   tests.forEach(function (key) {
     var test = powTests[key]
-    var header = new Header(new Buffer(test.header, 'hex'))
+    var header = new Header(Buffer.from(test.header, 'hex'))
 
     var headerHash = ethash.headerHash(header.raw)
-    t.equal(headerHash.toString('hex'), test.header_hash, 'generate header hash')
+    t.equal(
+      headerHash.toString('hex'),
+      test.header_hash,
+      'generate header hash'
+    )
 
-    var epoc = ethHashUtil.getEpoc(ethUtil.bufferToInt(header.number))
-    t.equal(ethHashUtil.getCacheSize(epoc), test.cache_size, 'generate cache size')
-    t.equal(ethHashUtil.getFullSize(epoc), test.full_size, 'generate full cache size')
+    var epoc = ethHashUtil.getEpoc(bufferToInt(header.number))
+    t.equal(
+      ethHashUtil.getCacheSize(epoc),
+      test.cache_size,
+      'generate cache size'
+    )
+    t.equal(
+      ethHashUtil.getFullSize(epoc),
+      test.full_size,
+      'generate full cache size'
+    )
 
-    ethash.mkcache(test.cache_size, new Buffer(test.seed, 'hex'))
-    t.equal(ethash.cacheHash().toString('hex'), test.cache_hash, 'generate cache')
+    ethash.mkcache(test.cache_size, Buffer.from(test.seed, 'hex'))
+    t.equal(
+      ethash.cacheHash().toString('hex'),
+      test.cache_hash,
+      'generate cache'
+    )
 
-    var r = ethash.run(headerHash, new Buffer(test.nonce, 'hex'), test.full_size)
+    var r = ethash.run(
+      headerHash,
+      Buffer.from(test.nonce, 'hex'),
+      test.full_size
+    )
     t.equal(r.hash.toString('hex'), test.result, 'generate result')
-    t.equal(r.mix.toString('hex'), test.mixhash, 'generate mix hash')
+    t.equal(r.mix.toString('hex'), test.mixHash, 'generate mix hash')
   })
   t.end()
 })
--- util.js
+++ util.js
@@ -4,22 +4,23 @@ const BN = ethUtil.BN
 
 exports.params = {
   DATASET_BYTES_INIT: 1073741824, // 2^30
-  DATASET_BYTES_GROWTH: 8388608,  // 2 ^ 23
-  CACHE_BYTES_INIT: 16777216,     // 2**24          # bytes in dataset at genesis
-  CACHE_BYTES_GROWTH: 131072,     // 2**17  cache growth per epoch
-  CACHE_MULTIPLIER: 1024,         // Size of the DAG relative to the cache
-  EPOCH_LENGTH: 30000,            // blocks per epoch
-  MIX_BYTES: 128,                 // width of mix
-  HASH_BYTES: 64,                 // hash length in bytes
-  DATASET_PARENTS: 256,           // number of parents of each dataset element
-  CACHE_ROUNDS: 3,                // number of rounds in cache production
+  DATASET_BYTES_GROWTH: 8388608, // 2 ^ 23
+  CACHE_BYTES_INIT: 16777216, // 2**24 number of bytes in dataset at genesis
+  CACHE_BYTES_GROWTH: 131072, // 2**17 cache growth per epoch
+  CACHE_MULTIPLIER: 1024, // Size of the DAG relative to the cache
+  EPOCH_LENGTH: 30000, // blocks per epoch
+  MIX_BYTES: 128, // width of mix
+  HASH_BYTES: 64, // hash length in bytes
+  DATASET_PARENTS: 256, // number of parents of each dataset element
+  CACHE_ROUNDS: 3, // number of rounds in cache production
   ACCESSES: 64,
   WORD_BYTES: 4
 }
 
 exports.getCacheSize = function (epoc) {
   var mr = new MR()
-  var sz = exports.params.CACHE_BYTES_INIT + exports.params.CACHE_BYTES_GROWTH * epoc
+  var sz =
+    exports.params.CACHE_BYTES_INIT + exports.params.CACHE_BYTES_GROWTH * epoc
   sz -= exports.params.HASH_BYTES
   while (!mr.test(new BN(sz / exports.params.HASH_BYTES))) {
     sz -= 2 * exports.params.HASH_BYTES
@@ -29,7 +30,9 @@ exports.getCacheSize = function (epoc) {
 
 exports.getFullSize = function (epoc) {
   var mr = new MR()
-  var sz = exports.params.DATASET_BYTES_INIT + exports.params.DATASET_BYTES_GROWTH * epoc
+  var sz =
+    exports.params.DATASET_BYTES_INIT +
+    exports.params.DATASET_BYTES_GROWTH * epoc
   sz -= exports.params.MIX_BYTES
   while (!mr.test(new BN(sz / exports.params.MIX_BYTES))) {
     sz -= 2 * exports.params.MIX_BYTES
@@ -56,12 +59,12 @@ exports.getSeed = function (seed, begin, end) {
   return seed
 }
 
-var fnv = exports.fnv = function (x, y) {
-  return (((x * 0x01000000 | 0) + (x * 0x193 | 0)) ^ y) >>> 0
-}
+var fnv = (exports.fnv = function (x, y) {
+  return ((((x * 0x01000000) | 0) + ((x * 0x193) | 0)) ^ y) >>> 0
+})
 
 exports.fnvBuffer = function (a, b) {
-  var r = new Buffer(a.length)
+  var r = Buffer.alloc(a.length)
   for (var i = 0; i < a.length; i = i + 4) {
     r.writeUInt32LE(fnv(a.readUInt32LE(i), b.readUInt32LE(i)), i)
   }
@@ -70,7 +73,7 @@ exports.fnvBuffer = function (a, b) {
 
 exports.bufReverse = function (a) {
   const length = a.length
-  var b = new Buffer(length)
+  var b = Buffer.alloc(length)
   for (var i = 0; i < length; i++) {
     b[i] = a[length - i - 1]
   }
