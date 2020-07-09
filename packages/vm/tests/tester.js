a9632093290ccb075ee8eb0137881e25f5ac4331
holgerd77 <Holger.Drewes@gmail.com>
2020-07-09 13:36:18 +0200

    vm: added test loader (ethereumjs-testing code logic) to test runner, added test runner initialization info

--- packages/vm/tests/tester.js
+++ packages/vm/tests/tester.js
@@ -2,8 +2,8 @@
 
 const argv = require('minimist')(process.argv.slice(2))
 const tape = require('tape')
-const testing = require('ethereumjs-testing')
 const config = require('./config')
+const testLoader = require('./testLoader')
 
 function runTests() {
   let name
@@ -35,7 +35,7 @@ function runTests() {
   /**
    * Run-time configuration
    */
-  let runnerArgs = argv
+  let runnerArgs = {}
   runnerArgs.forkConfigVM = FORK_CONFIG_VM
   runnerArgs.forkConfigTestSuite = FORK_CONFIG_TEST_SUITE
   runnerArgs.jsontrace = argv.jsontrace
@@ -44,11 +44,43 @@ function runTests() {
   runnerArgs.value = argv.value // GeneralStateTests
   runnerArgs.debug = argv.debug // BlockchainTests
 
+  /**
+   * Initialization output to console
+   */
+  const width = 50
+  const fillWidth = width
+  const fillParam = 20
+  const delimiter = `| `.padEnd(fillWidth) + ' |'
+  const formatArgs = (args) => {
+    return Object.assign({}, ...
+      Object.entries(args)
+      .filter(([k,v]) => v && v.length !== 0)
+      .map(([k,v]) => ({[k]:typeof(v) !== 'string' && v.length ? v.length : v}))
+    )
+  }
+  const formattedGetterArgs = formatArgs(testGetterArgs)
+  const formattedRunnerArgs = formatArgs(runnerArgs)
+  
+  console.log(`+${'-'.repeat(width)}+`)
+  console.log(`| VM -> ${name} `.padEnd(fillWidth) + ' |')
+  console.log(delimiter)
+  console.log(`| TestGetterArgs`.padEnd(fillWidth) + ' |')
+  for (const [key, value] of Object.entries(formattedGetterArgs)) {
+    console.log(`| ${key.padEnd(fillParam)}: ${value}`.padEnd(fillWidth) + ' |')
+  }
+  console.log(delimiter)
+  console.log(`| RunnerArgs`.padEnd(fillWidth) + ' |')
+  for (const [key, value] of Object.entries(formattedRunnerArgs)) {
+    console.log(`| ${key.padEnd(fillParam)}: ${value}`.padEnd(fillWidth) + ' |')
+  }
+  console.log(`+${'-'.repeat(width)}+`)
+  console.log()
+
   if (argv.customStateTest) {
     const stateTestRunner = require('./GeneralStateTestsRunner.js')
     let fileName = argv.customStateTest
     tape(name, t => {
-      testing.getTestFromSource(fileName, async (err, test) => {
+      testLoader.getTestFromSource(fileName, async (err, test) => {
         if (err) {
           return t.fail(err)
         }
@@ -67,7 +99,7 @@ function runTests() {
       if (testGetterArgs.forkConfig !== 'Istanbul') {
         name = 'LegacyTests/Constantinople/'.concat(name)
       }
-      testing.getTestsFromArgs(
+      testLoader.getTestsFromArgs(
         name,
         async (fileName, testName, test) => {
           let runSkipped = testGetterArgs.runSkipped
