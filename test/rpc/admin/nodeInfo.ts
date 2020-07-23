751cf0dcd074e1737e62ac6309c268d0076e9d57
PhilippLgh <philipp@ethereum.org>
2020-07-23 19:55:55 +0200

    chore: improve test

--- test/rpc/admin/nodeInfo.ts
+++ test/rpc/admin/nodeInfo.ts
@@ -3,7 +3,11 @@ import { startRPC, createManager, createNode, params, baseRequest } from '../hel
 
 const method = 'admin_nodeInfo'
 
+<<<<<<< HEAD:test/rpc/admin/nodeInfo.ts
 tape(method, (t) => {
+=======
+test(method, (t) => {
+>>>>>>> chore: improve test:test/rpc/admin/nodeInfo.js
   const manager = createManager(createNode({ opened: true }))
   const server = startRPC(manager.getMethods())
 
@@ -13,11 +17,10 @@ tape(method, (t) => {
     const msg = 'should return the correct info'
     const { result } = res.body
     if (result) {
-      t.pass(msg)
+      t.pass('admin_nodeInfo returns a value')
     } else {
       throw new Error(msg)
     }
   }
   baseRequest(t, server, req, 200, expectRes)
 })
-
