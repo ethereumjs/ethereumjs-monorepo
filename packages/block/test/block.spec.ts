89718cfc6326b9357be9c3755fb8aba1d3143d55
Everton Fraga <ev@ethereum.org>
2020-10-06 11:17:36 -0400

    block: fix typescript in tests

--- packages/block/test/block.spec.ts
+++ packages/block/test/block.spec.ts
@@ -1,4 +1,4 @@
-import * as tape from 'tape'
+import tape from 'tape'
 import { rlp } from 'ethereumjs-util'
 import Common from '@ethereumjs/common'
 import { Block } from '../src'
--- packages/block/test/difficulty.spec.ts
+++ packages/block/test/difficulty.spec.ts
@@ -1,4 +1,4 @@
-import * as tape from 'tape'
+import tape from 'tape'
 import { BN, toBuffer, bufferToInt } from 'ethereumjs-util'
 import Common from '@ethereumjs/common'
 import { Block } from '../src'
