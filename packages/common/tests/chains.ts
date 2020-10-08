rod-lin/ethereumjs-vm

common: lint, docs rebuild

packages/common/tests/chains.ts
@@ -7,8 +7,16 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.equal(c.chainName(), 'mainnet', 'should initialize with chain name')
     st.equal(c.chainId(), 1, 'should return correct chain Id')
     st.equal(c.networkId(), 1, 'should return correct network Id')
-    st.equal(c.hardfork(), 'petersburg', 'should set hardfork to the default hardfork')
-    st.equal(c._isSupportedHardfork('constantinople'), true, 'should not restrict supported HFs')
+    st.equal(
+      c.hardfork(),
+      'petersburg',
+      'should set hardfork to the default hardfork'
+    )
+    st.equal(
+      c._isSupportedHardfork('constantinople'),
+      true,
+      'should not restrict supported HFs'
+    )
 
     c = new Common({ chain: 1 })
     st.equal(c.chainName(), 'mainnet', 'should initialize with chain Id')
@@ -16,20 +24,28 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should initialize with chain and hardfork provided', function (st: tape.Test) {
+  t.test('Should initialize with chain and hardfork provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet', hardfork: 'byzantium' })
     st.equal(c.hardfork(), 'byzantium', 'should return correct hardfork name')
 
     st.end()
   })
 
-  t.test('Should initialize with supportedHardforks provided', function (st: tape.Test) {
+  t.test('Should initialize with supportedHardforks provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({
       chain: 'mainnet',
       hardfork: 'byzantium',
       supportedHardforks: ['byzantium', 'constantinople'],
     })
-    st.equal(c._isSupportedHardfork('byzantium'), true, 'should return true for supported HF')
+    st.equal(
+      c._isSupportedHardfork('byzantium'),
+      true,
+      'should return true for supported HF'
+    )
     const msg = 'should return false for unsupported HF'
     st.equal(c._isSupportedHardfork('spuriousDragon'), false, msg)
 
@@ -62,16 +78,29 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should provide correct access to chain parameters', function (st: tape.Test) {
+  t.test('Should provide correct access to chain parameters', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet' })
-    const hash = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
+    const hash =
+      '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     st.equal(c.genesis().hash, hash, 'should return correct genesis hash')
-    st.equal(c.hardforks()[3]['block'], 2463000, 'should return correct hardfork data')
-    st.equal(typeof c.bootstrapNodes()[0].port, 'number', 'should return a port as number')
+    st.equal(
+      c.hardforks()[3]['block'],
+      2463000,
+      'should return correct hardfork data'
+    )
+    st.equal(
+      typeof c.bootstrapNodes()[0].port,
+      'number',
+      'should return a port as number'
+    )
     st.end()
   })
 
-  t.test('Should provide the bootnode information in a uniform way', function (st: tape.Test) {
+  t.test('Should provide the bootnode information in a uniform way', function (
+    st: tape.Test
+  ) {
     const configs = ['mainnet', 'ropsten', 'rinkeby', 'goerli']
     for (const network of configs) {
       const c = new Common({ chain: network })
@@ -82,20 +111,23 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
       st.equal(
         typeof bootnode.location,
         'string',
-        'returns the location as string (empty string if unavailable)',
+        'returns the location as string (empty string if unavailable)'
       )
       st.equal(
         typeof bootnode.comment,
         'string',
-        'returns a comment as string (empty string if unavailable)',
+        'returns a comment as string (empty string if unavailable)'
       )
     }
     st.end()
   })
 
-  t.test('Should be able to access data for all chains provided', function (st: tape.Test) {
+  t.test('Should be able to access data for all chains provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet' })
-    let hash = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
+    let hash =
+      '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     st.equal(c.genesis().hash, hash, 'mainnet')
     c.setChain('ropsten')
     hash = '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d'
@@ -113,26 +145,37 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should provide correct access to private network chain parameters', function (
-    st: tape.Test,
+  t.test(
+    'Should provide correct access to private network chain parameters',
+    function (st: tape.Test) {
+      const chainParams = require('./testnet.json')
+      const c = new Common({ chain: chainParams, hardfork: 'byzantium' })
+      st.equal(c.chainName(), 'testnet', 'should initialize with chain name')
+      st.equal(c.chainId(), 12345, 'should return correct chain Id')
+      st.equal(c.networkId(), 12345, 'should return correct network Id')
+      st.equal(
+        c.genesis().hash,
+        '0xaa00000000000000000000000000000000000000000000000000000000000000',
+        'should return correct genesis hash'
+      )
+      st.equal(
+        c.hardforks()[3]['block'],
+        3,
+        'should return correct hardfork data'
+      )
+      st.equal(
+        c.bootstrapNodes()[1].ip,
+        '10.0.0.2',
+        'should return a bootstrap node array'
+      )
+
+      st.end()
+    }
+  )
+
+  t.test('Should handle custom chain parameters with missing field', function (
+    st: tape.Test
   ) {
-    const chainParams = require('./testnet.json')
-    const c = new Common({ chain: chainParams, hardfork: 'byzantium' })
-    st.equal(c.chainName(), 'testnet', 'should initialize with chain name')
-    st.equal(c.chainId(), 12345, 'should return correct chain Id')
-    st.equal(c.networkId(), 12345, 'should return correct network Id')
-    st.equal(
-      c.genesis().hash,
-      '0xaa00000000000000000000000000000000000000000000000000000000000000',
-      'should return correct genesis hash',
-    )
-    st.equal(c.hardforks()[3]['block'], 3, 'should return correct hardfork data')
-    st.equal(c.bootstrapNodes()[1].ip, '10.0.0.2', 'should return a bootstrap node array')
-
-    st.end()
-  })
-
-  t.test('Should handle custom chain parameters with missing field', function (st: tape.Test) {
     const chainParams = require('./testnet.json')
     delete chainParams['hardforks']
     st.throws(
@@ -140,10 +183,12 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
         new Common({ chain: chainParams })
       },
       /Missing required/,
-      'should throw an exception on missing parameter',
+      'should throw an exception on missing parameter'
     ) // eslint-disable-line no-new
 
-    st.comment('-----------------------------------------------------------------')
+    st.comment(
+      '-----------------------------------------------------------------'
+    )
     st.end()
   })
 })

ethereumjs/ethereumjs-monorepo

packages/common/tests/chains.ts
@@ -7,13 +7,21 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.equal(c.chainName(), 'mainnet', 'should initialize with chain name')
     st.equal(c.chainId(), 1, 'should return correct chain Id')
     st.equal(c.networkId(), 1, 'should return correct network Id')
-    st.equal(c.hardfork(), 'istanbul', 'should set hardfork to current default hardfork')
+    st.equal(
+      c.hardfork(),
+      'istanbul',
+      'should set hardfork to current default hardfork'
+    )
     st.equal(
       c.hardfork(),
       c.DEFAULT_HARDFORK,
-      'should set hardfork to hardfork set as DEFAULT_HARDFORK',
+      'should set hardfork to hardfork set as DEFAULT_HARDFORK'
     )
-    st.equal(c._isSupportedHardfork('constantinople'), true, 'should not restrict supported HFs')
+    st.equal(
+      c._isSupportedHardfork('constantinople'),
+      true,
+      'should not restrict supported HFs'
+    )
 
     c = new Common({ chain: 1 })
     st.equal(c.chainName(), 'mainnet', 'should initialize with chain Id')
@@ -21,20 +29,28 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should initialize with chain and hardfork provided', function (st: tape.Test) {
+  t.test('Should initialize with chain and hardfork provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet', hardfork: 'byzantium' })
     st.equal(c.hardfork(), 'byzantium', 'should return correct hardfork name')
 
     st.end()
   })
 
-  t.test('Should initialize with supportedHardforks provided', function (st: tape.Test) {
+  t.test('Should initialize with supportedHardforks provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({
       chain: 'mainnet',
       hardfork: 'byzantium',
       supportedHardforks: ['byzantium', 'constantinople'],
     })
-    st.equal(c._isSupportedHardfork('byzantium'), true, 'should return true for supported HF')
+    st.equal(
+      c._isSupportedHardfork('byzantium'),
+      true,
+      'should return true for supported HF'
+    )
     const msg = 'should return false for unsupported HF'
     st.equal(c._isSupportedHardfork('spuriousDragon'), false, msg)
 
@@ -67,16 +83,29 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should provide correct access to chain parameters', function (st: tape.Test) {
+  t.test('Should provide correct access to chain parameters', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet' })
-    const hash = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
+    const hash =
+      '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     st.equal(c.genesis().hash, hash, 'should return correct genesis hash')
-    st.equal(c.hardforks()[3]['block'], 2463000, 'should return correct hardfork data')
-    st.equal(typeof c.bootstrapNodes()[0].port, 'number', 'should return a port as number')
+    st.equal(
+      c.hardforks()[3]['block'],
+      2463000,
+      'should return correct hardfork data'
+    )
+    st.equal(
+      typeof c.bootstrapNodes()[0].port,
+      'number',
+      'should return a port as number'
+    )
     st.end()
   })
 
-  t.test('Should provide the bootnode information in a uniform way', function (st: tape.Test) {
+  t.test('Should provide the bootnode information in a uniform way', function (
+    st: tape.Test
+  ) {
     const configs = ['mainnet', 'ropsten', 'rinkeby', 'goerli']
     for (const network of configs) {
       const c = new Common({ chain: network })
@@ -87,20 +116,23 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
       st.equal(
         typeof bootnode.location,
         'string',
-        'returns the location as string (empty string if unavailable)',
+        'returns the location as string (empty string if unavailable)'
       )
       st.equal(
         typeof bootnode.comment,
         'string',
-        'returns a comment as string (empty string if unavailable)',
+        'returns a comment as string (empty string if unavailable)'
       )
     }
     st.end()
   })
 
-  t.test('Should be able to access data for all chains provided', function (st: tape.Test) {
+  t.test('Should be able to access data for all chains provided', function (
+    st: tape.Test
+  ) {
     const c = new Common({ chain: 'mainnet' })
-    let hash = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
+    let hash =
+      '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     st.equal(c.genesis().hash, hash, 'mainnet')
     c.setChain('ropsten')
     hash = '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d'
@@ -118,26 +150,37 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
     st.end()
   })
 
-  t.test('Should provide correct access to private network chain parameters', function (
-    st: tape.Test,
+  t.test(
+    'Should provide correct access to private network chain parameters',
+    function (st: tape.Test) {
+      const chainParams = require('./testnet.json')
+      const c = new Common({ chain: chainParams, hardfork: 'byzantium' })
+      st.equal(c.chainName(), 'testnet', 'should initialize with chain name')
+      st.equal(c.chainId(), 12345, 'should return correct chain Id')
+      st.equal(c.networkId(), 12345, 'should return correct network Id')
+      st.equal(
+        c.genesis().hash,
+        '0xaa00000000000000000000000000000000000000000000000000000000000000',
+        'should return correct genesis hash'
+      )
+      st.equal(
+        c.hardforks()[3]['block'],
+        3,
+        'should return correct hardfork data'
+      )
+      st.equal(
+        c.bootstrapNodes()[1].ip,
+        '10.0.0.2',
+        'should return a bootstrap node array'
+      )
+
+      st.end()
+    }
+  )
+
+  t.test('Should handle custom chain parameters with missing field', function (
+    st: tape.Test
   ) {
-    const chainParams = require('./testnet.json')
-    const c = new Common({ chain: chainParams, hardfork: 'byzantium' })
-    st.equal(c.chainName(), 'testnet', 'should initialize with chain name')
-    st.equal(c.chainId(), 12345, 'should return correct chain Id')
-    st.equal(c.networkId(), 12345, 'should return correct network Id')
-    st.equal(
-      c.genesis().hash,
-      '0xaa00000000000000000000000000000000000000000000000000000000000000',
-      'should return correct genesis hash',
-    )
-    st.equal(c.hardforks()[3]['block'], 3, 'should return correct hardfork data')
-    st.equal(c.bootstrapNodes()[1].ip, '10.0.0.2', 'should return a bootstrap node array')
-
-    st.end()
-  })
-
-  t.test('Should handle custom chain parameters with missing field', function (st: tape.Test) {
     const chainParams = require('./testnet.json')
     delete chainParams['hardforks']
     st.throws(
@@ -145,10 +188,12 @@ tape('[Common]: Initialization / Chain params', function (t: tape.Test) {
         new Common({ chain: chainParams })
       },
       /Missing required/,
-      'should throw an exception on missing parameter',
+      'should throw an exception on missing parameter'
     ) // eslint-disable-line no-new
 
-    st.comment('-----------------------------------------------------------------')
+    st.comment(
+      '-----------------------------------------------------------------'
+    )
     st.end()
   })
 })