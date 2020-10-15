399f81ee18a4015dcf90f0b624dccf06492f9d34
Jochem Brouwer <jochembrouwer96@gmail.com>
2020-10-15 21:37:57 +0200

    vm: re-check skipped tests

--- packages/vm/tests/BlockchainTestsRunner.ts
+++ packages/vm/tests/BlockchainTestsRunner.ts
@@ -109,12 +109,6 @@ export default async function runBlockchainTest(options: any, testData: any, t: 
       continue
     }
 
-    if (currentBlock.lt(lastBlock)) {
-      // "re-org": rollback the blockchain to currentBlock (i.e. delete that block number in the blockchain plus the children)
-      t.fail('re-orgs are not supported by the test suite')
-      return
-    }
-
     try {
       // check if we should update common.
       const newFork = common.setHardforkByBlockNumber(currentBlock)
