9bedc1a028b52783aa839c37f550f85cee09eaea
holgerd77 <Holger.Drewes@gmail.com>
2020-10-16 13:41:23 +0200

    lint: minor fixes

--- packages/tx/.eslintrc.js
+++ packages/tx/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: "@ethereumjs/eslint-config-defaults",
-  ignorePatterns: ["examples", "karma.conf.js"]
+  ignorePatterns: ["examples", "karma.conf.js", "test-build"]
 }
