c7f37d0a27158dc8e3b69c7e2a9b777a4182c7c1
Everton Fraga <ev@ethereum.org>
2020-10-29 13:11:42 -0400

    tooling: Shellcheck fixes

--- scripts/load-submodules.sh
+++ scripts/load-submodules.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+#
+# Yarn doesn't handle well git submodules inside package dependencies
+# This postinstall script loads this specific submodule, found in ethereumjs-testing.
+# The git clone is only executed if the directory is empty, to prevent bad experience.
+#
+
 TESTS_CONTENTS=$(ls node_modules/ethereumjs-testing/tests)
 
-if [[ ! $TESTS_CONTENTS ]]
+if [ ! "$TESTS_CONTENTS" ]
 then
-    cd node_modules/ethereumjs-testing
+    cd node_modules/ethereumjs-testing || exit
     echo "Cloning ethereum/tests test files..."
     git clone https://github.com/ethereum/tests --branch develop --single-branch --depth=1 2>/dev/null
-    cd -
 fi
 
