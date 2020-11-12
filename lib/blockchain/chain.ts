e318b65d4b7a3e85dbc8e706f9009645788a6f60
Ryan Ghods <ryan@ryanio.com>
2020-11-12 10:10:26 -0800

    upgrade to monorepo beta.2 libs

--- lib/blockchain/chain.ts
+++ lib/blockchain/chain.ts
@@ -110,7 +110,7 @@ export class Chain extends EventEmitter {
         db: options.db,
         common: this.config.common,
         validateBlocks: false,
-        validatePow: false,
+        validateConsensus: false,
       })
 
     this.db = this.blockchain.db
@@ -324,6 +324,6 @@ export class Chain extends EventEmitter {
    */
   async getTd(hash: Buffer, num: BN): Promise<BN> {
     await this.open()
-    return this.blockchain._getTd(hash, num)
+    return (this.blockchain as any)._getTd(hash, num)
   }
 }
