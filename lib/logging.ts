eebf9c414f5263246603c15a6f81dcd5455291d7
holgerd77 <Holger.Drewes@gmail.com>
2020-11-27 13:34:50 +0100

    Fixed logger-related REPL listener error on REPL invocation

--- lib/logging.ts
+++ lib/logging.ts
@@ -37,7 +37,7 @@ function logFormat() {
 }
 
 export function getLogger(options = { loglevel: 'info' }) {
-  const logger = createLogger({
+  let loggerOptions: any = {
     format: combine(
       errorFormat(),
       format.splat(),
@@ -47,10 +47,10 @@ export function getLogger(options = { loglevel: 'info' }) {
     ),
     level: options.loglevel,
     silent: options.loglevel === 'off',
-    transports: [new transports.Console()],
-    exceptionHandlers: [new transports.Console()],
-  })
+    transports: [new transports.Console()]
+  }
+
+  const logger = createLogger(loggerOptions)
   return logger
 }
 
-export const defaultLogger = getLogger({ loglevel: 'info' })
