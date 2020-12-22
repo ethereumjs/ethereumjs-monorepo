9705c301133a4b4524f2a948997be164c45682ec
holgerd77 <Holger.Drewes@gmail.com>
2020-12-22 23:21:27 +0100

    client -> vm execution: fixed linting in vm

--- packages/vm/lib/runBlockchain.ts
+++ packages/vm/lib/runBlockchain.ts
@@ -11,30 +11,34 @@ export default async function runBlockchain(this: VM, blockchain?: Blockchain, m
 
   blockchain = blockchain || this.blockchain
 
-  await blockchain.iterator('vm', async (block: Block, reorg: boolean) => {
-    // determine starting state for block run
-    // if we are just starting or if a chain re-org has happened
-    if (!headBlock || reorg) {
-      const parentBlock = await blockchain!.getBlock(block.header.parentHash)
-      parentState = parentBlock.header.stateRoot
-      // generate genesis state if we are at the genesis block
-      // we don't have the genesis state
-      if (!headBlock) {
-        await this.stateManager.generateCanonicalGenesis()
-      } else {
-        parentState = headBlock.header.stateRoot
+  await blockchain.iterator(
+    'vm',
+    async (block: Block, reorg: boolean) => {
+      // determine starting state for block run
+      // if we are just starting or if a chain re-org has happened
+      if (!headBlock || reorg) {
+        const parentBlock = await blockchain!.getBlock(block.header.parentHash)
+        parentState = parentBlock.header.stateRoot
+        // generate genesis state if we are at the genesis block
+        // we don't have the genesis state
+        if (!headBlock) {
+          await this.stateManager.generateCanonicalGenesis()
+        } else {
+          parentState = headBlock.header.stateRoot
+        }
       }
-    }
 
-    // run block, update head if valid
-    try {
-      await this.runBlock({ block, root: parentState })
-      // set as new head block
-      headBlock = block
-    } catch (error) {
-      // remove invalid block
-      await blockchain!.delBlock(block.header.hash())
-      throw error
-    }
-  }, maxBlocks)
+      // run block, update head if valid
+      try {
+        await this.runBlock({ block, root: parentState })
+        // set as new head block
+        headBlock = block
+      } catch (error) {
+        // remove invalid block
+        await blockchain!.delBlock(block.header.hash())
+        throw error
+      }
+    },
+    maxBlocks
+  )
 }
