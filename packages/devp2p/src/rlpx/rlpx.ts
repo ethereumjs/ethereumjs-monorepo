37e1721d6030f823100a2ba7c1b76170559c2718
holgerd77 <Holger.Drewes@gmail.com>
2021-01-05 18:26:51 +0100

    common: updated goerli bootnodes

--- packages/devp2p/src/rlpx/rlpx.ts
+++ packages/devp2p/src/rlpx/rlpx.ts
@@ -230,7 +230,9 @@ export class RLPx extends EventEmitter {
           `disconnect from ${socket.remoteAddress}:${socket.remotePort}, reason: ${DISCONNECT_REASONS[reason]}`
         )
       } else {
-        debug(`${socket.remoteAddress}:${socket.remotePort} disconnect, reason: ${DISCONNECT_REASONS[reason]}`)
+        debug(
+          `${socket.remoteAddress}:${socket.remotePort} disconnect, reason: ${DISCONNECT_REASONS[reason]}`
+        )
       }
 
       if (!disconnectWe && reason === DISCONNECT_REASONS.TOO_MANY_PEERS) {
