9dc86b9cd617c403ad61a8f3b0e170a5de867ce6
Jochem Brouwer <jochembrouwer96@gmail.com>
2021-01-15 22:44:36 +0100

    common: add EIP-2718, EIP-2930

--- packages/common/src/eips/index.ts
+++ packages/common/src/eips/index.ts
@@ -4,5 +4,7 @@ export const EIPs: eipsType = {
   2315: require('./2315.json'),
   2537: require('./2537.json'),
   2565: require('./2565.json'),
+  2718: require('./2718.json'),
   2929: require('./2929.json'),
+  2930: require('./2930.json'),
 }
