9cc8050e25f7b0d984e7b32c0fe62e033735d3f0
Ryan Ghods <ryan@ryanio.com>
2021-02-04 14:50:27 -0800

    fix karma:   * add tsconfig to silence errors   * remove custom types exclude (seems to be working without now)   * lint

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,16 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'karma-typescript'],
-    exclude: ["src/@types/**"],  // ref: https://github.com/monounity/karma-typescript/issues/254
     files: ['src/**/*.ts', 'test/**/*.ts'],
     preprocessors: {
-      '**/*.ts': ['karma-typescript'],
+      '**/*.ts': ['karma-typescript']
     },
     plugins: ['karma-mocha', 'karma-typescript', 'karma-chrome-launcher', 'karma-firefox-launcher'],
     karmaTypescriptConfig: {
       bundlerOptions: {
-        entrypoints: /\.spec\.ts$/,
+        entrypoints: /\.spec\.ts$/
       },
+      tsconfig: './tsconfig.json'
     },
     colors: true,
     reporters: ['progress', 'karma-typescript'],
@@ -19,6 +19,6 @@ module.exports = function(config) {
     concurrency: Infinity,
     // Fail after timeout
     browserDisconnectTimeout: 100000,
-    browserNoActivityTimeout: 100000,
+    browserNoActivityTimeout: 100000
   })
 }
