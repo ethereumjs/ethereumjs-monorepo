7519680572fa2bbdf8c6932c611ec65071a2fd11
holgerd77 <Holger.Drewes@gmail.com>
2021-02-25 16:23:46 +0100

    tx -> EIP-2930: renamed api tests to legacy

--- packages/tx/test/index.ts
+++ packages/tx/test/index.ts
@@ -2,12 +2,14 @@ import minimist from 'minimist'
 
 const argv = minimist(process.argv.slice(2))
 
-if (argv.a) {
-  require('./api.spec')
+if (argv.l) {
+  require('./legacy.spec')
+} else if (argv.e) {
+  require('./eip2930.spec')
 } else if (argv.t) {
   require('./transactionRunner')
 } else {
-  require('./api.spec')
+  require('./legacy.spec')
   require('./transactionRunner')
   require('./transactionFactory.spec')
   require('./eip2930.spec')
