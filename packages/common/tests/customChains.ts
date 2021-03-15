d9104857ae864af074df24cf6a84bc7ec3a038fb
Ryan Ghods <ryan@ryanio.com>
2021-03-15 12:11:51 -0700

    common: add tests for new bn methods

--- packages/common/tests/customChains.ts
+++ packages/common/tests/customChains.ts
@@ -11,7 +11,9 @@ tape('[Common]: Custom chains', function (t: tape.Test) {
       const c = new Common({ chain: testnet, hardfork: 'byzantium' })
       st.equal(c.chainName(), 'testnet', 'should initialize with chain name')
       st.equal(c.chainId(), 12345, 'should return correct chain Id')
+      st.ok(c.chainIdBN().eqn(12345), 'should return correct chain Id')
       st.equal(c.networkId(), 12345, 'should return correct network Id')
+      st.ok(c.networkIdBN().eqn(12345), 'should return correct network Id')
       st.equal(
         c.genesis().hash,
         '0xaa00000000000000000000000000000000000000000000000000000000000000',
@@ -50,7 +52,9 @@ tape('[Common]: Custom chains', function (t: tape.Test) {
     // From custom chain params
     st.equal(customChainCommon.chainName(), customChainParams.name)
     st.equal(customChainCommon.chainId(), customChainParams.chainId)
+    st.ok(customChainCommon.chainIdBN().eqn(customChainParams.chainId))
     st.equal(customChainCommon.networkId(), customChainParams.networkId)
+    st.ok(customChainCommon.networkIdBN().eqn(customChainParams.networkId))
 
     // Fallback params from mainnet
     st.equal(customChainCommon.genesis(), mainnetCommon.genesis())
