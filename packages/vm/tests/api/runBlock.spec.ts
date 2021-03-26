f60d01ccdc798325e9e09ae9b5645ee1c6ffab4f
holgerd77 <Holger.Drewes@gmail.com>
2021-03-26 11:51:08 +0100

    vm -> API tests: restructure runBlock() tests

--- packages/vm/tests/api/runBlock.spec.ts
+++ packages/vm/tests/api/runBlock.spec.ts
@@ -11,7 +11,104 @@ import VM from '../../lib/index'
 const testData = require('./testdata/blockchain.json')
 const common = new Common({ chain: 'mainnet', hardfork: 'berlin' })
 
-tape('runBlock', async (t) => {
+tape('runBlock() -> successful API parameter usage', async (t) => {
+  t.test('simple run (unmodified options)', async (t) => {
+    const vm = setupVM()
+
+    const genesisRlp = testData.genesisRLP
+    const genesis = Block.fromRLPSerializedBlock(genesisRlp)
+
+    const blockRlp = testData.blocks[0].rlp
+    const block = Block.fromRLPSerializedBlock(blockRlp)
+
+    //@ts-ignore
+    await setupPreConditions(vm.stateManager._trie, testData)
+
+    t.ok(
+      //@ts-ignore
+      vm.stateManager._trie.root.equals(genesis.header.stateRoot),
+      'genesis state root should match calculated state root'
+    )
+
+    const res = await vm.runBlock({
+      block,
+      // @ts-ignore
+      root: vm.stateManager._trie.root,
+      skipBlockValidation: true,
+    })
+
+    t.equal(
+      res.results[0].gasUsed.toString('hex'),
+      '5208',
+      'actual gas used should equal blockHeader gasUsed'
+    )
+
+    t.end()
+  })
+
+  t.test('hardforkByBlockNumber option', async (t) => {
+    const common1 = new Common({
+      chain: 'mainnet',
+      hardfork: 'muirGlacier',
+    })
+
+    // Have to use an unique common, otherwise the HF will be set to muirGlacier and then will not change back to chainstart.
+    const common2 = new Common({
+      chain: 'mainnet',
+      hardfork: 'chainstart',
+    })
+
+    const privateKey = Buffer.from(
+      'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109',
+      'hex'
+    )
+
+    function getBlock(common: Common): Block {
+      return Block.fromBlockData(
+        {
+          header: {
+            number: new BN(10000000),
+          },
+          transactions: [
+            Transaction.fromTxData(
+              {
+                data: '0x600154', // PUSH 01 SLOAD
+                gasLimit: new BN(100000),
+              },
+              { common }
+            ).sign(privateKey),
+          ],
+        },
+        { common }
+      )
+    }
+
+    const vm = new VM({ common: common1, hardforkByBlockNumber: true })
+    const vm_noSelect = new VM({ common: common2 })
+
+    const txResultMuirGlacier = await vm.runBlock({
+      block: getBlock(common1),
+      skipBlockValidation: true,
+      generate: true,
+    })
+    const txResultChainstart = await vm_noSelect.runBlock({
+      block: getBlock(common2),
+      skipBlockValidation: true,
+      generate: true,
+    })
+    t.ok(
+      txResultChainstart.results[0].gasUsed.toNumber() == 21000 + 68 * 3 + 3 + 50,
+      'tx charged right gas on chainstart hard fork'
+    )
+    t.ok(
+      txResultMuirGlacier.results[0].gasUsed.toNumber() == 21000 + 32000 + 16 * 3 + 3 + 800,
+      'tx charged right gas on muir glacier hard fork'
+    )
+    t.end()
+  })
+})
+
+tape('runBlock() -> API parameter usage/data errors', async (t) => {
   const vm = new VM({ common })
 
   t.test('should fail when runTx fails', async (st) => {
@@ -27,164 +124,68 @@ tape('runBlock', async (t) => {
 
     st.end()
   })
-})
 
-tape('should fail when block gas limit higher than 2^63-1', async (t) => {
-  const vm = new VM({ common })
+  t.test('should fail when block gas limit higher than 2^63-1', async (t) => {
+    const vm = new VM({ common })
 
-  const block = Block.fromBlockData({
-    header: {
-      ...testData.blocks[0].header,
-      gasLimit: Buffer.from('8000000000000000', 16),
-    },
-  })
-  await vm
-    .runBlock({ block })
-    .then(() => t.fail('should have returned error'))
-    .catch((e) => t.ok(e.message.includes('Invalid block')))
-
-  t.end()
-})
-
-tape('should fail when block validation fails', async (t) => {
-  const vm = new VM({ common })
-
-  const blockRlp = testData.blocks[0].rlp
-  const block = Object.create(Block.fromRLPSerializedBlock(blockRlp))
-  block.validate = async () => {
-    throw new Error('test')
-  }
-
-  await vm
-    .runBlock({ block })
-    .then(() => t.fail('should have returned error'))
-    .catch((e) => t.ok(e.message.includes('test')))
-
-  t.end()
-})
-
-tape('should fail when tx gas limit higher than block gas limit', async (t) => {
-  const vm = new VM({ common })
-
-  const blockRlp = testData.blocks[0].rlp
-  const block = Object.create(Block.fromRLPSerializedBlock(blockRlp))
-  // modify first tx's gasLimit
-  const { nonce, gasPrice, to, value, data, v, r, s } = block.transactions[0]
-
-  const gasLimit = new BN(Buffer.from('3fefba', 'hex'))
-  const opts = { common: block._common }
-  block.transactions[0] = new Transaction(
-    { nonce, gasPrice, gasLimit, to, value, data, v, r, s },
-    opts
-  )
-
-  await vm
-    .runBlock({ block, skipBlockValidation: true })
-    .then(() => t.fail('should have returned error'))
-    .catch((e) => t.ok(e.message.includes('higher gas limit')))
-
-  t.end()
-})
-
-tape('should correctly use the hardforkByBlockNumber option', async (t) => {
-  const common1 = new Common({
-    chain: 'mainnet',
-    hardfork: 'muirGlacier',
-  })
-
-  // Have to use an unique common, otherwise the HF will be set to muirGlacier and then will not change back to chainstart.
-  const common2 = new Common({
-    chain: 'mainnet',
-    hardfork: 'chainstart',
-  })
-
-  const privateKey = Buffer.from(
-    'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109',
-    'hex'
-  )
-
-  function getBlock(common: Common): Block {
-    return Block.fromBlockData(
-      {
-        header: {
-          number: new BN(10000000),
-        },
-        transactions: [
-          Transaction.fromTxData(
-            {
-              data: '0x600154', // PUSH 01 SLOAD
-              gasLimit: new BN(100000),
-            },
-            { common }
-          ).sign(privateKey),
-        ],
+    const block = Block.fromBlockData({
+      header: {
+        ...testData.blocks[0].header,
+        gasLimit: Buffer.from('8000000000000000', 16),
       },
-      { common }
+    })
+    await vm
+      .runBlock({ block })
+      .then(() => t.fail('should have returned error'))
+      .catch((e) => t.ok(e.message.includes('Invalid block')))
+
+    t.end()
+  })
+
+  t.test('should fail when block validation fails', async (t) => {
+    const vm = new VM({ common })
+
+    const blockRlp = testData.blocks[0].rlp
+    const block = Object.create(Block.fromRLPSerializedBlock(blockRlp))
+    block.validate = async () => {
+      throw new Error('test')
+    }
+
+    await vm
+      .runBlock({ block })
+      .then(() => t.fail('should have returned error'))
+      .catch((e) => t.ok(e.message.includes('test')))
+
+    t.end()
+  })
+
+  t.test('should fail when tx gas limit higher than block gas limit', async (t) => {
+    const vm = new VM({ common })
+
+    const blockRlp = testData.blocks[0].rlp
+    const block = Object.create(Block.fromRLPSerializedBlock(blockRlp))
+    // modify first tx's gasLimit
+    const { nonce, gasPrice, to, value, data, v, r, s } = block.transactions[0]
+
+    const gasLimit = new BN(Buffer.from('3fefba', 'hex'))
+    const opts = { common: block._common }
+    block.transactions[0] = new Transaction(
+      { nonce, gasPrice, gasLimit, to, value, data, v, r, s },
+      opts
     )
-  }
 
-  const vm = new VM({ common: common1, hardforkByBlockNumber: true })
-  const vm_noSelect = new VM({ common: common2 })
+    await vm
+      .runBlock({ block, skipBlockValidation: true })
+      .then(() => t.fail('should have returned error'))
+      .catch((e) => t.ok(e.message.includes('higher gas limit')))
 
-  const txResultMuirGlacier = await vm.runBlock({
-    block: getBlock(common1),
-    skipBlockValidation: true,
-    generate: true,
+    t.end()
   })
-  const txResultChainstart = await vm_noSelect.runBlock({
-    block: getBlock(common2),
-    skipBlockValidation: true,
-    generate: true,
-  })
-  t.ok(
-    txResultChainstart.results[0].gasUsed.toNumber() == 21000 + 68 * 3 + 3 + 50,
-    'tx charged right gas on chainstart hard fork'
-  )
-  t.ok(
-    txResultMuirGlacier.results[0].gasUsed.toNumber() == 21000 + 32000 + 16 * 3 + 3 + 800,
-    'tx charged right gas on muir glacier hard fork'
-  )
-  t.end()
 })
 
-tape('should run valid block', async (t) => {
-  const vm = setupVM()
-
-  const genesisRlp = testData.genesisRLP
-  const genesis = Block.fromRLPSerializedBlock(genesisRlp)
-
-  const blockRlp = testData.blocks[0].rlp
-  const block = Block.fromRLPSerializedBlock(blockRlp)
-
-  //@ts-ignore
-  await setupPreConditions(vm.stateManager._trie, testData)
-
-  t.ok(
-    //@ts-ignore
-    vm.stateManager._trie.root.equals(genesis.header.stateRoot),
-    'genesis state root should match calculated state root'
-  )
-
-  const res = await vm.runBlock({
-    block,
-    // @ts-ignore
-    root: vm.stateManager._trie.root,
-    skipBlockValidation: true,
-  })
-
-  t.equal(
-    res.results[0].gasUsed.toString('hex'),
-    '5208',
-    'actual gas used should equal blockHeader gasUsed'
-  )
-
-  t.end()
-})
-
-// this test actually checks if the DAO fork works. This is not checked in ethereum/tests
-tape(
-  'should transfer balance from DAO children to the Refund DAO account in the DAO fork',
-  async (t) => {
+tape('runBlock() -> runtime behavior', async (t) => {
+  // this test actually checks if the DAO fork works. This is not checked in ethereum/tests
+  t.test('DAO fork behavior', async (t) => {
     const common = getDAOCommon(1)
 
     const vm = setupVM({ common })
@@ -230,11 +231,14 @@ tape(
     t.ok(DAOFundedContractAccount2.balance.isZero()) // verify our funded account now has 0 balance
 
     const DAORefundAccount = await vm.stateManager.getAccount(DAORefundAddress)
-    t.ok(DAORefundAccount.balance.eq(new BN(Buffer.from('7777', 'hex')))) // verify that the refund account gets the summed balance of the original refund account + two child DAO accounts
+    // verify that the refund account gets the summed balance of the original refund account + two child DAO accounts
+    const msg =
+      'should transfer balance from DAO children to the Refund DAO account in the DAO fork'
+    t.ok(DAORefundAccount.balance.eq(new BN(Buffer.from('7777', 'hex'))), msg)
 
     t.end()
-  }
-)
+  })
+})
 
 async function runWithHf(hardfork: string) {
   const vm = setupVM({ common: new Common({ chain: 'mainnet', hardfork }) })
@@ -253,20 +257,23 @@ async function runWithHf(hardfork: string) {
   return res
 }
 
-tape('should return correct HF receipts', async (t) => {
-  let res = await runWithHf('byzantium')
-  t.equal(
-    (res.receipts[0] as PostByzantiumTxReceipt).status,
-    1,
-    'should return correct post-Byzantium receipt format'
-  )
+// TODO: complete on result values and add more usage scenario test cases
+tape('runBlock() -> API return values', async (t) => {
+  t.test('should return correct HF receipts', async (t) => {
+    let res = await runWithHf('byzantium')
+    t.equal(
+      (res.receipts[0] as PostByzantiumTxReceipt).status,
+      1,
+      'should return correct post-Byzantium receipt format'
+    )
 
-  res = await runWithHf('spuriousDragon')
-  t.deepEqual(
-    (res.receipts[0] as PreByzantiumTxReceipt).stateRoot,
-    Buffer.from('4477e2cfaf9fd2eed4f74426798b55d140f6a9612da33413c4745f57d7a97fcc', 'hex'),
-    'should return correct pre-Byzantium receipt format'
-  )
+    res = await runWithHf('spuriousDragon')
+    t.deepEqual(
+      (res.receipts[0] as PreByzantiumTxReceipt).stateRoot,
+      Buffer.from('4477e2cfaf9fd2eed4f74426798b55d140f6a9612da33413c4745f57d7a97fcc', 'hex'),
+      'should return correct pre-Byzantium receipt format'
+    )
 
-  t.end()
+    t.end()
+  })
 })
