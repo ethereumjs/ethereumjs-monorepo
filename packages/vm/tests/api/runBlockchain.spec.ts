b2dd07a0f684f70a82b740f1d03561f67c416914
holgerd77 <Holger.Drewes@gmail.com>
2021-03-26 10:03:28 +0100

    vm -> API tests: clean up runBlockchain() tests to use the available setupVM() setup function, call runBlockchain() in a VM context

--- packages/vm/tests/api/runBlockchain.spec.ts
+++ packages/vm/tests/api/runBlockchain.spec.ts
@@ -3,8 +3,7 @@ import { BN } from 'ethereumjs-util'
 import { Block } from '@ethereumjs/block'
 import Common from '@ethereumjs/common'
 import Blockchain from '@ethereumjs/blockchain'
-import runBlockchain from '../../lib/runBlockchain'
-import { DefaultStateManager } from '../../lib/state'
+import { setupVM } from './utils'
 
 const level = require('level-mem')
 
@@ -14,53 +13,39 @@ tape('runBlockchain', (t) => {
   const blockchainDB = level()
   const common = new Common({ chain: 'goerli', hardfork: 'chainstart' })
 
-  const blockchain = new Blockchain({
-    db: blockchainDB,
-    common,
-    validateBlocks: false,
-    validateConsensus: false,
-  })
-
-  const stateManager = new DefaultStateManager({ common })
-
-  const vm = {
-    stateManager,
-    blockchain,
-  }
-
   t.test('should run without a blockchain parameter', async (st) => {
+    const vm = setupVM({ common })
     st.doesNotThrow(async function () {
-      // @ts-ignore
-      await runBlockchain.bind(vm)()
+      await vm.runBlockchain()
       st.end()
     })
   })
 
   t.test('should run without blocks', async (st) => {
+    const vm = setupVM({ common })
     st.doesNotThrow(async function () {
-      // @ts-ignore
-      await runBlockchain.bind(vm)(blockchain)
+      await vm.runBlockchain()
       st.end()
     })
   })
 
   t.test('should run with genesis block', async (st) => {
     try {
-      const common = new Common({ chain: 'goerli', hardfork: 'chainstart' })
       const genesisBlock = Block.genesis(undefined, { common })
 
-      const newBlockchain = await Blockchain.create({
+      const blockchain = await Blockchain.create({
         db: blockchainDB,
         common,
         validateBlocks: false,
         validateConsensus: false,
         genesisBlock,
       })
+      const vm = setupVM({ genesisBlock, blockchain })
 
-      st.ok(newBlockchain.meta.genesis, 'genesis should be set for blockchain')
+      st.ok(blockchain.meta.genesis, 'genesis should be set for blockchain')
 
       // @ts-ignore
-      await runBlockchain.bind(vm)(newBlockchain)
+      await vm.runBlockchain()
       st.end()
     } catch (e) {
       st.end(e)
@@ -69,6 +54,19 @@ tape('runBlockchain', (t) => {
 
   t.test('should run with valid and invalid blocks', async (st) => {
     const blockchainDB = level()
+
+    const common = new Common({ chain: 'mainnet', hardfork: 'chainstart' })
+    const genesisBlock = Block.genesis(undefined, { common })
+    const blockchain = new Blockchain({
+      db: blockchainDB,
+      common,
+      validateBlocks: false,
+      validateConsensus: false,
+      genesisBlock,
+    })
+
+    const vm = setupVM({ genesisBlock, blockchain })
+
     // Produce error on the third time runBlock is called
     let runBlockInvocations = 0
     ;(<any>vm).runBlock = () =>
@@ -80,35 +78,24 @@ tape('runBlockchain', (t) => {
         resolve({})
       })
 
-    const common = new Common({ chain: 'mainnet', hardfork: 'chainstart' })
-    const genesisBlock = Block.genesis(undefined, { common })
-    const newBlockchain = new Blockchain({
-      db: blockchainDB,
-      common,
-      validateBlocks: false,
-      validateConsensus: false,
-      genesisBlock,
-    })
-
     const b1 = createBlock(genesisBlock, 1, { common })
     const b2 = createBlock(b1, 2, { common })
     const b3 = createBlock(b2, 3, { common })
 
-    await newBlockchain.putBlock(b1)
-    await newBlockchain.putBlock(b2)
-    await newBlockchain.putBlock(b3)
+    await blockchain.putBlock(b1)
+    await blockchain.putBlock(b2)
+    await blockchain.putBlock(b3)
 
-    let head = await newBlockchain.getHead()
+    let head = await blockchain.getHead()
     st.deepEqual(head.hash(), b3.hash(), 'block3 should be the current head')
 
     try {
-      // @ts-ignore
-      await runBlockchain.bind(vm)(newBlockchain)
+      await vm.runBlockchain()
       st.fail('should have returned error')
     } catch (e) {
       st.equal(e.message, 'test')
 
-      head = await newBlockchain.getHead()
+      head = await blockchain.getHead()
       st.deepEqual(head.hash(), b2.hash(), 'should have removed invalid block from head')
 
       st.end()
