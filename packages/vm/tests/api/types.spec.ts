173f4ce3a74d1a73e525af26a5c51cbaab3a7a55
Ryan Ghods <ryan@ryanio.com>
2021-04-05 15:54:35 -0700

    add type safety test from @alcuadrado

---
+++ packages/vm/tests/api/types.spec.ts
@@ -0,0 +1,43 @@
+import tape from 'tape'
+import Common from '@ethereumjs/common'
+import {
+  AccessListEIP2930Transaction,
+  AccessListEIP2930TxData,
+  Transaction,
+  TxData,
+} from '@ethereumjs/tx'
+import { Block, BlockData } from '@ethereumjs/block'
+
+tape('[Types]', function (t) {
+  t.test('should ensure that the actual objects can be safely used as their data types', (st) => {
+    type RequiredExceptOptionals<TypeT, OptionalFieldsT extends keyof TypeT> = Required<
+      Omit<TypeT, OptionalFieldsT>
+    > &
+      Pick<TypeT, OptionalFieldsT>
+
+    const common = new Common({ chain: 'mainnet', hardfork: 'berlin' })
+
+    // Block
+    const block: Required<BlockData> = Block.fromBlockData({}, { common })
+    st.ok(block, 'block')
+
+    // Transactions
+    type OptionalTxFields = 'to' | 'r' | 's' | 'v'
+
+    // Legacy tx
+    const legacyTx: RequiredExceptOptionals<TxData, OptionalTxFields> = Transaction.fromTxData(
+      {},
+      { common }
+    )
+    st.ok(legacyTx, 'legacy tx')
+
+    // Access List tx
+    const accessListTx: RequiredExceptOptionals<
+      AccessListEIP2930TxData,
+      OptionalTxFields
+    > = AccessListEIP2930Transaction.fromTxData({}, { common })
+    st.ok(accessListTx, 'accessList tx')
+
+    st.end()
+  })
+})
