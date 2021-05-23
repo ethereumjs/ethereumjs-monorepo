7cc1c8e335bdbb965b0fc28a7c604509e7ca18cd
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-05-23 09:17:11 -0400

    chore: replace eth_ with consensus_ and remove ethVersion

--- packages/client/lib/rpc/modules/consensus.ts
+++ packages/client/lib/rpc/modules/consensus.ts
@@ -6,25 +6,21 @@ import type { EthProtocol } from '../../net/protocol'
 import type VM from '@ethereumjs/vm'
 
 /**
- * eth_* RPC module
+ * consensus_* RPC module
  * @memberof module:rpc/modules
  */
 export class Consensus {
   private _chain: Chain
   private _vm: VM | undefined
-  public ethVersion: number
 
   /**
-   * Create eth_* RPC module
+   * Create consensus_* RPC module
    * @param client Client to which the module binds
    */
   constructor(client: EthereumClient) {
     const service = client.services.find((s) => s.name === 'eth') as EthereumService
     this._chain = service.chain
     this._vm = (service.synchronizer as any)?.execution?.vm
-
-    const ethProtocol = service.protocols.find((p) => p.name === 'eth') as EthProtocol
-    this.ethVersion = Math.max(...ethProtocol.versions)
 
     // TODO: validation for assembleBlock and newBlock methods, see https://github.com/ethereum/rayonism/blob/master/specs/merge.md#consensus-json-rpc
     this.assembleBlock = middleware(this.assembleBlock.bind(this), 1)
