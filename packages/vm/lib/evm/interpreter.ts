181270155c69415f8d12c1030a97c6da959ed77c
holgerd77 <Holger.Drewes@gmail.com>
2021-05-26 11:22:01 +0200

    vm: reintroduce debug logger with safe guard (Interpreter, EIP-1283, EIP-2200, EIP-2929)

--- packages/vm/lib/evm/interpreter.ts
+++ packages/vm/lib/evm/interpreter.ts
@@ -1,3 +1,4 @@
+import { debug as createDebugLogger } from 'debug'
 import { Account, Address, BN } from 'ethereumjs-util'
 import Common from '@ethereumjs/common'
 import { StateManager } from '../state/index'
@@ -65,6 +66,9 @@ export default class Interpreter {
   _state: StateManager
   _runState: RunState
   _eei: EEI
+
+  // Opcode debuggers (e.g. { 'push': [debug Object], 'sstore': [debug Object], ...})
+  private opDebuggers: any = {}
 
   constructor(vm: any, eei: EEI) {
     this._vm = vm // TODO: remove when not needed
@@ -142,7 +146,7 @@ export default class Interpreter {
     }
 
     // Reduce opcode's base fee
-    this._eei.useGas(new BN(opInfo.fee))
+    this._eei.useGas(new BN(opInfo.fee), `${opInfo.name} (base fee)`)
     // Advance program counter
     this._runState.programCounter++
 
@@ -190,6 +194,30 @@ export default class Interpreter {
       memory: this._runState.memory._store, // Return underlying array for backwards-compatibility
       memoryWordCount: this._runState.memoryWordCount,
       codeAddress: this._eei._env.codeAddress,
+    }
+
+    if (this._vm.DEBUG) {
+      // Create opTrace for debug functionality
+      let hexStack = []
+      hexStack = eventObj.stack.map((item: any) => {
+        return '0x' + new BN(item).toString(16, 0)
+      })
+
+      const name = eventObj.opcode.name
+
+      const opTrace = {
+        pc: eventObj.pc,
+        op: name,
+        gas: '0x' + eventObj.gasLeft.toString('hex'),
+        gasCost: '0x' + eventObj.opcode.fee.toString(16),
+        stack: hexStack,
+        depth: eventObj.depth,
+      }
+
+      if (!(name in this.opDebuggers)) {
+        this.opDebuggers[name] = createDebugLogger(`vm:ops:${name}`)
+      }
+      this.opDebuggers[name](JSON.stringify(opTrace))
     }
 
     /**
--- packages/vm/lib/evm/opcodes/EIP1283.ts
+++ packages/vm/lib/evm/opcodes/EIP1283.ts
@@ -12,7 +12,10 @@ export function updateSstoreGasEIP1283(runState: RunState, found: any, value: Bu
   const { original, current } = found
   if (current.equals(value)) {
     // If current value equals new value (this is a no-op), 200 gas is deducted.
-    runState.eei.useGas(new BN(runState._common.param('gasPrices', 'netSstoreNoopGas')))
+    runState.eei.useGas(
+      new BN(runState._common.param('gasPrices', 'netSstoreNoopGas')),
+      'EIP-1283 -> netSstoreNoopGas'
+    )
     return
   }
   // If current value does not equal new value
@@ -20,24 +23,39 @@ export function updateSstoreGasEIP1283(runState: RunState, found: any, value: Bu
     // If original value equals current value (this storage slot has not been changed by the current execution context)
     if (original.length === 0) {
       // If original value is 0, 20000 gas is deducted.
-      return runState.eei.useGas(new BN(runState._common.param('gasPrices', 'netSstoreInitGas')))
+      return runState.eei.useGas(
+        new BN(runState._common.param('gasPrices', 'netSstoreInitGas')),
+        'EIP-1283 -> netSstoreInitGas'
+      )
     }
     if (value.length === 0) {
       // If new value is 0, add 15000 gas to refund counter.
-      runState.eei.refundGas(new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')))
+      runState.eei.refundGas(
+        new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')),
+        'EIP-1283 -> netSstoreClearRefund'
+      )
     }
     // Otherwise, 5000 gas is deducted.
-    return runState.eei.useGas(new BN(runState._common.param('gasPrices', 'netSstoreCleanGas')))
+    return runState.eei.useGas(
+      new BN(runState._common.param('gasPrices', 'netSstoreCleanGas')),
+      'EIP-1283 -> netSstoreCleanGas'
+    )
   }
   // If original value does not equal current value (this storage slot is dirty), 200 gas is deducted. Apply both of the following clauses.
   if (original.length !== 0) {
     // If original value is not 0
     if (current.length === 0) {
       // If current value is 0 (also means that new value is not 0), remove 15000 gas from refund counter. We can prove that refund counter will never go below 0.
-      runState.eei.subRefund(new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')))
+      runState.eei.subRefund(
+        new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')),
+        'EIP-1283 -> netSstoreClearRefund'
+      )
     } else if (value.length === 0) {
       // If new value is 0 (also means that current value is not 0), add 15000 gas to refund counter.
-      runState.eei.refundGas(new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')))
+      runState.eei.refundGas(
+        new BN(runState._common.param('gasPrices', 'netSstoreClearRefund')),
+        'EIP-1283 -> netSstoreClearRefund'
+      )
     }
   }
   if (original.equals(value)) {
@@ -45,12 +63,19 @@ export function updateSstoreGasEIP1283(runState: RunState, found: any, value: Bu
     if (original.length === 0) {
       // If original value is 0, add 19800 gas to refund counter.
       runState.eei.refundGas(
-        new BN(runState._common.param('gasPrices', 'netSstoreResetClearRefund'))
+        new BN(runState._common.param('gasPrices', 'netSstoreResetClearRefund')),
+        'EIP-1283 -> netSstoreResetClearRefund'
       )
     } else {
       // Otherwise, add 4800 gas to refund counter.
-      runState.eei.refundGas(new BN(runState._common.param('gasPrices', 'netSstoreResetRefund')))
+      runState.eei.refundGas(
+        new BN(runState._common.param('gasPrices', 'netSstoreResetRefund')),
+        'EIP-1283 -> netSstoreResetRefund'
+      )
     }
   }
-  return runState.eei.useGas(new BN(runState._common.param('gasPrices', 'netSstoreDirtyGas')))
+  return runState.eei.useGas(
+    new BN(runState._common.param('gasPrices', 'netSstoreDirtyGas')),
+    'EIP-1283 -> netSstoreDirtyGas'
+  )
 }
