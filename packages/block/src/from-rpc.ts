89ef1a2a38f05ca1bfb084483ffa2a4dd38ba145
Amir <indigophi@protonmail.com>
2021-06-23 12:17:12 -0400

    block: check and convert difficulty, gasPrice, and value parameters

--- packages/block/src/from-rpc.ts
+++ packages/block/src/from-rpc.ts
@@ -1,6 +1,7 @@
 import { TransactionFactory, TypedTransaction, TxData } from '@ethereumjs/tx'
 import { toBuffer, setLengthLeft } from 'ethereumjs-util'
 import { Block, BlockOptions } from './index'
+import { numberToHex } from './helpers'
 
 import blockHeaderFromRpc from './header-from-rpc'
 
@@ -9,6 +10,10 @@ function normalizeTxParams(_txParams: any) {
 
   txParams.gasLimit = txParams.gasLimit === undefined ? txParams.gas : txParams.gasLimit
   txParams.data = txParams.data === undefined ? txParams.input : txParams.data
+
+  // check and convert gasPrice and value params
+  txParams.gasPrice = numberToHex(txParams.gasPrice)
+  txParams.value = numberToHex(txParams.value)
 
   // strict byte length checking
   txParams.to = txParams.to ? setLengthLeft(toBuffer(txParams.to), 20) : null
