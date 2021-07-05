068679e9ee247abd1271cf5941820cf8b3b981a2
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-07-04 21:52:32 -0400

    client: eth_syncing mocking and testing

--- packages/client/test/rpc/eth/syncing.spec.ts
+++ packages/client/test/rpc/eth/syncing.spec.ts
@@ -1,78 +1,130 @@
-// import tape from 'tape-catch'
-// import td from 'testdouble'
-// import { Config } from '../../../lib/config'
-// import { PeerPool } from '../../../lib/net/peerpool'
-// import { params, baseRequest, createClient, createManager, startRPC } from '../helpers'
-// import { EventEmitter } from 'events'
-// import Blockchain from '@ethereumjs/blockchain'
-// import { LightEthereumService } from '../../../lib/service'
+import { EventEmitter } from 'events'
+import tape from 'tape-catch'
+import td from 'testdouble'
+import { Config } from '../../../lib/config'
+import { baseRequest, createManager, params, startRPC } from '../helpers'
+import EthereumClient from '../../../lib/client'
+import { BN } from 'ethereumjs-util'
 
-// const method = 'eth_syncing'
+const method = 'eth_syncing'
 
-// tape(`${method} should return false when chain is synced`, async (t) => {
-//   const config = new Config({ transports: [], loglevel: 'error' })
-//   class FullEthereumService extends EventEmitter {
-//     open() {}
-//     start() {}
-//     stop() {}
-//     config = config
-//     pool = new PeerPool({ config })
-//   }
-//   FullEthereumService.prototype.open = td.func<any>()
-//   FullEthereumService.prototype.start = td.func<any>()
-//   FullEthereumService.prototype.stop = td.func<any>()
-//   td.replace('../../../lib/service', { FullEthereumService })
-//   td.when(FullEthereumService.prototype.open()).thenResolve()
-//   td.when(FullEthereumService.prototype.start()).thenResolve()
-//   td.when(FullEthereumService.prototype.stop()).thenResolve()
+tape(`${method}: should return false when the client is synchronized`, async (t) => {
+  const config = new Config({ transports: [], loglevel: 'error' })
+  const client = new EthereumClient({ config })
 
-//   class Server extends EventEmitter {
-//     open() {}
-//     start() {}
-//     stop() {}
-//     bootstrap() {}
-//   }
-//   Server.prototype.open = td.func<any>()
-//   Server.prototype.start = td.func<any>()
-//   Server.prototype.stop = td.func<any>()
-//   Server.prototype.bootstrap = td.func<any>()
-//   td.replace('../../../lib/net/server/server', { Server })
-//   td.when(Server.prototype.start()).thenResolve()
-//   td.when(Server.prototype.stop()).thenResolve()
-//   td.when(Server.prototype.bootstrap()).thenResolve()
+  const manager = createManager(client)
+  const server = startRPC(manager.getMethods())
 
-//   // const { default: EthereumClient } = await import('../../../lib/client')
+  await client.open()
 
-//   t.test('should initialize correctly', async (t) => {
-//     const blockchain = await Blockchain.create()
+  t.equals(client.synchronized, false, 'not synchronized yet')
+  client.services[0].emit('synchronized')
+  t.equals(client.synchronized, true, 'synchronized')
 
-//     const client = createClient({ blockchain })
-//     const manager = createManager(client)
-//     const server = startRPC(manager.getMethods())
+  const req = params(method, [])
+  const expectRes = (res: any) => {
+    const msg = 'should return false'
+    if (res.body.result === false) {
+      t.pass(msg)
+    } else {
+      throw new Error(msg)
+    }
+  }
+  baseRequest(t, server, req, 200, expectRes)
+})
 
-//     const service = client.services.find((s) => s.name === 'eth') as LightEthereumService
+tape(`${method}: should return no peer available error`, async (t) => {
+  const config = new Config({ transports: [], loglevel: 'error' })
+  const client = new EthereumClient({ config })
 
-//     // console.log('Synchronized? ', client.synchronized)
-//     service.synchronizer.emit('synchronized')
+  const manager = createManager(client)
+  const rpcServer = startRPC(manager.getMethods())
+  const synchronizer = client.services[0].synchronizer
 
-//     const req = params(method, [])
-//     // console.log(req)
-//     const expectRes = (res: any) => {
-//       const msg = 'should return false'
-//       t.equal(res.body.result, false, msg)
-//     }
-//     baseRequest(t, server, req, 200, expectRes)
-//   })
-// })
+  client.open = td.func<typeof client['open']>()
+  td.when(client.open()).thenResolve(false)
+  await client.open()
 
-// tape(`${method}: returns object with sync status when chain is not synced`, (t) => {
-//   const manager = createManager(createClient())
-//   const server = startRPC(manager.getMethods())
+  t.equals(client.synchronized, false, 'not synchronized yet')
 
-//   const req = params(method, [])
-//   const expectRes = (res: any) => {
-//     const msg = 'should result object with sync status'
-//     t.equal(res.body.result, 'TODO', msg)
-//   }
-//   baseRequest(t, server, req, 200, expectRes)
-// })
+  const req = params(method, [])
+  const expectRes = (res: any) => {
+    const msg = 'should return no peer available error'
+    if (res.body.result.message === 'no peer available for synchronization') {
+      t.pass(msg)
+    } else {
+      throw new Error(msg)
+    }
+  }
+
+  baseRequest(t, rpcServer, req, 200, expectRes)
+})
+
+tape(`${method}: should return highest block header unavailable error`, async (t) => {
+  const config = new Config({ transports: [], loglevel: 'error' })
+  const client = new EthereumClient({ config })
+
+  const manager = createManager(client)
+  const rpcServer = startRPC(manager.getMethods())
+  const synchronizer = client.services[0].synchronizer
+
+  synchronizer.best = td.func<typeof synchronizer['best']>()
+  td.when(synchronizer.best()).thenReturn('peer')
+
+  client.open = td.func<typeof client['open']>()
+  td.when(client.open()).thenResolve(false)
+  await client.open()
+
+  t.equals(client.synchronized, false, 'not synchronized yet')
+
+  const req = params(method, [])
+  const expectRes = (res: any) => {
+    const msg = 'should return highest block header unavailable error'
+    if (res.body.result.message === 'highest block header unavailable') {
+      t.pass(msg)
+    } else {
+      throw new Error(msg)
+    }
+  }
+
+  baseRequest(t, rpcServer, req, 200, expectRes)
+})
+
+tape(`${method}: should return syncing status object when unsynced`, async (t) => {
+  const config = new Config({ transports: [], loglevel: 'error' })
+  const client = new EthereumClient({ config })
+
+  const manager = createManager(client)
+  const rpcServer = startRPC(manager.getMethods())
+  const synchronizer = client.services[0].synchronizer
+
+  synchronizer.best = td.func<typeof synchronizer['best']>()
+  synchronizer.latest = td.func<typeof synchronizer['latest']>()
+  td.when(synchronizer.best()).thenReturn('peer')
+  td.when(synchronizer.latest('peer' as any)).thenResolve({ number: new BN(2) })
+
+  client.open = td.func<typeof client['open']>()
+  td.when(client.open()).thenResolve(false)
+  await client.open()
+
+  t.equals(client.synchronized, false, 'not synchronized yet')
+
+  const req = params(method, [])
+  const expectRes = (res: any) => {
+    const msg = 'should return syncing status object'
+    if (
+      res.body.result.startingBlock === '0x0' &&
+      res.body.result.currentBlock === '0x0' &&
+      res.body.result.highestBlock === '0x2'
+    ) {
+      t.pass(msg)
+    } else {
+      throw new Error(msg)
+    }
+  }
+
+  baseRequest(t, rpcServer, req, 200, expectRes)
+})
+
+// Forces closing of the client, otherwise the test hangs on
+tape.onFinish(() => process.exit(0))
