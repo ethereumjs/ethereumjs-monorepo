9562b5ec33ad1e4e6f0654b13398c52f23ab977e
Ryan Ghods <ryan@ryanio.com>
2021-07-06 21:37:30 -0700

    eth_syncing: fix hanging by using createClient mock

--- packages/client/test/rpc/eth/syncing.spec.ts
+++ packages/client/test/rpc/eth/syncing.spec.ts
@@ -1,24 +1,17 @@
-import { EventEmitter } from 'events'
 import tape from 'tape-catch'
 import td from 'testdouble'
-import { Config } from '../../../lib/config'
-import { baseRequest, createManager, params, startRPC } from '../helpers'
-import EthereumClient from '../../../lib/client'
+import { baseRequest, createManager, createClient, params, startRPC } from '../helpers'
 import { BN } from 'ethereumjs-util'
 
 const method = 'eth_syncing'
 
 tape(`${method}: should return false when the client is synchronized`, async (t) => {
-  const config = new Config({ transports: [], loglevel: 'error' })
-  const client = new EthereumClient({ config })
-
+  const client = createClient()
   const manager = createManager(client)
   const server = startRPC(manager.getMethods())
 
-  await client.open()
-
   t.equals(client.synchronized, false, 'not synchronized yet')
-  client.services[0].emit('synchronized')
+  client.synchronized = true
   t.equals(client.synchronized, true, 'synchronized')
 
   const req = params(method, [])
@@ -34,15 +27,9 @@ tape(`${method}: should return false when the client is synchronized`, async (t)
 })
 
 tape(`${method}: should return no peer available error`, async (t) => {
-  const config = new Config({ transports: [], loglevel: 'error' })
-  const client = new EthereumClient({ config })
-
+  const client = createClient({ noPeers: true })
   const manager = createManager(client)
   const rpcServer = startRPC(manager.getMethods())
-
-  client.open = td.func<typeof client['open']>()
-  td.when(client.open()).thenResolve(false)
-  await client.open()
 
   t.equals(client.synchronized, false, 'not synchronized yet')
 
@@ -60,19 +47,13 @@ tape(`${method}: should return no peer available error`, async (t) => {
 })
 
 tape(`${method}: should return highest block header unavailable error`, async (t) => {
-  const config = new Config({ transports: [], loglevel: 'error' })
-  const client = new EthereumClient({ config })
-
+  const client = createClient()
   const manager = createManager(client)
   const rpcServer = startRPC(manager.getMethods())
-  const synchronizer = client.services[0].synchronizer
 
+  const synchronizer = client.services[0].synchronizer
   synchronizer.best = td.func<typeof synchronizer['best']>()
   td.when(synchronizer.best()).thenReturn('peer')
-
-  client.open = td.func<typeof client['open']>()
-  td.when(client.open()).thenResolve(false)
-  await client.open()
 
   t.equals(client.synchronized, false, 'not synchronized yet')
 
@@ -90,21 +71,15 @@ tape(`${method}: should return highest block header unavailable error`, async (t
 })
 
 tape(`${method}: should return syncing status object when unsynced`, async (t) => {
-  const config = new Config({ transports: [], loglevel: 'error' })
-  const client = new EthereumClient({ config })
-
+  const client = createClient()
   const manager = createManager(client)
   const rpcServer = startRPC(manager.getMethods())
-  const synchronizer = client.services[0].synchronizer
 
+  const synchronizer = client.services[0].synchronizer
   synchronizer.best = td.func<typeof synchronizer['best']>()
   synchronizer.latest = td.func<typeof synchronizer['latest']>()
   td.when(synchronizer.best()).thenReturn('peer')
   td.when(synchronizer.latest('peer' as any)).thenResolve({ number: new BN(2) })
-
-  client.open = td.func<typeof client['open']>()
-  td.when(client.open()).thenResolve(false)
-  await client.open()
 
   t.equals(client.synchronized, false, 'not synchronized yet')
 
@@ -129,6 +104,3 @@ tape('should reset td', (t) => {
   td.reset()
   t.end()
 })
-
-// Forces closing of the client, otherwise the test hangs on
-tape.onFinish(() => process.exit(0))
