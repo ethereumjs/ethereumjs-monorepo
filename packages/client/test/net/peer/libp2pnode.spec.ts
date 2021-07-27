4fc3af640a7b1a9857fbad8e35e2999872f39333
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-07-26 21:02:58 -0400

    Remove two more unused-var declarations

--- packages/client/test/net/peer/libp2pnode.spec.ts
+++ packages/client/test/net/peer/libp2pnode.spec.ts
@@ -2,8 +2,7 @@ import tape from 'tape-catch'
 import td from 'testdouble'
 
 tape('[Libp2pNode]', async (t) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const libp2p = td.replace('libp2p')
+  const _libp2p = td.replace('libp2p')
   const { Libp2pNode } = await import('../../../lib/net/peer/libp2pnode')
 
   t.test('should be a libp2p bundle', (t) => {
