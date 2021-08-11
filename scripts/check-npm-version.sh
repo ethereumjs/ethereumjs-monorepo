32efabfa36755d1f6328057a36623e18a880ca1b
Ryan Ghods <ryan@ryanio.com>
2021-08-11 15:30:06 -0700

    ci: simplify actions/cache use

--- scripts/check-npm-version.sh
+++ scripts/check-npm-version.sh
@@ -7,13 +7,11 @@
 # Exit immediately on error
 set -o errexit
 
-NPM_MAJOR_V=$(npm -v | grep -o [0-9]*)
+NPM_MAJOR_V=$(npm -v | grep -Eo '^[0-9]+')
 
-# Remove all new line, carriage return, tab characters
-# from the string, to allow integer comparison
-NPM_MAJOR_V="${NPM_MAJOR_V//[$'\t\r\n ']}"
-
-if [ "$NPM_MAJOR_V" -ge 7 ]; then
-  echo 'npm >=v7 satisfied'; else
+if [[ "$NPM_MAJOR_V" -ge 7 ]]
+then
+  echo 'npm >=v7 satisfied'
+else
   echo 'npm version 7 or greater is required for workspaces support. Please update with "npm i -g npm@7"' && exit 1
 fi
