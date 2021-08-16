d7129da57546687b0bbc119e4ada60759d394fd2
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-08-16 09:52:50 -0400

    set hardfork to merge

--- packages/client/test/integration/mergeSync.spec.ts
+++ packages/client/test/integration/mergeSync.spec.ts
@@ -5,7 +5,7 @@ import { setup, destroy } from './util'
 import testnet from './chains/testnet.json'
 tape('[Integration:MergeSync]', async (t) => {
   t.test('should sync blocks on a chain with the merge', async (t) => {
-    const common = new Common({ chain: testnet })
+    const common = new Common({ chain: testnet, hardfork: Hardfork.Merge })
       const [remoteServer, remoteService] = await setup({
         location: '127.0.0.2',
         height: 15,
