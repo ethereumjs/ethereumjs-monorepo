3644d98d5e1703bc82157c37285b8d20b4069f10
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-08-17 20:36:42 -0400

    Consolidate test to one common

--- packages/blockchain/test/pos.spec.ts
+++ packages/blockchain/test/pos.spec.ts
@@ -8,83 +8,36 @@ import testnet from './testdata/testnet.json'
 const buildChain = async (blockchain: Blockchain, common: Common, height: number) => {
   const blocks: Block[] = []
   const londonBlockNumber = common.hardforkBlockBN('london')!.toNumber()
-  const mergeBlockNumber = common.hardforkBlockBN('merge')!.toNumber()
   const genesis = Block.genesis({}, { common })
-//  const londonCommon = new Common({ chain: testnet, eips: [2930, 1559], hardfork: Hardfork.London })
- // const initialBaseFee = new BN(londonCommon.param('gasConfig', 'initialBaseFee'))
-//  blocks.push(genesis)
 
-  // ADD blocks up to London fork
-// let chainHeight = height > londonBlockNumber ? londonBlockNumber : height
+  const initialBaseFee = new BN(common.param('gasConfig', 'initialBaseFee'))
+  blocks.push(genesis)
+
   for (let number = 1; number < height; number++) {
+    console.log(number)
+    let baseFeePerGas: BN = new BN(0);
+    if (number === londonBlockNumber) baseFeePerGas = new BN(10000)
+    else if (number > londonBlockNumber) baseFeePerGas = blocks[number - 1].header.calcNextBaseFee()
+    console.log(baseFeePerGas, initialBaseFee)
     const block = Block.fromBlockData(
       {
         header: {
           number: number,
           parentHash: blocks[number - 1].hash(),
           timestamp: blocks[number - 1].header.timestamp.addn(1),
-          gasLimit: new BN(5000),
+          gasLimit: number >= londonBlockNumber ? new BN(10000) : new BN(5000),
+          baseFeePerGas: number >= londonBlockNumber ? baseFeePerGas : undefined
         },
       },
       {
         calcDifficultyFromHeader: blocks[number - 1].header,
         common,
-      }
-    )
-//    blocks.push(block)
-    await blockchain.putBlock(block)
-  }
-/*
-  if (londonBlockNumber > height) return
-  // Add blocks from London fork to Merge fork
-  chainHeight = height > mergeBlockNumber ? mergeBlockNumber : height
-  for (let number = londonBlockNumber; number < chainHeight; number++) {
-    const block = Block.fromBlockData(
-      {
-        header: {
-          number: number,
-          difficulty:
-            number >= mergeBlockNumber ? 0 : blocks[number - 1].header.difficulty.add(new BN(1)),
-          parentHash: blocks[number - 1].hash(),
-          timestamp: blocks[number - 1].header.timestamp.addn(1),
-          baseFeePerGas:
-            number === londonBlockNumber
-              ? initialBaseFee
-              : blocks[number - 1].header.calcNextBaseFee(),
-          gasLimit: new BN(10000),
-        },
-      },
-      {
-        calcDifficultyFromHeader: blocks[number - 1].header,
-        common: londonCommon,
+        hardforkByBlockNumber: true
       }
     )
     blocks.push(block)
     await blockchain.putBlock(block)
   }
-
-  if (mergeBlockNumber > height) return
-  // Add blocks from Merge Fork block to total height
-  londonCommon.setHardforkByBlockNumber(new BN(mergeBlockNumber))
-  for (let number = mergeBlockNumber; number <= height; number++) {
-    const block = Block.fromBlockData(
-      {
-        header: {
-          number: number,
-          difficulty: 0,
-          parentHash: blocks[number - 1].hash(),
-          timestamp: blocks[number - 1].header.timestamp.addn(1),
-          baseFeePerGas: blocks[number - 1].header.calcNextBaseFee(),
-          gasLimit: new BN(10000),
-        },
-      },
-      {
-        common: londonCommon,
-      }
-    )
-    blocks.push(block)
-    await blockchain.putBlock(block)
-  }*/
 }
 
 tape('Proof of Stake - assembling a blockchain', async (t) => {
