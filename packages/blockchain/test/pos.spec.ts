0439caffa3c1018a3c99b859d901448728a17db1
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-08-21 18:51:30 -0400

    revert blockchain edits

--- packages/blockchain/test/pos.spec.ts
+++ packages/blockchain/test/pos.spec.ts
@@ -36,9 +36,6 @@ const buildChain = async (blockchain: Blockchain, common: Common, height: number
     )
     blocks.push(block)
     await blockchain.putBlock(block)
-    if (number > 10) {
- //     console.log(await blockchain.getTotalDifficulty(block.hash()))
-    }
   }
 }
 
