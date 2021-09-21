e289a7825dd8d701204ecccfc90ad40111d58faf
Ryan Ghods <ryan@ryanio.com>
2021-09-21 00:31:30 -0700

    monorepo: dedupe prepublish script (#1481)
    * add config prepublish script
    
    * remove greenkeeper.json (greenkeeper no longer being run since 2020-06-03)

--- scripts/e2e-publish.sh
+++ scripts/e2e-publish.sh
@@ -45,6 +45,9 @@ npm version minor \
 git config user.email "someone@example.com"
 git config user.name "someone"
 
+# Clear prepublish script (no need to run clean/build/test)
+cp /dev/null config/cli/prepublish.sh
+
 # Publish to e2e tag
 npm publish \
   --dist-tag e2e \
