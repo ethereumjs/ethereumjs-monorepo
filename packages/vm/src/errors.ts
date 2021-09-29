17831d2632e589a20e743ca2e784ebbcc302fc82
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-09-28 20:05:27 -0400

    vm: add makeError method to ErrorLogger

--- packages/vm/src/errors.ts
+++ packages/vm/src/errors.ts
@@ -36,19 +36,61 @@ export type CodedGeneralError<T> = T extends ErrorCode.INVALID_BLOCK_HEADER
   ? UnknownError
   : never
 
-export class Logger {
+export class ErrorLogger {
   static errors = ErrorCode
+
+  makeError<T>(codedError: CodedGeneralError<T>): Error {
+    let { message } = codedError
+    const { code } = codedError
+    const messageDetails: Array<string> = []
+
+    if (isError(codedError, ErrorCode.INVALID_BLOCK_HEADER)) {
+      messageDetails.push('Invalid param' + '=' + codedError.param)
+    }
+
+    if (isError(codedError, ErrorCode.UNKNOWN_ERROR)) {
+      Object.keys(codedError)
+        .filter((key) => key !== 'message' && key !== 'code')
+        .forEach((key) => {
+          const value = codedError[key]
+          try {
+            messageDetails.push(key + '=' + JSON.stringify(value))
+          } catch {
+            messageDetails.push(key + '=' + JSON.stringify(codedError[key].toString()))
+          }
+        })
+    }
+
+    messageDetails.push(`code=${codedError.code}`)
+
+    if (messageDetails.length) {
+      message += ' (' + messageDetails.join(', ') + ')'
+    }
+
+    const error = new Error(message) as CodedGeneralError<T>
+    error.code = code
+    error['name'] = 'hi'
+
+    Object.keys(codedError)
+      .filter((key) => key !== 'message' && key !== 'code')
+      .forEach((key) => {
+        const typedKey = key as keyof typeof codedError
+        error[typedKey] = codedError[typedKey]
+      })
+
+    throw error
+  }
 
   throwError<T extends ErrorCode>(error: CodedGeneralError<T>): never {
     if (!error.code) {
       error.code = ErrorCode.UNKNOWN_ERROR
     }
 
-    throw error
+    throw this.makeError(error)
   }
 }
 
-export const log = new Logger()
+export const errorLog = new ErrorLogger()
 
 export function isError<K extends ErrorCode, T extends CodedGeneralError<K>>(
   error: Error,
