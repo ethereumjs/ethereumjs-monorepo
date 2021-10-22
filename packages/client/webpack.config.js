752547ca8e485c73f2120a5f5b356442c1ab4fad
Ryan Ghods <ryan@ryanio.com>
2021-10-22 09:57:57 -0700

    fix new webpack polyfill errors from node-fetch, which we don't use so can safely pass false

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -44,10 +44,13 @@ module.exports = {
       crypto: require.resolve('crypto-browserify'), // used by: rlpxpeer, bin/cli.ts
       dgram: false, // used by: rlpxpeer via @ethereumjs/devp2p
       fs: false, // used by: FullSynchronizer via @ethereumjs/vm
+      http: false, // polyfill error from node-fetch (unused so can pass false)
+      https: false, // polyfill error from node-fetch (unused so can pass false)
       net: false, // used by: rlpxpeer
       os: require.resolve('os-browserify/browser'), // used by: bin/cli.ts, web3_clientVersion rpc
       path: false, // used by: bin/cli.ts
       stream: require.resolve('stream-browserify'), // used by: fetcher
+      zlib: false, // polyfill error from node-fetch (unused so can pass false)
     },
   },
   performance: {
