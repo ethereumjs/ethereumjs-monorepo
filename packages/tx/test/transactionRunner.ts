5776292b06fd2a8626df3f0d25486b6c7257ea07
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-11-16 11:41:01 -0500

    tx: lint fix

--- packages/tx/test/transactionRunner.ts
+++ packages/tx/test/transactionRunner.ts
@@ -1,6 +1,6 @@
 import tape from 'tape'
 import minimist from 'minimist'
-import { rlp, toBuffer } from 'ethereumjs-util'
+import { toBuffer } from 'ethereumjs-util'
 import Common from '@ethereumjs/common'
 import { Transaction } from '../src/'
 import { ForkName, ForkNamesMap, OfficialTransactionTestData } from './types'
