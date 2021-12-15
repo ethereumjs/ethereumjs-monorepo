ea665d6854564e0daf51970f71b028c71ab3da7f
Ryan Ghods <ryan@ryanio.com>
2021-12-15 12:45:11 -1000

    disable no-extra-semi, remove overrides

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -572,7 +572,6 @@ export default class Common extends EventEmitter {
         )
       }
       if (EIPs[eip].requiredEIPs) {
-        // eslint-disable-next-line no-extra-semi
         ;(EIPs[eip].requiredEIPs as number[]).forEach((elem) => {
           if (!(eips.includes(elem) || this.isActivatedEIP(elem))) {
             throw new Error(`${eip} requires EIP ${elem}, but is not included in the EIP list`)
