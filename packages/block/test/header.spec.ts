c34985015ed8a4db1e19ba8a6f7a1dfc10fce9bd
Ryan Ghods <ryan@ryanio.com>
2022-01-17 10:37:02 -0800

    format rlp to buffers before using fromRLPSerializedBlock

--- packages/block/test/header.spec.ts
+++ packages/block/test/header.spec.ts
@@ -1,6 +1,6 @@
 import tape from 'tape'
 import RLP from 'rlp'
-import { Address, BN, zeros, KECCAK256_RLP, KECCAK256_RLP_ARRAY } from 'ethereumjs-util'
+import { Address, BN, toBuffer, zeros, KECCAK256_RLP, KECCAK256_RLP_ARRAY } from 'ethereumjs-util'
 import Common, { Chain, Hardfork } from '@ethereumjs/common'
 import { BlockHeader } from '../src/header'
 import { Block } from '../src'
@@ -268,7 +268,8 @@ tape('[Block]: Header functions', function (t) {
     const common = new Common({ chain: Chain.Goerli })
     const blockchain = new Mockchain()
 
-    const block = Block.fromRLPSerializedBlock(testData.genesisRLP, { common })
+    const genesisRlp = toBuffer(testData.genesisRLP)
+    const block = Block.fromRLPSerializedBlock(genesisRlp, { common })
     await blockchain.putBlock(block)
 
     headerData.number = 1
@@ -349,7 +350,7 @@ tape('[Block]: Header functions', function (t) {
       '64bf9cc30328b0e42387b3c82c614e6386259136235e20c1357bd11cdee86993',
       'hex'
     )
-    const poaBlock = Block.fromRLPSerializedBlock(testData.genesisRLP, { common, cliqueSigner })
+    const poaBlock = Block.fromRLPSerializedBlock(genesisRlp, { common, cliqueSigner })
     await poaBlockchain.putBlock(poaBlock)
 
     header = BlockHeader.fromHeaderData(headerData, { common, cliqueSigner })
@@ -378,9 +379,9 @@ tape('[Block]: Header functions', function (t) {
     const bcBlockGasLimitTestData = testData.BlockGasLimit2p63m1
 
     Object.keys(bcBlockGasLimitTestData).forEach((key) => {
-      const genesisRlp = bcBlockGasLimitTestData[key].genesisRLP
+      const genesisRlp = toBuffer(bcBlockGasLimitTestData[key].genesisRLP)
       const parentBlock = Block.fromRLPSerializedBlock(genesisRlp)
-      const blockRlp = bcBlockGasLimitTestData[key].blocks[0].rlp
+      const blockRlp = toBuffer(bcBlockGasLimitTestData[key].blocks[0].rlp)
       const block = Block.fromRLPSerializedBlock(blockRlp)
       st.equal(block.validateGasLimit(parentBlock), true)
     })
