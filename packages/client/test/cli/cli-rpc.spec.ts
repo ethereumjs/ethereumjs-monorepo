55bb90d3be7e1f2e9949c20e08fe5a8bde5773e0
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-01-17 16:00:44 -0500

    Add libp2p test

--- packages/client/test/cli/cli-rpc.spec.ts
+++ packages/client/test/cli/cli-rpc.spec.ts
@@ -7,6 +7,8 @@ const cliArgs = ['--rpc', '--ws', '--dev', '--transports=rlpx']
 const end = (child: ChildProcessWithoutNullStreams, hasEnded: boolean, st: tape.Test) => {
   if (hasEnded) return
   hasEnded = true
+  child.stdout.removeAllListeners()
+  child.stderr.removeAllListeners()
   const res = child.kill('SIGINT')
   st.ok(res === true, 'client shut down successfully')
   st.end()
@@ -67,14 +69,72 @@ tape('[CLI] rpc', (t) => {
       if (message.includes('address=ws://')) {
         st.fail('ws endpoint should not be enabled')
       }
-      if (message.includes('Exiting')) {
-        st.pass('client received shutdown signal')
-      }
       if (message.includes('Miner: Assembling block')) {
         st.pass('miner started and no rpc endpoints started')
         end(child, hasEnded, st)
-        child.stdout.removeAllListeners()
-        child.stderr.removeAllListeners()
+      }
+    })
+
+    child.stderr.on('data', (data) => {
+      const message = data.toString()
+      st.fail(`stderr: ${message}`)
+      end(child, hasEnded, st)
+    })
+
+    child.on('close', (code) => {
+      if (code && code > 0) {
+        st.fail(`child process exited with code ${code}`)
+        end(child, hasEnded, st)
+      }
+    })
+  })
+
+  t.test('libp2p should start up', (st) => {
+    const file = require.resolve('../../dist/bin/cli.js')
+    const child = spawn(process.execPath, [
+      file,
+      ...[
+        '--transports=libp2p',
+        '--dev',
+        '--lightserv=true',
+        '--multiaddrs=/ip4/127.0.0.1/tcp/50505/',
+      ],
+    ])
+    let child2: ChildProcessWithoutNullStreams
+    const hasEnded = false
+
+    child.stdout.on('data', async (data) => {
+      const message = data.toString()
+
+      if (message.includes('transport=libp2p')) {
+        st.pass('libp2p server started')
+        const bootnodeAddressArray = message.split(' ')
+        const bootnodeAddressIndex = bootnodeAddressArray.findIndex((chunk: string) =>
+          chunk.startsWith('url=')
+        )
+        const bootNodeAddress = bootnodeAddressArray[bootnodeAddressIndex].split('=')[1]
+        child2 = spawn(process.execPath, [
+          file,
+          ...[
+            '--transports=libp2p',
+            `--bootnodes=${bootNodeAddress}`,
+            '--datadir=data2',
+            '--mine=false',
+            '--dev',
+            '--multiaddrs=/ip4/0.0.0.0/tcp/50506',
+            '--syncmode=light',
+            '--loglevel=debug',
+          ],
+        ])
+        child2.stdout.on('data', async (data) => {
+          const message = data.toString()
+          if (message.includes('Peer added')) {
+            st.pass('connected to peer over libp2p')
+            child2.kill('SIGINT')
+            child2.stdout.removeAllListeners()
+            end(child, false, st)
+          }
+        })
       }
     })
 
