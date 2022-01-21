a81236d758c1f8b0c6c9b3b5ebc4714c7d92c939
emersonmacro <77563348+emersonmacro@users.noreply.github.com>
2022-01-21 00:07:14 -0800

    Common, VM: Remove unused HF options from Common, move supportedHardforks to VM (#1649)
    * common: remove onlySupported and onlyActive hf options
    
    * vm: move supportedHardforks option from common
    
    * common: remove supportedHardforks option
    
    * common: update README for supportedHardforks removal
    
    * vm: update README for supportedHardforks addition
    
    * common: cleanup in src/index.ts
    
    * vm: refactor supportedHardforks per PR feedback
    
    * common: remove _chooseHardfork method per PR feedback
    
    * vm: updated README per PR feedback
    
    * vm: supportedHardforks local variable, switch to enum

--- packages/vm/src/index.ts
+++ packages/vm/src/index.ts
@@ -1,7 +1,7 @@
 import { SecureTrie as Trie } from 'merkle-patricia-tree'
 import { Account, Address, BNLike } from 'ethereumjs-util'
 import Blockchain from '@ethereumjs/blockchain'
-import Common, { Chain } from '@ethereumjs/common'
+import Common, { Chain, Hardfork } from '@ethereumjs/common'
 import { StateManager, DefaultStateManager } from './state/index'
 import { default as runCode, RunCodeOpts } from './runCode'
 import { default as runCall, RunCallOpts } from './runCall'
@@ -221,14 +221,32 @@ export default class VM extends AsyncEventEmitter {
         'preMerge',
         'merge',
       ]
-      this._common = new Common({
-        chain: DEFAULT_CHAIN,
-        supportedHardforks,
-      })
+      this._common = new Common({ chain: DEFAULT_CHAIN })
     }
     this._common.on('hardforkChanged', () => {
       this._opcodes = getOpcodesForHF(this._common)
     })
+
+    const supportedHardforks = [
+      Hardfork.Chainstart,
+      Hardfork.Homestead,
+      Hardfork.Dao,
+      Hardfork.TangerineWhistle,
+      Hardfork.SpuriousDragon,
+      Hardfork.Byzantium,
+      Hardfork.Constantinople,
+      Hardfork.Petersburg,
+      Hardfork.Istanbul,
+      Hardfork.MuirGlacier,
+      Hardfork.Berlin,
+      Hardfork.London,
+      Hardfork.ArrowGlacier,
+    ]
+    if (!supportedHardforks.includes(this._common.hardfork() as Hardfork)) {
+      throw new Error(
+        `Hardfork ${this._common.hardfork()} not set as supported in supportedHardforks`
+      )
+    }
 
     // Set list of opcodes based on HF
     // TODO: make this EIP-friendly
