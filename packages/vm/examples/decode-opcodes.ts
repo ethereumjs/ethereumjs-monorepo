be06be6be600186392d5c4e76bbe8f0916e0f3fa
Cesar Brazon <cesarbrazon10@gmail.com>
2022-02-03 12:44:15 +0100

    VM: trying to simplify examples (#1670)
    Co-authored-by: Holger Drewes <Holger.Drewes@gmail.com>

--- packages/vm/examples/decode-opcodes.ts
+++ packages/vm/examples/decode-opcodes.ts
@@ -1,5 +1,9 @@
+// Decode opcodes example
+//
+// 1. Takes binary EVM code and decodes it into opcodes
+
 import Common, { Chain, Hardfork } from '@ethereumjs/common'
-import { getOpcodesForHF } from '../../src/evm/opcodes'
+import { getOpcodesForHF } from '../src/evm/opcodes'
 
 const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.Istanbul })
 const opcodes = getOpcodesForHF(common)
--- packages/vm/examples/run-blockchain.ts
+++ packages/vm/examples/run-blockchain.ts
@@ -1,10 +1,17 @@
+// The example does these things:
+//
+// 1. Instantiates a VM and a Blockchain
+// 2. Creates the accounts from ../utils/blockchain-mock-data "pre" attribute
+// 3. Creates a genesis block
+// 4. Puts the blocks from ../utils/blockchain-mock-data "blocks" attribute into the Blockchain
+// 5. Runs the Blockchain on the VM.
+
 import { Account, Address, toBuffer, setLengthLeft } from 'ethereumjs-util'
 import { Block } from '@ethereumjs/block'
 import Blockchain from '@ethereumjs/blockchain'
 import Common, { ConsensusType } from '@ethereumjs/common'
-import VM from '../../'
-
-const testData = require('./test-data')
+import VM from '../'
+import { testData } from './helpers/blockchain-mock-data'
 const level = require('level')
 
 async function main() {
