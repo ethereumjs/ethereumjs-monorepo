8a3e8a8b43be272f1c66c9de7a96b0a5ca5281e9
ScottyPoi <66335769+ScottyPoi@users.noreply.github.com>
2022-02-07 13:33:01 -0700

    common: delete deprecated hardforkBlock method

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -750,17 +750,6 @@ export default class Common extends EventEmitter {
    * Returns the hardfork change block for hardfork provided or set
    * @param hardfork Hardfork name, optional if HF set
    * @returns Block number or null if unscheduled
-   * @deprecated Please use {@link Common.hardforkBlockBN} for large number support
-   */
-  hardforkBlock(hardfork?: string | Hardfork): number | null {
-    const block = this.hardforkBlockBN(hardfork)
-    return toType(block, TypeOutput.Number)
-  }
-
-  /**
-   * Returns the hardfork change block for hardfork provided or set
-   * @param hardfork Hardfork name, optional if HF set
-   * @returns Block number or null if unscheduled
    */
   hardforkBlockBN(hardfork?: string | Hardfork): BN | null {
     hardfork = hardfork ?? this._hardfork
