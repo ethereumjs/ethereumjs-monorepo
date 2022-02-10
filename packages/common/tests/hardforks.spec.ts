3af122707375260549039d560b23bf640005adc2
ScottyPoi <66335769+ScottyPoi@users.noreply.github.com>
2022-02-09 17:23:14 -0700

    common/tests: rename nextHardforkBlockBN() to nextHardforkBlock()

--- packages/common/tests/hardforks.spec.ts
+++ packages/common/tests/hardforks.spec.ts
@@ -108,22 +108,22 @@ tape('[Common]: Hardfork logic', function (t: tape.Test) {
     let c = new Common({ chain: Chain.Rinkeby, hardfork: Hardfork.Chainstart })
     let msg =
       'should work with HF set / return correct next HF block for chainstart (rinkeby: chainstart -> homestead)'
-    st.ok(c.nextHardforkBlockBN()!.eqn(1), msg)
+    st.ok(c.nextHardforkBlock()!.eqn(1), msg)
 
     msg =
       'should correctly skip a HF where block is set to null (rinkeby: homestead -> (dao) -> tangerineWhistle)'
-    st.ok(c.nextHardforkBlockBN('homestead')!.eqn(2), msg)
+    st.ok(c.nextHardforkBlock('homestead')!.eqn(2), msg)
 
     msg = 'should return correct next HF (rinkeby: byzantium -> constantinople)'
-    st.ok(c.nextHardforkBlockBN(Hardfork.Byzantium)!.eqn(3660663), msg)
+    st.ok(c.nextHardforkBlock(Hardfork.Byzantium)!.eqn(3660663), msg)
 
     msg = 'should return null if next HF is not available (rinkeby: london -> shanghai)'
-    st.equal(c.nextHardforkBlockBN(Hardfork.London), null, msg)
+    st.equal(c.nextHardforkBlock(Hardfork.London), null, msg)
 
     msg =
       'should work correctly along the need to skip several forks (ropsten: chainstart -> (homestead) -> (dao) -> (tangerineWhistle) -> spuriousDragon)'
     c = new Common({ chain: Chain.Ropsten, hardfork: Hardfork.Chainstart })
-    st.ok(c.nextHardforkBlockBN()!.eqn(10), msg)
+    st.ok(c.nextHardforkBlock()!.eqn(10), msg)
 
     st.end()
   })
@@ -233,7 +233,7 @@ tape('[Common]: Hardfork logic', function (t: tape.Test) {
     // update this test to next unscheduled hardfork.
     st.equal(c.hardforkBlockBN(Hardfork.Shanghai), null, msg)
     st.equal(c.hardforkBlockBN(Hardfork.Shanghai), null, msg)
-    st.equal(c.nextHardforkBlockBN(Hardfork.Shanghai), null, msg)
+    st.equal(c.nextHardforkBlock(Hardfork.Shanghai), null, msg)
 
     st.end()
   })
