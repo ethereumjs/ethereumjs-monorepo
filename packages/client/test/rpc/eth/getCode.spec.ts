730760bd7923cb248353a389707a66c0b21b3c22
Emerson Macro <emersonmacro@gmail.com>
2022-02-21 21:47:14 -0800

    client: fix client tests for default hardfork change

--- packages/client/test/rpc/eth/getCode.spec.ts
+++ packages/client/test/rpc/eth/getCode.spec.ts
@@ -1,6 +1,7 @@
 import tape from 'tape'
 import { Block } from '@ethereumjs/block'
 import Blockchain from '@ethereumjs/blockchain'
+import Common, { Chain, Hardfork } from '@ethereumjs/common'
 import { Transaction } from '@ethereumjs/tx'
 import { Address, BN } from 'ethereumjs-util'
 import { INVALID_PARAMS } from '../../../lib/rpc/error-code'
@@ -10,10 +11,12 @@ import { VMExecution } from '../../../lib/execution'
 
 const method = 'eth_getCode'
 
-tape(`${method}: call with valid arguments`, async (t) => {
-  const blockchain = await Blockchain.create()
+const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.Istanbul })
 
-  const client = createClient({ blockchain, includeVM: true })
+tape(`${method}: call with valid arguments`, async (t) => {
+  const blockchain = await Blockchain.create({ common })
+
+  const client = createClient({ blockchain, commonChain: common, includeVM: true })
   const manager = createManager(client)
   const server = startRPC(manager.getMethods())
 
@@ -36,9 +39,13 @@ tape(`${method}: call with valid arguments`, async (t) => {
 })
 
 tape(`${method}: ensure returns correct code`, async (t) => {
-  const blockchain = await Blockchain.create({ validateBlocks: false, validateConsensus: false })
+  const blockchain = await Blockchain.create({
+    common,
+    validateBlocks: false,
+    validateConsensus: false,
+  })
 
-  const client = createClient({ blockchain, includeVM: true })
+  const client = createClient({ blockchain, commonChain: common, includeVM: true })
   const manager = createManager(client)
   const server = startRPC(manager.getMethods())
 
@@ -58,7 +65,7 @@ tape(`${method}: ensure returns correct code`, async (t) => {
 
   // construct block with tx
   const gasLimit = 2000000
-  const tx = Transaction.fromTxData({ gasLimit, data }, { freeze: false })
+  const tx = Transaction.fromTxData({ gasLimit, data }, { common, freeze: false })
   tx.getSenderAddress = () => {
     return address
   }
@@ -71,7 +78,7 @@ tape(`${method}: ensure returns correct code`, async (t) => {
         gasLimit,
       },
     },
-    { calcDifficultyFromHeader: parent }
+    { common, calcDifficultyFromHeader: parent }
   )
   block.transactions[0] = tx
 
