066ff05cf29728556ef721806c2ac7783af46724
Holger Drewes <Holger.Drewes@gmail.com>
2022-02-23 12:26:02 +0100

    monorepo: develop rebase 2022-02-23 fixes

--- packages/vm/tests/api/EIPs/eip-4399-supplant-difficulty-opcode-with-prevrando.spec.ts
+++ packages/vm/tests/api/EIPs/eip-4399-supplant-difficulty-opcode-with-prevrando.spec.ts
@@ -1,7 +1,7 @@
 import tape from 'tape'
 import { Block } from '@ethereumjs/block'
 import Common, { Chain, Hardfork } from '@ethereumjs/common'
-import { BN } from 'ethereumjs-util'
+import { bigIntToBN, BN } from 'ethereumjs-util'
 import VM from '../../../src'
 import type { InterpreterStep } from '../../../src/evm/interpreter'
 
@@ -32,10 +32,10 @@ tape('EIP-4399 -> 0x44 (DIFFICULTY) should return PREVRANDAO', (t) => {
 
     const runCodeArgs = {
       code: Buffer.from('4400', 'hex'),
-      gasLimit: new BN(0xffff),
+      gasLimit: BigInt(0xffff),
     }
     await vm.runCode({ ...runCodeArgs, block })
-    st.ok(stack[0].eq(block.header.difficulty), '0x44 returns DIFFICULTY (London)')
+    st.ok(bigIntToBN(stack[0]).eq(block.header.difficulty), '0x44 returns DIFFICULTY (London)')
 
     common.setHardfork(Hardfork.Merge)
     const prevRandao = Buffer.alloc(32, 1)
@@ -49,7 +49,7 @@ tape('EIP-4399 -> 0x44 (DIFFICULTY) should return PREVRANDAO', (t) => {
       { common }
     )
     await vm.runCode({ ...runCodeArgs, block })
-    st.ok(stack[0].eq(new BN(prevRandao)), '0x44 returns PREVRANDAO (Merge)')
+    st.ok(bigIntToBN(stack[0]).eq(new BN(prevRandao)), '0x44 returns PREVRANDAO (Merge)')
 
     st.end()
   })
