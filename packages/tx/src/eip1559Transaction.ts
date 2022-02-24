4ac43d03146bfa4e98e58f358e5d6eb41ed247d4
Scotty <66335769+ScottyPoi@users.noreply.github.com>
2022-02-24 10:40:52 -0700

    Tx: Execute on deprecation notes (#1742)
    * Tx: Remove baseTransaction.transactionType
    
    * tx: remove get senderR()
    
    * tx: remove test for senderR()
    
    * tx: remove senderS()
    
    * tx: remove test for senderS()
    
    * tx: remove yParity / replace with v
    
    * tx: remove test for yParity()
    
    * Tx: remove fromRlpSerializedTx()
    
    * Tx: remove test for fromRlpSerializedTx()
    
    * Tx: remove _unsignedTxImplementsEIP155
    
    * Tx: Remove _signedTxImplementsEIP155()
    
    * Tx: Remove TransactionFactory.getTransactionClass
    
    * Tx: Remove test for .getTransactionClass()

--- packages/tx/src/eip1559Transaction.ts
+++ packages/tx/src/eip1559Transaction.ts
@@ -49,33 +49,6 @@ export default class FeeMarketEIP1559Transaction extends BaseTransaction<FeeMark
   protected DEFAULT_HARDFORK = 'london'
 
   /**
-   * EIP-2930 alias for `r`
-   *
-   * @deprecated use `r` instead
-   */
-  get senderR() {
-    return this.r
-  }
-
-  /**
-   * EIP-2930 alias for `s`
-   *
-   * @deprecated use `s` instead
-   */
-  get senderS() {
-    return this.s
-  }
-
-  /**
-   * EIP-2930 alias for `v`
-   *
-   * @deprecated use `v` instead
-   */
-  get yParity() {
-    return this.v
-  }
-
-  /**
    * Instantiate a transaction from a data dictionary.
    *
    * Format: { chainId, nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data,
@@ -111,19 +84,6 @@ export default class FeeMarketEIP1559Transaction extends BaseTransaction<FeeMark
     }
 
     return FeeMarketEIP1559Transaction.fromValuesArray(values as any, opts)
-  }
-
-  /**
-   * Instantiate a transaction from the serialized tx.
-   * (alias of {@link FeeMarketEIP1559Transaction.fromSerializedTx})
-   *
-   * Note: This means that the Buffer should start with 0x01.
-   *
-   * @deprecated this constructor alias is deprecated and will be removed
-   * in favor of the {@link FeeMarketEIP1559Transaction.fromSerializedTx} constructor
-   */
-  public static fromRlpSerializedTx(serialized: Buffer, opts: TxOptions = {}) {
-    return FeeMarketEIP1559Transaction.fromSerializedTx(serialized, opts)
   }
 
   /**
--- packages/tx/src/legacyTransaction.ts
+++ packages/tx/src/legacyTransaction.ts
@@ -53,17 +53,6 @@ export default class Transaction extends BaseTransaction<Transaction> {
   }
 
   /**
-   * Instantiate a transaction from the serialized tx.
-   * (alias of {@link Transaction.fromSerializedTx})
-   *
-   * @deprecated this constructor alias is deprecated and will be removed
-   * in favor of the {@link Transaction.fromSerializedTx} constructor
-   */
-  public static fromRlpSerializedTx(serialized: Buffer, opts: TxOptions = {}) {
-    return Transaction.fromSerializedTx(serialized, opts)
-  }
-
-  /**
    * Create a transaction from a values array.
    *
    * Format: `[nonce, gasPrice, gasLimit, to, value, data, v, r, s]`
@@ -419,35 +408,6 @@ export default class Transaction extends BaseTransaction<Transaction> {
       }
     }
     return this._getCommon(common, chainIdBN)
-  }
-
-  /**
-   * @deprecated if you have called this internal method please use `tx.supports(Capabilities.EIP155ReplayProtection)` instead
-   */
-  private _unsignedTxImplementsEIP155() {
-    return this.common.gteHardfork('spuriousDragon')
-  }
-
-  /**
-   * @deprecated if you have called this internal method please use `tx.supports(Capabilities.EIP155ReplayProtection)` instead
-   */
-  private _signedTxImplementsEIP155() {
-    if (!this.isSigned()) {
-      const msg = this._errorMsg('This transaction is not signed')
-      throw new Error(msg)
-    }
-    const onEIP155BlockOrLater = this.common.gteHardfork('spuriousDragon')
-
-    // EIP155 spec:
-    // If block.number >= 2,675,000 and v = CHAIN_ID * 2 + 35 or v = CHAIN_ID * 2 + 36, then when computing the hash of a transaction for purposes of signing or recovering, instead of hashing only the first six elements (i.e. nonce, gasprice, startgas, to, value, data), hash nine elements, with v replaced by CHAIN_ID, r = 0 and s = 0.
-    const v = this.v!
-
-    const chainIdDoubled = this.common.chainId().muln(2)
-
-    const vAndChainIdMeetEIP155Conditions =
-      v.eq(chainIdDoubled.addn(35)) || v.eq(chainIdDoubled.addn(36))
-
-    return vAndChainIdMeetEIP155Conditions && onEIP155BlockOrLater
   }
 
   /**
