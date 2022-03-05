90ff2028350e2ca0a01af0e3b4b10c1e3b279bac
Holger Drewes <Holger.Drewes@gmail.com>
2022-03-05 09:51:13 +0100

    Develop Rebase Fixes (2022-03-05)

--- packages/vm/src/index.ts
+++ packages/vm/src/index.ts
@@ -293,6 +293,8 @@ export default class VM extends AsyncEventEmitter {
       Hardfork.Berlin,
       Hardfork.London,
       Hardfork.ArrowGlacier,
+      Hardfork.PreMerge,
+      Hardfork.Merge,
     ]
     if (!supportedHardforks.includes(this._common.hardfork() as Hardfork)) {
       throw new Error(
