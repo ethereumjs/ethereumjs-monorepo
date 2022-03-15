a31a5ea7fdb3731f8c3e120f2f9c68f920292856
ScottyPoi <scott.simpson@ethereum.org>
2022-03-15 14:12:16 -0700

    blockchain: fix linting errors

--- packages/blockchain/test/pos.spec.ts
+++ packages/blockchain/test/pos.spec.ts
@@ -71,7 +71,7 @@ tape('Proof of Stake - inserting blocks into blockchain', async (t) => {
     await buildChain(blockchain, s.common, 15)
 
     const latestHeader = await blockchain.getCanonicalHeadHeader()
-    t.equal(latestHeader.number.toNumber(), 15, 'blockchain is at correct height')
+    t.equal(latestHeader.number, BigInt(15), 'blockchain is at correct height')
 
     t.equal(
       (blockchain as any)._common.hardfork(),
