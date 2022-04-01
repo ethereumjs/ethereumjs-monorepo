cab3d5cb226210ee9e970d9fd253c1b23667b57a
Emerson Macro <emersonmacro@gmail.com>
2022-03-31 20:42:14 -0700

    block: update header for consensus class changes in blockchain

--- packages/block/test/poaMockchain.ts
+++ packages/block/test/poaMockchain.ts
@@ -5,14 +5,16 @@ import { Block, Blockchain } from '../src'
 
 export class PoaMockchain implements Blockchain {
   private HashMap: { [key: string]: Block } = {}
+  public consensus = {
+    cliqueActiveSigners() {
+      const signer = new Address(Buffer.from('0b90087d864e82a284dca15923f3776de6bb016f', 'hex'))
+      return [signer]
+    },
+  }
   async getBlock(hash: Buffer) {
     return this.HashMap[hash.toString('hex')]
   }
   async putBlock(block: Block) {
     this.HashMap[block.hash().toString('hex')] = block
-  }
-  cliqueActiveSigners() {
-    const signer = new Address(Buffer.from('0b90087d864e82a284dca15923f3776de6bb016f', 'hex'))
-    return [signer]
   }
 }
