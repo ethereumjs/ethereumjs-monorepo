eccc50b7d96dd95b255729a4a1b242419f721bc9
Holger Drewes <Holger.Drewes@gmail.com>
2022-04-14 10:16:08 +0200

    Blockchain: encapsulate consensus mechanism (new) (#1841)
    * blockchain: encapsulate consensus WIP
    
    * blockchain: fix lint errors
    
    * blockchain: move initialization to constructor in ethash consensus class
    
    * blockchain: refactor to consensus interface only
    
    * block: update header for consensus class changes in blockchain
    
    * client: update miner for consensus class changes in blockchain
    
    * blockchain: lint cleanup
    
    * vm: update examples for consensus class changes in blockchain
    
    * blockchain: re-export consensus classes
    
    * client: use CliqueConsensus class in miner
    
    * client: use CliqueConsensus class in miner
    
    * Rebase fixes
    
    * blockchain: rebase reconcile
    
    * Update packages/client/lib/miner/miner.ts
    
    Co-authored-by: Ryan Ghods <ryan@ryanio.com>
    
    * block: fix error text in validateCliqueDifficulty
    
    Co-authored-by: Emerson Macro <emersonmacro@gmail.com>
    Co-authored-by: emersonmacro <77563348+emersonmacro@users.noreply.github.com>
    Co-authored-by: Ryan Ghods <ryan@ryanio.com>

--- packages/blockchain/src/db/operation.ts
+++ packages/blockchain/src/db/operation.ts
@@ -7,9 +7,6 @@ import {
   bodyKey,
   numberToHashKey,
   hashToNumberKey,
-  CLIQUE_SIGNERS_KEY as CLIQUE_SIGNER_STATES_KEY,
-  CLIQUE_VOTES_KEY,
-  CLIQUE_BLOCK_SIGNERS_KEY,
 } from './constants'
 
 import { CacheMap } from './manager'
@@ -100,18 +97,6 @@ export class DBOp {
       case DBTarget.Header: {
         this.baseDBOp.key = headerKey(key!.blockNumber!, key!.blockHash!)
         this.cacheString = 'header'
-        break
-      }
-      case DBTarget.CliqueSignerStates: {
-        this.baseDBOp.key = CLIQUE_SIGNER_STATES_KEY
-        break
-      }
-      case DBTarget.CliqueVotes: {
-        this.baseDBOp.key = CLIQUE_VOTES_KEY
-        break
-      }
-      case DBTarget.CliqueBlockSigners: {
-        this.baseDBOp.key = CLIQUE_BLOCK_SIGNERS_KEY
         break
       }
     }
