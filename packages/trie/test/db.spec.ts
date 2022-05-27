b8e69ce9e9df87d348f5859183e01b91b35a3234
Brian Faust <hello@basecode.sh>
2022-05-27 13:53:46 +0300

    refactor(trie): abstract database implementation with an interface (#1917)

--- packages/trie/test/db.spec.ts
+++ packages/trie/test/db.spec.ts
@@ -1,36 +1,38 @@
 import tape from 'tape'
-import { DB, BatchDBOp } from '../src/db'
+import { BatchDBOp, MemoryDB, LevelDB } from '../src/db'
 
-tape('DB tests', (t) => {
-  const db = new DB()
+for (const DB of [MemoryDB, LevelDB]) {
+  tape('DB tests', (t) => {
+    const db = new DB()
 
-  const k = Buffer.from('k1')
-  const v = Buffer.from('v1')
-  const k2 = Buffer.from('k2')
-  const v2 = Buffer.from('v2')
+    const k = Buffer.from('k1')
+    const v = Buffer.from('v1')
+    const k2 = Buffer.from('k2')
+    const v2 = Buffer.from('v2')
 
-  t.test('Operations: puts and gets value', async (st) => {
-    await db.put(k, v)
-    const res = await db.get(k)
-    st.ok(v.equals(res!))
-    st.end()
+    t.test('Operations: puts and gets value', async (st) => {
+      await db.put(k, v)
+      const res = await db.get(k)
+      st.ok(v.equals(res!))
+      st.end()
+    })
+
+    t.test('Operations: deletes value', async (st) => {
+      await db.del(k)
+      const res = await db.get(k)
+      st.notOk(res)
+      st.end()
+    })
+
+    t.test('Operations: batch ops', async (st) => {
+      const ops = [
+        { type: 'put', key: k, value: v },
+        { type: 'put', key: k2, value: v2 },
+      ] as BatchDBOp[]
+      await db.batch(ops)
+      const res = await db.get(k2)
+      st.ok(v2.equals(res!))
+      st.end()
+    })
   })
-
-  t.test('Operations: deletes value', async (st) => {
-    await db.del(k)
-    const res = await db.get(k)
-    st.notOk(res)
-    st.end()
-  })
-
-  t.test('Operations: batch ops', async (st) => {
-    const ops = [
-      { type: 'put', key: k, value: v },
-      { type: 'put', key: k2, value: v2 },
-    ] as BatchDBOp[]
-    await db.batch(ops)
-    const res = await db.get(k2)
-    st.ok(v2.equals(res!))
-    st.end()
-  })
-})
+}
--- packages/trie/test/encoding.spec.ts
+++ packages/trie/test/encoding.spec.ts
@@ -1,14 +1,17 @@
 import tape from 'tape'
 import { toBuffer } from 'ethereumjs-util'
 import { CheckpointTrie } from '../src'
+import { MemoryDB, LevelDB } from '../src/db'
 
-const trie = new CheckpointTrie()
-const trie2 = new CheckpointTrie()
-const hex = 'FF44A3B3'
+for (const DB of [MemoryDB, LevelDB]) {
+  const trie = new CheckpointTrie({ db: new DB() })
+  const trie2 = new CheckpointTrie({ db: new DB() })
+  const hex = 'FF44A3B3'
 
-tape('encoding hex prefixes', async function (t) {
-  await trie.put(Buffer.from(hex, 'hex'), Buffer.from('test'))
-  await trie2.put(toBuffer(`0x${hex}`), Buffer.from('test'))
-  t.equal(trie.root.toString('hex'), trie2.root.toString('hex'))
-  t.end()
-})
+  tape('encoding hex prefixes', async function (t) {
+    await trie.put(Buffer.from(hex, 'hex'), Buffer.from('test'))
+    await trie2.put(toBuffer(`0x${hex}`), Buffer.from('test'))
+    t.equal(trie.root.toString('hex'), trie2.root.toString('hex'))
+    t.end()
+  })
+}
