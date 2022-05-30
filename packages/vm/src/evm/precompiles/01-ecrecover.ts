f2dbdaa974f7668f2a27c46db9fd4b08af5fb745
Jochem Brouwer <jochembrouwer96@gmail.com>
2022-05-30 21:41:49 +0200

    vm: fix ecrecover precompile for v=0 and v=1

--- packages/vm/src/evm/precompiles/01-ecrecover.ts
+++ packages/vm/src/evm/precompiles/01-ecrecover.ts
@@ -16,6 +16,18 @@ export default function (opts: PrecompileInput): ExecResult {
 
   const msgHash = data.slice(0, 32)
   const v = data.slice(32, 64)
+  const vBN = new BN(v)
+
+  // Guard against util's `ecrecover`: without providing chainId this will return
+  // a signature in most of the cases in the cases that `v=0` or `v=1`
+  // However, this should throw, only 27 and 28 is allowed as input
+  if (!vBN.eqn(27) && !vBN.eqn(28)) {
+    return {
+      gasUsed,
+      returnValue: Buffer.alloc(0),
+    }
+  }
+
   const r = data.slice(64, 96)
   const s = data.slice(96, 128)
 
