6234975da0d15a565e5728998d1378f550e3c15f
harkamal <gajinder@g11.in>
2022-06-01 00:46:48 +0530

    Add other methods of snapsync

--- packages/client/lib/net/protocol/snapprotocol.ts
+++ packages/client/lib/net/protocol/snapprotocol.ts
@@ -21,6 +21,32 @@ type GetAccountRangeOpts = {
   bytes: BN
 }
 
+type GetStorageRangesOpts = {
+  reqId?: BN
+  root: Buffer
+  accounts: Buffer[]
+  origin: Buffer
+  limit: Buffer
+  bytes: BN
+}
+
+type StorageData = {
+  hash: Buffer
+  body: Buffer
+}
+
+type GetByteCodesOpts = {
+  reqId?: BN
+  hashes: Buffer[]
+  bytes: BN
+}
+
+type GetTrieNodesOpts = {
+  reqId?: BN
+  root: Buffer
+  paths: Buffer[][]
+  bytes: BN
+}
 /*
  * Messages with responses that are added as
  * methods in camelCase to BoundProtocol.
@@ -87,6 +113,111 @@ export class SnapProtocol extends Protocol {
           reqId: new BN(reqId),
           accounts: accounts.map(([hash, body]: any) => ({ hash, body } as AccountData)),
           proof,
+        }
+      },
+    },
+    {
+      name: 'GetStorageRanges',
+      code: 0x02,
+      response: 0x03,
+      encode: ({ reqId, root, accounts, origin, limit, bytes }: GetStorageRangesOpts) => {
+        return [
+          (reqId === undefined ? id.iaddn(1) : new BN(reqId)).toArrayLike(Buffer),
+          root,
+          accounts,
+          origin,
+          limit,
+          bytes,
+        ]
+      },
+      decode: ([reqId, root, accounts, origin, limit, bytes]: any) => {
+        return {
+          reqId: new BN(reqId),
+          root,
+          accounts,
+          origin,
+          limit,
+          bytes: new BN(bytes),
+        }
+      },
+    },
+    {
+      name: 'StorageRanges',
+      code: 0x03,
+      encode: ({ reqId, slots, proof }: { reqId: BN; slots: StorageData[]; proof: Buffer[] }) => {
+        return [reqId.toArrayLike(Buffer), slots.map((slot) => [slot.hash, slot.body]), proof]
+      },
+      decode: ([reqId, slots, proof]: any) => {
+        return {
+          reqId: new BN(reqId),
+          slots: slots.map(([hash, body]: any) => ({ hash, body } as StorageData)),
+          proof,
+        }
+      },
+    },
+    {
+      name: 'GetByteCodes',
+      code: 0x04,
+      response: 0x05,
+      encode: ({ reqId, hashes, bytes }: GetByteCodesOpts) => {
+        return [
+          (reqId === undefined ? id.iaddn(1) : new BN(reqId)).toArrayLike(Buffer),
+          hashes,
+          bytes,
+        ]
+      },
+      decode: ([reqId, hashes, bytes]: any) => {
+        return {
+          reqId: new BN(reqId),
+          hashes,
+          bytes: new BN(bytes),
+        }
+      },
+    },
+    {
+      name: 'ByteCodes',
+      code: 0x05,
+      encode: ({ reqId, codes }: { reqId: BN; codes: Buffer[] }) => {
+        return [reqId.toArrayLike(Buffer), codes]
+      },
+      decode: ([reqId, codes]: any) => {
+        return {
+          reqId: new BN(reqId),
+          codes,
+        }
+      },
+    },
+    {
+      name: 'GetTrieNodes',
+      code: 0x06,
+      response: 0x07,
+      encode: ({ reqId, root, paths, bytes }: GetTrieNodesOpts) => {
+        return [
+          (reqId === undefined ? id.iaddn(1) : new BN(reqId)).toArrayLike(Buffer),
+          root,
+          paths,
+          bytes,
+        ]
+      },
+      decode: ([reqId, root, paths, bytes]: any) => {
+        return {
+          reqId: new BN(reqId),
+          root,
+          paths,
+          bytes: new BN(bytes),
+        }
+      },
+    },
+    {
+      name: 'TrieNodes',
+      code: 0x07,
+      encode: ({ reqId, nodes }: { reqId: BN; nodes: Buffer[] }) => {
+        return [reqId.toArrayLike(Buffer), nodes]
+      },
+      decode: ([reqId, nodes]: any) => {
+        return {
+          reqId: new BN(reqId),
+          nodes,
         }
       },
     },
