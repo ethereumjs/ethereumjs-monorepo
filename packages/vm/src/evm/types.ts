5155cd281b07fed6acf16424ba610b96cbd0ab66
Jochem Brouwer <jochembrouwer96@gmail.com>
2022-06-10 13:13:58 +0200

    evm: type cleanup

--- packages/vm/src/evm/types.ts
+++ packages/vm/src/evm/types.ts
@@ -1,7 +1,7 @@
 import { Block } from '@ethereumjs/block'
 import { StateAccess } from '@ethereumjs/statemanager'
 import { AccessList } from '@ethereumjs/tx'
-import { Account, Address } from '@ethereumjs/util'
+import { Address } from '@ethereumjs/util'
 import EVM, { EVMResult, ExecResult, NewContractEvent } from './evm'
 import { InterpreterStep } from './interpreter'
 import Message from './message'
@@ -173,117 +173,6 @@ export interface RunCodeOpts {
 export interface TxContext {
   gasPrice: bigint
   origin: Address
-}
-
-/**
- * The base frame environment
- * This is available for both CALLs and CREATEs
- */
-interface BaseFrameEnvironment {
-  caller?: Address // The caller address
-  gasLeft?: bigint // The current gas left
-  depth?: number // The call depth (defaults to 0)
-  value?: bigint // The call/create value
-  code?: Buffer // Code in the current frame (this code is set as "calldata" in a CREATE frame, in a CREATE frame there is no `data` field)
-}
-
-type CreateFrameEnvironment = BaseFrameEnvironment
-type CallFrameEnvironment = BaseFrameEnvironment & {
-  to: Address // If a `to` field is available this is automatically a CALL frame. Otherwise, it is a CREATE frame
-  data?: Buffer // The calldata
-}
-
-/**
- * The frame environment holds all info of the current call/create frame
- * Note that this will internally also hold information such as selfdestructed addresses,
- * if the frame is static (due to previous STATICCALL), whatever AUTH parameter is set, etc.
- */
-export type FrameEnvironment = CreateFrameEnvironment | CallFrameEnvironment
-
-/**
- * The global environment makes data available to the EVM which is necessary for some opcodes
- */
-export interface GlobalEnvironment {
-  origin: Address // The address which created this transaction
-  gasPrice: bigint // The gasPrice of the transaction
-  block: Block // Current block environment
-  chainId: bigint // The chainId of the current chain
-}
-
-/**
- * The EVMEnvironment is used to setup the necessary context in order to run EVM calls/creates
- */
-export interface EVMEnvironment {
-  FrameEnvironment: FrameEnvironment
-  GlobalEnvironment: GlobalEnvironment
-}
-
-/**
- * The ExternalInterface provides the interface which the EVM will use
- * to interact with the external environment, such as getting and storing code,
- * getting and storing storage, getting account balances, etc.
- */
-export interface ExternalInterface {
-  // TODO this is a skeleton and not implemented yet
-  /**
-   * Returns code of an account.
-   * @param address - Address of account
-   */
-  getCode(address: Address): Promise<Buffer>
-  /**
-   * Returns the hash of one of the 256 most recent complete blocks.
-   * @param num - Number of block
-   */
-  getBlockHash(num: bigint): Promise<bigint>
-  /**
-   * Store 256-bit a value in memory to persistent storage.
-   */
-  storageStore(address: Address, key: Buffer, value: Buffer): Promise<void>
-  /**
-   * Loads a 256-bit value to memory from persistent storage.
-   * @param address - Address of the storage trie to load
-   * @param key - Storage key
-   *    * @param original - If true, return the original storage value (default: false). This is used for gas metering dependend upon the original storage values
-   */
-  storageLoad(address: Address, key: Buffer): Promise<Buffer>
-
-  /**
-   * Returns an account at the given `address
-   * @param address Address to lookup
-   */
-  getAccount(address: Address): Promise<Account>
-
-  /**
-   * Returns true if account is empty or non-existent (according to EIP-161).
-   * @param address - Address of account
-   */
-  isAccountEmpty(address: Address): Promise<boolean>
-
-  /**
-   * Returns true if account exists in the state trie (it can be empty). Returns false if the account is `null`.
-   * @param address - Address of account
-   */
-  accountExists(address: Address): Promise<boolean>
-
-  /**
-   * Checkpoints the current external state
-   */
-  checkpoint(): Promise<void>
-  /**
-   * Commits the current external state
-   * Note: can still be reverted at a higher level
-   */
-  commit(): Promise<void>
-  /**
-   * Revert the current external state
-   */
-  revert(): Promise<void>
-
-  /**
-   * Returns an account at the given `address
-   * @param address Address to lookup
-   */
-  getAccount(address: Address): Promise<Account>
 }
 
 export type EVMEvents = {
