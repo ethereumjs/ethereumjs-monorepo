cca0e65e60a2a64c3e353ab3a19eaa3ff52a2ce2
Jochem Brouwer <jochembrouwer96@gmail.com>
2022-06-21 14:06:51 +0200

    evm/vm: move EEI back into VM

--- packages/evm/src/index.ts
+++ packages/evm/src/index.ts
@@ -1,12 +1,10 @@
 import { getActivePrecompiles } from './evm/precompiles'
-import EEI from './eei/eei'
 import { EEIInterface, EVMInterface, VmStateAccess, Log } from './evm/types'
 import EVM, { EVMResult } from './evm/evm'
 import { EvmError } from './exceptions'
 
 export {
   getActivePrecompiles,
-  EEI,
   EEIInterface,
   EVMInterface,
   EvmError,
