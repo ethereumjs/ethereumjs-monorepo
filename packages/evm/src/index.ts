703a85d15a169bbda00851ecc2fbfd25d3487199
Jochem Brouwer <jochembrouwer96@gmail.com>
2022-06-21 14:19:36 +0200

    evm: lint

--- packages/evm/src/index.ts
+++ packages/evm/src/index.ts
@@ -3,14 +3,6 @@ import { EEIInterface, EVMInterface, VmStateAccess, Log } from './types'
 import EVM, { EVMResult } from './evm'
 import { EvmError } from './exceptions'
 
-export {
-  getActivePrecompiles,
-  EEIInterface,
-  EVMInterface,
-  EvmError,
-  VmStateAccess,
-  Log,
-  EVMResult,
-}
+export { getActivePrecompiles, EEIInterface, EVMInterface, EvmError, VmStateAccess, Log, EVMResult }
 
 export default EVM
