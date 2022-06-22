d56f195bda99c50b8881ce71d67880689d3da65a
ScottyPoi <scott.simpson@ethereum.org>
2022-06-21 18:36:32 -0600

    block: include grayGlacier HF in tests

--- packages/block/test/difficulty.spec.ts
+++ packages/block/test/difficulty.spec.ts
@@ -37,6 +37,9 @@ const hardforkTestData: TestData = {
   arrowGlacier:
     require('../../ethereum-tests/DifficultyTests/dfArrowGlacier/difficultyArrowGlacier.json')
       .difficultyArrowGlacier.ArrowGlacier,
+  grayGlacier:
+    require('../../ethereum-tests/DifficultyTests/dfGrayGlacier/difficultyGrayGlacier.json')
+      .difficultyGrayGlacier.GrayGlacier,
 }
 
 const chainTestData: TestData = {
