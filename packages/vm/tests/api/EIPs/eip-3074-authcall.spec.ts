914b2eaea0f5eb568cce7a5aaf2043cea7428099
ScottyPoi <scott.simpson@ethereum.org>
2022-07-04 23:16:42 -0600

    vm:update signature interface in test

--- packages/vm/tests/api/EIPs/eip-3074-authcall.spec.ts
+++ packages/vm/tests/api/EIPs/eip-3074-authcall.spec.ts
@@ -675,6 +675,7 @@ tape('EIP-3074 AUTHCALL', (t) => {
       v: signature.v,
       r: signature.s,
       s: signature.s,
+      recovery: signature.recovery,
     }
     const code = Buffer.concat([
       getAuthCode(message, signature, authAddress),
