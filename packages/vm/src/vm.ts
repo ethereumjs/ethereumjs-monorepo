9f9b35c339aea2842643dc3466aa16ea3bbbea92
ScottyPoi <scott.simpson@ethereum.org>
2022-07-07 16:52:36 -0400

    vm: include hardfork options in VM.copy()

--- packages/vm/src/vm.ts
+++ packages/vm/src/vm.ts
@@ -230,6 +230,8 @@ export class VM extends AsyncEventEmitter<VMEvents> {
       common: commonCopy,
       evm: evmCopy,
       eei: eeiCopy,
+      hardforkByBlockNumber: this._hardforkByBlockNumber ? true : undefined,
+      hardforkByTD: this._hardforkByTD ? this._hardforkByTD : undefined,
     })
   }
 
