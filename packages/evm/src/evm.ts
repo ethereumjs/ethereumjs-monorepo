3f319fefda8fbcea86ba177ef7a0a8e0ede18162
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-07-15 06:36:09 -0400

    EVM: set sensible default caller account nonce (#2010)

--- packages/evm/src/evm.ts
+++ packages/evm/src/evm.ts
@@ -691,7 +691,15 @@ export class EVM extends AsyncEventEmitter<EVMEvents> implements EVMInterface {
         origin: opts.origin ?? opts.caller ?? Address.zero(),
       }
 
-      const caller = opts.caller ?? Address.zero()
+      let caller = opts.caller
+      if (!caller) {
+        // Set sensible default with correct nonce if no caller provided
+        caller = Address.zero()
+        const callerAccount = await this.eei.getAccount(caller)
+        callerAccount.nonce += BigInt(1)
+        await this.eei.putAccount(caller, callerAccount)
+      }
+
       const value = opts.value ?? BigInt(0)
       if (opts.skipBalance === true) {
         // if skipBalance, add `value` to caller balance to ensure sufficient funds
