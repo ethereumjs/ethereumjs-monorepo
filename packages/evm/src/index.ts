7325eb3a6c27da53a6a5d0990a21ee323913dcc7
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-07-21 06:26:56 -0400

    evm: add new exports requested by hardhat

--- packages/evm/src/index.ts
+++ packages/evm/src/index.ts
@@ -1,15 +1,20 @@
 import { getActivePrecompiles } from './precompiles'
 import { EEIInterface, EVMInterface, EVMStateAccess, Log } from './types'
-import { EVM, EVMResult } from './evm'
-import { EvmError } from './exceptions'
-
+import { EVM, EVMResult, ExecResult } from './evm'
+import { EvmError, ERROR as EvmErrorMessage } from './exceptions'
+import { InterpreterStep } from './interpreter'
+import { Message } from './message'
 export {
   getActivePrecompiles,
   EEIInterface,
   EVM,
   EVMInterface,
   EvmError,
+  EvmErrorMessage,
   EVMStateAccess,
+  ExecResult,
   Log,
   EVMResult,
+  InterpreterStep,
+  Message,
 }
