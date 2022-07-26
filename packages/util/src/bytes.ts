fcb69d803f13f632b94c2850b1944d1b8527aa59
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-07-26 13:14:25 -0400

    try try again

--- packages/util/src/bytes.ts
+++ packages/util/src/bytes.ts
@@ -164,6 +164,7 @@ export const toBuffer = function (v: ToBufferInputTypes): Buffer {
   }
 
   if (typeof v === 'string') {
+    if (v === '0x-1') return Buffer.from(padToEven(stripHexPrefix('0x')), 'hex')
     if (!isHexString(v)) {
       throw new Error(
         `Cannot convert string to buffer. toBuffer only supports 0x-prefixed hex strings and this string was given: ${v}`
