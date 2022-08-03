dcefee58623eda9ac6bebce609a628ea4e29a849
Brian Faust <hello@basecode.sh>
2022-08-03 17:56:39 +0300

    chore: wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ scripts/update-deps-patch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
+    cd $dir
+    echo $PWD
+    npx npm-check-updates -u --target=patch
+    cd ../..
+done
