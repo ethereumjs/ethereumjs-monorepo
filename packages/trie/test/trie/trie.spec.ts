1b7e7bdb7c4673399feea83d2b8dfa883c859de5
Brian Faust <hello@basecode.sh>
2022-08-04 03:24:09 +0300

    chore: wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/trie/test/trie/trie.spec.ts
+++ packages/trie/test/trie/trie.spec.ts
@@ -29,107 +29,139 @@ for (const { constructor, title } of [
     dbTmpDir = tmpdir()
   }
 
-  tape(`${title} (Persistence)`, function ({ test }) {
-    test('creates an instance via the static constructor `create` function and defaults to `true` with a database', async function (assert) {
-      assert.true(
-        ((await constructor.create({ db: new LevelDB(new Level(dbTmpDir)) })) as any)._persistRoot
-      )
-    })
+  tape(`${title} (Persistence)`, function (t) {
+    t.test(
+      'creates an instance via the static constructor `create` function and defaults to `true` with a database',
+      async function (st) {
+        st.true(
+          ((await constructor.create({ db: new LevelDB(new Level(dbTmpDir)) })) as any)._persistRoot
+        )
 
-    test('creates an instance via the static constructor `create` function and respects the `persistRoot` option with a database', async function (st) {
-      st.false(
-        ((await Trie.create({ db: new LevelDB(new Level(tmpdir())), persistRoot: false })) as any)
-          ._persistRoot
-      )
-    })
+        st.end()
+      }
+    )
 
-    test('creates an instance via the static constructor `create` function and respects the `persistRoot` option with a database', async function (assert) {
-      assert.false(
-        (
-          (await constructor.create({
-            db: new LevelDB(new Level(dbTmpDir)),
-            persistRoot: false,
-          })) as any
-        )._persistRoot
-      )
-    })
+    t.test(
+      'creates an instance via the static constructor `create` function and respects the `persistRoot` option with a database',
+      async function (st) {
+        st.false(
+          ((await Trie.create({ db: new LevelDB(new Level(tmpdir())), persistRoot: false })) as any)
+            ._persistRoot
+        )
 
-    test('creates an instance via the static constructor `create` function and defaults to `false` without a database', async function (assert) {
-      assert.false(((await constructor.create()) as any)._persistRoot)
-    })
+        st.end()
+      }
+    )
 
-    test('persist the root if the `persistRoot` option is not `false`', async function (assert) {
+    t.test(
+      'creates an instance via the static constructor `create` function and respects the `persistRoot` option with a database',
+      async function (st) {
+        st.false(
+          (
+            (await constructor.create({
+              db: new LevelDB(new Level(dbTmpDir)),
+              persistRoot: false,
+            })) as any
+          )._persistRoot
+        )
+
+        st.end()
+      }
+    )
+
+    t.test(
+      'creates an instance via the static constructor `create` function and defaults to `false` without a database',
+      async function (st) {
+        st.false(((await constructor.create()) as any)._persistRoot)
+
+        st.end()
+      }
+    )
+
+    t.test('persist the root if the `persistRoot` option is not `false`', async function (st) {
       const trie = await constructor.create({ db: new LevelDB(new MemoryLevel()) })
 
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
 
       await trie.put(Buffer.from('foo'), Buffer.from('bar'))
 
-      assert.notEqual(await trie.db.get(ROOT_DB_KEY), null)
+      st.notEqual(await trie.db.get(ROOT_DB_KEY), null)
+
+      st.end()
     })
 
-    test('persist the root if the `root` option is given', async function (assert) {
+    t.test('persist the root if the `root` option is given', async function (st) {
       const trie = await constructor.create({
         db: new LevelDB(new MemoryLevel()),
         root: KECCAK256_RLP,
       })
 
-      assert.true((await trie.db.get(ROOT_DB_KEY))?.equals(KECCAK256_RLP))
+      st.true((await trie.db.get(ROOT_DB_KEY))?.equals(KECCAK256_RLP))
 
       await trie.put(Buffer.from('foo'), Buffer.from('bar'))
 
-      assert.false((await trie.db.get(ROOT_DB_KEY))?.equals(KECCAK256_RLP))
+      st.false((await trie.db.get(ROOT_DB_KEY))?.equals(KECCAK256_RLP))
+
+      st.end()
     })
 
-    test('does not persist the root if the `persistRoot` option is `false`', async function (assert) {
+    t.test('does not persist the root if the `persistRoot` option is `false`', async function (st) {
       const trie = await constructor.create({
         db: new LevelDB(new MemoryLevel()),
         persistRoot: false,
       })
 
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
 
       await trie.put(Buffer.from('do_not_persist_with_db'), Buffer.from('bar'))
 
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
+
+      st.end()
     })
 
-    test('does not persist the root if the `db` option is not provided', async function (assert) {
+    t.test('does not persist the root if the `db` option is not provided', async function (st) {
       const trie = await constructor.create()
 
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
 
       await trie.put(Buffer.from('do_not_persist_without_db'), Buffer.from('bar'))
 
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
+
+      st.end()
     })
 
-    test('persist and restore the root', async function (assert) {
+    t.test('persist and restore the root', async function (st) {
       const db = new LevelDB(new MemoryLevel())
 
       const trie = await constructor.create({ db })
-      assert.equal(await trie.db.get(ROOT_DB_KEY), null)
+      st.equal(await trie.db.get(ROOT_DB_KEY), null)
       await trie.put(Buffer.from('foo'), Buffer.from('bar'))
-      assert.notEqual(await trie.db.get(ROOT_DB_KEY), null)
+      st.notEqual(await trie.db.get(ROOT_DB_KEY), null)
 
       // Using the same database as `trie` so we should have restored the root
       const copy = await constructor.create({ db })
-      assert.notEqual(await copy.db.get(ROOT_DB_KEY), null)
+      st.notEqual(await copy.db.get(ROOT_DB_KEY), null)
 
       // New trie with a new database so we shouldn't find a root to restore
       const empty = await constructor.create({ db: new LevelDB(new MemoryLevel()) })
-      assert.equal(await empty.db.get(ROOT_DB_KEY), null)
+      st.equal(await empty.db.get(ROOT_DB_KEY), null)
+
+      st.end()
     })
 
-    test('put fails if the key is the ROOT_DB_KEY', async function (assert) {
+    t.test('put fails if the key is the ROOT_DB_KEY', async function (st) {
       const trie = new constructor({ db: new LevelDB() })
 
       try {
         await trie.put(ROOT_DB_KEY, Buffer.from('bar'))
-        assert.fail("Attempting to set '__root__' should fail but it did not.")
+        st.fail("Attempting to set '__root__' should fail but it did not.")
       } catch ({ message }) {
-        assert.equal(message, "Attempted to set '__root__' key but it is not allowed.")
+        st.equal(message, "Attempted to set '__root__' key but it is not allowed.")
       }
+
+      st.end()
     })
   })
 }
