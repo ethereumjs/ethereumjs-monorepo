8dc5f11a452d6650ec609a7ab9fc7be48d9941df
Brian Faust <hello@basecode.sh>
2022-08-04 03:20:24 +0300

    Update .eslintrc.js

--- packages/rlp/.eslintrc.js
+++ packages/rlp/.eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: '../../config/eslint.js',
-  env: {
-    mocha: true,
-  },
   rules: {
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
