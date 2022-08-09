8179a33d3d1796497fe423ed665f3cd9418def0b
harkamal <gajinder@g11.in>
2022-08-09 17:27:52 +0530

    add account fetcher getter setter in snapsync

--- packages/client/lib/sync/fetcher/index.ts
+++ packages/client/lib/sync/fetcher/index.ts
@@ -1,8 +1,8 @@
 /**
  * @module sync/fetcher
  */
+export * from './accountfetcher'
 export * from './blockfetcher'
 export * from './fetcher'
 export * from './headerfetcher'
-export * from './accountfetcher'
 export * from './reverseblockfetcher'
