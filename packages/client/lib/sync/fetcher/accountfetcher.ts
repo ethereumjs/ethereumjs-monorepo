78540519afad29821ca9543a6d766e18a83ca7f1
Scorbajio <indigophi@protonmail.com>
2022-08-18 00:11:03 +0000

    Correct some messages

--- packages/client/lib/sync/fetcher/accountfetcher.ts
+++ packages/client/lib/sync/fetcher/accountfetcher.ts
@@ -155,9 +155,9 @@ export class AccountFetcher extends Fetcher<
 			await trie.put(hash, value)
 		}
 
-		// Step 1: validate the proof
+		// validate the proof
 		try {
-			// verify account data for account range returned using proof and state root
+			// verify account data for account range received from peer using proof and state root
 			const checkRangeProof = await trie.verifyRangeProof(this.root, hashes[0], hashes[hashes.length-1], hashes, values, proof)
 			this.debug('Proof for account range found to be valid: ' + checkRangeProof)
 			if (!checkRangeProof) {
@@ -170,7 +170,7 @@ export class AccountFetcher extends Fetcher<
 			return undefined
 		}
 
-		// TODO I am not sure if this step is necessary since proof verification should be establishing the correctness of every newly put account data
+		// TODO I am not sure if this check is necessary since proof verification should be establishing the correctness of every newly put account data
 		// verify that it is possible to get the accounts, and that the values are correct
 		for (let i = 0; i <= accounts.length - 1; i++) {
 			const account = accounts[i]
