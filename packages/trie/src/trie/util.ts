d9e5c5b666047476a081e13c32676e481e7e5e01
Brian Faust <hello@basecode.sh>
2022-08-22 05:53:14 +0300

    fix(trie): assign changes to opts copy

--- packages/trie/src/trie/util.ts
+++ packages/trie/src/trie/util.ts
@@ -3,11 +3,11 @@ import type { TrieOpts } from '../types'
 export async function prepareTrieOpts(key: Uint8Array, opts?: TrieOpts) {
   const result: TrieOpts = { ...opts }
 
-  if (opts?.db !== undefined && opts?.persistRoot === true) {
-    if (opts?.root === undefined) {
-      opts.root = (await opts?.db.get(Buffer.from(key))) ?? undefined
+  if (result?.db !== undefined && result?.persistRoot === true) {
+    if (result?.root === undefined) {
+      result.root = (await result?.db.get(Buffer.from(key))) ?? undefined
     } else {
-      await opts?.db.put(Buffer.from(key), opts.root)
+      await result?.db.put(Buffer.from(key), result.root)
     }
   }
 
