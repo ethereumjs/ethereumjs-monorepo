17e3dc0a7ff921adf6809ca5bd8d455be1d09524
Holger Drewes <Holger.Drewes@gmail.com>
2022-08-22 18:52:53 +0200

    Block: do not auto-activate hardforkByBlockNumber in fromRLPSerializedHeader static BlockHeader constructor (#2205)

--- packages/block/test/header.spec.ts
+++ packages/block/test/header.spec.ts
@@ -67,16 +67,13 @@ tape('[Block]: Header functions', function (t) {
   })
 
   t.test('Initialization -> fromRLPSerializedHeader()', function (st) {
-    let header = BlockHeader.fromHeaderData({}, { freeze: false, hardforkByBlockNumber: false })
+    let header = BlockHeader.fromHeaderData({}, { freeze: false })
 
     const rlpHeader = header.serialize()
-    header = BlockHeader.fromRLPSerializedHeader(rlpHeader, { hardforkByBlockNumber: false })
+    header = BlockHeader.fromRLPSerializedHeader(rlpHeader)
     st.ok(Object.isFrozen(header), 'block should be frozen by default')
 
-    header = BlockHeader.fromRLPSerializedHeader(rlpHeader, {
-      freeze: false,
-      hardforkByBlockNumber: false,
-    })
+    header = BlockHeader.fromRLPSerializedHeader(rlpHeader, { freeze: false })
     st.ok(!Object.isFrozen(header), 'block should not be frozen when freeze deactivated in options')
 
     st.throws(
@@ -93,7 +90,8 @@ tape('[Block]: Header functions', function (t) {
       Buffer.from(
         'f90214a00000000000000000000000000000000000000000000000000000000000000000a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347940000000000000000000000000000000000000000a0d7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000850400000000808213888080a011bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82faa00000000000000000000000000000000000000000000000000000000000000000880000000000000042',
         'hex'
-      )
+      ),
+      { hardforkByBlockNumber: true }
     )
 
     st.equal(
