70306497cff0cbb342540299142c07545a6b93e0
Brian Faust <hello@basecode.sh>
2022-08-23 05:53:22 +0300

    chore: wip

--- scripts/list-external-dependencies.js
+++ scripts/list-external-dependencies.js
@@ -1,13 +1,39 @@
 const { readFileSync } = require('fs')
 const { join } = require('path')
 
+const COLORS = {
+  RESET: '\x1b[0m',
+  FG_RED: '\x1b[31m',
+  FG_GREEN: '\x1b[32m',
+  FG_YELLOW: '\x1b[33m',
+  FG_BLUE: '\x1b[34m',
+}
+
+const LOGGER = {
+  danger(message) {
+    console.log(`${COLORS.FG_RED}%s${COLORS.RESET}`, message)
+  },
+  warning(message) {
+    console.log(`${COLORS.FG_YELLOW}%s${COLORS.RESET}`, message)
+  },
+  info(message) {
+    console.log(`${COLORS.FG_BLUE}%s${COLORS.RESET}`, message)
+  },
+  success(message) {
+    console.log(`${COLORS.FG_GREEN}%s${COLORS.RESET}`, message)
+  },
+}
+
 function countExternalDependencies(dependency) {
   try {
-    const { dependencies, devDependencies } = JSON.parse(readFileSync(`./node_modules/${dependency}/package.json`).toString())
+    const { dependencies, devDependencies } = JSON.parse(
+      readFileSync(`./node_modules/${dependency}/package.json`).toString()
+    )
 
     return {
       dependencies: dependencies === undefined ? 0 : new Set(Object.keys(dependencies)).size,
-      devDependencies: devDependencies === undefined ? 0 : new Set(Object.keys(devDependencies)).size,
+      devDependencies:
+        devDependencies === undefined ? 0 : new Set(Object.keys(devDependencies)).size,
     }
   } catch {
     return {
@@ -17,11 +43,27 @@ function countExternalDependencies(dependency) {
   }
 }
 
-function logDependencies(type, set) {
-  console.log(`>>> ${set.length} ${type}`)
+function logDependencies(type, map) {
+  console.log(`>>> ${map.size} ${type}`)
 
-  for (const { dependency, dependencies, devDependencies } of set) {
-    console.log(`${dependency} (${dependencies} dependencies, ${devDependencies} devDependencies)`)
+  for (const [dependency, { dependencies, devDependencies }] of map.entries()) {
+    if (dependencies >= 10) {
+      LOGGER.danger(
+        `${dependency} (${dependencies} dependencies, ${devDependencies} devDependencies)`
+      )
+    } else if (dependencies >= 5) {
+      LOGGER.warning(
+        `${dependency} (${dependencies} dependencies, ${devDependencies} devDependencies)`
+      )
+    } else if (dependencies >= 1) {
+      LOGGER.info(
+        `${dependency} (${dependencies} dependencies, ${devDependencies} devDependencies)`
+      )
+    } else {
+      LOGGER.success(
+        `${dependency} (${dependencies} dependencies, ${devDependencies} devDependencies)`
+      )
+    }
   }
 }
 
@@ -51,8 +93,8 @@ const directories = require('fs')
 // Always include the root package.json too
 directories.push(require('../package.json'))
 
-let prod = new Set()
-let devs = new Set()
+let prod = new Map()
+let devs = new Map()
 for (const { dependencies, devDependencies } of directories) {
   if (dependencies) {
     for (const dependency of Object.keys(dependencies)) {
@@ -60,7 +102,7 @@ for (const { dependencies, devDependencies } of directories) {
         continue
       }
 
-      prod.add({ dependency, ...countExternalDependencies(dependency) })
+      prod.set(dependency, countExternalDependencies(dependency))
     }
   }
 
@@ -70,10 +112,10 @@ for (const { dependencies, devDependencies } of directories) {
         continue
       }
 
-      devs.add({ dependency, ...countExternalDependencies(dependency) })
+      devs.set(dependency, countExternalDependencies(dependency))
     }
   }
 }
 
-logDependencies("dependencies", [...prod].sort())
-logDependencies("devDependencies", [...devs].sort())
+logDependencies('dependencies', new Map([...prod.entries()].sort()))
+logDependencies('devDependencies', new Map([...devs.entries()].sort()))
