06b5c966f5c79f311a846a3ab3a91d7c5916a3eb
Gabriel Rocheleau <contact@rockwaterweb.com>
2022-08-24 21:44:50 -0400

    tx: suffix tests with .spec.ts

--- packages/tx/test/index.ts
+++ packages/tx/test/index.ts
@@ -22,7 +22,7 @@ if (isTruthy(argv.b)) {
   require('./eip1559.spec')
   require('./transactionFactory.spec')
 } else {
-  require('./transactionRunner')
+  require('./transactionRunner.spec')
   require('./base.spec')
   require('./legacy.spec')
   require('./typedTxsAndEIP2930.spec')
