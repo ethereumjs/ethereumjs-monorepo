9aa335c5a5dfc9f36af1e4c1172913838949926d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-09-01 10:23:47 -0400

    trie: adds profiling script back

---
+++ packages/trie/benchmarks/random.ts
@@ -0,0 +1,30 @@
+'use strict'
+import { keccak256 } from 'ethereum-cryptography/keccak'
+import { Trie } from '../dist'
+
+// References:
+// https://eth.wiki/en/fundamentals/benchmarks#the-trie
+// https://gist.github.com/heikoheiko/0fa2b322560ba7794f22
+
+const ROUNDS = 1000
+const KEY_SIZE = 32
+
+export const runTrie = async (eraSize = 9, symmetric = false) => {
+  const trie = new Trie({ useKeyHashing: true })
+  let key = Buffer.alloc(KEY_SIZE)
+
+  for (let i = 0; i <= ROUNDS; i++) {
+    key = Buffer.from(keccak256(key))
+
+    if (symmetric) {
+      await trie.put(key, key)
+    } else {
+      const val = Buffer.from(keccak256(key))
+      await trie.put(key, val)
+    }
+
+    if (i % eraSize === 0) {
+      key = trie.root()
+    }
+  }
+}
