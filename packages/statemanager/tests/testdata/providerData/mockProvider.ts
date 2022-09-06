b7745621960bbcf074156497e083d1f60f61c6bf
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-09-06 16:11:11 -0400

    Add more test data and mock provider updates

--- packages/statemanager/tests/testdata/providerData/mockProvider.ts
+++ packages/statemanager/tests/testdata/providerData/mockProvider.ts
@@ -7,15 +7,19 @@ export class MockProvider extends JsonRpcProvider {
         return this.getProofValues(params as any)
       case 'eth_getBlockByNumber':
         return this.getBlockValues(params as any)
+      case 'eth_chainId': // Always pretends to be mainnet
+        return 1
+      case 'eth_getCode':
+        return 0
       default:
-        return 'method not implemented'
+        throw new Error(`method ${method} not implemented`)
     }
   }
 
   private getProofValues = (params: [address: string, _: [], blockTag: bigint | string]) => {
     const [address, _, blockTag] = params
     const account = require(`./accounts/${address}.json`)
-    return account[blockTag.toString()]
+    return account[blockTag.toString() ?? 'latest']
   }
 
   private getBlockValues = (params: [blockTag: string, _: boolean]) => {
