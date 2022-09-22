34f3dcdf37d2fbeffeb41dc3de693f59b91c46bc
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-09-22 10:36:23 -0400

    block: Fix from rpc methods (#2302)
    * block: Fix fromRpc type handling
    
    * Add tests with provider block data
    
    * Fix import
    
    * Update packages/block/src/from-rpc.ts
    
    Co-authored-by: Gabriel Rocheleau <contact@rockwaterweb.com>
    
    Co-authored-by: Gabriel Rocheleau <contact@rockwaterweb.com>

--- packages/block/src/from-rpc.ts
+++ packages/block/src/from-rpc.ts
@@ -1,23 +1,22 @@
 import { TransactionFactory } from '@ethereumjs/tx'
-import { setLengthLeft, toBuffer } from '@ethereumjs/util'
+import { TypeOutput, setLengthLeft, toBuffer, toType } from '@ethereumjs/util'
 
 import { blockHeaderFromRpc } from './header-from-rpc'
-import { numberToHex } from './helpers'
 
 import { Block } from './index'
 
 import type { BlockOptions, JsonRpcBlock } from './index'
 import type { TxData, TypedTransaction } from '@ethereumjs/tx'
 
-function normalizeTxParams(_txParams: any) {
+export function normalizeTxParams(_txParams: any) {
   const txParams = Object.assign({}, _txParams)
 
-  txParams.gasLimit = txParams.gasLimit === undefined ? txParams.gas : txParams.gasLimit
+  txParams.gasLimit = toType(txParams.gasLimit ?? txParams.gas, TypeOutput.BigInt)
   txParams.data = txParams.data === undefined ? txParams.input : txParams.data
 
   // check and convert gasPrice and value params
-  txParams.gasPrice = numberToHex(txParams.gasPrice)
-  txParams.value = numberToHex(txParams.value)
+  txParams.gasPrice = txParams.gasPrice !== undefined ? BigInt(txParams.gasPrice) : undefined
+  txParams.value = txParams.value !== undefined ? BigInt(txParams.value) : undefined
 
   // strict byte length checking
   txParams.to =
@@ -25,11 +24,7 @@ function normalizeTxParams(_txParams: any) {
       ? setLengthLeft(toBuffer(txParams.to), 20)
       : null
 
-  // v as raw signature value {0,1}
-  // v is the recovery bit and can be either {0,1} or {27,28}.
-  // https://ethereum.stackexchange.com/questions/40679/why-the-value-of-v-is-always-either-27-11011-or-28-11100
-  const v: number = txParams.v
-  txParams.v = v < 27 ? v + 27 : v
+  txParams.v = toType(txParams.v, TypeOutput.BigInt)
 
   return txParams
 }
@@ -50,7 +45,7 @@ export function blockFromRpc(
 
   const transactions: TypedTransaction[] = []
   const opts = { common: header._common }
-  for (const _txParams of blockParams.transactions) {
+  for (const _txParams of blockParams.transactions ?? []) {
     const txParams = normalizeTxParams(_txParams)
     const tx = TransactionFactory.fromTxData(txParams as TxData, opts)
     transactions.push(tx)
