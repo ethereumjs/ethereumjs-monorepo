0454b5adc87d0fbe391d41406d575941bdd7a1d5
scorbajio <indigophi@protonmail.com>
2022-09-24 19:23:33 +0000

    Add tests for compact encoding helpers

---
+++ packages/util/test/encoding.spec.ts
@@ -0,0 +1,61 @@
+import * as tape from 'tape'
+import { hexToCompact, compactToHex, keybytesToHex } from '@ethereumjs/util'
+
+tape('encoding', function (t) {
+  t.test('hexToCompact should encode hex data correctly', function (t) {
+    const tests = [
+      { hex: new Uint8Array(), compact: new Uint8Array(Buffer.from('00', 'hex')) },
+      { hex: new Uint8Array([16]), compact: new Uint8Array(Buffer.from('20', 'hex')) },
+      {
+        hex: new Uint8Array([1, 2, 3, 4, 5]),
+        compact: new Uint8Array(Buffer.from('112345', 'hex')),
+      },
+      {
+        hex: new Uint8Array([0, 1, 2, 3, 4, 5]),
+        compact: new Uint8Array(Buffer.from('00012345', 'hex')),
+      },
+      {
+        hex: new Uint8Array([15, 1, 12, 11, 8, 16]),
+        compact: new Uint8Array(Buffer.from('3f1cb8', 'hex')),
+      },
+      {
+        hex: new Uint8Array([0, 15, 1, 12, 11, 8, 16]),
+        compact: new Uint8Array(Buffer.from('200f1cb8', 'hex')),
+      },
+    ]
+
+    for (let i = 0; i < tests.length; i++) {
+      const test = tests[i]
+      t.equal(JSON.stringify(hexToCompact(test.hex)), JSON.stringify(test.compact))
+      t.equal(JSON.stringify(compactToHex(test.compact)), JSON.stringify(test.hex))
+    }
+
+    t.end()
+  })
+
+  t.test('keybytesToHex should encode data correctly', function (t) {
+    const tests = [
+      { key: new Uint8Array(), hexOut: new Uint8Array([16]) },
+      { key: new Uint8Array(), hexOut: new Uint8Array([16]) },
+      {
+        key: new Uint8Array(Buffer.from('123456', 'hex')),
+        hexOut: new Uint8Array([1, 2, 3, 4, 5, 6, 16]),
+      },
+      {
+        key: new Uint8Array(Buffer.from('12345', 'hex')),
+        hexOut: new Uint8Array([1, 2, 3, 4, 0, 5, 16]),
+      },
+      {
+        key: new Uint8Array(Buffer.from('123456', 'hex')),
+        hexOut: new Uint8Array([1, 2, 3, 4, 5, 6, 16]),
+      },
+    ]
+
+    for (let i = 0; i < tests.length; i++) {
+      const test = tests[i]
+      t.equal(JSON.stringify(keybytesToHex(test.key)), JSON.stringify(test.hexOut))
+    }
+
+    t.end()
+  })
+})
