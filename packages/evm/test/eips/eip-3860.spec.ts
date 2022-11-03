882de652258f27bbaecc2a424ea8c01f4023cecd
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-11-03 11:25:12 -0400

    add test for Create2

--- packages/evm/test/eips/eip-3860.spec.ts
+++ packages/evm/test/eips/eip-3860.spec.ts
@@ -1,5 +1,4 @@
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
-import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
 import { Address, privateToAddress } from '@ethereumjs/util'
 import * as tape from 'tape'
 
@@ -7,17 +6,15 @@ import { EVM } from '../../src'
 import { getEEI } from '../utils'
 
 const pkey = Buffer.from('20'.repeat(32), 'hex')
-const GWEI = BigInt('1000000000')
 const sender = new Address(privateToAddress(pkey))
 
 tape('EIP 3860 tests', (t) => {
-  const common = new Common({
-    chain: Chain.Mainnet,
-    hardfork: Hardfork.London,
-    eips: [3860],
-  })
-
   t.test('EIP-3860 tests', async (st) => {
+    const common = new Common({
+      chain: Chain.Mainnet,
+      hardfork: Hardfork.London,
+      eips: [3860],
+    })
     const eei = await getEEI()
     const evm = await EVM.create({ common, eei })
 
@@ -43,5 +40,50 @@ tape('EIP 3860 tests', (t) => {
       (result.execResult.exceptionError?.error as string) === 'initcode exceeds max initcode size',
       'initcode exceeds max size'
     )
+  })
+
+  t.test('ensure EIP-3860 gas are applied on CREATE/CREATE2 calls', async (st) => {
+    const commonWith3860 = new Common({
+      chain: Chain.Mainnet,
+      hardfork: Hardfork.London,
+      eips: [3860],
+    })
+    const commonWithout3860 = new Common({
+      chain: Chain.Mainnet,
+      hardfork: Hardfork.London,
+      eips: [],
+    })
+    const caller = Address.fromString('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b')
+    const eei = await getEEI()
+    const evm = await EVM.create({ common: commonWith3860, eei })
+    const evmWithout3860 = await EVM.create({ common: commonWithout3860, eei: eei.copy() })
+    const contractFactory = Address.fromString('0xb94f5374fce5edbc8e2a8697c15331677e6ebf0b')
+    const contractAccount = await evm.eei.getAccount(contractFactory)
+    await evm.eei.putAccount(contractFactory, contractAccount)
+    await evmWithout3860.eei.putAccount(contractFactory, contractAccount)
+    const factoryCode = Buffer.from(
+      '7f600a80600080396000f3000000000000000000000000000000000000000000006000526000355a60008260006000f55a8203600a55806000556001600155505050',
+      'hex'
+    )
+
+    await evm.eei.putContractCode(contractFactory, factoryCode)
+    await evmWithout3860.eei.putContractCode(contractFactory, factoryCode)
+    const data = Buffer.from(
+      '000000000000000000000000000000000000000000000000000000000000c000',
+      'hex'
+    )
+    const runCallArgs = {
+      from: caller,
+      to: contractFactory,
+      data,
+      gasLimit: BigInt(0xfffffffff),
+    }
+    const res = await evm.runCall(runCallArgs)
+    const res2 = await evmWithout3860.runCall(runCallArgs)
+    st.ok(
+      res.execResult.executionGasUsed > res2.execResult.executionGasUsed,
+      'execution gas used is higher with EIP 3860 active'
+    )
+    st.end()
   })
 })
