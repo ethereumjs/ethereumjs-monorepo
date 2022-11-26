ad2e9d1f0f72c5b79dcb843d582e3c3cbe8a3533
ScottyPoi <scott.simpson@ethereum.org>
2022-11-26 00:30:44 -0700

    EVM: check for 'ethjs' DEVUG variable

--- packages/evm/src/evm.ts
+++ packages/evm/src/evm.ts
@@ -298,10 +298,8 @@ export class EVM implements EVMInterface {
       promisify(this.events.emit.bind(this.events))
     )
 
-    // Safeguard if "process" is not available (browser)
-    if (typeof process?.env.DEBUG !== 'undefined') {
-      this.DEBUG = true
-    }
+    // Skip DEBUG calls unless 'ethjs' included in environmental DEBUG variables
+    this.DEBUG = process?.env?.DEBUG?.includes('ethjs') ?? false
   }
 
   protected async init(): Promise<void> {
