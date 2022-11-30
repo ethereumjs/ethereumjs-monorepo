7380500dfd7e4d207243acb9fdf4d1b16a2e7eb4
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-11-30 16:19:28 -0500

    Add reference on txGenerator script

--- packages/client/txGenerator.ts
+++ packages/client/txGenerator.ts
@@ -1,3 +1,4 @@
+// Adapted from - https://github.com/Inphi/eip4844-interop/blob/master/blob_tx_generator/blob.js
 import { randomBytes } from 'crypto'
 import { Address } from '@ethereumjs/util'
 import { Client } from 'jayson/promise'
