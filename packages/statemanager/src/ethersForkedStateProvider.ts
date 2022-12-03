5898c2022982450f2a405c8345a77a855d38e77f
ScottyPoi <scott.simpson@ethereum.org>
2022-12-02 18:31:07 -0700

    create wrapper for getCode

--- packages/statemanager/src/ethersForkedStateProvider.ts
+++ packages/statemanager/src/ethersForkedStateProvider.ts
@@ -1,4 +1,6 @@
-import ethers from 'ethers'
+import { Address, bufferToHex, toBuffer } from '@ethereumjs/util'
+import { ethers } from 'ethers'
+
 import { EthersStateManager } from '.'
 export class EthersForkedStateProvider extends ethers.providers.JsonRpcProvider {
   private fallbackProvider: ethers.providers.JsonRpcProvider
@@ -8,5 +10,14 @@ export class EthersForkedStateProvider extends ethers.providers.JsonRpcProvider 
     this.fallbackProvider =
       typeof provider === 'string' ? new ethers.providers.JsonRpcProvider(provider) : provider
     this.ethersStateManager = new EthersStateManager({ blockTag: 'earliest', provider })
+  }
+  async getCode(addressOrName: string | Promise<string>): Promise<string> {
+    const address = new Address(toBuffer(await addressOrName))
+    const result = await this.ethersStateManager.getContractCode(address)
+    if (result.length > 0) {
+      return bufferToHex(result)
+    } else {
+      return this.fallbackProvider.getCode(addressOrName)
+    }
   }
 }
