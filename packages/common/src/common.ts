e2b8d2c2d2cb8f77257caec3110835c8939bfe8e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-12-12 20:32:34 -0500

    Update comments

--- packages/common/src/common.ts
+++ packages/common/src/common.ts
@@ -905,10 +905,9 @@ export class Common extends EventEmitter {
    * Expected returns (parameters must be present in
    * the respective chain json files):
    *
-   * ethash: -
+   * ethash: empty object
    * clique: period, epoch
-   * aura: -
-   * casper: -
+   * casper: empty object
    *
    * Note: This value can update along a Hardfork.
    */
