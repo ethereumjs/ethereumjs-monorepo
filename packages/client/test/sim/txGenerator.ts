d0a9766564b0a9c4517ae73e7761ac75cc5bb2ff
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-12-22 06:33:17 -0500

    Cleanup/readme updates

--- packages/client/test/sim/txGenerator.ts
+++ packages/client/test/sim/txGenerator.ts
@@ -1,15 +1,15 @@
 // Adapted from - https://github.com/Inphi/eip4844-interop/blob/master/blob_tx_generator/blob.js
-import { randomBytes } from 'crypto'
-import { Address } from '@ethereumjs/util'
-import { Client } from 'jayson/promise'
 import { BlobEIP4844Transaction } from '@ethereumjs/tx'
 import {
   blobsToCommitments,
   commitmentsToVersionedHashes,
 } from '@ethereumjs/tx/test/utils/blobHelpers'
-import { loadTrustedSetup, freeTrustedSetup } from 'c-kzg'
-const input = process.argv[2]
-const expected_kzgs = process.argv[3]
+import { Address } from '@ethereumjs/util'
+import { freeTrustedSetup, loadTrustedSetup } from 'c-kzg'
+import { randomBytes } from 'crypto'
+import { Client } from 'jayson/promise'
+const clientPort = process.argv[2]
+const input = process.argv[3]
 
 const BYTES_PER_FIELD_ELEMENT = 32
 const FIELD_ELEMENTS_PER_BLOB = 4096
@@ -18,8 +18,11 @@ const MAX_BLOBS_PER_TX = 2
 const MAX_USEFUL_BYTES_PER_TX = USEFUL_BYTES_PER_BLOB * MAX_BLOBS_PER_TX - 1
 const BLOB_SIZE = BYTES_PER_FIELD_ELEMENT * FIELD_ELEMENTS_PER_BLOB
 
+const pkey = Buffer.from('45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8', 'hex')
+const sender = Address.fromPrivateKey(pkey)
+
 function get_padded(data: any, blobs_len: number) {
-  let pdata = Buffer.alloc(blobs_len * USEFUL_BYTES_PER_BLOB)
+  const pdata = Buffer.alloc(blobs_len * USEFUL_BYTES_PER_BLOB)
   const datalen = Buffer.byteLength(data)
   pdata.fill(data, 0, datalen)
   // TODO: if data already fits in a pad, then ka-boom
@@ -28,9 +31,9 @@ function get_padded(data: any, blobs_len: number) {
 }
 
 function get_blob(data: any) {
-  let blob = Buffer.alloc(BLOB_SIZE, 'binary')
+  const blob = Buffer.alloc(BLOB_SIZE, 'binary')
   for (let i = 0; i < FIELD_ELEMENTS_PER_BLOB; i++) {
-    let chunk = Buffer.alloc(32, 'binary')
+    const chunk = Buffer.alloc(32, 'binary')
     chunk.fill(data.subarray(i * 31, (i + 1) * 31), 0, 31)
     blob.fill(chunk, i * 32, (i + 1) * 32)
   }
@@ -53,10 +56,10 @@ function get_blobs(data: any) {
 
   const pdata = get_padded(data, blobs_len)
 
-  let blobs: Buffer[] = []
+  const blobs: Buffer[] = []
   for (let i = 0; i < blobs_len; i++) {
-    let chunk = pdata.subarray(i * USEFUL_BYTES_PER_BLOB, (i + 1) * USEFUL_BYTES_PER_BLOB)
-    let blob = get_blob(chunk)
+    const chunk = pdata.subarray(i * USEFUL_BYTES_PER_BLOB, (i + 1) * USEFUL_BYTES_PER_BLOB)
+    const blob = get_blob(chunk)
     blobs.push(blob)
   }
 
@@ -69,19 +72,12 @@ function sleep(ms: number) {
   })
 }
 
-async function estimateGas(client: Client, tx: any) {
-  const num = (await client.request('eth_blockNumber', [], 2.0)).result
-  const res = await client.request('eth_estimateGas', [tx, num])
-  console.log(res)
-  return res.result
-}
-
 async function getNonce(client: Client, account: string) {
   const nonce = await client.request('eth_getTransactionCount', [account, 'latest'], 2.0)
   return nonce.result
 }
-async function run(data: any, expected_kzgs: any) {
-  const client = Client.http({ port: 8947 })
+async function run(data: any) {
+  const client = Client.http({ port: parseInt(clientPort) })
   while (true) {
     const num = parseInt((await client.request('eth_blockNumber', [], 2.0)).result)
     if (num >= 1) {
@@ -99,11 +95,11 @@ async function run(data: any, expected_kzgs: any) {
 
   const account = Address.fromPrivateKey(randomBytes(32))
   const txData = {
-    from: '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+    from: sender.toString(),
     to: account.toString(),
     data: '0x',
     chainId: '0x1',
-    blobs: blobs,
+    blobs,
     kzgCommitments: commitments,
     versionedHashes: hashes,
     gas: undefined,
@@ -118,13 +114,10 @@ async function run(data: any, expected_kzgs: any) {
   txData['maxPriorityFeePerGas'] = BigInt(100000000) as any
   txData['maxFeePerDataGas'] = BigInt(1000) as any
   txData['gasLimit'] = BigInt(28000000) as any
-  const nonce = await getNonce(client, '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b')
+  const nonce = await getNonce(client, sender.toString())
   txData['nonce'] = BigInt(nonce) as any
-  const blobTx = BlobEIP4844Transaction.fromTxData(txData).sign(
-    Buffer.from('45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8', 'hex')
-  )
+  const blobTx = BlobEIP4844Transaction.fromTxData(txData).sign(pkey)
 
-  console.log(`sending to ${account.toString()} from ${blobTx.getSenderAddress().toString()}`)
   const serializedWrapper = blobTx.serializeNetworkWrapper()
 
   const res = await client.request(
@@ -132,18 +125,16 @@ async function run(data: any, expected_kzgs: any) {
     ['0x' + serializedWrapper.toString('hex')],
     2.0
   )
-  console.log(res)
-  if (res.result.error) {
-    return false
-  }
 
-  if (expected_kzgs === undefined) {
-    return true
+  if (res.result.error !== undefined) {
+    console.log('error sending transaction')
+    console.log(res.result.error)
+    return false
   }
 
   let blob_kzg = null
   try {
-    let res = (
+    const res = (
       await (await fetch('http://127.0.0.1:9596/eth/v1/beacon/headers', { method: 'get' })).json()
     ).data[0].header.message.slot
     const start = parseInt(res)
@@ -151,17 +142,19 @@ async function run(data: any, expected_kzgs: any) {
       const res = (
         await (await fetch(`http://127.0.0.1:9596/eth/v2/beacon/blocks/${start + i}`)).json()
       ).data.message.body.blob_kzg_commitments
-      if (res && res.length > 0) {
+      if (res !== undefined && res.length > 0) {
         blob_kzg = res[0]
+        break
       }
-      while (true) {
+      let done = false
+      while (!done) {
         const current =
           (await (await fetch('http://127.0.0.1:9596/eth/v1/beacon/headers')).json()).data[0].header
             .message.slot - 1
         if (current > start + i) {
-          break
+          done = true
         }
-        console.log(`waiting for tx to be included in block.... bn=${current}`)
+        console.log(`waiting for tx to be included in block.... block number=${current}`)
         await sleep(1000)
       }
     }
@@ -171,16 +164,14 @@ async function run(data: any, expected_kzgs: any) {
     return false
   }
 
-  if (blob_kzg !== expected_kzgs) {
-    console.log(`Unexpected KZG value: expected ${expected_kzgs}, got ${blob_kzg}`)
+  const expected_kzgs = '0x' + blobTx.kzgCommitments![0].toString('hex')
+  if (blob_kzg !== '0x' + blobTx.kzgCommitments![0].toString('hex')) {
+    console.log(`Unexpected KZG commitment: expected ${expected_kzgs}, got ${blob_kzg}`)
     return false
   } else {
-    console.log(`Found expected KZG value: ${blob_kzg}`)
+    console.log(`Found expected KZG commitment: ${blob_kzg}`)
   }
 
   return true
 }
-
-;(async () => {
-  process.exit((await run(input, expected_kzgs)) ? 0 : 1)
-})()
+void run(input)
