f5138360166db59639cc24f060970a2580bcc8f1
Jochem Brouwer <jochembrouwer96@gmail.com>
2022-12-28 14:51:35 +0100

    evm, eof container updates [no ci]

--- packages/evm/src/eof/eofContainer.ts
+++ packages/evm/src/eof/eofContainer.ts
@@ -2,12 +2,24 @@ import { KIND_CODE, KIND_DATA, KIND_TYPE, MAGIC, TERMINATOR } from './constants'
 
 const VERSION = 0x01
 
+function safeReadUint16(input: Buffer, position: number): number | undefined {
+  if (input.length < position + 2) {
+    return undefined
+  } else {
+    return input.readUint16BE(position)
+  }
+}
+
 export class EOFSectionHeader {
   sectionKind: number
   sectionSize: number[]
   constructor(kind: number, size: number | number[]) {
     this.sectionKind = kind
-    this.sectionSize = [...size]
+    if (typeof size === 'number') {
+      this.sectionSize = [size]
+    } else {
+      this.sectionSize = [...size]
+    }
   }
   buffer = () => {
     const buf = Buffer.from([])
@@ -26,16 +38,27 @@ export class EofHeader {
   dataSize: number
   static validate(header: Buffer) {
     header = header instanceof EOFSectionHeader ? header.buffer() : header
-    const numCodeSections = header.readUint16BE(7)
     if (!header.slice(0, 2).equals(MAGIC)) {
       throw new Error('Should always begin with MAGIC bytes')
-    } else if (VERSION !== header.readUint8(2)) {
+    } else if (VERSION !== header[2]) {
       throw new Error(`Only VERSION "0x01" supported`)
-    } else if (KIND_TYPE !== header.readUint8(3)) {
+    } else if (KIND_TYPE !== header[3]) {
       throw new Error(`Expected KIND_TYPE byte at index ${3}`)
-    } else if (KIND_DATA !== header.readUint8(9 + numCodeSections * 2)) {
+    } else if (header.length < 6) {
+      throw new Error('Invalid types section size')
+    } else if (KIND_CODE !== header[6]) {
+      throw new Error(`Expected KIND_CODE byte at index ${6}`)
+    } else if (header.length < 9) {
+      throw new Error('Invalid code section size')
+    }
+    const numCodeSections = header.readUint16BE(7)
+    if (header.length < 9 + numCodeSections * 2) {
+      throw new Error('Invalid code section size')
+    } else if (KIND_DATA !== header[9 + numCodeSections * 2]) {
       throw new Error(`Expected KIND_DATA byte at index ${9 + numCodeSections * 2}`)
-    } else if (TERMINATOR !== header.readUint8(9 + numCodeSections * 2 + 3)) {
+    } else if (header.length < 9 + numCodeSections * 2 + 3) {
+      throw new Error('Invalid data section size')
+    } else if (TERMINATOR !== header[9 + numCodeSections * 2 + 3]) {
       throw new Error(`Expected TERMINATOR byte at index ${9 + numCodeSections * 2 + 3}`)
     }
   }
@@ -49,9 +72,9 @@ export class EofHeader {
     const numCodeSections = buf.readUint16BE(7)
     const codeSize = []
     for (let i = 0; i < numCodeSections * 2; i += 2) {
-      codeSize.push(buf.readUInt16BE(i + 10))
+      codeSize.push(buf.readUInt16BE(i + 9))
     }
-    const dataSize = buf.readUint16BE(10 + 2 * codeSize.length)
+    const dataSize = buf.readUint16BE(12 + 2 * codeSize.length)
     return new EofHeader(typeSize, codeSize, dataSize)
   }
   constructor(typeSize: number, codeSize: number[], dataSize: number) {
@@ -77,18 +100,29 @@ export class EofHeader {
   }
 
   buffer() {
-    const buf = Buffer.from(MAGIC)
-    buf.writeUint8(VERSION, buf.length)
-    buf.writeUint8(KIND_TYPE, buf.length)
-    buf.writeUint16BE(this.typeSize, buf.length)
-    buf.writeUint8(KIND_CODE, buf.length)
-    buf.writeUint16BE(this.numCodeSections, buf.length)
-    buf.writeUint8(KIND_DATA, buf.length)
+    const size = 13 + 2 * this.codeSize.length
+    const buf = Buffer.allocUnsafe(size)
+    Buffer.from(MAGIC).copy(buf)
+    let ptr = 2
+    buf.writeUint8(VERSION, ptr)
+    ptr++
+    buf.writeUint8(KIND_TYPE, ptr)
+    ptr++
+    buf.writeUint16BE(this.typeSize, ptr)
+    ptr += 2
+    buf.writeUint8(KIND_CODE, ptr)
+    ptr++
+    buf.writeUint16BE(this.numCodeSections, ptr)
+    ptr += 2
+    buf.writeUint8(KIND_DATA, ptr)
+    ptr++
     for (const codeSize of [...this.codeSize]) {
-      buf.writeUint16BE(codeSize, buf.length)
+      buf.writeUint16BE(codeSize, ptr)
+      ptr += 2
     }
-    buf.writeUint16BE(this.dataSize, buf.length)
-    buf.writeUint8(TERMINATOR, buf.length)
+    buf.writeUint16BE(this.dataSize, ptr)
+    ptr += 2
+    buf.writeUint8(TERMINATOR, ptr)
     return buf
   }
 }
@@ -98,9 +132,15 @@ export class TypeSection {
   outputs: number
   maxStackHeight: number
   static fromBuffer = (buf: Buffer) => {
+    if (buf.length < 4) {
+      throw new Error('Incomplete type section')
+    }
     const _inputs = buf.readUint8()
     const _outputs = buf.readUint8(1)
     const _msh = buf.readUint16BE(2)
+    if (_msh > 1023) {
+      throw new Error('max stack height is 1023')
+    }
     return new TypeSection(_inputs, _outputs, _msh)
   }
   constructor(inputs: number = 0, outputs: number = 0, maxStackHeight: number = 1023) {
@@ -125,7 +165,7 @@ class EOFBody {
     if (body.sections().length === 0) {
       throw new Error(`There MUST be at least one section`)
     }
-    if (body.typeSections.length !== body.codeSections.length + 1) {
+    if (body.typeSections.length !== body.codeSections.length) {
       throw new Error(`There must be a type header for each section`)
     }
     if (body.typeSections[0].inputs !== 0 || body.typeSections[0].outputs !== 0) {
@@ -134,8 +174,8 @@ class EOFBody {
     return body
   }
   static fromBytes = (header: EofHeader, _body: Buffer) => {
-    const typeSections = new Array(header.codeSize.length + 1).fill(0).map((v, i) => {
-      return TypeSection.fromBuffer(_body.subarray(i * 4, i * 4 + 4))
+    const typeSections = new Array(header.codeSize.length).fill(undefined).map((v, i) => {
+      return TypeSection.fromBuffer(_body.slice(i * 4, i * 4 + 4))
     })
     const codeSections = header.codeSize.map((v, i) => {
       const start =
@@ -191,12 +231,13 @@ export class EOFContainer {
   body: EOFBody
   static validate(container: EOFContainer): EOFContainer {
     const sizes = container.header.sectionSizes()
+    const bodySizes = container.body.sectionSizes()
+    console.log(sizes)
+    console.log(bodySizes)
     for (const [idx, size] of sizes.entries()) {
-      if (size !== container.body.sectionSizes()[idx]) {
+      if (size !== bodySizes[idx]) {
         throw new Error(
-          `Size: ${
-            container.body.sectionSizes()[idx]
-          } of section ${idx} does not match expected size: ${size}`
+          `Body section ${idx} should have size ${size}, but has size ${bodySizes[idx]}`
         )
       }
     }
