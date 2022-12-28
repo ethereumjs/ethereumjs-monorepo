f0f960ea04084938f676a36c529951c02029a1b5
ScottyPoi <scott.simpson@ethereum.org>
2022-12-27 17:43:35 -0700

    evm/eof: define class for EOF Header

--- packages/evm/src/eofTypes.ts
+++ packages/evm/src/eofTypes.ts
@@ -22,3 +22,32 @@ export class EOFSectionHeader {
     return buf
   }
 }
+export class EofHeader {
+  typeSize: number
+  numCodeSections: number
+  codeSize: number[]
+  dataSize: number
+  constructor(typeSize: number, codeSize: number[], dataSize: number) {
+    this.typeSize = typeSize
+    this.numCodeSections = codeSize.length
+    this.codeSize = codeSize
+    this.dataSize = dataSize
+  }
+
+  buffer() {
+    const buf = Buffer.from([])
+    buf.writeUint16BE(MAGIC)
+    buf.writeUint8(VERSION, buf.length)
+    buf.writeUint8(KIND_TYPE, buf.length)
+    buf.writeUint16BE(this.typeSize, buf.length)
+    buf.writeUint8(KIND_CODE, buf.length)
+    buf.writeUint16BE(this.numCodeSections, buf.length)
+    buf.writeUint8(KIND_DATA, buf.length)
+    for (const codeSize of [...this.codeSize]) {
+      buf.writeUint16BE(codeSize, buf.length)
+    }
+    buf.writeUint16BE(this.dataSize, buf.length)
+    buf.writeUint8(TERMINATOR, buf.length)
+    return buf
+  }
+}
