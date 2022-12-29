42a80c9ddddd7b914580f0671613372913d6ce21
ScottyPoi <scott.simpson@ethereum.org>
2022-12-29 15:00:56 -0700

    vm/tests/EIPs: update 3860 test to import from util file

--- packages/vm/test/api/EIPs/eip-3860.spec.ts
+++ packages/vm/test/api/EIPs/eip-3860.spec.ts
@@ -1,26 +1,11 @@
-import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
-import { Address, privateToAddress } from '@ethereumjs/util'
 import * as tape from 'tape'
 
-import { VM } from '../../../src/vm'
-const pkey = Buffer.from('20'.repeat(32), 'hex')
-const GWEI = BigInt('1000000000')
-const sender = new Address(privateToAddress(pkey))
+import { eip_util } from './eipUtils'
 
 tape('EIP 3860 tests', (t) => {
-  const common = new Common({
-    chain: Chain.Mainnet,
-    hardfork: Hardfork.London,
-    eips: [3860],
-  })
-
   t.test('EIP-3860 tests', async (st) => {
-    const vm = await VM.create({ common })
-    const account = await vm.stateManager.getAccount(sender)
-    const balance = GWEI * BigInt(21000) * BigInt(10000000)
-    account.balance = balance
-    await vm.stateManager.putAccount(sender, account)
+    const vm = await eip_util.setUpVM([3860])
 
     const buffer = Buffer.allocUnsafe(1000000).fill(0x60)
     const tx = FeeMarketEIP1559Transaction.fromTxData({
@@ -30,7 +15,7 @@ tape('EIP 3860 tests', (t) => {
       gasLimit: 100000000000,
       maxFeePerGas: 7,
       nonce: 0,
-    }).sign(pkey)
+    }).sign(eip_util.pkey)
     const result = await vm.runTx({ tx })
     st.ok(
       (result.execResult.exceptionError?.error as string) === 'initcode exceeds max initcode size',
