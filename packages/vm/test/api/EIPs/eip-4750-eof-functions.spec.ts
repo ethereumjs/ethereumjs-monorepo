9207fadc953c0e4fa73c1e77f88784153e8a6cd6
ScottyPoi <scott.simpson@ethereum.org>
2022-12-29 15:00:25 -0700

    vm/test/EIP: update 4750 test to import from util file

--- packages/vm/test/api/EIPs/eip-4750-eof-functions.spec.ts
+++ packages/vm/test/api/EIPs/eip-4750-eof-functions.spec.ts
@@ -1,103 +1,13 @@
-import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { ERROR } from '@ethereumjs/evm/dist/exceptions'
-import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
-import { Address, privateToAddress } from '@ethereumjs/util'
 import * as tape from 'tape'
 
-import { VM } from '../../../src/vm'
-const pkey = Buffer.from('20'.repeat(32), 'hex')
-const GWEI = BigInt('1000000000')
-const sender = new Address(privateToAddress(pkey))
+import { eip_util } from './eipUtils'
 
-async function runTx(vm: VM, data: string, nonce: number) {
-  const tx = FeeMarketEIP1559Transaction.fromTxData({
-    data,
-    gasLimit: 1000000,
-    maxFeePerGas: 7,
-    nonce,
-  }).sign(pkey)
-  const result = await vm.runTx({ tx })
-  const created = result.createdAddress
-  const code = created ? await vm.stateManager.getContractCode(created!) : undefined
-  return { result, code }
-}
-
-function getEOFCode(
-  code: string,
-  codeSizes: number[],
-  codeInputOutputs: [number, number, number?][]
-) {
-  if (codeInputOutputs.length !== codeSizes.length) {
-    throw new Error('each code should have input/output fields')
-  }
-  const typeSize = (codeSizes.length * 4).toString(16).padStart(4, '0')
-  const codeSize = codeSizes.length.toString(16).padStart(4, '0')
-  let str = '0xEF0001'
-  str += '01' + typeSize
-  str += '02' + codeSize
-  for (const codeSize of codeSizes) {
-    str += codeSize.toString(16).padStart(4, '0')
-  }
-  str += '030000' // Data section
-  str += '00'
-  for (const codeInputOutput of codeInputOutputs) {
-    const input = codeInputOutput[0]
-    const output = codeInputOutput[1]
-    str += input.toString(16).padStart(2, '0')
-    str += output.toString(16).padStart(2, '0')
-    if (codeInputOutput[2] !== undefined) {
-      str += codeInputOutput[2].toString(16).padStart(4, '0')
-    } else {
-      str += '0000' // max stack height
-    }
-  }
-  str += code
-  // no data section
-  return str
-}
-
-/**
- * Create valid EOF container
- * @param functions This is a list of functions. The first index is a hex string of the function code
- *                  The second index is the amount of inputs. The third index is the amount of outputs.
- *                  The last index (optional, defaults to 0) is the max stack height of this function
- * @returns
- */
-function createEOFCode(functions: [string, number, number, number?][]) {
-  let code = ''
-  const codeSizes = []
-  const codeInputOutputs: [number, number, number?][] = []
-  for (const func of functions) {
-    code += func[0]
-    codeSizes.push(func[0].length / 2)
-    codeInputOutputs.push([func[1], func[2], func[3]])
-  }
-  return getEOFCode(code, codeSizes, codeInputOutputs)
-}
-
-function callFunc(func: number) {
-  return 'B0' + func.toString(16).padStart(4, '0')
-}
-
-type eipTestCase = {
-  code: [string, number, number][]
-  expect: string
-  name: string
-}
+import type { eipTestCase } from './eipUtils'
 
 tape('EIP 4750 tests', (t) => {
-  const common = new Common({
-    chain: Chain.Mainnet,
-    hardfork: Hardfork.London,
-    eips: [3540, 5450, 3860, 5450, 4200, 4750, 3670],
-  })
-
   t.test('test CALLF errors', async (st) => {
-    const vm = await VM.create({ common })
-    const account = await vm.stateManager.getAccount(sender)
-    const balance = GWEI * BigInt(21000) * BigInt(10000000)
-    account.balance = balance
-    await vm.stateManager.putAccount(sender, account)
+    const vm = await eip_util.setUpVM([3540, 5450, 3860, 5450, 4200, 4750, 3670])
     let nonce = 0
 
     const cases: [string, number[], [number, number, number?][], string][] = [
@@ -105,8 +15,8 @@ tape('EIP 4750 tests', (t) => {
     ]
 
     for (const testCase of cases) {
-      const code = getEOFCode(testCase[0], testCase[1], testCase[2])
-      const { result } = await runTx(vm, code, nonce++)
+      const code = eip_util.getEOFCode(testCase[0], testCase[1], testCase[2])
+      const { result } = await eip_util.runTx(vm, code, nonce++)
       st.equal(
         result.execResult.exceptionError?.error,
         testCase[3],
@@ -116,11 +26,7 @@ tape('EIP 4750 tests', (t) => {
   })
 
   t.test('test CALLF/RETF execution', async (st) => {
-    const vm = await VM.create({ common })
-    const account = await vm.stateManager.getAccount(sender)
-    const balance = GWEI * BigInt(21000) * BigInt(10000000)
-    account.balance = balance
-    await vm.stateManager.putAccount(sender, account)
+    const vm = await eip_util.setUpVM([3540, 5450, 3860, 5450, 4200, 4750, 3670])
     let nonce = 0
 
     // ADD RETF
@@ -130,20 +36,28 @@ tape('EIP 4750 tests', (t) => {
 
     const cases: eipTestCase[] = [
       {
-        code: [[callFunc(2) + callFunc(2) + callFunc(1) + '60005500', 0, 0], codeAdd, output5],
+        code: [
+          [eip_util.callFunc(2) + eip_util.callFunc(2) + eip_util.callFunc(1) + '60005500', 0, 0],
+          codeAdd,
+          output5,
+        ],
         expect: '0a', // 10
         name: 'simple add test',
       },
       {
-        code: [[callFunc(1) + callFunc(1) + callFunc(2) + '60005500', 0, 0], output5, codeAdd],
+        code: [
+          [eip_util.callFunc(1) + eip_util.callFunc(1) + eip_util.callFunc(2) + '60005500', 0, 0],
+          output5,
+          codeAdd,
+        ],
         expect: '0a', // 10
         name: 'simple add test (functions now in different order)',
       },
     ]
 
     for (const testCase of cases) {
-      const code = createEOFCode(testCase.code)
-      const { result } = await runTx(vm, code, nonce++)
+      const code = eip_util.createEOFCode(testCase.code)
+      const { result } = await eip_util.runTx(vm, code, nonce++)
       const value = await vm.stateManager.getContractStorage(
         result.createdAddress!,
         Buffer.from('00'.repeat(32), 'hex')
