7d9b7819f682d409161e64f729efe85b43607020
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-01-02 14:26:37 +0100

    evm: add stack deltas

---
+++ packages/evm/src/eof/stackDelta.ts
@@ -0,0 +1,728 @@
+export const stackDelta: {
+  [key: number]: {
+    minStackHeight: number
+    inputs: number
+    outputs: number
+  }
+} = {
+  0x00: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+  0x01: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x02: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x03: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x04: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x05: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x06: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x07: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x08: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 1,
+  },
+  0x09: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 1,
+  },
+  0x0a: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x0b: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x10: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x11: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x12: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x13: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x14: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x15: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x16: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x17: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x18: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x19: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x1a: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x1b: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x1c: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x1d: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x20: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x30: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x31: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x32: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x33: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x34: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x35: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x36: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x37: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 0,
+  },
+  0x38: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x39: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 0,
+  },
+  0x3a: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x3b: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x3c: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x3d: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x3e: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 0,
+  },
+  0x3f: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x40: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x41: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x42: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x43: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x44: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x45: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x46: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x47: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x48: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x50: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x51: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x52: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x53: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x54: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 1,
+  },
+  0x55: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 1,
+  },
+  0x59: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x5a: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x5b: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+  0x5c: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+  0x5d: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 0,
+  },
+  0x5e: {
+    minStackHeight: 1,
+    inputs: 1,
+    outputs: 0,
+  },
+  0x5f: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x60: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x61: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x62: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x63: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x64: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x65: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x66: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x67: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x68: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x69: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6a: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6b: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6c: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6d: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6e: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x6f: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x70: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x71: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x72: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x73: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x74: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x75: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x76: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x77: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x78: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x79: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7a: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7b: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7c: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7d: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7e: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x7f: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x80: {
+    minStackHeight: 1,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x81: {
+    minStackHeight: 2,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x82: {
+    minStackHeight: 3,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x83: {
+    minStackHeight: 4,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x84: {
+    minStackHeight: 5,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x85: {
+    minStackHeight: 6,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x86: {
+    minStackHeight: 7,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x87: {
+    minStackHeight: 8,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x88: {
+    minStackHeight: 9,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x89: {
+    minStackHeight: 10,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8a: {
+    minStackHeight: 11,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8b: {
+    minStackHeight: 12,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8c: {
+    minStackHeight: 13,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8d: {
+    minStackHeight: 14,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8e: {
+    minStackHeight: 15,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x8f: {
+    minStackHeight: 16,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x90: {
+    minStackHeight: 2,
+    inputs: 0,
+    outputs: 0,
+  },
+  0x91: {
+    minStackHeight: 3,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x92: {
+    minStackHeight: 4,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x93: {
+    minStackHeight: 5,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x94: {
+    minStackHeight: 6,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x95: {
+    minStackHeight: 7,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x96: {
+    minStackHeight: 8,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x97: {
+    minStackHeight: 9,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x98: {
+    minStackHeight: 10,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x99: {
+    minStackHeight: 11,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9a: {
+    minStackHeight: 12,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9b: {
+    minStackHeight: 13,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9c: {
+    minStackHeight: 14,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9d: {
+    minStackHeight: 15,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9e: {
+    minStackHeight: 16,
+    inputs: 0,
+    outputs: 1,
+  },
+  0x9f: {
+    minStackHeight: 17,
+    inputs: 0,
+    outputs: 1,
+  },
+  0xa0: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 0,
+  },
+  0xa1: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 0,
+  },
+  0xa2: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 0,
+  },
+  0xa3: {
+    minStackHeight: 4,
+    inputs: 4,
+    outputs: 0,
+  },
+  0xa4: {
+    minStackHeight: 5,
+    inputs: 5,
+    outputs: 0,
+  },
+  0xb0: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+  0xb1: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+  0xf0: {
+    minStackHeight: 3,
+    inputs: 3,
+    outputs: 1,
+  },
+  0xf1: {
+    minStackHeight: 7,
+    inputs: 7,
+    outputs: 1,
+  },
+  0xf3: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 0,
+  },
+  0xf4: {
+    minStackHeight: 6,
+    inputs: 6,
+    outputs: 1,
+  },
+  0xf5: {
+    minStackHeight: 4,
+    inputs: 4,
+    outputs: 1,
+  },
+  0xfa: {
+    minStackHeight: 6,
+    inputs: 6,
+    outputs: 1,
+  },
+  0xfd: {
+    minStackHeight: 2,
+    inputs: 2,
+    outputs: 0,
+  },
+  0xfe: {
+    minStackHeight: 0,
+    inputs: 0,
+    outputs: 0,
+  },
+}
