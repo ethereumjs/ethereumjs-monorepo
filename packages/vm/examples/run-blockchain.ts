ea44a450efbd9777cb3103d73b25abf98cf01240
harkamal <develop@g11tech.io>
2023-01-16 15:37:25 +0530

    skip validation in vm examples run

--- packages/vm/examples/run-blockchain.ts
+++ packages/vm/examples/run-blockchain.ts
@@ -37,7 +37,7 @@ async function main() {
     const parentBlock = await blockchain!.getBlock(block.header.parentHash)
     const parentState = parentBlock.header.stateRoot
     // run block
-    await vm.runBlock({ block, root: parentState })
+    await vm.runBlock({ block, root: parentState, skipHardForkValidation: true })
   })
 
   const blockchainHead = await vm.blockchain.getIteratorHead!()
