963abd9d33f5e5e2ad470c70fa5c7fcb79c31413
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-01-17 11:23:52 -0500

    Move BLS_MODULUS back to precompile

--- packages/evm/src/precompiles/14-kzg-point-evaluation.ts
+++ packages/evm/src/precompiles/14-kzg-point-evaluation.ts
@@ -9,10 +9,13 @@ import type { PrecompileInput } from './types'
 
 const { verifyKzgProof } = kzg
 
+const BLS_MODULUS = BigInt(
+  '52435875175126190479447740508185965837690552500527637822603658699938581184513'
+)
+
 export async function precompile14(opts: PrecompileInput): Promise<ExecResult> {
   const gasUsed = opts._common.param('gasPrices', 'kzgPointEvaluationGasPrecompilePrice')
   const version = Number(opts._common.paramByEIP('sharding', 'blobCommitmentVersionKzg', 4844))
-  const blsModulus = opts._common.paramByEIP('sharding', 'blsModulus', 4844)!
   const fieldElementsPerBlob = opts._common.paramByEIP('sharding', 'fieldElementsPerBlob', 4844)!
   const versionedHash = opts.data.slice(0, 32)
   const z = opts.data.slice(32, 64)
@@ -20,7 +23,7 @@ export async function precompile14(opts: PrecompileInput): Promise<ExecResult> {
   const commitment = opts.data.slice(96, 144)
   const kzgProof = opts.data.slice(144, 192)
 
-  if (bufferToBigInt(z) >= blsModulus || bufferToBigInt(y) >= blsModulus) {
+  if (bufferToBigInt(z) >= BLS_MODULUS || bufferToBigInt(y) >= BLS_MODULUS) {
     return EvmErrorResult(new EvmError(ERROR.POINT_GREATER_THAN_BLS_MODULUS), opts.gasLimit)
   }
 
@@ -35,7 +38,7 @@ export async function precompile14(opts: PrecompileInput): Promise<ExecResult> {
 
   // Return value - FIELD_ELEMENTS_PER_BLOB and BLS_MODULUS as padded 32 byte big endian values
   const fieldElementsBuffer = setLengthLeft(bigIntToBuffer(fieldElementsPerBlob), 32)
-  const modulusBuffer = setLengthLeft(bigIntToBuffer(blsModulus), 32)
+  const modulusBuffer = setLengthLeft(bigIntToBuffer(BLS_MODULUS), 32)
   return {
     executionGasUsed: gasUsed,
     returnValue: Buffer.concat([fieldElementsBuffer, modulusBuffer]),
