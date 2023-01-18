0836d5696132f860745a352a3eb763888447cc4e
harkamal <develop@g11tech.io>
2023-01-18 21:47:43 +0530

    better naming

--- packages/util/src/ssz.ts
+++ packages/util/src/ssz.ts
@@ -9,15 +9,15 @@ import {
 import { MAX_WITHDRAWALS_PER_PAYLOAD } from './constants'
 
 export const UintNum64 = new UintNumberType(8)
-export const UintBn64 = new UintBigintType(8)
+export const UintBigInt64 = new UintBigintType(8)
 export const Bytes20 = new ByteVectorType(20)
 
 export const Withdrawal = new ContainerType(
   {
-    index: UintBn64,
-    validatorIndex: UintBn64,
+    index: UintBigInt64,
+    validatorIndex: UintBigInt64,
     address: Bytes20,
-    amount: UintBn64,
+    amount: UintBigInt64,
   },
   { typeName: 'Withdrawal', jsonCase: 'eth2' }
 )
