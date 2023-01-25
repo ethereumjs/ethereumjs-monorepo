f946f027e30240ab7f45a3ae5eefd3d21527bc08
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-01-25 14:16:52 +0100

    blockchain: ensure genesis with timestamp hardforks can be imported correctly

--- packages/blockchain/src/blockchain.ts
+++ packages/blockchain/src/blockchain.ts
@@ -1248,7 +1248,7 @@ export class Blockchain implements BlockchainInterface {
    */
   createGenesisBlock(stateRoot: Buffer): Block {
     const common = this._common.copy()
-    common.setHardforkByBlockNumber(0)
+    common.setHardforkByBlockNumber(0, undefined, common.genesis().timestamp)
 
     const header: BlockData['header'] = {
       ...common.genesis(),
