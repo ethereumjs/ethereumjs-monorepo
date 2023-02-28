073d3d0e941057d66cef8a8300b1a9ce92c808a1
Amir <indigophi@protonmail.com>
2023-02-28 18:44:14 +0000

    Add tests for requests and proof verification

--- packages/client/test/sync/fetcher/storagefetcher.spec.ts
+++ packages/client/test/sync/fetcher/storagefetcher.spec.ts
@@ -1,8 +1,15 @@
+import { RLP } from '@ethereumjs/rlp'
+
 import * as tape from 'tape'
 import * as td from 'testdouble'
 
+import { Chain } from '../../../lib/blockchain'
 import { Config } from '../../../lib/config'
+import { SnapProtocol } from '../../../lib/net/protocol'
 import { wait } from '../../integration/util'
+
+const _storageRangesRLP =
+  'f83e0bf83af838f7a0290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e5639594053cd080a26cb03d5e6d2956cebb31c56e7660cac0'
 
 tape('[StorageFetcher]', async (t) => {
   class PeerPool {
@@ -148,6 +155,127 @@ tape('[StorageFetcher]', async (t) => {
     remainingStorageData.completed = true
     results = fetcher.process(job as any, remainingStorageData)
     t.equal((results as any)[0].length, 5, 'Should return full results')
+    t.end()
+  })
+
+  t.test('should request correctly', async (t) => {
+    const config = new Config({ transports: [] })
+    const chain = new Chain({ config })
+    const pool = new PeerPool() as any
+    const p = new SnapProtocol({ config, chain })
+    const fetcher = new StorageFetcher({
+      config,
+      pool,
+      root: Buffer.from(''),
+    })
+    const partialResult: any = [
+      [
+        [{ hash: Buffer.from(''), body: Buffer.from('') }],
+        [{ hash: Buffer.from(''), body: Buffer.from('') }],
+      ],
+    ]
+
+    const task = {
+      storageRequests: [
+        {
+          accountHash: Buffer.from(
+            'e9a5016cb1a53dbc750d06e725514ac164231d71853cafdcbff42f5adb6ca6f1',
+            'hex'
+          ),
+          storageRoot: Buffer.from(
+            '69522138e4770e642ec8d7bd5e2b71a23fb732bb447cd4faf838b45cfe3b2a92',
+            'hex'
+          ),
+          first: BigInt(0),
+          count: BigInt(2) ** BigInt(256) - BigInt(1),
+        },
+      ],
+    }
+    const resData = RLP.decode(Buffer.from(_storageRangesRLP, 'hex')) as unknown
+    const res = p.decode(
+      p.messages.filter((message) => message.name === 'StorageRanges')[0],
+      resData
+    )
+    const { reqId, slots, proof } = res
+    const mockedGetStorageRanges = td.func<any>()
+    td.when(mockedGetStorageRanges(td.matchers.anything())).thenReturn({
+      reqId,
+      slots,
+      proof,
+    })
+    const peer = {
+      snap: { getStorageRanges: mockedGetStorageRanges },
+      id: 'random',
+      address: 'random',
+    }
+    const job = { peer, partialResult, task }
+    await fetcher.request(job as any)
+    td.verify(
+      job.peer.snap.getStorageRanges({
+        root: Buffer.from(''),
+        accounts: [
+          Buffer.from('e9a5016cb1a53dbc750d06e725514ac164231d71853cafdcbff42f5adb6ca6f1', 'hex'),
+        ],
+        origin: td.matchers.anything(),
+        limit: td.matchers.anything(),
+        bytes: BigInt(50000),
+      })
+    )
+    t.end()
+  })
+
+  t.test('should verify proof correctly', async (t) => {
+    const config = new Config({ transports: [] })
+    const chain = new Chain({ config })
+    const pool = new PeerPool() as any
+    const p = new SnapProtocol({ config, chain })
+    const fetcher = new StorageFetcher({
+      config,
+      pool,
+      root: Buffer.from(''),
+    })
+    const partialResult: any = [
+      [
+        [{ hash: Buffer.from(''), body: Buffer.from('') }],
+        [{ hash: Buffer.from(''), body: Buffer.from('') }],
+      ],
+    ]
+
+    const task = {
+      storageRequests: [
+        {
+          accountHash: Buffer.from(
+            '00009e5969eba9656d7e4dad5b0596241deb87c29bbab71c23b602c2b88a7276',
+            'hex'
+          ),
+          storageRoot: Buffer.from(
+            '4431bd7d69241190bb930b74485c1e31ff75552f67d758d0b6612e7bd9226121',
+            'hex'
+          ),
+          first: BigInt(0),
+          count: BigInt(2) ** BigInt(256) - BigInt(1),
+        },
+      ],
+    }
+    const resData = RLP.decode(Buffer.from(_storageRangesRLP, 'hex')) as unknown
+    const res = p.decode(
+      p.messages.filter((message) => message.name === 'StorageRanges')[0],
+      resData
+    )
+    const { reqId, slots, proof } = res
+    const mockedGetStorageRanges = td.func<any>()
+    td.when(mockedGetStorageRanges(td.matchers.anything())).thenReturn({
+      reqId,
+      slots,
+      proof,
+    })
+    const peer = {
+      snap: { getStorageRanges: mockedGetStorageRanges },
+      id: 'random',
+      address: 'random',
+    }
+    const job = { peer, partialResult, task }
+    t.ok(await fetcher.request(job as any), 'Proof verification is completed without errors')
     t.end()
   })
 
