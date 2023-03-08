1d65ea035c1cc61abb0ee24e7a5d4ffc7e1b2c8d
Holger Drewes <Holger.Drewes@gmail.com>
2023-03-08 15:38:13 +0100

    EVM: Precompile Debug Logger Improvements (#2572)
    * EVM: Added general precompile debug logger setup, first ECRECOVER exemplary debug logging
    
    * EVM: Added remaining precompile debug loggers
    
    * EVM: added error cases to BLS precompile debug log
    
    * EVM: Added missing precompile return value debug logs
    
    * Small fixes

--- packages/evm/src/precompiles/02-sha256.ts
+++ packages/evm/src/precompiles/02-sha256.ts
@@ -1,4 +1,4 @@
-import { toBuffer } from '@ethereumjs/util'
+import { short, toBuffer } from '@ethereumjs/util'
 import { sha256 } from 'ethereum-cryptography/sha256'
 
 import { OOGResult } from '../evm'
@@ -12,12 +12,28 @@ export function precompile02(opts: PrecompileInput): ExecResult {
   let gasUsed = opts._common.param('gasPrices', 'sha256')
   gasUsed += opts._common.param('gasPrices', 'sha256Word') * BigInt(Math.ceil(data.length / 32))
 
+  if (opts._debug) {
+    opts._debug(
+      `Run KECCAK256 (0x02) precompile data=${short(opts.data)} length=${
+        opts.data.length
+      } gasLimit=${opts.gasLimit} gasUsed=${gasUsed}`
+    )
+  }
+
   if (opts.gasLimit < gasUsed) {
+    if (opts._debug) {
+      opts._debug(`KECCAK256 (0x02) failed: OOG`)
+    }
     return OOGResult(opts.gasLimit)
+  }
+
+  const hash = toBuffer(sha256(data))
+  if (opts._debug) {
+    opts._debug(`KECCAK256 (0x02) return hash=${hash.toString('hex')}`)
   }
 
   return {
     executionGasUsed: gasUsed,
-    returnValue: toBuffer(sha256(data)),
+    returnValue: hash,
   }
 }
--- packages/evm/src/precompiles/04-identity.ts
+++ packages/evm/src/precompiles/04-identity.ts
@@ -1,3 +1,5 @@
+import { short } from '@ethereumjs/util'
+
 import { OOGResult } from '../evm'
 
 import type { ExecResult } from '../evm'
@@ -8,9 +10,23 @@ export function precompile04(opts: PrecompileInput): ExecResult {
 
   let gasUsed = opts._common.param('gasPrices', 'identity')
   gasUsed += opts._common.param('gasPrices', 'identityWord') * BigInt(Math.ceil(data.length / 32))
+  if (opts._debug) {
+    opts._debug(
+      `Run IDENTITY (0x04) precompile data=${short(opts.data)} length=${
+        opts.data.length
+      } gasLimit=${opts.gasLimit} gasUsed=${gasUsed}`
+    )
+  }
 
   if (opts.gasLimit < gasUsed) {
+    if (opts._debug) {
+      opts._debug(`IDENTITY (0x04) failed: OOG`)
+    }
     return OOGResult(opts.gasLimit)
+  }
+
+  if (opts._debug) {
+    opts._debug(`IDENTITY (0x04) return data=${short(opts.data)}`)
   }
 
   return {
