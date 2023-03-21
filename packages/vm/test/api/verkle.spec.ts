c7b137e4b3fb5d90c18b263d32fe3c7138ea566a
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-03-20 23:23:36 -0400

    vm: update verkle test data

--- packages/vm/test/api/verkle.spec.ts
+++ packages/vm/test/api/verkle.spec.ts
@@ -1,13 +1,12 @@
 import { Block } from '@ethereumjs/block'
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { StatelessVerkleStateManager } from '@ethereumjs/statemanager'
-import { Address } from '@ethereumjs/util'
 import * as tape from 'tape'
 
 import { VM } from '../../src/vm'
 
 import * as testnetVerkle from './testdata/testnetVerkleBeverlyHills.json'
-import * as verkleBlockJSON from './testdata/verkleBeverlyHillsBlock93261.json'
+import * as verkleBlockJSON from './testdata/verkleBeverlyHillsBlock83.json'
 import * as simpleVerkleBlockJSON from './testdata/verkleSampleBlock.json'
 
 tape('Verkle-enabled VM', async (t) => {
@@ -45,9 +44,6 @@ tape('Verkle-enabled VM', async (t) => {
     await vm.runBlock({
       block,
       skipHeaderValidation: true,
-      skipBlockValidation: true,
-      skipBalance: true,
-      skipNonce: true,
     })
 
     st.pass('Should run verkle block successfully')
