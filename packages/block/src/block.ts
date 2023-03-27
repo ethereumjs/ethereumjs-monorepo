5e71d3c3e1ca275b1146e4bff7fa5f9b6eb8ba61
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-03-27 17:40:22 +0200

    block: move eip4895 check to fromValues

--- packages/block/src/block.ts
+++ packages/block/src/block.ts
@@ -140,16 +140,6 @@ export class Block {
       throw new Error('Invalid serialized block input. Must be array')
     }
 
-    if (
-      opts?.common !== undefined &&
-      opts?.common?.isActivatedEIP(4895) &&
-      (values[3] === undefined || !Array.isArray(values[3]))
-    ) {
-      throw new Error(
-        'Invalid serialized block input: EIP-4895 is active, and no withdrawals were provided as array'
-      )
-    }
-
     return Block.fromValuesArray(values, opts)
   }
 
@@ -163,6 +153,16 @@ export class Block {
     if (values.length > 4) {
       throw new Error('invalid block. More values than expected were received')
     }
+    if (
+      opts?.common !== undefined &&
+      opts?.common?.isActivatedEIP(4895) &&
+      (values[3] === undefined || !Array.isArray(values[3]))
+    ) {
+      throw new Error(
+        'Invalid serialized block input: EIP-4895 is active, and no withdrawals were provided as array'
+      )
+    }
+
     const [headerData, txsData, uhsData, withdrawalsBuffer] = values
 
     const header = BlockHeader.fromValuesArray(headerData, opts)
