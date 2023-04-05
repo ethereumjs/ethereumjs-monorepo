60995fb50aa56e5749d471eb7fa424403c3e6249
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-04-05 18:54:13 +0200

    Tx: correctly decode rpc txs (#2613)
    * tx: correctly decode rpc txs
    
    * tx/vm: fix test
    
    * tx: add fromRPC method
    
    * Reuse `fromRpcTx` in fromEthersProvider
    
    ---------
    
    Co-authored-by: acolytec3 <17355484+acolytec3@users.noreply.github.com>

--- packages/tx/test/fromRpc.spec.ts
+++ packages/tx/test/fromRpc.spec.ts
@@ -6,6 +6,10 @@ import { normalizeTxParams } from '../src/fromRpc'
 
 import { MockProvider } from './mockProvider'
 
+const optimismTx = require('./json/optimismTx.json')
+
+const txTypes = [0, 1, 2]
+
 tape('[fromEthersProvider]', async (t) => {
   const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.London })
   const provider = new MockProvider()
@@ -30,4 +34,15 @@ tape('[normalizeTxParams]', (t) => {
     'converted normed tx data to transaction objec'
   )
   t.end()
+})
+
+tape('fromRPCTx: ensure v/r/s values of 0x0 are interpreted as undefined', async function (st) {
+  for (const txType of txTypes) {
+    optimismTx.type = txType
+    const tx = await TransactionFactory.fromRPCTx(optimismTx)
+    st.ok(tx.v === undefined)
+    st.ok(tx.s === undefined)
+    st.ok(tx.r === undefined)
+  }
+  st.end()
 })
