e8cf85bb8207a273d0ecdb0bf132d22b9b5304bc
Frederik Bolding <frederik.bolding@gmail.com>
2023-04-08 09:45:34 +0200

    Bump `@chainsafe/ssz` to `0.11.0` (#2622)
    * Bump chainsafe/ssz to 0.11.0
    
    * Try fixing karma
    
    * Add karma aliases
    
    * Add `common` configuration
    
    ---------
    
    Co-authored-by: acolytec3 <17355484+acolytec3@users.noreply.github.com>

--- packages/vm/karma.conf.js
+++ packages/vm/karma.conf.js
@@ -29,6 +29,10 @@ module.exports = function (config) {
         resolve: {
           alias: {
             'bigint-crypto-utils': '../../node_modules/bigint-crypto-utils/dist/bundles/umd.js',
+            '@chainsafe/persistent-merkle-tree/hasher':
+              '../../node_modules/@chainsafe/persistent-merkle-tree/lib/hasher/noble.js',
+            '@chainsafe/as-sha256/hashObject':
+              '../../node_modules/@chainsafe/as-sha256/lib/hashObject.js',
           },
         },
       },
