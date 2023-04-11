e02451560d67aa26afd32934a090d377c4d1951f
harkamal <gajinder@g11.in>
2023-04-11 22:37:45 +0530

    increase coverage

--- packages/client/test/sync/fetcher/bytecodefetcher.spec.ts
+++ packages/client/test/sync/fetcher/bytecodefetcher.spec.ts
@@ -52,6 +52,59 @@ tape('[ByteCodeFetcher]', async (t) => {
     t.end()
   })
 
+  t.test('should process', (t) => {
+    const config = new Config({ transports: [] })
+    const pool = new PeerPool() as any
+    const fetcher = new ByteCodeFetcher({
+      config,
+      pool,
+      trie: new Trie({ useKeyHashing: false }),
+      hashes: [],
+    })
+
+    const fullResult: any = [Buffer.from('')]
+
+    const ByteCodeResponse: any = [Buffer.from('')]
+    ByteCodeResponse.completed = true
+    const task = {
+      hashes: [Buffer.from('')],
+    }
+    ;(fetcher as any).running = true
+    fetcher.enqueueTask(task)
+    const job = (fetcher as any).in.peek()
+    t.deepEquals((fetcher.process(job, ByteCodeResponse) as any)[0], fullResult[0], 'got results')
+    t.notOk(fetcher.process({} as any, { ByteCodeResponse: [] } as any), 'bad results')
+    t.end()
+  })
+
+  t.test('should adopt correctly', (t) => {
+    const config = new Config({ transports: [] })
+    const pool = new PeerPool() as any
+    const fetcher = new ByteCodeFetcher({
+      config,
+      pool,
+      trie: new Trie({ useKeyHashing: false }),
+      hashes: [],
+    })
+    const ByteCodeResponse: any = [Buffer.from(''), Buffer.from('')]
+    ByteCodeResponse.completed = false
+    const task = {
+      hashes: [Buffer.from('')],
+    }
+    ;(fetcher as any).running = true
+    fetcher.enqueueTask(task)
+    const job = (fetcher as any).in.peek()
+    let results = fetcher.process(job as any, ByteCodeResponse)
+    t.equal((fetcher as any).in.length, 1, 'Fetcher should still have same job')
+    t.equal(job?.partialResult.length, 2, 'Should have two partial results')
+    t.equal(results, undefined, 'Process should not return full results yet')
+    const remainingBytesCodeData: any = [Buffer.from(''), Buffer.from(''), Buffer.from('')]
+    remainingBytesCodeData.completed = true
+    results = fetcher.process(job as any, remainingBytesCodeData)
+    t.equal((results as any).length, 5, 'Should return full results')
+    t.end()
+  })
+
   t.test('should find a fetchable peer', async (t) => {
     const config = new Config({ transports: [] })
     const pool = new PeerPool() as any
