b6c46523253ee8baa863dee39594938282111667
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-04-14 11:47:16 -0400

    Change noBuffer to be any use of reference Buffer

--- eslint/noBuffer.js
+++ eslint/noBuffer.js
@@ -5,16 +5,10 @@ module.exports = {
     return {
       Identifier: function (node) {
         if (node.name === 'Buffer') {
-          if (
-            (node.parent.property.type === 'Identifier' && node.parent.property.name === 'from') ||
-            (node.parent.property.type === 'Identifier' &&
-              node.parent.property.name.includes('alloc'))
-          ) {
-            context.report({
-              node: node.parent,
-              message: 'No Buffer constructors - use Uint8Array instead',
-            })
-          }
+          context.report({
+            node: node.parent,
+            message: 'No Buffers - use Uint8Array instead',
+          })
         }
       },
     }
