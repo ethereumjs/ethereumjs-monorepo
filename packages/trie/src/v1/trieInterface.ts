9d7722893f58f9bc5a88077a32cee4d1ebee9f06
ScottyPoi <scott.simpson@ethereum.org>
2023-04-14 16:14:04 -0600

    create interface for prev Trie version

--- packages/trie/src/v1/trieInterface.ts
+++ packages/trie/src/v1/trieInterface.ts
@@ -57,3 +57,31 @@ export interface IexternalTrieV<TVersion extends TrieVersion> {
   ): Promise<IexternalTrieV<TVersion>>
   createReadStream(): Promise<ReadStream>
 }
+
+export interface IprevTrie extends IexternalTrieV<0> {
+  create(opts: CreateTrieOptsV<0>): Promise<IprevTrie>
+  database(db?: DBv<0>): Promise<DBv<0>>
+  findPath(key: Uint8Array): Promise<PathV<0>>
+  walkTrie(root: Uint8Array, onFound: FoundNodeFunctionV<0>): Promise<void>
+  _createInitialNode(key: Uint8Array, value: Uint8Array): Promise<void>
+  lookupNode(node: Uint8Array | Uint8Array[]): Promise<TrieNodeV<0> | null>
+  _updateNode(
+    k: Uint8Array,
+    value: Uint8Array,
+    keyRemainder: NibblesV<0>,
+    stack: TrieNodeV<0>[]
+  ): Promise<void>
+  _deleteNode(k: Uint8Array, stack: TrieNodeV<0>[]): Promise<void>
+  _saveStack(key: NibblesV<0>, stack: TrieNodeV<0>[], opStack: BatchDBOpV<0>[]): Promise<void>
+  _formatNode(
+    node: TrieNodeV<0>,
+    topLevel: boolean,
+    opStack: BatchDBOpV<0>[],
+    remove?: boolean
+  ): Uint8Array | (EmbeddedNodeV<0> | null)[]
+  verifyPrunedIntegrity(): Promise<boolean>
+  _findDbNodes(onFound: FoundNodeFunctionV<0>): Promise<void>
+  appliedKey(key: Uint8Array): Uint8Array
+  hash(msg: Uint8Array): Uint8Array
+  hasCheckpoints(): boolean
+}
