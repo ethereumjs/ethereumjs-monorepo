aa0ff6b578eeb4ebeff7d4e94d4a259d7176b55c
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-04-15 09:37:17 -0400

    vm: kaustinene verkle test

--- packages/vm/test/api/verkle.spec.ts
+++ packages/vm/test/api/verkle.spec.ts
@@ -5,8 +5,10 @@ import * as tape from 'tape'
 
 import { VM } from '../../src/vm'
 
-import * as testnetVerkle from './testdata/testnetVerkleBeverlyHills.json'
-import * as verkleBlockJSON from './testdata/verkleBeverlyHillsBlock83.json'
+import * as testnetVerkleBeverlyHills from './testdata/testnetVerkleBeverlyHills.json'
+import * as testnetVerkleKaustinen from './testdata/testnetVerkleKaustinen.json'
+import * as verkleBlockBeverlyHillsJSON from './testdata/verkleBeverlyHillsBlock83.json'
+import * as verkleBlockKaustinenJSON from './testdata/verkleKaustinenBlock.json'
 import * as simpleVerkleBlockJSON from './testdata/verkleSampleBlock.json'
 
 tape('Verkle-enabled VM', async (t) => {
@@ -28,14 +30,37 @@ tape('Verkle-enabled VM', async (t) => {
     st.end()
   })
 
-  t.test('should run a verkle block', async (st) => {
-    const common = Common.fromGethGenesis(testnetVerkle, {
+  t.test('should run a verkle block (beverly hills)', async (st) => {
+    const common = Common.fromGethGenesis(testnetVerkleBeverlyHills, {
       chain: 'customChain',
       hardfork: Hardfork.Merge,
       eips: [999001],
     })
 
-    const block = Block.fromBlockData(verkleBlockJSON as any, { common })
+    const block = Block.fromBlockData(verkleBlockBeverlyHillsJSON as any, { common })
+    const stateManager = new StatelessVerkleStateManager()
+    stateManager.initPreState(block.header.verkleProof!, block.header.verklePreState!)
+
+    const vm = await VM.create({ common, stateManager })
+
+    await vm.runBlock({
+      block,
+      skipHeaderValidation: true,
+    })
+
+    st.pass('Should run verkle block successfully')
+
+    st.end()
+  })
+
+  t.test('should run a verkle block (Kaustinen)', async (st) => {
+    const common = Common.fromGethGenesis(testnetVerkleKaustinen, {
+      chain: 'customChain',
+      hardfork: Hardfork.Merge,
+      eips: [999001],
+    })
+
+    const block = Block.fromBlockData(verkleBlockKaustinenJSON as any, { common })
     const stateManager = new StatelessVerkleStateManager()
     stateManager.initPreState(block.header.verkleProof!, block.header.verklePreState!)
 
