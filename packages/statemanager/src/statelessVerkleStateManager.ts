5fbc80529e6cda4fda61e6c8b3e5a24f42229c98
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-04-16 20:07:22 -0400

    stateManager: implement statelessVerkleStateManager

--- packages/statemanager/src/statelessVerkleStateManager.ts
+++ packages/statemanager/src/statelessVerkleStateManager.ts
@@ -3,20 +3,24 @@
 import {
   Account,
   bytesToHex,
+  readBigInt64LE,
   readInt32LE,
   setLengthLeft,
   setLengthRight,
   toBytes,
+  writeBigInt64LE,
+  writeInt32LE,
   zeros,
 } from '@ethereumjs/util'
-import { hexToBytes } from 'ethereum-cryptography/utils'
+import { concatBytes, hexToBytes } from 'ethereum-cryptography/utils'
 
-import { Cache, get } from './cache/cache'
+import { AccountCache, CacheType, StorageCache } from './cache'
+import { Cache } from './cache/cache'
 
 import type { StateManager } from '.'
-import type { put } from './cache/cache'
-import type { StorageDump } from './interface'
+import type { AccountFields, StorageDump } from './interface'
 import type { Address, PrefixedHexString } from '@ethereumjs/util'
+import type { Debugger } from 'debug'
 
 const wasm = require('../../rust-verkle-wasm/rust_verkle_wasm')
 
@@ -24,10 +28,56 @@ export interface VerkleState {
   [key: PrefixedHexString]: PrefixedHexString
 }
 
+type CacheOptions = {
+  /**
+   * Allows for cache deactivation
+   *
+   * Depending on the use case and underlying datastore (and eventual concurrent cache
+   * mechanisms there), usage with or without cache can be faster
+   *
+   * Default: false
+   */
+  deactivate?: boolean
+
+  /**
+   * Cache type to use.
+   *
+   * Available options:
+   *
+   * ORDERED_MAP: Cache with no fixed upper bound and dynamic allocation,
+   * use for dynamic setups like testing or similar.
+   *
+   * LRU: LRU cache with pre-allocation of memory and a fixed size.
+   * Use for larger and more persistent caches.
+   */
+  type?: CacheType
+
+  /**
+   * Size of the cache (only for LRU cache)
+   *
+   * Default: 100000 (account cache) / 20000 (storage cache)
+   *
+   * Note: the cache/trie interplay mechanism is designed in a way that
+   * the theoretical number of max modified accounts between two flush operations
+   * should be smaller than the cache size, otherwise the cache will "forget" the
+   * old modifications resulting in an incomplete set of trie-flushed accounts.
+   */
+  size?: number
+}
+
+type CacheSettings = {
+  deactivate: boolean
+  type: CacheType
+  size: number
+}
+
 /**
  * Options dictionary.
  */
-export interface StatelessVerkleStateManagerOpts {}
+export interface StatelessVerkleStateManagerOpts {
+  accountCacheOpts?: CacheOptions
+  storageCacheOpts?: CacheOptions
+}
 
 /**
  * Tree key constants.
@@ -48,6 +98,22 @@ const PUSH1 = PUSH_OFFSET + 1
 const PUSH32 = PUSH_OFFSET + 32
 
 export class StatelessVerkleStateManager implements StateManager {
+  _accountCache?: AccountCache
+  _storageCache?: StorageCache
+  _codeCache: { [key: string]: Uint8Array }
+  protected readonly _accountCacheSettings: CacheSettings
+  protected readonly _storageCacheSettings: CacheSettings
+
+  /**
+   * StateManager is run in DEBUG mode (default: false)
+   * Taken from DEBUG environment variable
+   *
+   * Safeguards on debug() calls are added for
+   * performance reasons to avoid string literal evaluation
+   * @hidden
+   */
+  protected readonly DEBUG: boolean = false
+
   private _proof: PrefixedHexString = '0x'
 
   // State along execution (should update)
@@ -60,7 +126,33 @@ export class StatelessVerkleStateManager implements StateManager {
    * Instantiate the StateManager interface.
    */
   constructor(opts: StatelessVerkleStateManagerOpts = {}) {
-    super(opts)
+    this._accountCacheSettings = {
+      deactivate: opts.accountCacheOpts?.deactivate ?? false,
+      type: opts.accountCacheOpts?.type ?? CacheType.ORDERED_MAP,
+      size: opts.accountCacheOpts?.size ?? 100000,
+    }
+
+    if (!this._accountCacheSettings.deactivate) {
+      this._accountCache = new AccountCache({
+        size: this._accountCacheSettings.size,
+        type: this._accountCacheSettings.type,
+      })
+    }
+
+    this._storageCacheSettings = {
+      deactivate: opts.storageCacheOpts?.deactivate ?? false,
+      type: opts.storageCacheOpts?.type ?? CacheType.ORDERED_MAP,
+      size: opts.storageCacheOpts?.size ?? 20000,
+    }
+
+    if (!this._storageCacheSettings.deactivate) {
+      this._storageCache = new StorageCache({
+        size: this._storageCacheSettings.size,
+        type: this._storageCacheSettings.type,
+      })
+    }
+
+    this._codeCache = {}
 
     /*
      * For a custom StateManager implementation adopt these
@@ -68,12 +160,12 @@ export class StatelessVerkleStateManager implements StateManager {
      * the `get`, `put` and `delete` operations with the
      * desired backend.
      */
-    const getCb: get = async (address) => {
-      return undefined
-    }
-    const putCb: put = async (keyBuf, accountRlp) => {}
-    const deleteCb = async (keyBuf: Buffer) => {}
-    this._cache = new Cache({ get, putCb, deleteCb })
+    // const getCb: get = async (address) => {
+    //   return undefined
+    // }
+    // const putCb: put = async (keyBuf, accountRlp) => {}
+    // const deleteCb = async (keyBuf: Uint8Array) => {}
+    // this._cache = new Cache({ get, putCb, deleteCb })
   }
 
   public initPreState(proof: PrefixedHexString, preState: VerkleState) {
@@ -82,25 +174,24 @@ export class StatelessVerkleStateManager implements StateManager {
     this._state = preState
   }
 
-  private pedersenHash(input: Buffer) {
+  private pedersenHash(input: Uint8Array): Uint8Array {
     const pedersenHash = wasm.pedersen_hash(input)
 
     if (pedersenHash === null) {
       throw new Error('Wrong pedersenHash input. This might happen if length is not correct.')
     }
 
-    return arrToBufArr(pedersenHash)
+    return pedersenHash
   }
 
-  private getTreeKey(address: Address, treeIndex: number, subIndex: number): Buffer {
+  private getTreeKey(address: Address, treeIndex: number, subIndex: number): Uint8Array {
     const address32 = setLengthLeft(address.toBytes(), 32)
 
-    const treeIndexB = Buffer.alloc(32)
-    treeIndexB.writeInt32LE(treeIndex)
+    const treeIndexB = writeInt32LE(treeIndex)
 
-    const input = Buffer.concat([address32, treeIndexB])
+    const input = concatBytes(address32, treeIndexB)
 
-    const treeKey = Buffer.concat([this.pedersenHash(input).slice(0, 31), toBytes(subIndex)])
+    const treeKey = concatBytes(this.pedersenHash(input).slice(0, 31), toBytes(subIndex))
 
     return treeKey
   }
@@ -191,17 +282,17 @@ export class StatelessVerkleStateManager implements StateManager {
    * @param address - Address of the `account` to add the `code` for
    * @param value - The value of the `code`
    */
-  async putContractCode(address: Address, value: Buffer): Promise<void> {
+  async putContractCode(address: Address, value: Uint8Array): Promise<void> {
     // TODO
   }
 
   /**
    * Gets the code corresponding to the provided `address`.
    * @param address - Address to get the `code` for
-   * @returns {Promise<Buffer>} -  Resolves with the code corresponding to the provided address.
-   * Returns an empty `Buffer` if the account has no associated code.
+   * @returns {Promise<Uint8Array>} -  Resolves with the code corresponding to the provided address.
+   * Returns an empty `Uint8Array` if the account has no associated code.
    */
-  async getContractCode(address: Address): Promise<Buffer> {
+  async getContractCode(address: Address): Promise<Uint8Array> {
     // Get the contract code size
     const codeHashKey = this.getTreeKeyForCodeHash(address)
     const codeSizeKey = this.getTreeKeyForCodeSize(address)
@@ -211,7 +302,7 @@ export class StatelessVerkleStateManager implements StateManager {
     // Calculate number of chunks
     const chunks = Math.ceil(readInt32LE(codeSizeLE) / 32)
 
-    const retrievedChunks: Buffer[] = []
+    const retrievedChunks: Uint8Array[] = []
 
     // Retrieve all code chunks
     for (let chunkId = 0; chunkId < chunks; chunkId++) {
@@ -219,7 +310,7 @@ export class StatelessVerkleStateManager implements StateManager {
     }
 
     // Aggregate code chunks
-    const code = Buffer.concat(retrievedChunks)
+    const code = concatBytes(...retrievedChunks)
 
     // Return code chunks
     return code
@@ -230,13 +321,13 @@ export class StatelessVerkleStateManager implements StateManager {
    * the shortest representation of the stored value.
    * @param address -  Address of the account to get the storage for
    * @param key - Key in the account's storage to get the value for. Must be 32 bytes long.
-   * @returns {Promise<Buffer>} - The storage value for the account
+   * @returns {Promise<Uint8Array>} - The storage value for the account
    * corresponding to the provided address at the provided key.
-   * If this does not exist an empty `Buffer` is returned.
+   * If this does not exist an empty `Uint8Array` is returned.
    */
-  async getContractStorage(address: Address, key: Buffer): Promise<Buffer> {
-    const storageKey = this.getTreeKeyForStorageSlot(address, Number(bufferToHex(key)))
-    const storage = toBuffer(this._state[bufferToHex(storageKey)])
+  async getContractStorage(address: Address, key: Uint8Array): Promise<Uint8Array> {
+    const storageKey = this.getTreeKeyForStorageSlot(address, Number(bytesToHex(key)))
+    const storage = toBytes(this._state[bytesToHex(storageKey)])
 
     return storage
   }
@@ -248,9 +339,9 @@ export class StatelessVerkleStateManager implements StateManager {
    * @param key - Key to set the value at. Must be 32 bytes long.
    * @param value - Value to set at `key` for account corresponding to `address`. Cannot be more than 32 bytes. Leading zeros are stripped. If it is a empty or filled with zeros, deletes the value.
    */
-  async putContractStorage(address: Address, key: Buffer, value: Buffer): Promise<void> {
-    const storageKey = this.getTreeKeyForStorageSlot(address, Number(bufferToHex(key)))
-    this._state[bufferToHex(storageKey)] = value.toString('hex')
+  async putContractStorage(address: Address, key: Uint8Array, value: Uint8Array): Promise<void> {
+    const storageKey = this.getTreeKeyForStorageSlot(address, Number(bytesToHex(key)))
+    this._state[bytesToHex(storageKey)] = bytesToHex(value)
   }
 
   /**
@@ -268,14 +359,14 @@ export class StatelessVerkleStateManager implements StateManager {
     const nonceKey = this.getTreeKeyForNonce(address)
     const codeHashKey = this.getTreeKeyForCodeHash(address)
 
-    const balanceLE = toBuffer(this._state[bufferToHex(balanceKey)])
-    const nonceLE = toBuffer(this._state[bufferToHex(nonceKey)])
-    const codeHash = toBuffer(this._state[bufferToHex(codeHashKey)])
+    const balanceLE = toBytes(this._state[bytesToHex(balanceKey)])
+    const nonceLE = toBytes(this._state[bytesToHex(nonceKey)])
+    const codeHash = toBytes(this._state[bytesToHex(codeHashKey)])
 
     return Account.fromAccountData({
-      balance: balanceLE.length > 0 ? balanceLE.readBigInt64LE() : 0n,
+      balance: balanceLE.length > 0 ? readBigInt64LE(balanceLE) : 0n,
       codeHash: codeHash.length > 0 ? codeHash : zeros(32),
-      nonce: nonceLE.length > 0 ? nonceLE.readBigInt64LE() : 0n,
+      nonce: nonceLE.length > 0 ? readBigInt64LE(nonceLE) : 0n,
     })
   }
 
@@ -285,14 +376,34 @@ export class StatelessVerkleStateManager implements StateManager {
     const nonceKey = this.getTreeKeyForNonce(address)
     const codeHashKey = this.getTreeKeyForCodeHash(address)
 
-    const balanceBuf = Buffer.alloc(32)
-    balanceBuf.writeBigInt64LE(account.balance)
-    const nonceBuf = Buffer.alloc(32)
-    nonceBuf.writeBigInt64LE(account.nonce)
+    const balanceBuf = writeBigInt64LE(account.balance)
+    const nonceBuf = writeBigInt64LE(account.nonce)
 
-    this._state[bufferToHex(balanceKey)] = bufferToHex(balanceBuf)
-    this._state[bufferToHex(nonceKey)] = bufferToHex(nonceBuf)
-    this._state[bufferToHex(codeHashKey)] = bufferToHex(account.codeHash)
+    this._state[bytesToHex(balanceKey)] = bytesToHex(balanceBuf)
+    this._state[bytesToHex(nonceKey)] = bytesToHex(nonceBuf)
+    this._state[bytesToHex(codeHashKey)] = bytesToHex(account.codeHash)
+  }
+
+  /**
+   * Deletes an account from state under the provided `address`.
+   * @param address - Address of the account which should be deleted
+   */
+  async deleteAccount(address: Address) {
+    this._accountCache!.del(address)
+
+    if (!this._storageCacheSettings.deactivate) {
+      this._storageCache?.clearContractStorage(address)
+    }
+  }
+
+  async modifyAccountFields(address: Address, accountFields: AccountFields): Promise<void> {
+    const account = await this.getAccount(address)
+
+    account.nonce = accountFields.nonce ?? account.nonce
+    account.balance = accountFields.balance ?? account.balance
+    account.storageRoot = accountFields.storageRoot ?? account.storageRoot
+    account.codeHash = accountFields.codeHash ?? account.codeHash
+    await this.putAccount(address, account)
   }
 
   /**
@@ -302,7 +413,7 @@ export class StatelessVerkleStateManager implements StateManager {
    */
   async checkpoint(): Promise<void> {
     this._checkpoints.push(this._state)
-    await super.checkpoint()
+    this._accountCache!.checkpoint()
   }
 
   /**
@@ -311,11 +422,11 @@ export class StatelessVerkleStateManager implements StateManager {
    */
   async commit(): Promise<void> {
     this._checkpoints.pop()
-    await super.commit()
+    this._accountCache!.commit()
   }
 
   // TODO
-  async hasStateRoot(root: Buffer): Promise<boolean> {
+  async hasStateRoot(root: Uint8Array): Promise<boolean> {
     return true
   }
 
@@ -324,21 +435,25 @@ export class StatelessVerkleStateManager implements StateManager {
    * last call to checkpoint.
    */
   async revert(): Promise<void> {
-    if (this._checkpoints.length === 0) {
-      throw new Error('StatelessVerkleStateManager state cannot be reverted, no checkpoints set')
-    }
-    this._state = this._checkpoints.pop()!
-    await super.revert()
+    // setup trie checkpointing
+    this._storageCache?.revert()
+    this._accountCache?.revert()
+    this._codeCache = {}
   }
+
+  /**
+   * Writes all cache items to the trie
+   */
+  async flush(): Promise<void> {}
 
   /**
    * Gets the verkle root.
    * NOTE: this needs some examination in the code where this is needed
    * and if we have the verkle root present
-   * @returns {Promise<Buffer>} - Returns the verkle root of the `StateManager`
+   * @returns {Promise<Uint8Array>} - Returns the verkle root of the `StateManager`
    */
-  async getStateRoot(): Promise<Buffer> {
-    return Buffer.alloc(0)
+  async getStateRoot(): Promise<Uint8Array> {
+    return new Uint8Array(0)
   }
 
   /**
@@ -346,7 +461,7 @@ export class StatelessVerkleStateManager implements StateManager {
    * Maybe in this context: reset to original pre state suffice
    * @param stateRoot - The verkle root to reset the instance to
    */
-  async setStateRoot(stateRoot: Buffer): Promise<void> {}
+  async setStateRoot(stateRoot: Uint8Array): Promise<void> {}
 
   /**
    * Dumps the RLP-encoded storage values for an `account` specified by `address`.
@@ -376,5 +491,13 @@ export class StatelessVerkleStateManager implements StateManager {
    */
   async accountExists(address: Address): Promise<boolean> {
     return false
+  }
+
+  /**
+   * Clears all underlying caches
+   */
+  clearCaches() {
+    this._accountCache?.clear()
+    this._storageCache?.clear()
   }
 }
