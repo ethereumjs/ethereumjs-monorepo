608172347b1c00214bf89ce459c26cde4ee7af45
Frederik Bolding <frederik.bolding@gmail.com>
2023-04-21 16:27:57 +0200

    Bump chainsafe/ssz to 0.11.1

--- packages/block/karma.conf.js
+++ packages/block/karma.conf.js
@@ -13,14 +13,6 @@ module.exports = function (config) {
         acornOptions: {
           ecmaVersion: 12,
         },
-        resolve: {
-          alias: {
-            '@chainsafe/persistent-merkle-tree/hasher':
-              '../../node_modules/@chainsafe/persistent-merkle-tree/lib/hasher/noble.js',
-            '@chainsafe/as-sha256/hashObject':
-              '../../node_modules/@chainsafe/as-sha256/lib/hashObject.js',
-          },
-        },
       },
     },
     concurrency: 1,
