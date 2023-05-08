01d88bc1fb22d705540cba24b868250162a5f2aa
g11tech <gajinder@g11.in>
2023-05-08 16:09:24 +0530

    client: Add 4844 devnet5 blob post utility (#2674)
    * client: Add 4844 devnet5 blob post utility
    
    * fx
    
    * update error handling
    
    * lint
    
    * fix sharding spec
    
    * update image
    
    * add instructions
    
    * allow gas specification via env vars
    
    * undo tx lib change and cleanup

--- packages/client/test/sim/sharding.spec.ts
+++ packages/client/test/sim/sharding.spec.ts
@@ -106,8 +106,16 @@ tape('sharding/eip4844 hardfork tests', async (t) => {
       4,
       4096,
       pkey,
-      bytesToPrefixedHexString(randomBytes(20)),
-      undefined,
+      // Start with nonce of 1 since a tx previous has already been posted
+      1,
+      {
+        to: bytesToPrefixedHexString(randomBytes(20)),
+        chainId: 1,
+        maxFeePerDataGas: BigInt(1000) as any,
+        maxPriorityFeePerGas: BigInt(1) as any,
+        maxFeePerGas: '0xff' as any,
+        gasLimit: BigInt(1000000) as any,
+      },
       { common }
     )
     const txHashes = []
