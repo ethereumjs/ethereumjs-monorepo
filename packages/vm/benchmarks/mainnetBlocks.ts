177c1cc1e55e03fe06b65bc0f62e49e9d06046b0
Holger Drewes <Holger.Drewes@gmail.com>
2023-05-11 10:11:05 +0200

    Fix VM benchmark block fromRPC() import

--- packages/vm/benchmarks/mainnetBlocks.ts
+++ packages/vm/benchmarks/mainnetBlocks.ts
@@ -2,7 +2,6 @@ import { readFileSync } from 'fs'
 import Benchmark from 'benchmark'
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { Block } from '@ethereumjs/block'
-import { blockFromRpc } from '@ethereumjs/block/dist/from-rpc'
 import { VM } from '../dist'
 import { getPreState, getBlockchain, verifyResult } from './util'
 
@@ -31,7 +30,7 @@ export async function mainnetBlocks(suite?: Benchmark.Suite, numSamples?: number
   const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.MuirGlacier })
 
   for (const blockData of data) {
-    const block = blockFromRpc(blockData.block, [], { common })
+    const block = Block.fromRPC(blockData.block, [], { common })
     const blockNumber = Number(block.header.number)
     const { receipts, preState, blockhashes } = blockData
 
