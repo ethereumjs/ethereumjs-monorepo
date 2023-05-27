cc6240bc2a281a55c53659e5c79dac0c0d17d87e
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-05-27 15:52:20 +0200

    vm/statemanager: buffer->uint8array + lint

--- packages/vm/src/runTx.ts
+++ packages/vm/src/runTx.ts
@@ -507,13 +507,12 @@ async function _runTx(this: VM, opts: RunTxOpts): Promise<RunTxResult> {
     const keys = Object.keys(results.execResult.selfdestruct)
     for (const k of keys) {
       const address = new Address(hexToBytes(k))
-      await this.evm.journal.deleteAccount(address)
       if (this._common.isActivatedEIP(6780)) {
         if (!results.execResult.createdAddresses![address.toString()]) {
           continue
         }
       }
-      await state.deleteAccount(address)
+      await this.evm.journal.deleteAccount(address)
       if (this.DEBUG) {
         debug(`tx selfdestruct on address=${address}`)
       }
--- packages/vm/test/api/EIPs/eip-6780-selfdestruct-same-tx.spec.ts
+++ packages/vm/test/api/EIPs/eip-6780-selfdestruct-same-tx.spec.ts
@@ -1,10 +1,12 @@
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { Transaction } from '@ethereumjs/tx'
-import { Address, privateToAddress } from '@ethereumjs/util'
+import { Account, Address, privateToAddress } from '@ethereumjs/util'
+import { equalsBytes, hexToBytes } from 'ethereum-cryptography/utils'
 import * as tape from 'tape'
 
 import { VM } from '../../../src/vm'
-const pkey = Buffer.from('20'.repeat(32), 'hex')
+const pkey = hexToBytes('20'.repeat(32))
+
 const GWEI = BigInt(1000000000)
 const sender = new Address(privateToAddress(pkey))
 
@@ -14,11 +16,11 @@ const common = new Common({
   eips: [6780],
 })
 
-const payload = Buffer.from('60016001556001FF', 'hex')
+const payload = hexToBytes('60016001556001FF')
 
 async function getVM(common: Common) {
   const vm = await VM.create({ common })
-  const account = await vm.stateManager.getAccount(sender)
+  const account = (await vm.stateManager.getAccount(sender)) ?? new Account()
   const balance = GWEI * BigInt(21000) * BigInt(10000000)
   account.balance = balance
   await vm.stateManager.putAccount(sender, account)
@@ -41,20 +43,17 @@ tape('EIP 6780 tests', (t) => {
     const result = await vm.runTx({ tx })
     const createdAddress = result.createdAddress!
 
-    const contract = await vm.stateManager.getAccount(createdAddress)
+    const contract = (await vm.stateManager.getAccount(createdAddress)) ?? new Account()
     st.equals(contract.balance, BigInt(0), 'value sent')
     st.equals(contract.nonce, BigInt(0), 'contract nonce 0')
-    st.ok(
-      (
-        await vm.evm.eei.getContractStorage(
-          createdAddress,
-          Buffer.from('00'.repeat(31) + '01', 'hex')
-        )
-      ).equals(Buffer.from('', 'hex')),
-      'storage cleared'
-    )
+
+    const exists = await vm.evm.stateManager.accountExists(createdAddress)
+
+    // Account does not exist...
+    st.ok(!exists, 'account does not exist, so storage is cleared')
     st.equals(
-      (await vm.stateManager.getAccount(Address.fromString('0x' + '00'.repeat(19) + '01'))).balance,
+      (await vm.stateManager.getAccount(Address.fromString('0x' + '00'.repeat(19) + '01')))!
+        .balance,
       BigInt(value),
       'balance sent to target'
     )
@@ -69,7 +68,7 @@ tape('EIP 6780 tests', (t) => {
 
       await vm.stateManager.putContractCode(target, payload)
       const targetContract = await vm.stateManager.getAccount(target)
-      targetContract.nonce = BigInt(1)
+      targetContract!.nonce = BigInt(1)
       await vm.stateManager.putAccount(target, targetContract)
 
       const value = 1
@@ -82,20 +81,16 @@ tape('EIP 6780 tests', (t) => {
 
       await vm.runTx({ tx })
 
-      const contract = await vm.stateManager.getAccount(target)
+      const contract = (await vm.stateManager.getAccount(target)) ?? new Account()
       st.equals(contract.balance, BigInt(0), 'value sent')
       st.equals(contract.nonce, BigInt(1), 'nonce 1')
-      st.ok(
-        (
-          await vm.stateManager.getContractStorage(
-            target,
-            Buffer.from('00'.repeat(31) + '01', 'hex')
-          )
-        ).equals(Buffer.from('01', 'hex')),
-        'storage not cleared'
-      )
+
+      const key = hexToBytes('00'.repeat(31) + '01')
+      const storage = await vm.stateManager.getContractStorage(target, key)
+
+      st.ok(equalsBytes(storage, hexToBytes('01')), 'storage not cleared')
       st.equals(
-        (await vm.stateManager.getAccount(Address.fromString('0x' + '00'.repeat(19) + '01')))
+        (await vm.stateManager.getAccount(Address.fromString('0x' + '00'.repeat(19) + '01')))!
           .balance,
         BigInt(value),
         'balance sent to target'
