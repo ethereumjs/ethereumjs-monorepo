55cfe3f33c280157a96a2c57ee3de13d826526b0
Amir <indigophi@protonmail.com>
2023-06-04 16:52:31 -0700

    Remove references to functions accountExists and flush that no longer exist on StateManager

--- packages/vm/test/api/copy.spec.ts
+++ packages/vm/test/api/copy.spec.ts
@@ -13,21 +13,23 @@ tape('VM Copy Test', async (t) => {
     const address = Address.fromString(`0x` + '1234'.repeat(10))
     await vm.stateManager.putAccount(address, account)
 
-    st.ok(await vm.stateManager.accountExists(address), 'account exists before copy')
+    st.ok((await vm.stateManager.getAccount(address)) !== undefined, 'account exists before copy')
 
     const vmCopy = await vm.copy()
     st.notok(
-      await vmCopy.stateManager.accountExists(address),
+      (await vmCopy.stateManager.getAccount(address)) !== undefined,
       'non-committed checkpoints will not be copied'
     )
 
     await vm.stateManager.checkpoint()
     await vm.stateManager.commit()
-    await vm.stateManager.flush()
 
     const vmCopy2 = await vm.copy()
 
-    st.ok(await vmCopy2.stateManager.accountExists(address), 'committed checkpoints will be copied')
+    st.ok(
+      (await vmCopy2.stateManager.getAccount(address)) !== undefined,
+      'committed checkpoints will be copied'
+    )
 
     st.end()
   })
