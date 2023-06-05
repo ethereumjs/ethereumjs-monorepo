90266e56a53899de86398b73ecd9508026753b02
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-06-05 09:47:43 -0400

    Remove obsolete test

--- packages/wallet/test/index.spec.ts
+++ packages/wallet/test/index.spec.ts
@@ -687,16 +687,6 @@ tape('should strip leading "0x" from salt, iv, uuid', async (t) => {
   t.equal(fixtureWallet.getPrivateKeyString(), (await Wallet.fromV3(w2, pw)).getPrivateKeyString())
 })
 
-/*
-tape('.fromV1()',  (t) => {
-  t.test('should work', function () {
-    const sample = '{"Address":"d4584b5f6229b7be90727b0fc8c6b91bb427821f","Crypto":{"CipherText":"07533e172414bfa50e99dba4a0ce603f654ebfa1ff46277c3e0c577fdc87f6bb4e4fe16c5a94ce6ce14cfa069821ef9b","IV":"16d67ba0ce5a339ff2f07951253e6ba8","KeyHeader":{"Kdf":"scrypt","KdfParams":{"DkLen":32,"N":262144,"P":1,"R":8,"SaltLen":32},"Version":"1"},"MAC":"8ccded24da2e99a11d48cda146f9cc8213eb423e2ea0d8427f41c3be414424dd","Salt":"06870e5e6a24e183a5c807bd1c43afd86d573f7db303ff4853d135cd0fd3fe91"},"Id":"0498f19a-59db-4d54-ac95-33901b4f1870","Version":"1"}'
-    const wallet = Wallet.fromV1(sample, 'foo')
-    t.deepEqual(wallet.getAddressString(), '0xd4584b5f6229b7be90727b0fc8c6b91bb427821f')
-  })
-})
-*/
-
 tape('.fromV3()', async (t) => {
   t.test('should work with PBKDF2', async (st) => {
     const w =
