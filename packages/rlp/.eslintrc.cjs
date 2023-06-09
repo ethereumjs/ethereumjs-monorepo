8ae9c9f177ca249198e21039fa30b18957cda7fa
Holger Drewes <Holger.Drewes@gmail.com>
2023-06-09 11:29:30 +0200

    Util: tape -> vitest

--- packages/rlp/.eslintrc.cjs
+++ packages/rlp/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: '../../config/eslint.js',
+  extends: '../../config/eslint.cjs',
   rules: {
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
