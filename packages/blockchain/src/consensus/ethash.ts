61ef515ebf54929a29d636f850584eea387941ae
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-06-10 11:00:09 -0400

    blockchain: cleanup instances of as any

--- packages/blockchain/src/consensus/ethash.ts
+++ packages/blockchain/src/consensus/ethash.ts
@@ -44,7 +44,7 @@ export class EthashConsensus implements Consensus {
   public async genesisInit(): Promise<void> {}
   public async setup({ blockchain }: ConsensusOptions): Promise<void> {
     this.blockchain = blockchain
-    this._ethash = new Ethash(this.blockchain.db as any)
+    this._ethash = new Ethash(this.blockchain.db)
   }
   public async newBlock(): Promise<void> {}
 }
