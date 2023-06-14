742cad116036be7b7f7e96cd4b2d904ae6be0258
Holger Drewes <Holger.Drewes@gmail.com>
2023-06-14 12:40:52 +0200

    Trie: tape -> vitest test transition

--- packages/trie/test/encoding.spec.ts
+++ packages/trie/test/encoding.spec.ts
@@ -1,5 +1,5 @@
 import { bytesToHex, hexStringToBytes, toBytes, utf8ToBytes } from '@ethereumjs/util'
-import * as tape from 'tape'
+import { assert, describe, it } from 'vitest'
 
 import { Trie } from '../src'
 
@@ -7,9 +7,10 @@ const trie = new Trie()
 const trie2 = new Trie()
 const hex = 'FF44A3B3'
 
-tape('encoding hex prefixes', async function (t) {
-  await trie.put(hexStringToBytes(hex), utf8ToBytes('test'))
-  await trie2.put(toBytes(`0x${hex}`), utf8ToBytes('test'))
-  t.equal(bytesToHex(trie.root()), bytesToHex(trie2.root()))
-  t.end()
+describe('encoding hex prefixes', () => {
+  it('should work', async () => {
+    await trie.put(hexStringToBytes(hex), utf8ToBytes('test'))
+    await trie2.put(toBytes(`0x${hex}`), utf8ToBytes('test'))
+    assert.equal(bytesToHex(trie.root()), bytesToHex(trie2.root()))
+  })
 })
