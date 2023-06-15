854242f0cb51189709cf7f7ffcfaff48ee00fef0
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-06-15 11:08:21 -0400

    vm: update vm verkle test

--- packages/vm/test/api/verkle.spec.ts
+++ packages/vm/test/api/verkle.spec.ts
@@ -5,54 +5,29 @@ import * as tape from 'tape'
 
 import { VM } from '../../src/vm'
 
-import * as testnetVerkleBeverlyHills from './testdata/testnetVerkleBeverlyHills.json'
 import * as testnetVerkleKaustinen from './testdata/testnetVerkleKaustinen.json'
-import * as verkleBlockBeverlyHillsJSON from './testdata/verkleBeverlyHillsBlock83.json'
-import * as verkleBlockKaustinenJSON from './testdata/verkleKaustinenBlock.json'
+import * as block1JSON from './testdata/verkleKaustinenBlock1.json'
+import * as block2JSON from './testdata/verkleKaustinenBlock2.json'
+import * as block3JSON from './testdata/verkleKaustinenBlock3.json'
 
 tape('Verkle-enabled VM', async (t) => {
-  t.test('should run a verkle block (beverly hills)', async (st) => {
-    const common = Common.fromGethGenesis(testnetVerkleBeverlyHills, {
-      chain: 'customChain',
-      eips: [999001],
-    })
-
-    const block = Block.fromBlockData(verkleBlockBeverlyHillsJSON, { common })
-    const stateManager = new StatelessVerkleStateManager()
-    stateManager.initPreState(block.header.verkleProof!, block.header.verklePreState!)
-
-    const vm = await VM.create({ common, stateManager })
-
-    await vm.runBlock({
-      block,
-      skipHeaderValidation: true,
-    })
-
-    st.pass('Should run verkle block successfully')
-
-    st.end()
-  })
-
-  t.test('should run a verkle block (kaustinen)', async (st) => {
+  t.test('should run verkle blocks (kaustinen)', async (st) => {
     const common = Common.fromGethGenesis(testnetVerkleKaustinen, {
       chain: 'customChain',
       eips: [999001],
     })
 
-    const block = Block.fromBlockData(verkleBlockKaustinenJSON, { common })
-    const stateManager = new StatelessVerkleStateManager()
-    stateManager.initPreState(block.header.verkleProof!, block.header.verklePreState!)
+    for (const [index, blockJSON] of [block1JSON, block2JSON, block3JSON].entries()) {
+      const block = Block.fromBlockData(blockJSON, { common })
+      const stateManager = new StatelessVerkleStateManager()
+      stateManager.initVerkleExecutionWitness(block.header.executionWitness!)
+      const vm = await VM.create({ common, stateManager })
+      await vm.runBlock({
+        block,
+      })
 
-    const vm = await VM.create({ common, stateManager })
-
-    await vm.runBlock({
-      block,
-      skipHeaderValidation: true,
-      skipBlockValidation: true,
-      skipBalance: true,
-    })
-
-    st.pass('Should run verkle block successfully')
+      st.pass(`Should run verkle block ${index + 1} successfully`)
+    }
 
     st.end()
   })
