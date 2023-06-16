5b6dfb90fbc25b15f70bc817de11bb52f5457acc
harkamal <gajinder@g11.in>
2023-06-16 17:30:22 +0530

    bring test uptodate with new framework

--- packages/trie/test/util/genesisState.spec.ts
+++ packages/trie/test/util/genesisState.spec.ts
@@ -1,30 +1,28 @@
 import { bytesToHex, parseGethGenesisState } from '@ethereumjs/util'
-import * as tape from 'tape'
+import { assert, describe, it } from 'vitest'
 
 import { genesisStateRoot } from '../../src/util/genesisState'
 
-tape('[Util/genesisStateRoot]', (t) => {
-  t.test('should properly generate stateRoot from gethGenesis', async (st) => {
+describe('[Util/genesisStateRoot]', () => {
+  it('should properly generate stateRoot from gethGenesis', async () => {
     // kiln genesis with deposit contract storage set
     const json = require(`../../../blockchain/test/testdata/geth-genesis-kiln.json`)
     const genesisState = parseGethGenesisState(json)
     const stateRoot = await genesisStateRoot(genesisState)
-    st.equal(
+    assert.equal(
       bytesToHex(stateRoot),
       '52e628c7f35996ba5a0402d02b34535993c89ff7fc4c430b2763ada8554bee62',
       'kiln stateRoot matches'
     )
-    st.end()
   })
 
-  t.test('should correctly derive ropsten stateRoot from ethereumjs genesis', async (st) => {
+  it('should correctly derive ropsten stateRoot from ethereumjs genesis', async () => {
     const json = require(`../../../genesis/src/genesisStates/ropsten.json`)
     const stateRoot = await genesisStateRoot(json)
-    st.equal(
+    assert.equal(
       bytesToHex(stateRoot),
       '217b0bbcfb72e2d57e28f33cb361b9983513177755dc3f33ce3e7022ed62b77b',
       'ropsten stateRoot matches'
     )
-    st.end()
   })
 })
--- packages/util/test/genesis.spec.ts
+++ packages/util/test/genesis.spec.ts
@@ -1,20 +1,20 @@
-import * as tape from 'tape'
+import { assert, describe, it } from 'vitest'
 
 import { parseGethGenesisState } from '../src/genesis'
 
-tape('[Util/genesis]', (t) => {
-  t.test('should properly generate stateRoot from gethGenesis', async (st) => {
+describe('[Util/genesis]', () => {
+  it('should properly generate stateRoot from gethGenesis', () => {
     // kiln genesis with deposit contract storage set
     const json = require(`../../blockchain/test/testdata/geth-genesis-kiln.json`)
     const genesisState = parseGethGenesisState(json)
     // just check for deposit contract inclusion
-    st.ok(
+    assert.equal(
       genesisState['0x4242424242424242424242424242424242424242'][1].includes(
         // sample data check
         '0x60806040526004361061003'
       ),
+      true,
       'should have deposit contract'
     )
-    st.end()
   })
 })
