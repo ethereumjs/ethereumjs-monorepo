1fa6057971a2d362252eba38364afabcd6d24c83
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-06-17 17:49:45 -0400

    block: migrate verkle tests to vitest

--- packages/block/test/verkle.spec.ts
+++ packages/block/test/verkle.spec.ts
@@ -1,29 +1,27 @@
 import { Common } from '@ethereumjs/common'
-import * as tape from 'tape'
+import { assert, describe, it } from 'vitest'
 
 import { Block } from '../src'
 
 import * as testnetVerkleKaustinen from './testdata/testnetVerkleKaustinen.json'
 import * as verkleBlockJSON from './testdata/verkleKaustinenBlock1.json'
 
-tape('[VerkleBlock]: Verkle Block Functionality (FakeEIP-999001)', function (t) {
+describe('[VerkleBlock]: Verkle Block Functionality (FakeEIP-999001)', () => {
   const common = Common.fromGethGenesis(testnetVerkleKaustinen, {
     chain: 'customChain',
     eips: [999001],
   })
   const verkleBlock = Block.fromBlockData(verkleBlockJSON, { common })
 
-  t.test('should test block initialization', function (st) {
-    st.ok(
+  it('should test block initialization', () => {
+    assert.ok(
       (verkleBlock.header.executionWitness?.stateDiff?.length ?? 0) > 0,
       'executionWitness stateDiff is defined'
     )
-    st.ok(
+    assert.ok(
       (verkleBlock.header.executionWitness?.verkleProof?.commitmentsByPath.length ?? 0) > 0 !==
         undefined,
       'executionWitness verkleProof is defined'
     )
-
-    st.end()
   })
 })
