8b731bf9b701ffe30a10767a69390401ca96595f
ScottyPoi <scott.simpson@ethereum.org>
2023-06-21 02:42:22 -0600

    Update vitest.config so that test:CLI runs

--- packages/client/vitest.config.js
+++ packages/client/vitest.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: ['test/(integration|sim)/**/*.spec.ts'],
     //   silent: true,
     isolate: true,
   },
