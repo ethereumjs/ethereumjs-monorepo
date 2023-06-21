f840c4718103f407e183279836200c4d707d8dd0
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-06-21 13:34:05 -0400

    Remove util from browser tests for now

--- packages/util/vitest.config.browser.ts
+++ packages/util/vitest.config.browser.ts
@@ -2,12 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [
-      ...configDefaults.exclude,
-      // access control
-      'test/provider.spec.ts',
-      'test/account.spec.ts',
-      'test/address.spec.ts',
-    ],
+    exclude: [...configDefaults.exclude],
   },
 })
