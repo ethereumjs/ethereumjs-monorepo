1e1bdf8346c1c78dcd212d76edd95a80d59c5812
Holger Drewes <Holger.Drewes@gmail.com>
2023-06-21 14:21:40 +0200

    Update packages to playwright test provider, updated browser workflow file

---
+++ packages/util/vitest.config.browser.ts
@@ -0,0 +1,11 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      // access control
+      'test/provider.spec.ts',
+    ],
+  },
+})
