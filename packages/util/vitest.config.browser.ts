00df5c17335150cfc6494edc3f854bb6ba5def7e
Holger Drewes <Holger.Drewes@gmail.com>
2023-06-22 10:37:50 +0200

    Util: switch to type commonjs for main package.json, skip provider tests, re-add to browser CI run

--- packages/util/vitest.config.browser.ts
+++ packages/util/vitest.config.browser.ts
@@ -2,6 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude],
+    exclude: [...configDefaults.exclude, 'test/provider.spec.ts'],
   },
 })
