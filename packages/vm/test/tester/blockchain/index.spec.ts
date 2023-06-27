a55f73111bc0373d691ddea561b1b4f094390655
ScottyPoi <scott.simpson@ethereum.org>
2023-06-27 03:11:35 -0600

    Edit CI scripts

--- packages/vm/test/tester/blockchain/index.spec.ts
+++ packages/vm/test/tester/blockchain/index.spec.ts
@@ -5,20 +5,23 @@ import { defaultBlockchainTestArgs } from '../runners/runnerUtils'
 
 const forks = process.env.FORK ?? 'Paris'
 const hardforks = forks.split(',')
-const files =
-  process.env.FILE !== undefined ? forks.split(process.env.FILE) : [defaultBlockchainTestArgs.file]
-describe(`VM Blockchain Test: ${files !== undefined ? files : hardforks} `, async () => {
-  for await (const file of files) {
-    const testArgs = defaultBlockchainTestArgs
-    testArgs.file = file
-    for await (const hardfork of hardforks) {
-      file !== undefined && (testArgs['verify-test-amount-alltests'] = 0)
-      testArgs.blockchain = true
-      testArgs.fork = hardfork
-      const test = new BlockchainTests(testArgs)
-      it(`${testArgs.fork} (${test.expectedTests})`, async () => {
-        await test.runTests()
-      })
-    }
+const file = process.env.FILE
+const dir = process.env.DIR
+const excludeDir = process.env.EXCLUDEDIR
+const count = process.env.COUNT
+describe(`VM Blockchain Test: ${file !== undefined ? file : hardforks} `, async () => {
+  const testArgs = defaultBlockchainTestArgs
+  testArgs.file = file
+  testArgs.dir = dir
+  testArgs.excludeDir = excludeDir
+  for await (const hardfork of hardforks) {
+    file !== undefined && (testArgs['verify-test-amount-alltests'] = 0)
+    count !== undefined && (testArgs['expected-test-amount'] = parseInt(count))
+    testArgs.blockchain = true
+    testArgs.fork = hardfork
+    const test = new BlockchainTests(testArgs)
+    it(`${testArgs.fork} (${test.expectedTests})`, async () => {
+      await test.runTests()
+    })
   }
 })
