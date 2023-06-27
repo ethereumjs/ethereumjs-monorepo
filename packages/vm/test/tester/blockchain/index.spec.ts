3109f8abf682245fc0d8b1950da8bd91e12cbbe0
ScottyPoi <scott.simpson@ethereum.org>
2023-06-27 03:19:33 -0600

    Fix timeouts

--- packages/vm/test/tester/blockchain/index.spec.ts
+++ packages/vm/test/tester/blockchain/index.spec.ts
@@ -9,19 +9,23 @@ const file = process.env.FILE
 const dir = process.env.DIR
 const excludeDir = process.env.EXCLUDEDIR
 const count = process.env.COUNT
-describe(`VM Blockchain Test: ${file !== undefined ? file : hardforks} `, async () => {
-  const testArgs = defaultBlockchainTestArgs
-  testArgs.file = file
-  testArgs.dir = dir
-  testArgs.excludeDir = excludeDir
-  for await (const hardfork of hardforks) {
-    file !== undefined && (testArgs['verify-test-amount-alltests'] = 0)
-    count !== undefined && (testArgs['expected-test-amount'] = parseInt(count))
-    testArgs.blockchain = true
-    testArgs.fork = hardfork
-    const test = new BlockchainTests(testArgs)
-    it(`${testArgs.fork} (${test.expectedTests})`, async () => {
-      await test.runTests()
-    })
-  }
-})
+describe(
+  `VM Blockchain Test: ${file !== undefined ? file : hardforks} `,
+  async () => {
+    const testArgs = defaultBlockchainTestArgs
+    testArgs.file = file
+    testArgs.dir = dir
+    testArgs.excludeDir = excludeDir
+    for await (const hardfork of hardforks) {
+      file !== undefined && (testArgs['verify-test-amount-alltests'] = 0)
+      count !== undefined && (testArgs['expected-test-amount'] = parseInt(count))
+      testArgs.blockchain = true
+      testArgs.fork = hardfork
+      const test = new BlockchainTests(testArgs)
+      it(`${testArgs.fork} (${test.expectedTests})`, async () => {
+        await test.runTests()
+      })
+    }
+  },
+  { timeout: 10000 }
+)
