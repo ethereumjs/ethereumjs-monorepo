6d8f583a0c5da0bac2a20be05f10c7771be03948
ScottyPoi <scott.simpson@ethereum.org>
2023-06-29 17:55:23 -0600

    VM:State -- adjust npm script for github

--- packages/vm/test/tester/state/index.spec.ts
+++ packages/vm/test/tester/state/index.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest'
+import { suite } from 'vitest'
 
 import { GeneralStateTests } from '../runners/GeneralStateTestsRunner'
 import { defaultStateTestArgs } from '../runners/runnerUtils'
@@ -14,7 +14,9 @@ const parseInput = (input: string | undefined, bool: boolean = false) => {
 }
 
 const input = {
-  forks: parseInput(process.env.FORK)?.split(',') ?? ['paris'],
+  ['verify-test-amount-alltests']: 0,
+  count: parseInput(process.env.COUNT) !== undefined ? parseInt(process.env.COUNT!) : undefined,
+  fork: parseInput(process.env.FORK) ?? 'Paris',
   test: parseInput(process.env.TEST) ?? defaultStateTestArgs.test,
   skip: parseInput(process.env.SKIP) ?? defaultStateTestArgs.skip,
   customStateTest: parseInput(process.env.CUSTOMSTATETEST) ?? defaultStateTestArgs.customStateTest,
@@ -24,24 +26,46 @@ const input = {
   value: parseInput(process.env.VALUE) !== undefined ? parseInt(process.env.VALUE!) : undefined,
 }
 
-describe(`VM State Test: ${input.forks} `, async () => {
-  const testArgs = { ...defaultStateTestArgs, ...input }
-  if (testArgs.test !== undefined || testArgs.customStateTest !== undefined) {
-    testArgs['verify-test-amount-alltests'] = 0
+const testArgs = { ...defaultStateTestArgs, ...input }
+if (testArgs.test !== undefined || testArgs.customStateTest !== undefined) {
+  testArgs['verify-test-amount-alltests'] = 0
+}
+const test = new GeneralStateTests(testArgs)
+console.log({
+  test: 'state',
+  skip: testArgs.skip,
+  runSkipped: testArgs.runSkipped,
+  fork: testArgs.fork,
+  expected: test.expectedTests,
+})
+const runSuite = suite(`${testArgs.fork} (${test.expectedTests})`, async () => {
+  await test.runTests()
+})
+
+runSuite.on('beforeAll', (suite) => {
+  const testSuite = {
+    ...testArgs,
+    test: 'state',
+    verifyTestAmountAllTests: testArgs['verify-test-amount-alltests'] > 0 ? true : false,
+    expected: test.expectedTests > 0 ? test.expectedTests : suite.tasks.length,
+    tasks: suite.tasks.length,
   }
-  for await (const hardfork of input.forks) {
-    testArgs.state = true
-    testArgs.fork = hardfork
-    const test = new GeneralStateTests(testArgs)
-    console.log({
-      test: 'state',
-      skip: testArgs.skip,
-      runSkipped: testArgs.runSkipped,
-      fork: testArgs.fork,
-      expected: test.expectedTests,
-    })
-    it(`${testArgs.fork} (${test.expectedTests})`, async () => {
-      await test.runTests()
-    })
+  console.log(
+    Object.fromEntries(
+      Object.entries(testSuite).filter(([_, value]) => value !== undefined && value !== '')
+    )
+  )
+})
+
+runSuite.on('afterAll', (suite) => {
+  const completed: Map<string, string[]> = new Map()
+  for (const task of suite.tasks) {
+    const [name, id] = task.name.split(':')
+    if (!completed.has(name)) {
+      completed.set(name, [])
+    }
+    completed.get(name)!.push(id.slice(name.length + 1))
   }
+  console.log(`Completed: ${suite.tasks.length}`)
+  console.log(Object.fromEntries([...completed.entries()].map(([name, ids]) => [name, ids.length])))
 })
