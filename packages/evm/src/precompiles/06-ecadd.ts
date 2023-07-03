2984257521ecf6f978706e0b7b5dda6636b697a7
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-07-03 17:51:04 +0200

    evm: fix hexToBytes

--- packages/evm/src/precompiles/06-ecadd.ts
+++ packages/evm/src/precompiles/06-ecadd.ts
@@ -1,4 +1,4 @@
-import { bytesToHex, bytesToUnprefixedHex, short, unprefixedHexToBytes } from '@ethereumjs/util'
+import { bytesToHex, bytesToUnprefixedHex, hexToBytes, short } from '@ethereumjs/util'
 import { ec_add } from 'rustbn-wasm'
 
 import { OOGResult } from '../evm.js'
@@ -24,7 +24,7 @@ export function precompile06(opts: PrecompileInput): ExecResult {
     return OOGResult(opts.gasLimit)
   }
 
-  const returnData = unprefixedHexToBytes(ec_add(inputData))
+  const returnData = hexToBytes(ec_add(inputData))
 
   // check ecadd success or failure by comparing the output length
   if (returnData.length !== 64) {
