810e9cda7c72db90c7840be075e3170ce282c87e
Amir <indigophi@protonmail.com>
2023-07-13 11:30:25 -0700

    Test tasks and path request queing functions and helpers

--- packages/client/test/sync/fetcher/trienodefetcher.spec.ts
+++ packages/client/test/sync/fetcher/trienodefetcher.spec.ts
@@ -1,4 +1,6 @@
 import { hexToBytes } from '@ethereumjs/util'
+import { bytesToHex, equalsBytes } from 'ethereum-cryptography/utils'
+import { OrderedMap } from 'js-sdsl'
 import * as tape from 'tape'
 import * as td from 'testdouble'
 
@@ -38,7 +40,7 @@ tape('[TrieNodeFetcher]', async (t) => {
     t.end()
   })
 
-  t.test('should process', (t) => {
+  t.test('should process', async (t) => {
     const config = new Config({ transports: [], accountCache: 10000, storageCache: 1000 })
     const pool = new PeerPool() as any
     const fetcher = new TrieNodeFetcher({
@@ -105,8 +107,89 @@ tape('[TrieNodeFetcher]', async (t) => {
     t.end()
   })
 
-  t.test('should reset td', (t) => {
+  t.test('should reset td', async (t) => {
     td.reset()
+    t.end()
+  })
+
+  t.test('should return an array of tasks with pathStrings and paths', async (t) => {
+    const config = new Config({ transports: [], accountCache: 10000, storageCache: 1000 })
+    const pool = new PeerPool() as any
+    const fetcher = new TrieNodeFetcher({
+      config,
+      pool,
+      root: new Uint8Array(0),
+    })
+
+    fetcher.pathToNodeRequestData = new OrderedMap<string, any>()
+    fetcher.pathToNodeRequestData.setElement('', {
+      nodeHash: bytesToHex(new Uint8Array(0)),
+      nodeParentHash: '',
+    })
+
+    const maxTasks = 1
+    const tasks = fetcher.tasks(maxTasks)
+
+    t.equal(tasks.length, maxTasks, `should return ${maxTasks} tasks`)
+    t.equal(tasks[0].pathStrings.length, 1, 'should have pathStrings')
+    t.equal(tasks[0].paths.length, 1, 'should have paths')
+
+    t.end()
+  })
+
+  t.test('should return an object with pathStrings', (t) => {
+    const config = new Config({ transports: [], accountCache: 10000, storageCache: 1000 })
+    const pool = new PeerPool() as any
+    const fetcher = new TrieNodeFetcher({
+      config,
+      pool,
+      root: new Uint8Array(0),
+    })
+
+    fetcher.pathToNodeRequestData = new OrderedMap<string, any>()
+    fetcher.pathToNodeRequestData.setElement('0x0b', {
+      nodeHash: bytesToHex(new Uint8Array(0)),
+      nodeParentHash: '', // root node does not have a parent
+    })
+    fetcher.pathToNodeRequestData.setElement('0x0a', {
+      nodeHash: bytesToHex(new Uint8Array(0)),
+      nodeParentHash: '', // root node does not have a parent
+    })
+
+    const result = fetcher.getSortedPathStrings()
+
+    t.deepEqual(result.pathStrings, ['0x0a', '0x0b'], 'should return pathStrings')
+
+    t.end()
+  })
+
+  t.test('should merge and format pathStrings into paths', async (t) => {
+    const config = new Config({ transports: [], accountCache: 10000, storageCache: 1000 })
+    const pool = new PeerPool() as any
+    const fetcher = new TrieNodeFetcher({
+      config,
+      pool,
+      root: new Uint8Array(0),
+    })
+
+    // should merge all syncPaths that have the same base account path
+    const pathStrings = ['0x0a', '0x0a/0x0b', '0x0a/0x0c', '0x0a/0x0d', '0x0e', '0x0e/0x0a', '0x0f']
+
+    const paths = fetcher.mergeAndFormatPaths(pathStrings)
+
+    t.equal(
+      paths.reduce((count, subArray) => count + subArray.length, 0),
+      7,
+      'should have correct number of paths'
+    )
+    t.deepEqual(
+      paths[0],
+      [Uint8Array.of(26), Uint8Array.of(27), Uint8Array.of(28), Uint8Array.of(29)],
+      'should merge paths correctly'
+    )
+    t.deepEqual(paths[1], [Uint8Array.of(30), Uint8Array.of(26)], 'should merge paths correctly')
+    t.deepEqual(paths[2], [Uint8Array.of(31)], 'should merge paths correctly')
+
     t.end()
   })
 })
