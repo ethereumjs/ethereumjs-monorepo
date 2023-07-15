7bef1c9a987b6625d62a4d7bab7e011ccc4c6585
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-07-15 10:52:38 -0400

    client: fix lightsync integration test timeouts

--- packages/client/test/integration/lightsync.spec.ts
+++ packages/client/test/integration/lightsync.spec.ts
@@ -6,75 +6,87 @@ import { Event } from '../../src/types'
 import { destroy, setup, wait } from './util'
 
 describe('[Integration:LightSync]', async () => {
-  it('should sync headers', async () => {
-    const [remoteServer, remoteService] = await setup({
-      location: '127.0.0.2',
-      height: 20,
-      syncmode: SyncMode.Full,
-    })
-    const [localServer, localService] = await setup({
-      location: '127.0.0.1',
-      height: 0,
-      syncmode: SyncMode.Light,
-    })
-    await localService.synchronizer!.stop()
-    await localServer.discover('remotePeer1', '127.0.0.2')
-    localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
-      assert.equal(localService.chain.headers.height, BigInt(20), 'synced')
+  it(
+    'should sync headers',
+    async () => {
+      const [remoteServer, remoteService] = await setup({
+        location: '127.0.0.2',
+        height: 20,
+        syncmode: SyncMode.Full,
+      })
+      const [localServer, localService] = await setup({
+        location: '127.0.0.1',
+        height: 0,
+        syncmode: SyncMode.Light,
+      })
+      await localService.synchronizer!.stop()
+      await localServer.discover('remotePeer1', '127.0.0.2')
+      localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
+        assert.equal(localService.chain.headers.height, BigInt(20), 'synced')
+        await destroy(localServer, localService)
+        await destroy(remoteServer, remoteService)
+      })
+      await localService.synchronizer!.start()
+    },
+    { timeout: 10000 }
+  )
+
+  it(
+    'should not sync with stale peers',
+    async () => {
+      const [remoteServer, remoteService] = await setup({
+        location: '127.0.0.2',
+        height: 9,
+        syncmode: SyncMode.Full,
+      })
+      const [localServer, localService] = await setup({
+        location: '127.0.0.1',
+        height: 10,
+        syncmode: SyncMode.Light,
+      })
+      localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
+        assert.fail('synced with a stale peer')
+      })
+      await localServer.discover('remotePeer', '127.0.0.2')
+      await wait(100)
       await destroy(localServer, localService)
       await destroy(remoteServer, remoteService)
-    })
-    await localService.synchronizer!.start()
-  })
+      assert.ok(true, 'did not sync')
+    },
+    { timeout: 10000 }
+  )
 
-  it('should not sync with stale peers', async () => {
-    const [remoteServer, remoteService] = await setup({
-      location: '127.0.0.2',
-      height: 9,
-      syncmode: SyncMode.Full,
-    })
-    const [localServer, localService] = await setup({
-      location: '127.0.0.1',
-      height: 10,
-      syncmode: SyncMode.Light,
-    })
-    localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
-      assert.fail('synced with a stale peer')
-    })
-    await localServer.discover('remotePeer', '127.0.0.2')
-    await wait(100)
-    await destroy(localServer, localService)
-    await destroy(remoteServer, remoteService)
-    assert.ok(true, 'did not sync')
-  })
-
-  it('should sync with best peer', async () => {
-    const [remoteServer1, remoteService1] = await setup({
-      location: '127.0.0.2',
-      height: 9,
-      syncmode: SyncMode.Full,
-    })
-    const [remoteServer2, remoteService2] = await setup({
-      location: '127.0.0.3',
-      height: 10,
-      syncmode: SyncMode.Full,
-    })
-    const [localServer, localService] = await setup({
-      location: '127.0.0.1',
-      height: 0,
-      syncmode: SyncMode.Light,
-    })
-    await localService.synchronizer!.stop()
-    await localServer.discover('remotePeer1', '127.0.0.2')
-    await localServer.discover('remotePeer2', '127.0.0.3')
-    localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
-      if (localService.chain.headers.height === BigInt(10)) {
-        assert.ok(true, 'synced with best peer')
-        await destroy(localServer, localService)
-        await destroy(remoteServer1, remoteService1)
-        await destroy(remoteServer2, remoteService2)
-      }
-    })
-    await localService.synchronizer!.start()
-  })
+  it(
+    'should sync with best peer',
+    async () => {
+      const [remoteServer1, remoteService1] = await setup({
+        location: '127.0.0.2',
+        height: 9,
+        syncmode: SyncMode.Full,
+      })
+      const [remoteServer2, remoteService2] = await setup({
+        location: '127.0.0.3',
+        height: 10,
+        syncmode: SyncMode.Full,
+      })
+      const [localServer, localService] = await setup({
+        location: '127.0.0.1',
+        height: 0,
+        syncmode: SyncMode.Light,
+      })
+      await localService.synchronizer!.stop()
+      await localServer.discover('remotePeer1', '127.0.0.2')
+      await localServer.discover('remotePeer2', '127.0.0.3')
+      localService.config.events.on(Event.SYNC_SYNCHRONIZED, async () => {
+        if (localService.chain.headers.height === BigInt(10)) {
+          assert.ok(true, 'synced with best peer')
+          await destroy(localServer, localService)
+          await destroy(remoteServer1, remoteService1)
+          await destroy(remoteServer2, remoteService2)
+        }
+      })
+      await localService.synchronizer!.start()
+    },
+    { timeout: 30000 }
+  )
 })
