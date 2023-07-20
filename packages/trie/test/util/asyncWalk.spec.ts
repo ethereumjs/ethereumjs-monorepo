4412639c56e8c54343cd6bdae2b5127c11f565e7
ScottyPoi <scott.simpson@ethereum.org>
2023-07-20 01:34:27 -0600

    write test/demo script

---
+++ packages/trie/test/util/asyncWalk.spec.ts
@@ -0,0 +1,122 @@
+/* eslint-disable no-console */
+import { bytesToHex, equalsBytes, hexToBytes, utf8ToBytes } from '@ethereumjs/util'
+import { assert, describe, it } from 'vitest'
+
+import { BranchNode, ExtensionNode, LeafNode, Trie } from '../../src/index.js'
+import { _walkTrie } from '../../src/util/asyncWalk.js'
+import trieTests from '../fixtures/trietest.json'
+
+import type { TrieNode } from '../../src/index.js'
+
+function getNodeType(node: TrieNode): string {
+  if (node instanceof BranchNode) {
+    return 'BranchNode'
+  }
+  if (node instanceof ExtensionNode) {
+    return 'Ext_Node'
+  }
+  if (node instanceof LeafNode) {
+    return 'LeafNode'
+  }
+  throw new Error(`Unknown node type: ${node}`)
+}
+
+function logNode(trie: Trie, node: TrieNode, currentKey: number[]): void {
+  console.log('--------------------------')
+  console.log(`------- \u2705 { ${getNodeType(node)} } \u2705 `)
+  if (equalsBytes((trie as any).hash(node.serialize()), trie.root())) {
+    console.log(
+      `{ 0x${bytesToHex((trie as any).hash(node.serialize())).slice(
+        0,
+        12
+      )}... } ---- \uD83D\uDCA5  \u211B \u2134 \u2134 \u0164  \u0147 \u2134 \u0221 \u2211  \u2737`
+    )
+  } else {
+    console.log(`{ 0x${bytesToHex((trie as any).hash(node.serialize())).slice(0, 12)}... } ----`)
+  }
+  console.log(
+    'walk from',
+    `[${currentKey}]`,
+    node instanceof ExtensionNode ? `((${node._nibbles}))` : ''
+  )
+  if ('_nibbles' in node) {
+    console.log(`  -- to =>`, `[${node._nibbles}]`)
+    console.log(`  -- next key: [${[...currentKey, node._nibbles]}]`)
+  } else if ('_branches' in node) {
+    let first = true
+    for (const k of [...node._branches.entries()]
+      .filter(([_, child]) => child !== null && child.length > 0)
+      .map(([nibble, _]) => nibble)) {
+      first || console.log('\uD83D\uDDD8  \u0026')
+      first = false
+      console.log(`  -- to =>`, `[${k}]`)
+      console.log(`  -- next key: [${[...currentKey, [k]]}]`)
+    }
+  }
+  console.log('--------------------------')
+}
+
+describe('official tests', async () => {
+  const testNames = Object.keys(trieTests.tests)
+
+  for await (const testName of testNames) {
+    const trie = new Trie()
+    describe(testName, async () => {
+      const inputs = (trieTests as any).tests[testName].in
+      const expect = (trieTests as any).tests[testName].root
+      const testKeys: Map<string, Uint8Array | null> = new Map()
+      const testStrings: Map<string, [string, string | null]> = new Map()
+      for await (const [idx, input] of inputs.entries()) {
+        const stringPair: [string, string] = [inputs[idx][0], inputs[idx][1] ?? 'null']
+        describe(`put: ${stringPair}`, async () => {
+          for (let i = 0; i < 2; i++) {
+            if (typeof input[i] === 'string' && input[i].slice(0, 2) === '0x') {
+              input[i] = hexToBytes(input[i])
+            } else if (typeof input[i] === 'string') {
+              input[i] = utf8ToBytes(input[i])
+            }
+          }
+          try {
+            await trie.put(input[0], input[1])
+            assert(true)
+          } catch (e) {
+            assert(false, (e as any).message)
+          }
+          trie.checkpoint()
+          await trie.commit()
+          trie.flushCheckpoints()
+          testKeys.set(bytesToHex(input[0]), input[1])
+          testStrings.set(bytesToHex(input[0]), stringPair)
+          describe(`should get all keys`, async () => {
+            for await (const [key, val] of testKeys.entries()) {
+              const retrieved = await trie.get(hexToBytes(key))
+              it(`should get ${testStrings.get(key)}`, async () => {
+                assert.deepEqual(retrieved, val)
+              })
+            }
+          })
+        })
+      }
+      it(`should have root ${expect}`, async () => {
+        assert.equal(bytesToHex(trie.root()), expect)
+      })
+      describe('walkTrie', async () => {
+        const walker = _walkTrie.bind(trie)(trie.root(), [])
+        console.log(`----------- { { { test: ${testName} } } } ---------`)
+        testName === 'branchingTests' &&
+          console.log(
+            `          \uD83C\uDF10  \u267B this trie should be empty \u267B \uD83C\uDF10 `
+          )
+        testName === 'branchingTests' && console.log('--------------------------')
+
+        for await (const { currentKey, node } of walker) {
+          logNode(trie, node, currentKey)
+        }
+
+        it('should be done', async () => {
+          assert.equal(true, true)
+        })
+      })
+    })
+  }
+})
