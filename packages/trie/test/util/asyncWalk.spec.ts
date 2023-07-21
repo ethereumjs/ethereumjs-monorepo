3e1185c4f05ec6e81f192a3ed84c7e206df7dea6
ScottyPoi <scott.simpson@ethereum.org>
2023-07-21 00:26:47 -0600

    update test with sparse trie example

--- packages/trie/test/util/asyncWalk.spec.ts
+++ packages/trie/test/util/asyncWalk.spec.ts
@@ -4,6 +4,7 @@ import { assert, describe, it } from 'vitest'
 
 import { BranchNode, ExtensionNode, LeafNode, Trie } from '../../src/index.js'
 import { _walkTrie } from '../../src/util/asyncWalk.js'
+import { bytesToNibbles } from '../../src/util/nibbles.js'
 import trieTests from '../fixtures/trietest.json'
 
 import type { TrieNode } from '../../src/index.js'
@@ -56,7 +57,7 @@ function logNode(trie: Trie, node: TrieNode, currentKey: number[]): void {
   console.log('--------------------------')
 }
 
-describe('official tests', async () => {
+describe('walk the tries from official tests', async () => {
   const testNames = Object.keys(trieTests.tests)
 
   for await (const testName of testNames) {
@@ -118,5 +119,65 @@ describe('official tests', async () => {
         })
       })
     })
+  }
+})
+
+describe('walk a sparse trie', async () => {
+  const trie = new Trie()
+  const inputs = (trieTests as any).tests.jeff.in
+  const expect = (trieTests as any).tests.jeff.root
+
+  // Build a Trie
+  for await (const input of inputs) {
+    for (let i = 0; i < 2; i++) {
+      if (typeof input[i] === 'string' && input[i].slice(0, 2) === '0x') {
+        input[i] = hexToBytes(input[i])
+      } else if (typeof input[i] === 'string') {
+        input[i] = utf8ToBytes(input[i])
+      }
+    }
+    await trie.put(input[0], input[1])
+  }
+  // Check the root
+  it(`should have root ${expect}`, async () => {
+    assert.equal(bytesToHex(trie.root()), expect)
+  })
+  // Generate a proof for inputs[0]
+  const proofKey = inputs[0][0]
+  const proof = await trie.createProof(proofKey)
+  assert.ok(await trie.verifyProof(trie.root(), proofKey, proof))
+
+  // Build a sparse trie from the proof
+  const fromProof = new Trie()
+  await fromProof.fromProof(proof)
+
+  // Walk the sparse trie
+  const walker = fromProof.walkTrieIterable(fromProof.root())
+  let found = 0
+  for await (const { currentKey, node } of walker) {
+    if (equalsBytes((fromProof as any).hash(node.serialize()), fromProof.root())) {
+      // The root of proof trie should be same as original
+      assert.deepEqual(fromProof.root(), trie.root())
+    }
+    if (node instanceof LeafNode) {
+      // The only leaf node should be leaf from the proof
+      const fullKeyNibbles = [...currentKey, ...node._nibbles]
+      assert.deepEqual(fullKeyNibbles, bytesToNibbles(proofKey))
+      assert.deepEqual(node.value(), inputs[0][1])
+    }
+    // Count the nodes...nodes from the proof should be only nodes in the trie
+    found++
+  }
+  assert.equal(found, proof.length)
+  assert.ok(true, 'Walking sparse trie should not throw error')
+
+  // Walk the same sparse trie with WalkController
+  try {
+    await fromProof.walkTrie(fromProof.root(), async (noderef, node, key, wc) => {
+      wc.allChildren(node!)
+    })
+    assert.fail('Will throw when it meets a missing node in a sparse trie')
+  } catch (err) {
+    assert.equal((err as any).message, 'Missing node in DB')
   }
 })
