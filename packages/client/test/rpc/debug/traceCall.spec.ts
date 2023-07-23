5504b1169fda184ea990bbf5de63fba2cacefa42
ScottyPoi <scott.simpson@ethereum.org>
2023-07-23 02:53:53 -0600

    Add simple method call test

--- packages/client/test/rpc/debug/traceCall.spec.ts
+++ packages/client/test/rpc/debug/traceCall.spec.ts
@@ -1,6 +1,19 @@
-import { describe, expect, it } from 'vitest'
+import { Block } from '@ethereumjs/block'
+import { TransactionFactory } from '@ethereumjs/tx'
+import { bytesToHex } from '@ethereumjs/util'
+import { assert, describe, expect, expectTypeOf, it } from 'vitest'
 
-import { baseRequest, createClient, createManager, params, startRPC } from '../helpers'
+import genesisJSON from '../../testdata/geth-genesis/debug.json'
+import {
+  baseRequest,
+  createClient,
+  createManager,
+  dummy,
+  params,
+  runBlockWithTxs,
+  setupChain,
+  startRPC,
+} from '../helpers'
 
 const method = 'debug_traceCall'
 
@@ -35,5 +48,81 @@ describe(method, () => {
       return res
     }
     await baseRequest(server, req, 200, expectRes)
+  })
+})
+
+describe('trace a call', async () => {
+  const { chain, common, execution, server } = await setupChain(genesisJSON, 'post-merge', {
+    txLookupLimit: 0,
+  })
+  // construct block with tx
+  const tx = TransactionFactory.fromTxData(
+    {
+      type: 0x2,
+      gasLimit: 0xfffff,
+      maxFeePerGas: 10,
+      maxPriorityFeePerGas: 1,
+      value: 10000,
+      data: '0x60AA',
+    },
+    { common, freeze: false }
+  ).sign(dummy.privKey)
+  tx.getSenderAddress = () => {
+    return dummy.addr
+  }
+  const block = Block.fromBlockData({}, { common })
+  block.transactions[0] = tx
+  await runBlockWithTxs(chain, execution, [tx], true)
+
+  it('call debug_traceCall with valid parameters', async () => {
+    const rpcTxReq = params('eth_getTransactionByHash', [bytesToHex(tx.hash())])
+    let rpcTx: any
+    const expectResTx = async (res: any) => {
+      const t = res._body.result
+
+      rpcTx = {
+        from: t.from,
+        gas: t.gas,
+        gasPrice: t.gasPrice,
+        value: t.value,
+        data: t.input,
+        maxPriorityFeePerGas: t.maxPriorityFeePerGas,
+        maxFeePerGas: t.maxFeePerGas,
+        type: t.type,
+      }
+    }
+    await baseRequest(server, rpcTxReq, 200, expectResTx, true)
+    const req2 = params('debug_traceCall', [rpcTx, '0x1', {}])
+    const expectRes2 = (res2: any) => {
+      expectTypeOf(res2._body.result)
+        .toHaveProperty('gas')
+        .toHaveProperty('returnValue')
+        .toHaveProperty('failed')
+        .toHaveProperty('structLogs')
+
+      assert.deepEqual(
+        res2._body.result,
+        {
+          gas: '0x3',
+          returnValue: '0x',
+          failed: false,
+          structLogs: [
+            {
+              pc: 0,
+              op: 'PUSH1',
+              gasCost: 6,
+              gas: 1048575,
+              depth: 0,
+              error: null,
+              stack: [],
+              storage: {},
+              memory: [],
+            },
+          ],
+        },
+        'produced a correct trace'
+      )
+    }
+    await baseRequest(server, req2, 200, expectRes2, true)
   })
 })
