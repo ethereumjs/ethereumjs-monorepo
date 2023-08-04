30ecc260947769521eb854ccac5d939cfa632464
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-08-04 09:11:05 -0400

    verkle: move rust verkle wasm to verkle package

---
+++ packages/verkle/src/rust-verkle-wasm/rust_verkle_wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * @param {Uint8Array} address_tree_index
+ * @returns {any}
+ */
+export function pedersen_hash(address_tree_index: Uint8Array): any
+/**
+ * @param {Uint8Array} js_root
+ * @param {Uint8Array} js_proof
+ * @param {Map<any, any>} js_key_values
+ * @returns {any}
+ */
+export function verify_update(
+  js_root: Uint8Array,
+  js_proof: Uint8Array,
+  js_key_values: Map<any, any>
+): any
