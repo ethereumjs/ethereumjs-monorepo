051fa68331ecac4776a467241857902a046677d3
ScottyPoi <scott.simpson@ethereum.org>
2023-08-06 00:08:00 -0600

    resolve trie test errors

--- packages/trie/test/trie/trie.spec.ts
+++ packages/trie/test/trie/trie.spec.ts
@@ -97,14 +97,13 @@ for (const { constructor, defaults, title } of [
         root: KECCAK256_RLP,
         useRootPersistence: true,
       })
-
-      // @ts-expect-error
-      assert.ok(equalsBytes((await trie._db.get(ROOT_DB_KEY))!, KECCAK256_RLP))
+      assert.equal(await (<any>trie)._db.db.get(bytesToHex(ROOT_DB_KEY)), bytesToHex(KECCAK256_RLP))
 
       await trie.put(utf8ToBytes('foo'), utf8ToBytes('bar'))
 
-      // @ts-expect-error
-      assert.isFalse(equalsBytes((await trie._db.get(ROOT_DB_KEY))!, KECCAK256_RLP))
+      assert.isFalse(
+        equalsBytes((await (<any>trie)._db.db.get(bytesToHex(ROOT_DB_KEY)))!, KECCAK256_RLP)
+      )
     })
 
     it('does not persist the root if the `useRootPersistence` option is `false`', async () => {
