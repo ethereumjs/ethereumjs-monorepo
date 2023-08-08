59030e5b5efc1ee55d1df07e1c807e34bfb7149f
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-08-08 09:09:44 -0400

    verkle: remove unnecessary constants & methods

--- packages/verkle/src/util/crypto.ts
+++ packages/verkle/src/util/crypto.ts
@@ -2,16 +2,6 @@ import { type Address, concatBytes, setLengthLeft, toBytes, writeInt32LE } from 
 
 import * as rustVerkleWasm from '../rust-verkle-wasm/rust_verkle_wasm.js'
 
-import {
-  BALANCE_LEAF_KEY,
-  CODE_KECCAK_LEAF_KEY,
-  CODE_OFFSET,
-  CODE_SIZE_LEAF_KEY,
-  NONCE_LEAF_KEY,
-  VERKLE_NODE_WIDTH,
-  VERSION_LEAF_KEY,
-} from './constants.js'
-
 export function pedersenHash(input: Uint8Array): Uint8Array {
   const pedersenHash = rustVerkleWasm.pedersen_hash(input)
 
@@ -24,7 +14,15 @@ export function pedersenHash(input: Uint8Array): Uint8Array {
   return pedersenHash
 }
 
-function getTreeKey(address: Address, treeIndex: number, subIndex: number): Uint8Array {
+/**
+ * @dev Returns the tree key for a given address, tree index, and sub index.
+ * @dev Assumes that the verkle node width = 256
+ * @param address The address to generate the tree key for.
+ * @param treeIndex The index of the tree to generate the key for.
+ * @param subIndex The sub index of the tree to generate the key for.
+ * @return The tree key as a Uint8Array.
+ */
+export function getTreeKey(address: Address, treeIndex: number, subIndex: number): Uint8Array {
   const address32 = setLengthLeft(address.toBytes(), 32)
 
   const treeIndexB = writeInt32LE(treeIndex)
@@ -34,32 +32,4 @@ function getTreeKey(address: Address, treeIndex: number, subIndex: number): Uint
   const treeKey = concatBytes(pedersenHash(input).slice(0, 31), toBytes(subIndex))
 
   return treeKey
-}
-
-export function getTreeKeyForVersion(address: Address) {
-  return getTreeKey(address, 0, VERSION_LEAF_KEY)
-}
-
-export function getTreeKeyForBalance(address: Address) {
-  return getTreeKey(address, 0, BALANCE_LEAF_KEY)
-}
-
-export function getTreeKeyForNonce(address: Address) {
-  return getTreeKey(address, 0, NONCE_LEAF_KEY)
-}
-
-export function getTreeKeyForCodeHash(address: Address) {
-  return getTreeKey(address, 0, CODE_KECCAK_LEAF_KEY)
-}
-
-export function getTreeKeyForCodeSize(address: Address) {
-  return getTreeKey(address, 0, CODE_SIZE_LEAF_KEY)
-}
-
-export function getTreeKeyForCodeChunk(address: Address, chunkId: number) {
-  return getTreeKey(
-    address,
-    Math.floor((CODE_OFFSET + chunkId) / VERKLE_NODE_WIDTH),
-    (CODE_OFFSET + chunkId) % VERKLE_NODE_WIDTH
-  )
 }
---
+++ packages/verkle/src/util/index.ts
@@ -0,0 +1,2 @@
+export * from './crypto.js'
+export * from './lock.js'
