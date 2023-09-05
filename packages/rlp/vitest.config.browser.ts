d81e64ee8a1ec75a2053c7b5caa2f298e9244d26
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-09-05 12:46:46 -0400

    fix config

--- packages/rlp/vitest.config.browser.ts
+++ packages/rlp/vitest.config.browser.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     silent: true,
-    exclude: ['test/cli'],
+    exclude: ['test/cli.spec.ts'],
     testTimeout: 180000,
   },
 })
