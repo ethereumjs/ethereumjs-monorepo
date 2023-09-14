67a20de06d965a21ac045c543b533962f4ace859
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-09-14 15:45:38 -0400

    tx: improve tx capability handling (#3010)
    * tx: TxCapability interfaces and rename generic capability to legacy
    
    * tx: txTypeBytes helper
    
    * tx: add missing methods and props to tx interfaces
    
    * tx: refactor and improve serialize helper
    
    * tx: implement update serialize helper and txTypeBytes helper
    
    * tx: refactor getDataFee for use in legacyTransaction
    
    * tx: remove redundant prop from EIP4844CompatibleTxInterface
    
    * tx: shorten compatibletxInterface name
    
    * tx: typedTransaction -> EIP2718CompatibleTx
    
    * tx: remove implements interface
    
    * tx: add legacytxinterface and move accesslists props to eip2930 interface

--- packages/tx/src/capabilities/eip2930.ts
+++ packages/tx/src/capabilities/eip2930.ts
@@ -1,42 +1,12 @@
-import { RLP } from '@ethereumjs/rlp'
-import { concatBytes, hexToBytes } from '@ethereumjs/util'
-import { keccak256 } from 'ethereum-cryptography/keccak.js'
-
-import { BaseTransaction } from '../baseTransaction.js'
-import { type TypedTransaction } from '../types.js'
 import { AccessLists } from '../util.js'
 
-import type { Transaction, TransactionType } from '../types.js'
+import * as Legacy from './legacy.js'
 
-type TypedTransactionEIP2930 = Exclude<TypedTransaction, Transaction[TransactionType.Legacy]>
+import type { EIP2930CompatibleTx } from '../types.js'
 
 /**
  * The amount of gas paid for the data in this tx
  */
-export function getDataFee(tx: TypedTransactionEIP2930): bigint {
-  if (tx['cache'].dataFee && tx['cache'].dataFee.hardfork === tx.common.hardfork()) {
-    return tx['cache'].dataFee.value
-  }
-
-  let cost = BaseTransaction.prototype.getDataFee.bind(tx)()
-  cost += BigInt(AccessLists.getDataFeeEIP2930(tx.accessList, tx.common))
-
-  if (Object.isFrozen(tx)) {
-    tx['cache'].dataFee = {
-      value: cost,
-      hardfork: tx.common.hardfork(),
-    }
-  }
-
-  return cost
-}
-
-export function getHashedMessageToSign(tx: TypedTransactionEIP2930): Uint8Array {
-  return keccak256(tx.getMessageToSign())
-}
-
-export function serialize(tx: TypedTransactionEIP2930): Uint8Array {
-  const base = tx.raw()
-  const txTypeBytes = hexToBytes('0x' + tx.type.toString(16).padStart(2, '0'))
-  return concatBytes(txTypeBytes, RLP.encode(base))
+export function getDataFee(tx: EIP2930CompatibleTx): bigint {
+  return Legacy.getDataFee(tx, BigInt(AccessLists.getDataFeeEIP2930(tx.accessList, tx.common)))
 }
