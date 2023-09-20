8e4b5fba392f9ffb972fd5d8d54bb7fcbf65c8a1
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-09-20 16:29:24 +0200

    blockchain: edit holesky genesis hash

--- packages/blockchain/test/index.spec.ts
+++ packages/blockchain/test/index.spec.ts
@@ -42,7 +42,7 @@ describe('blockchain test', () => {
 
   it('should initialize holesky correctly', async () => {
     // Taken from: https://github.com/eth-clients/holesky/blob/36e4ff2d5138dcb2eb614f0f60fdb060b2adc1e2/README.md#metadata
-    const holeskyHash = '0xff9006519a8ce843ac9c28549d24211420b546e12ce2d170c77a8cca7964f23d'
+    const holeskyHash = '0xb5f7f912443c940f21fd611f12828d75b534364ed9e95ca4e307729a4661bde4'
     const common = new Common({ chain: Chain.Holesky })
     const blockchain = await Blockchain.create({
       common,
