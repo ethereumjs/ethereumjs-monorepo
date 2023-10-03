7920f8cb4a69dd0d78695883a6d3f9e2743a3571
Amir <indigophi@protonmail.com>
2023-10-03 13:43:54 -0700

    Update test

--- packages/client/test/rpc/eth/coinbase.spec.ts
+++ packages/client/test/rpc/eth/coinbase.spec.ts
@@ -6,16 +6,20 @@ const method = 'eth_coinbase'
 
 describe(method, () => {
   it('call', async () => {
+    const coinbase: string = 'abc'
     const { server } = baseSetup({
-      minerCoinbase: 'abc',
+      minerCoinbase: coinbase,
     })
 
     const req = params(method, [])
     const expectRes = (res: any) => {
       const responseBlob = res.body
-      const msg = 'coinbase address should be a string'
-      console.log(responseBlob)
-      assert.equal(typeof responseBlob.result, 'string', msg)
+      assert.equal(typeof responseBlob.result, 'string', 'coinbase address should be a string')
+      assert.equal(
+        responseBlob.result,
+        coinbase,
+        "coinbase address should be same as value it's been set to"
+      )
     }
     await baseRequest(server, req, 200, expectRes)
   })
