a1adf565a26efd115706817c14ce68d613fe3bd7
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-10-26 21:22:21 -0400

    verkle: fix linting

--- packages/verkle/.eslintrc.cjs
+++ packages/verkle/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.json', './tsconfig.benchmarks.json'],
   },
+  ignorePatterns: ['src/rust-verkle-wasm/rust_verkle_wasm.js', '**/vendor/*.js'],
   overrides: [
     {
       files: ['benchmarks/*.ts'],
