14ff37cdd97c430c94da8141fc0fa9f7900b2b78
harkamal <develop@g11tech.io>
2023-10-31 21:45:03 +0530

    comment cleanup

--- packages/client/src/rpc/modules/engine.ts
+++ packages/client/src/rpc/modules/engine.ts
@@ -919,19 +919,12 @@ export class Engine {
       const errorMsg = `${error}`.toLowerCase()
       if (errorMsg.includes('block') && errorMsg.includes('not found')) {
         if (blocks.length > 1) {
-          // this error can come if the block tries to load a previous block yet not in the chain
-          // error coding of the evm errors should be a better way to handle this
+          // this error can come if the block tries to load a previous block yet not in the chain via BLOCKHASH
+          // opcode.
           //
-          // at Blockchain.getBlock (/home/gajinder/ethereumjs-monorepo/packages/blockchain/src/blockchain.ts:763:15)
-          // at async i (/home/gajinder/ethereumjs-monorepo/packages/evm/src/opcodes/functions.ts:606:21)
-          // at async Interpreter.runStep (/home/gajinder/ethereumjs-monorepo/packages/evm/src/interpreter.ts:320:9)
-          // at async Interpreter.run (/home/gajinder/ethereumjs-monorepo/packages/evm/src/interpreter.ts:257:9)
-          // at async EVM.runInterpreter (/home/gajinder/ethereumjs-monorepo/packages/evm/src/evm.ts:594:28)
-          // at async EVM._executeCall (/home/gajinder/ethereumjs-monorepo/packages/evm/src/evm.ts:301:16)
-          // at async EVM.runCall (/home/gajinder/ethereumjs-monorepo/packages/evm/src/evm.ts:718:16)
-          // at async Interpreter._baseCall (/home/gajinder/ethereumjs-monorepo/packages/evm/src/interpreter.ts:930:21)
-          // at async i (/home/gajinder/ethereumjs-monorepo/packages/evm/src/opcodes/functions.ts:1086:19)
-          // at async Interpreter.runStep (/home/gajinder/ethereumjs-monorepo/packages/evm/src/interpreter.ts:320:9)
+          // i)  error coding of the evm errors should be a better way to handle this OR
+          // ii) figure out a way to pass let the evm access the above blocks which is what connects this
+          //     chain to vmhead. to be handled in skeleton refactoring to blockchain class
 
           const response = { status: Status.SYNCING, latestValidHash, validationError: null }
           return response
