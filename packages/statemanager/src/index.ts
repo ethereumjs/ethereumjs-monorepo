902eabdd2f3e1f3fcc8b07880fd160742941e290
Amir <indigophi@protonmail.com>
2023-11-28 16:37:36 -0700

    Export flatStateManager

--- packages/statemanager/src/index.ts
+++ packages/statemanager/src/index.ts
@@ -1,4 +1,5 @@
 export * from './cache/index.js'
 export * from './ethersStateManager.js'
+export * from './flat/flatStateManager.js'
 export * from './flat/snapshot.js'
 export * from './stateManager.js'
