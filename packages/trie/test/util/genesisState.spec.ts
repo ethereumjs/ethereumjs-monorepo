e3c3929fec0293fe09d3d2b0ca080c0281904c16
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-03 11:16:21 -0800

    remove circulare dependency
    fix genesis state root change
    
    fix: lint
    
    fix:lint

--- packages/trie/test/util/genesisState.spec.ts
+++ packages/trie/test/util/genesisState.spec.ts
@@ -1,12 +1,10 @@
-import { getGenesis } from '@ethereumjs/genesis'
 import { bytesToHex, parseGethGenesisState } from '@ethereumjs/util'
 import { assert, describe, it } from 'vitest'
 
 import { genesisStateRoot } from '../../src/util/genesisState.js'
 // kiln genesis with deposit contract storage set
 import gethGenesisKilnJSON from '../fixtures/geth-genesis-kiln.json'
-
-import type { GenesisState } from '@ethereumjs/util'
+import mainnetGenesis from '../fixtures/mainnet-genesis.json'
 
 describe('[Util/genesisStateRoot]', () => {
   it('should properly generate stateRoot from gethGenesis', async () => {
@@ -21,9 +19,8 @@ describe('[Util/genesisStateRoot]', () => {
 })
 
 it('should correctly derive mainnet stateRoot from ethereumjs genesis', async () => {
-  const stateRoot = await genesisStateRoot(getGenesis(1) as GenesisState)
   assert.equal(
-    bytesToHex(stateRoot),
+    bytesToHex(await genesisStateRoot(mainnetGenesis)),
     '0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544',
     'mainnet stateRoot matches'
   )
