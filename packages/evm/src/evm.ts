a90747b2908418577be64091eb67fda485fc83c3
Amir <indigophi@protonmail.com>
2023-12-04 16:52:26 -0700

    Use FSM for evm and vm blockchain test

--- packages/evm/src/evm.ts
+++ packages/evm/src/evm.ts
@@ -1,5 +1,5 @@
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
-import { DefaultStateManager } from '@ethereumjs/statemanager'
+import { DefaultStateManager, FlatStateManager } from '@ethereumjs/statemanager'
 import {
   Account,
   Address,
@@ -159,7 +159,8 @@ export class EVM implements EVMInterface {
     }
 
     this.blockchain = blockchain
-    this.stateManager = opts.stateManager ?? new DefaultStateManager()
+    // this.stateManager = opts.stateManager ?? new DefaultStateManager()
+    this.stateManager = new FlatStateManager()
 
     // Supported EIPs
     const supportedEIPs = [
