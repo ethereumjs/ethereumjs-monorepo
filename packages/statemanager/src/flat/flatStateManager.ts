ff319755e6cd7a52e6ed3dc4a9b37adce55d0c1d
Amir <indigophi@protonmail.com>
2023-12-05 10:27:43 -0700

    Implement shallowCopy

--- packages/statemanager/src/flat/flatStateManager.ts
+++ packages/statemanager/src/flat/flatStateManager.ts
@@ -26,32 +26,7 @@ export interface FlatStateManagerOpts {
   /**
    * A {@link Trie} instance
    */
-  trie?: Trie
-  /**
-   * Option to prefix codehashes in the database. This defaults to `true`.
-   * If this is disabled, note that it is possible to corrupt the trie, by deploying code
-   * which code is equal to the preimage of a trie-node.
-   * E.g. by putting the code `0x80` into the empty trie, will lead to a corrupted trie.
-   */
-  prefixCodeHashes?: boolean
-
-  /**
-   * Option to prefix the keys for the storage tries with the first 7 bytes from the
-   * associated account address. Activating this option gives a noticeable performance
-   * boost for storage DB reads when operating on larger tries.
-   *
-   * Note: Activating/deactivating this option causes continued state reads to be
-   * incompatible with existing databases.
-   *
-   * Default: false (for backwards compatibility reasons)
-   */
-  prefixStorageTrieKeys?: boolean
-
-  // accountCacheOpts?: CacheOptions
-
-  // storageCacheOpts?: CacheOptions
-
-  // codeCacheOpts?: CacheOptions
+  // trie?: Trie
 
   /**
    * The common to use
@@ -428,8 +403,16 @@ export class FlatStateManager implements EVMStateManagerInterface {
    * Cache values are generally not copied along regardless of the
    * `downlevelCaches` setting.
    */
-  shallowCopy(downlevelCaches: boolean = true): EVMStateManagerInterface {
-    throw new Error('Not yet implemented')
+  shallowCopy(downlevelCaches: boolean = true): FlatStateManager {
+    const common = this.common.copy()
+    common.setHardfork(this.common.hardfork())
+
+    const snapshot = new Snapshot(this._snapshot._db)
+
+    return new FlatStateManager({
+      common,
+      snapshot,
+    })
   }
 
   /**
