59aac4df9bb985129ee15fb1a4d43b298bf928ff
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-12-05 09:40:58 -0500

    block: add description to verkle execution witness

--- packages/block/src/types.ts
+++ packages/block/src/types.ts
@@ -92,8 +92,21 @@ export interface VerkleStateDiff {
   }[]
 }
 
+/**
+ * Experimental, object format could eventual change.
+ * An object that provides the state and proof necessary for verkle stateless execution
+ * */
 export interface VerkleExecutionWitness {
+  /**
+   * An array of state diffs.
+   * Each item corresponding to state accesses or state modifications of the block.
+   * In the current design, it also contains the resulting state of the block execution (post-state).
+   */
   stateDiff: VerkleStateDiff[]
+  /**
+   * The verkle proof for the block.
+   * Proves that the provided stateDiff belongs to the canonical verkle tree.
+   */
   verkleProof: VerkleProof
 }
 
