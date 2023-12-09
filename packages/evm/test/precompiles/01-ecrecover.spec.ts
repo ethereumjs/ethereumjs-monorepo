d46a9967c4d3bd45fc5a430cdd36bdc89662af2f
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-12-09 12:20:46 +0100

    evm: fix ecrecover test

--- packages/evm/test/precompiles/01-ecrecover.spec.ts
+++ packages/evm/test/precompiles/01-ecrecover.spec.ts
@@ -1,11 +1,11 @@
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
-import { bytesToHex, hexToBytes } from '@ethereumjs/util'
+import { bytesToHex, bytesToUnprefixedHex, hexToBytes, utf8ToBytes } from '@ethereumjs/util'
 import { keccak256 } from 'ethereum-cryptography/keccak.js'
 import { assert, describe, it } from 'vitest'
 
 import { EVM, getActivePrecompiles } from '../../src/index.js'
 
-const prefix = '\x19Ethereum Signed Message:\n32'
+const prefix = bytesToUnprefixedHex(utf8ToBytes('\x19Ethereum Signed Message:\n32'))
 const _hash = '852daa74cc3c31fe64542bb9b8764cfb91cc30f9acf9389071ffb44a9eefde46'
 const _v = '000000000000000000000000000000000000000000000000000000000000001b'
 const _r = 'b814eaab5953337fed2cf504a5b887cddd65a54b7429d7b191ff1331ca0726b1'
@@ -21,7 +21,7 @@ describe('Precompiles: ECRECOVER', () => {
     const addressStr = '0000000000000000000000000000000000000001'
     const ECRECOVER = getActivePrecompiles(common).get(addressStr)!
 
-    const prefixedMessage = keccak256(hexToBytes(`0x${prefix}${_hash}`))
+    const prefixedMessage = bytesToUnprefixedHex(keccak256(hexToBytes(`0x${prefix}${_hash}`)))
     const data = hexToBytes(`0x${prefixedMessage}${_v}${_r}${_s}`)
     const result = await ECRECOVER({
       data,
@@ -30,6 +30,10 @@ describe('Precompiles: ECRECOVER', () => {
       _EVM: evm,
     })
 
-    assert.deepEqual(bytesToHex(result.returnValue), address, 'should recover expected address')
+    assert.deepEqual(
+      bytesToHex(result.returnValue.slice(-20)),
+      address,
+      'should recover expected address'
+    )
   })
 })
