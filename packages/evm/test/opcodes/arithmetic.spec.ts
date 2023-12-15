76211c58f1a033194be282aa76235849e9619a7b
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-12-15 03:41:08 +0100

    evm: add arithmetic tests + test framework

--- packages/evm/test/opcodes/arithmetic.spec.ts
+++ packages/evm/test/opcodes/arithmetic.spec.ts
@@ -1,2 +1,180 @@
 // All tests for arithmetic opcodes
 // Range: 0x01 - 0x0B
+
+import {
+  BIGINT_160,
+  BIGINT_224,
+  BIGINT_2EXP160,
+  BIGINT_2EXP224,
+  BIGINT_2EXP96,
+  BIGINT_96,
+} from '@ethereumjs/util'
+import { describe, it } from 'vitest'
+
+import { toTwos } from '../../src/opcodes/util.js'
+
+import { runOpcodeTest } from './utils.js'
+
+import type { Expected, InputStackItems } from './utils.js'
+
+const BIGINT_2EXP256 = BigInt(2) ** BigInt(256)
+const STACK_MAX_NUMBER = BIGINT_2EXP256 - BigInt(1)
+
+function pow2(pow: number) {
+  return BigInt(2) ** BigInt(pow)
+}
+
+function sign(input: bigint | number) {
+  if (typeof input === 'number') {
+    return toTwos(BigInt(input))
+  }
+  return toTwos(input)
+}
+
+const testCases: {
+  [opcodeName: string]: {
+    stack: InputStackItems
+    expected: Expected
+    name?: string
+  }[]
+} = {
+  ADD: [
+    { stack: [0, 0], expected: 0 },
+    { stack: [1, 0], expected: 1 },
+    { stack: [0, 1], expected: 1 },
+    { stack: [STACK_MAX_NUMBER, 1], expected: 0, name: 'overflow check' },
+  ],
+  MUL: [
+    { stack: [0, 0], expected: 0 },
+    { stack: [0, 1], expected: 0 },
+    { stack: [1, 0], expected: 0 },
+    { stack: [1, 1], expected: 1 },
+    { stack: [1, 2], expected: 2 },
+    { stack: [2, 2], expected: 4 },
+    { stack: [BIGINT_2EXP256 / BigInt(2), 2], expected: 0, name: 'overflow check' },
+  ],
+  SUB: [
+    { stack: [0, 0], expected: 0 },
+    { stack: [0, 1], expected: STACK_MAX_NUMBER, name: 'underflow check' },
+    { stack: [1, 0], expected: 1 },
+    { stack: [1, 1], expected: 0 },
+    { stack: [2, 1], expected: 1 },
+    { stack: [4, 2], expected: 2 },
+  ],
+  DIV: [
+    { stack: [0, 0], expected: 0, name: '0/0 check' },
+    { stack: [0, 1], expected: 0 },
+    { stack: [1, 0], expected: 0, name: '1/0 check' },
+    { stack: [1, 1], expected: 1 },
+    { stack: [2, 1], expected: 2 },
+    { stack: [4, 2], expected: 2 },
+    { stack: [pow2(255), 2], expected: pow2(254) },
+  ],
+  SDIV: [
+    { stack: [0, 0], expected: 0, name: '0/0 check' },
+    { stack: [0, 1], expected: 0 },
+    { stack: [1, 0], expected: 0, name: '1/0 check' },
+    { stack: [1, 1], expected: 1 },
+    { stack: [2, 1], expected: 2 },
+    { stack: [4, 2], expected: 2 },
+    { stack: [0, sign(-1)], expected: 0 },
+    { stack: [1, sign(-1)], expected: sign(-1) },
+    { stack: [sign(-1), sign(-1)], expected: 1 },
+    { stack: [2, sign(-1)], expected: sign(-2) },
+    { stack: [4, sign(-2)], expected: sign(-2) },
+  ],
+  MOD: [
+    { stack: [0, 0], expected: 0, name: '0%0 check' },
+    { stack: [1, 0], expected: 0, name: '1%0 check' },
+    { stack: [1, 1], expected: 0 },
+    { stack: [2, 1], expected: 0 },
+    { stack: [4, 2], expected: 0 },
+    { stack: [5, 2], expected: 1 },
+    { stack: [STACK_MAX_NUMBER, 2], expected: 1 },
+  ],
+  SMOD: [
+    { stack: [0, 0], expected: 0, name: '0%0 check' },
+    { stack: [1, 0], expected: 0, name: '1%0 check' },
+    { stack: [1, 1], expected: 0 },
+    { stack: [2, 1], expected: 0 },
+    { stack: [4, 2], expected: 0 },
+    { stack: [5, 2], expected: 1 },
+    { stack: [sign(-1), 2], expected: sign(-1) },
+    { stack: [sign(-1), sign(-2)], expected: sign(-1) },
+    { stack: [sign(-1), sign(-1)], expected: sign(0) },
+  ],
+  ADDMOD: [
+    { stack: [0, 0, 0], expected: 0, name: '(0+0)%0 check' },
+    { stack: [0, 1, 0], expected: 0, name: '(0+1)%0 check' },
+    { stack: [1, 0, 0], expected: 0, name: '(1+0)%0 check' },
+    { stack: [1, 1, 0], expected: 0, name: '(1+1)%0 check' },
+    { stack: [1, 1, 2], expected: 0 },
+    { stack: [1, 2, 2], expected: 1 },
+    { stack: [STACK_MAX_NUMBER, 2, 2], expected: 1 },
+    { stack: [STACK_MAX_NUMBER, STACK_MAX_NUMBER, 2], expected: 0 },
+  ],
+  MULMOD: [
+    { stack: [0, 0, 0], expected: 0, name: '(0*0)%0 check' },
+    { stack: [0, 1, 0], expected: 0, name: '(0*1)%0 check' },
+    { stack: [1, 0, 0], expected: 0, name: '(1*0)%0 check' },
+    { stack: [1, 1, 0], expected: 0, name: '(1*1)%0 check' },
+    { stack: [1, 1, 2], expected: 1 },
+    { stack: [1, 2, 2], expected: 0 },
+    { stack: [STACK_MAX_NUMBER, 2, 2], expected: 0 },
+    { stack: [STACK_MAX_NUMBER, STACK_MAX_NUMBER, 2], expected: 1 },
+  ],
+  EXP: [
+    // Note: the return values have been verified against results on Sepolia
+    { stack: [0, 0], expected: 1, name: '0**0 check' },
+    { stack: [0, 1], expected: 0, name: '0**1 check' },
+    { stack: [1, 0], expected: 1, name: '1**0 check' },
+    { stack: [1, 1], expected: 1 },
+    { stack: [2, BIGINT_96], expected: BIGINT_2EXP96, name: '2**96 check' },
+    { stack: [2, BIGINT_160], expected: BIGINT_2EXP160, name: '2**160 check' },
+    { stack: [2, BIGINT_224], expected: BIGINT_2EXP224, name: '2**224 check' },
+    { stack: [3, 2], expected: 3 ** 2 },
+    { stack: [3, 3], expected: 3 ** 3 },
+    { stack: [STACK_MAX_NUMBER, STACK_MAX_NUMBER], expected: STACK_MAX_NUMBER },
+  ],
+  SIGNEXTEND: [
+    // SIGNEXTEND opcode explanation:
+    // There are two items on the stack, the [byteLength, value]:
+    // The value is interpreted to have the signed bit set at index `byteLength * 8` + 7 (call this `signIndex`)
+    // Now, the signed value is extended to 256 bits
+    // In practice this means:
+    // If value[signIndex] = 1, this means the number is negative
+    // Now left-fill all bits with 1 (so, when interpeting this number as signed in EVM, the original number does not change)
+    // If value[signIndex] = 0, left-fill all bits with 0
+    // This is thus handy if on stack there is an item which should be interpreted as, lets say int16 or uint16
+    // If one would take this item from stack from dirty memory, this could mean that the bytes left of this
+    // value are dirty, so we can clean it up.
+    // Also handy to left-fill bytes with either 1s or 0s depending on the situation
+    { stack: [0, 0b10000000], expected: sign(-128) },
+    { stack: [0, 0b1111111100000011], expected: 3 },
+    { stack: [32, STACK_MAX_NUMBER], expected: STACK_MAX_NUMBER },
+    { stack: [32, 1], expected: 1 },
+  ],
+}
+
+describe('Arithmetic tests', () => {
+  for (const opcodeName in testCases) {
+    it(`should test arithmetic opcode ${opcodeName}`, async () => {
+      const testDataArray = testCases[opcodeName]
+      for (const testData of testDataArray) {
+        let testName: string
+        if (testData.name === undefined) {
+          testName = `${opcodeName}: ${testData.stack.join()}`
+        } else {
+          testName = `${opcodeName}: ${testData.name}`
+        }
+        await runOpcodeTest({
+          testName,
+          opcodeName,
+          expected: testData.expected,
+          expectedReturnType: 'topStack',
+          input: testData.stack,
+        })
+      }
+    })
+  }
+})
---
+++ packages/evm/test/opcodes/utils.ts
@@ -0,0 +1,216 @@
+import { Chain, Common, Hardfork } from '@ethereumjs/common'
+import {
+  BIGINT_0,
+  bytesToHex,
+  bytesToUnprefixedHex,
+  equalsBytes,
+  hexToBytes,
+  setLengthLeft,
+  toBytes,
+} from '@ethereumjs/util'
+import { assert } from 'vitest'
+
+import { EVM } from '../../src/index.js'
+import { eipOpcodes, hardforkOpcodes, opcodes } from '../../src/opcodes/codes.js'
+
+const allHardforks = Object.keys(Hardfork)
+
+export type Expected = string | 'fails' | number | bigint
+export type InputStackItems = (number | bigint | Uint8Array)[]
+export type TestReturnType = 'topStack' | 'memory' | 'none'
+
+type OpcodeTestOpts = {
+  hardforks?: Hardfork[]
+  testName: string
+  opcodeName: string
+  expected: Expected
+  expectedReturnType: TestReturnType
+  input: InputStackItems
+  gasLimit?: bigint
+}
+
+const defaultOpcodeTestOpts = {
+  hardforks: [Hardfork.Shanghai],
+  gasLimit: BigInt(30_000_000),
+}
+
+type TestOpts = {
+  hardforks?: Hardfork[] | 'all'
+  testName: string
+  testCode: string
+  profile?: boolean
+  profileOpcode?: string | false
+  gasLimit?: bigint
+}
+
+const defaultTestOpts: Partial<TestOpts> = {
+  hardforks: [Hardfork.Shanghai],
+  profile: false,
+  profileOpcode: false,
+  gasLimit: BigInt(30_000_00),
+}
+
+function removeHexPrefix(input: string) {
+  return input.startsWith('0x') ? input.slice(2) : input
+}
+
+export function getOpcodeByte(name: string) {
+  for (const opNumber in opcodes) {
+    const opcode = opcodes[opNumber]
+    if (opcode.name === name) {
+      return parseInt(opNumber).toString(16).padStart(2, '0')
+    }
+  }
+  for (const entry of hardforkOpcodes) {
+    for (const opNumber in entry.opcodes) {
+      const opcode = entry.opcodes[opNumber]
+      if (opcode.name === name) {
+        return parseInt(opNumber).toString(16).padStart(2, '0')
+      }
+    }
+  }
+  for (const entry of eipOpcodes) {
+    for (const opNumber in entry.opcodes) {
+      const opcode = entry.opcodes[opNumber]
+      if (opcode.name === name) {
+        return parseInt(opNumber).toString(16).padStart(2, '0')
+      }
+    }
+  }
+  throw new Error(`Opcode ${name} not found`)
+}
+
+/**
+ * Setups stack items in the EVM
+ * @param stack Stack items to push. The first item of the stack is the topmost item of the resulting stack
+ * @returns Hex string output to setup this stack
+ */
+export function initStack(stack: (bigint | Uint8Array)[]) {
+  let output = ''
+  for (const entry of stack.reverse()) {
+    if (entry === BIGINT_0) {
+      output += '6000'
+    } else {
+      const bytes = toBytes(entry)
+      if (bytes.length === 0) {
+        output += '6000'
+      } else {
+        if (bytes.length > 32) {
+          throw new Error(
+            `Cannot push item, bytes length of ${bytes.length} larger than max of 32 bytes`
+          )
+        }
+        output += (0x60 + bytes.length - 1).toString(16)
+        output += bytesToUnprefixedHex(bytes)
+      }
+    }
+  }
+  return output
+}
+
+// String to return the topmost stack item from the current frame
+export const returnStackItem = '60005260206000F3'
+
+// String to return the contents of memory
+export const returnMemory = '596000F3'
+
+/**
+ * Returns code which loops forever
+ */
+export function makeLoopCode(input: string) {
+  '5B' + removeHexPrefix(input) + '600056'
+}
+
+export function createBytecode(input: string[]) {
+  return '0x' + input.join('')
+}
+
+export function createOpcodeTest(
+  input: InputStackItems,
+  opcodeName: string,
+  returnType: TestReturnType = 'topStack'
+) {
+  input = input.map((e) => {
+    if (typeof e === 'number') {
+      return BigInt(e)
+    } else {
+      return e
+    }
+  })
+  const opcode = getOpcodeByte(opcodeName)
+  const inputStack = initStack(<(bigint | Uint8Array)[]>input)
+  let returnCode = ''
+  switch (returnType) {
+    case 'topStack':
+      returnCode = returnStackItem
+      break
+    case 'memory':
+      returnCode = returnMemory
+      break
+  }
+  return createBytecode([inputStack, opcode, returnCode])
+}
+
+export async function runTest(opts: TestOpts) {
+  opts = {
+    ...defaultTestOpts,
+    ...opts,
+  }
+  const { hardforks, testCode, gasLimit } = <Required<TestOpts>>opts
+
+  const hfs = hardforks === 'all' ? allHardforks : hardforks
+  for (const hf of hfs) {
+    const common = new Common({ chain: Chain.Mainnet, hardfork: hf })
+    const evm = new EVM({
+      common,
+    })
+    await evm.runCode({
+      code: hexToBytes(testCode),
+      gasLimit,
+    })
+  }
+}
+
+export async function runOpcodeTest(opts: OpcodeTestOpts) {
+  opts = {
+    ...defaultOpcodeTestOpts,
+    ...opts,
+  }
+  const { hardforks, gasLimit, expected, input, expectedReturnType, opcodeName } = <
+    Required<OpcodeTestOpts>
+  >opts
+  const code = createOpcodeTest(input, opcodeName, expectedReturnType)
+  for (const hf of hardforks) {
+    const common = new Common({ chain: Chain.Mainnet, hardfork: hf })
+    const evm = new EVM({
+      common,
+    })
+    const result = await evm.runCode({
+      code: hexToBytes(code),
+      gasLimit,
+    })
+    if (expected === 'fails') {
+      if (result.exceptionError !== undefined) {
+        assert.fail(`Test ${opts.testName} should throw, but did not throw`)
+      }
+    } else {
+      let expectedReturn: Uint8Array
+      if (opts.expected === 0 || opts.expected === BigInt(0)) {
+        if (expectedReturnType === 'topStack') {
+          expectedReturn = new Uint8Array(32)
+        } else {
+          expectedReturn = new Uint8Array()
+        }
+      } else {
+        expectedReturn = toBytes(opts.expected)
+      }
+      expectedReturn = setLengthLeft(expectedReturn, Math.ceil(expectedReturn.length / 32) * 32)
+      assert.ok(
+        equalsBytes(result.returnValue, expectedReturn),
+        `Test ${opts.testName} failed. Output expected: ${bytesToHex(
+          expectedReturn
+        )}, got ${bytesToHex(result.returnValue)}, hardfork: ${hf}, input code: ${code}`
+      )
+    }
+  }
+}
