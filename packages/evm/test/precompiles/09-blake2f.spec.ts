e73f79cc87428049bdac2e5fe2a0d4fc333b82d6
Jochem Brouwer <jochembrouwer96@gmail.com>
2023-12-16 01:39:32 +0100

    evm: fix blake2f "it.only"

--- packages/evm/test/precompiles/09-blake2f.spec.ts
+++ packages/evm/test/precompiles/09-blake2f.spec.ts
@@ -115,7 +115,7 @@ describe('Precompiles: BLAKE2F', () => {
     })
   }
 
-  it.only('should also work on non-zero aligned inputs', async () => {
+  it('should also work on non-zero aligned inputs', async () => {
     const addr = Address.zero()
     // Blake2f calldata from https://etherscan.io/tx/0x4f2e13a0a3f14033630ab2b8cdad09d316826375f761ded5b31253bb42e0a476
     // (This tx calls into Blake2f multiple times, but one of them is taken)
