783dae067e6d6ea43e0295e1dfddd61fb33652a8
Amir <indigophi@protonmail.com>
2023-12-19 16:38:15 -0700

    Comment out tests to see how codecov handles report generation and processing

--- packages/rlp/test/invalid.spec.ts
+++ packages/rlp/test/invalid.spec.ts
@@ -24,25 +24,25 @@ describe('invalid tests', () => {
     })
   }
 
-  it('should pass long string sanity check test', function () {
-    // long string invalid test; string length > 55
-    const longBufferTest = RLP.encode(
-      'zoo255zoo255zzzzzzzzzzzzssssssssssssssssssssssssssssssssssssssssssssss'
-    )
-    // sanity checks
-    assert.ok(longBufferTest[0] > 0xb7)
-    assert.ok(longBufferTest[0] <= 0xbf)
+  // it('should pass long string sanity check test', function () {
+  //   // long string invalid test; string length > 55
+  //   const longBufferTest = RLP.encode(
+  //     'zoo255zoo255zzzzzzzzzzzzssssssssssssssssssssssssssssssssssssssssssssss'
+  //   )
+  //   // sanity checks
+  //   assert.ok(longBufferTest[0] > 0xb7)
+  //   assert.ok(longBufferTest[0] <= 0xbf)
 
-    // try to decode the partial buffer
-    assert.throws(
-      () => {
-        RLP.decode(longBufferTest.slice(1, longBufferTest.length - 1))
-      },
-      undefined,
-      undefined,
-      'string longer than 55 bytes: should throw'
-    )
-  })
+  //   // try to decode the partial buffer
+  //   assert.throws(
+  //     () => {
+  //       RLP.decode(longBufferTest.slice(1, longBufferTest.length - 1))
+  //     },
+  //     undefined,
+  //     undefined,
+  //     'string longer than 55 bytes: should throw'
+  //   )
+  // })
 })
 
 // The tests below are taken from Geth
