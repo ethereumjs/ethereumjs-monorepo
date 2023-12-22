de7ac90a77a32adb485b0087e53ca1a767a67835
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-12-22 12:57:44 -0500

    add test for custom ecrecover

--- packages/common/test/customCrypto.spec.ts
+++ packages/common/test/customCrypto.spec.ts
@@ -7,6 +7,15 @@ describe('[Common]: Custom Crypto', () => {
   const customKeccak256 = (msg: Uint8Array) => {
     return concatBytes(msg, new Uint8Array([1]))
   }
+  const customEcrecover = (
+    msgHash: Uint8Array,
+    v: bigint,
+    r: Uint8Array,
+    s: Uint8Array,
+    _chainID?: bigint
+  ) => {
+    return concatBytes(msgHash, Uint8Array.from([Number(v)]), r, s)
+  }
 
   it('keccak256', () => {
     const customCrypto = {
@@ -25,5 +34,21 @@ describe('[Common]: Custom Crypto', () => {
     c = Common.custom(customChainParams, { customCrypto })
     msg = 'Should initialize with custom keccak256 function and use properly (custom() constructor)'
     assert.deepEqual(c.customCrypto.keccak256!(value), new Uint8Array([2, 1]), msg)
+  })
+
+  it('ecrecover', () => {
+    const customCrypto = {
+      ecrecover: customEcrecover,
+    }
+    const c = new Common({ chain: Chain.Mainnet, customCrypto })
+    assert.deepEqual(
+      Uint8Array.from([1, 2, 3, 4]),
+      c.customCrypto.ecrecover!(
+        Uint8Array.from([1]),
+        BigInt(2),
+        Uint8Array.from([3]),
+        Uint8Array.from([4])
+      )
+    )
   })
 })
