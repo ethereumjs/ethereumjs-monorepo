b40ab2ce326c1e74ddb23fda8fc87881da5449cc
Amir <indigophi@protonmail.com>
2023-12-27 11:38:17 -0700

    Reduce eslint comments by reordering definitions

--- packages/trie/src/db/flat/stackTrie.ts
+++ packages/trie/src/db/flat/stackTrie.ts
@@ -38,7 +38,7 @@ abstract class BaseNode {
 
 export class EmptyNode extends BaseNode {
   insert(key: Nibbles, value: Uint8Array): BaseNode {
-    //eslint-disable-next-line
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     return new STLeafNode(key, value)
   }
 
@@ -52,97 +52,6 @@ export class EmptyNode extends BaseNode {
 
   hash(): Uint8Array {
     return KECCAK256_RLP
-  }
-}
-
-export class STLeafNode extends BaseNode {
-  _key: Nibbles
-  _value: Uint8Array
-
-  constructor(key: Nibbles, value: Uint8Array) {
-    super()
-    this._key = key
-    this._value = value
-  }
-
-  insert(key: Nibbles, value: Uint8Array): BaseNode {
-    const commonLen = matchingNibbleLength(this._key, key)
-    assert.ok(commonLen < this._key.length, 'replacing leaf is not supported')
-
-    const newLeaf = new STLeafNode(key.slice(commonLen + 1), value)
-
-    //eslint-disable-next-line
-    const branch = new STBranchNode()
-    branch.setChild(key[commonLen], newLeaf)
-    branch.setChild(this._key[commonLen], this)
-
-    let root: BaseNode = branch
-    // Need extension node for common path
-    if (commonLen > 0) {
-      //eslint-disable-next-line
-      root = new STExtensionNode(this._key.slice(0, commonLen), branch)
-    }
-
-    // Slice common part from current leaf's key
-    this._key = this._key.slice(commonLen + 1)
-
-    return root
-  }
-
-  raw(): [Uint8Array, Uint8Array] {
-    const val: Nibbles = this._key.slice(0)
-    const encodedKey = addHexPrefix(val, true) // add terminator value to end of key
-    return [nibbleTypeToPackedBytes(encodedKey), this._value]
-  }
-}
-
-export class STExtensionNode extends BaseNode {
-  _key: Nibbles
-  _child: BaseNode
-
-  constructor(key: Nibbles, child: BaseNode) {
-    super()
-    this._key = key
-    this._child = child
-  }
-
-  insert(key: Nibbles, value: Uint8Array): BaseNode {
-    const commonLen = matchingNibbleLength(this._key, key)
-    // If matching prefix, recurse into child node
-    if (this._key.length === commonLen) {
-      this._child = this._child.insert(key.slice(commonLen), value)
-      return this
-    }
-
-    // Otherwise we'll need a new leaf, a branch and possibly an extension
-    const newLeaf = new STLeafNode(key.slice(commonLen + 1), value)
-
-    //eslint-disable-next-line
-    const branch = new STBranchNode()
-    branch.setChild(key[commonLen], newLeaf)
-
-    if (commonLen < this._key.length - 1) {
-      branch.setChild(this._key[commonLen], this)
-      this._key = this._key.slice(commonLen + 1)
-    } else {
-      branch.setChild(this._key[commonLen], this._child)
-    }
-
-    let root: BaseNode = branch
-    // Need extension for common prefix
-    if (commonLen > 0) {
-      root = new STExtensionNode(key.slice(0, commonLen), branch)
-    }
-
-    return root
-  }
-
-  raw(): any {
-    const childRaw = this._child.raw()
-    const childSerialized = RLP.encode(childRaw)
-    const value = childSerialized.length < 32 ? childRaw : keccak256(childSerialized)
-    const encodedKey = addHexPrefix(this._key.slice(0), false)
-    return [nibbleTypeToPackedBytes(encodedKey), value]
   }
 }
 
@@ -162,7 +71,6 @@ export class HashNode extends BaseNode {
     return this._serialized.length < 32
   }
 
-  // eslint-disable-next-line
   insert(key: Nibbles, value: Uint8Array): BaseNode {
     throw new Error("Can't insert into hash node")
   }
@@ -179,6 +87,7 @@ export class HashNode extends BaseNode {
     return keccak256(this._serialized)
   }
 }
+
 export class STBranchNode extends BaseNode {
   _children: (BaseNode | null)[]
   _value: Uint8Array | null
@@ -229,6 +138,95 @@ export class STBranchNode extends BaseNode {
     }
     raw.push(this._value)
     return raw
+  }
+}
+
+export class STLeafNode extends BaseNode {
+  _key: Nibbles
+  _value: Uint8Array
+
+  constructor(key: Nibbles, value: Uint8Array) {
+    super()
+    this._key = key
+    this._value = value
+  }
+
+  insert(key: Nibbles, value: Uint8Array): BaseNode {
+    const commonLen = matchingNibbleLength(this._key, key)
+    assert.ok(commonLen < this._key.length, 'replacing leaf is not supported')
+
+    const newLeaf = new STLeafNode(key.slice(commonLen + 1), value)
+
+    const branch = new STBranchNode()
+    branch.setChild(key[commonLen], newLeaf)
+    branch.setChild(this._key[commonLen], this)
+
+    let root: BaseNode = branch
+    // Need extension node for common path
+    if (commonLen > 0) {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      root = new STExtensionNode(this._key.slice(0, commonLen), branch)
+    }
+
+    // Slice common part from current leaf's key
+    this._key = this._key.slice(commonLen + 1)
+
+    return root
+  }
+
+  raw(): [Uint8Array, Uint8Array] {
+    const val: Nibbles = this._key.slice(0)
+    const encodedKey = addHexPrefix(val, true) // add terminator value to end of key
+    return [nibbleTypeToPackedBytes(encodedKey), this._value]
+  }
+}
+
+export class STExtensionNode extends BaseNode {
+  _key: Nibbles
+  _child: BaseNode
+
+  constructor(key: Nibbles, child: BaseNode) {
+    super()
+    this._key = key
+    this._child = child
+  }
+
+  insert(key: Nibbles, value: Uint8Array): BaseNode {
+    const commonLen = matchingNibbleLength(this._key, key)
+    // If matching prefix, recurse into child node
+    if (this._key.length === commonLen) {
+      this._child = this._child.insert(key.slice(commonLen), value)
+      return this
+    }
+
+    // Otherwise we'll need a new leaf, a branch and possibly an extension
+    const newLeaf = new STLeafNode(key.slice(commonLen + 1), value)
+
+    const branch = new STBranchNode()
+    branch.setChild(key[commonLen], newLeaf)
+
+    if (commonLen < this._key.length - 1) {
+      branch.setChild(this._key[commonLen], this)
+      this._key = this._key.slice(commonLen + 1)
+    } else {
+      branch.setChild(this._key[commonLen], this._child)
+    }
+
+    let root: BaseNode = branch
+    // Need extension for common prefix
+    if (commonLen > 0) {
+      root = new STExtensionNode(key.slice(0, commonLen), branch)
+    }
+
+    return root
+  }
+
+  raw(): any {
+    const childRaw = this._child.raw()
+    const childSerialized = RLP.encode(childRaw)
+    const value = childSerialized.length < 32 ? childRaw : keccak256(childSerialized)
+    const encodedKey = addHexPrefix(this._key.slice(0), false)
+    return [nibbleTypeToPackedBytes(encodedKey), value]
   }
 }
 
