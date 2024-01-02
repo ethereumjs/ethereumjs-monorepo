6d082e61aa8854b9270f8ca4a3b6619ea750a0dd
Amir <indigophi@protonmail.com>
2024-01-02 16:52:03 -0700

    Remove commented out imports

--- packages/statemanager/src/rpcStateManager.ts
+++ packages/statemanager/src/rpcStateManager.ts
@@ -9,8 +9,6 @@ import {
   toBytes,
 } from '@ethereumjs/util'
 import debugDefault from 'debug'
-// import { ethers } from 'ethers'
-// import { keccak256 } from 'ethereum-cryptography/keccak.js'
 
 import { AccountCache, CacheType, OriginalStorageCache, StorageCache } from './cache/index.js'
 
