961bde27ef8344353955a2fd5eb0a9e28c430424
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-01-10 09:32:29 -0500

    Add attribution link

--- packages/common/src/crc.ts
+++ packages/common/src/crc.ts
@@ -1,3 +1,8 @@
+/**
+ * This code was duplicated from https://github.com/alexgorbatchev/crc/ under MIT license.
+ * The code below is copied largely unmodified from the below file
+ * https://github.com/alexgorbatchev/crc/blob/master/src/calculators/crc32.ts
+ */
 let TABLE: Array<number> | Int32Array = [
   0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
   0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
