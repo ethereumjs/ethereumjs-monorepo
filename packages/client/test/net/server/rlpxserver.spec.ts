0ccca2af52fcfc2f4819789dd4934e40b5b0887d
ScottyPoi <scott.simpson@ethereum.org>
2024-01-10 20:37:35 -0700

    client: fix rlpxserver.spec.ts nesting

--- packages/client/test/net/server/rlpxserver.spec.ts
+++ packages/client/test/net/server/rlpxserver.spec.ts
@@ -6,64 +6,71 @@ import { assert, describe, expect, it, vi } from 'vitest'
 import { Config } from '../../../src/config'
 import { Event } from '../../../src/types'
 
-describe('[RlpxServer]', async () => {
-  class RlpxPeer extends EventEmitter {
-    accept(_: any, _2: any) {}
-    getId() {
-      return new Uint8Array([1])
-    }
-    getDisconnectPrefix(_: any) {
-      return 'MockedReason'
-    }
-    static capabilities(_: any) {
-      return []
-    }
-    _socket = { remoteAddress: 'mock', remotePort: 101 }
+class RlpxPeer extends EventEmitter {
+  accept(_: any, _2: any) {}
+  getId() {
+    return new Uint8Array([1])
   }
-  RlpxPeer.prototype.accept = vi.fn((input: object) => {
-    if (!(input instanceof RlpxPeer)) throw Error('expected RlpxPeer type as input')
-  })
-  RlpxPeer.capabilities = vi.fn()
-  vi.doMock('../../../src/net/peer/rlpxpeer', () => {
-    {
-      RlpxPeer
-    }
-  })
-
-  class RLPx extends EventEmitter {
-    listen(_: any, _2: any) {}
+  getDisconnectPrefix(_: any) {
+    return 'MockedReason'
   }
-  RLPx.prototype.listen = vi.fn()
-  class DPT extends EventEmitter {
-    bind(_: any, _2: any) {}
-    getDnsPeers() {}
+  static capabilities(_: any) {
+    return []
   }
-  DPT.prototype.bind = vi.fn()
-  DPT.prototype.getDnsPeers = vi.fn()
+  _socket = { remoteAddress: 'mock', remotePort: 101 }
+}
+RlpxPeer.prototype.accept = vi.fn((input: object) => {
+  if (!(input instanceof RlpxPeer)) throw Error('expected RlpxPeer type as input')
+})
+RlpxPeer.capabilities = vi.fn()
+vi.doMock('../../../src/net/peer/rlpxpeer', () => {
+  {
+    RlpxPeer
+  }
+})
 
-  vi.doMock('@ethereumjs/devp2p', () => {
-    {
-      RLPx
-    }
-  })
+class RLPx extends EventEmitter {
+  listen(_: any, _2: any) {}
+}
+RLPx.prototype.listen = vi.fn()
+class DPT extends EventEmitter {
+  bind(_: any, _2: any) {}
+  getDnsPeers() {}
+}
+DPT.prototype.bind = vi.fn()
+DPT.prototype.getDnsPeers = vi.fn()
 
+vi.doMock('@ethereumjs/devp2p', () => {
+  {
+    RLPx
+  }
+})
+
+describe('should initialize correctly', async () => {
   const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
-
-  it('should initialize correctly', async () => {
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const server = new RlpxServer({
-      config,
-      bootnodes: '10.0.0.1:1234,enode://abcd@10.0.0.2:1234',
-      key: 'abcd',
-    })
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const server = new RlpxServer({
+    config,
+    bootnodes: '10.0.0.1:1234,enode://abcd@10.0.0.2:1234',
+    key: 'abcd',
+  })
+  it('should get rlpx server name', () => {
     assert.equal(server.name, 'rlpx', 'get name')
+  })
+  it('should parse server key', () => {
     assert.ok(equalsBytes(server.key!, hexToBytes('0xabcd')), 'key parse')
+  })
+  it('should split bootnodes', () => {
     assert.deepEqual(
       server.bootnodes,
       [multiaddr('/ip4/10.0.0.1/tcp/1234'), multiaddr('/ip4/10.0.0.2/tcp/1234')],
       'bootnodes split'
     )
   })
+})
+
+describe('should start/stop server', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
 
   it('should start/stop server', async () => {
     const config = new Config({ accountCache: 10000, storageCache: 1000 })
@@ -103,6 +110,10 @@ describe('[RlpxServer]', async () => {
     assert.notOk(server.running, 'stopped')
     assert.notOk(await server.stop(), 'already stopped')
   })
+})
+
+describe('should bootstrap with dns acquired peers', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
 
   it('should bootstrap with dns acquired peers', async () => {
     const dnsPeerInfo = { address: '10.0.0.5', udpPort: 1234, tcpPort: 1234 }
@@ -130,6 +141,10 @@ describe('[RlpxServer]', async () => {
     await server.bootstrap()
     await server.stop()
   })
+})
+
+describe('should return rlpx server info with ip4 as default', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
 
   it('should return rlpx server info with ip4 as default', async () => {
     const config = new Config({ accountCache: 10000, storageCache: 1000 })
@@ -177,6 +192,10 @@ describe('[RlpxServer]', async () => {
     )
     await server.stop()
   })
+})
+
+describe('should return rlpx server info with ip6', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
 
   it('should return rlpx server info with ip6', async () => {
     const config = new Config({
@@ -228,12 +247,16 @@ describe('[RlpxServer]', async () => {
     )
     await server.stop()
   })
+})
+
+describe('should handle errors', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
 
   it('should handle errors', () => {
     let count = 0
     const config = new Config({ accountCache: 10000, storageCache: 1000 })
     const server = new RlpxServer({ config })
-    server.config.events.on(Event.SERVER_ERROR, (err: any) => {
+    server.config.events.on(Event.SERVER_ERROR, (err) => {
       count = count + 1
       if (err.message === 'err0') assert.ok(true, 'got server error - err0')
       if (err.message === 'err1') assert.ok(true, 'got peer error - err1')
@@ -245,8 +268,12 @@ describe('[RlpxServer]', async () => {
     }, 100)
     ;(server as any).error(new Error('err1'))
   })
+})
 
-  it('should ban peer', async () => {
+describe('should ban peer', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
+
+  it('shouldnt ban if no peer', () => {
     const config = new Config({ accountCache: 10000, storageCache: 1000 })
     const server = new RlpxServer({ config })
     assert.notOk(server.ban('123'), 'not started')
@@ -263,70 +290,93 @@ describe('[RlpxServer]', async () => {
     ;(server as any).rlpx = { destroy: vi.fn(), disconnect: vi.fn() }
     server.ban('112233', 1234)
   })
+})
 
-  it('should init dpt', async () => {
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const server = new RlpxServer({ config })
-    ;(server as any).initDpt().catch((error: Error) => {
-      throw error
-    })
-    config.events.on(Event.SERVER_ERROR, (err) => assert.equal(err.message, 'err0', 'got error'))
-    server['dpt']?.events.emit('error', new Error('err0'))
+describe('should init dpt', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
+
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const server = new RlpxServer({ config })
+  ;(server as any).initDpt().catch((error: Error) => {
+    throw error
   })
-
-  it('should init rlpx', async () => {
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const server = new RlpxServer({ config })
-    const rlpxPeer = new RlpxPeer()
-
-    ;(rlpxPeer as any).getId = vi.fn().mockReturnValue(new Uint8Array([1]))
-    RlpxPeer.prototype.accept = vi.fn((input) => {
-      if (JSON.stringify(input[0]) === JSON.stringify(rlpxPeer) && input[1] instanceof RlpxPeer) {
-        return
-      } else {
-        throw new Error('expected input check has failed')
-      }
+  config.events.on(Event.SERVER_ERROR, (err) =>
+    it('should throw', async () => {
+      assert.equal(err.message, 'err0', 'got error')
     })
-    ;(server as any).initRlpx().catch((error: Error) => {
-      throw error
-    })
-    config.events.on(Event.PEER_CONNECTED, async (peer) =>
+  )
+  server['dpt']?.events.emit('error', new Error('err0'))
+})
+
+describe('should init rlpx', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
+
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const server = new RlpxServer({ config })
+  const rlpxPeer = new RlpxPeer()
+
+  ;(rlpxPeer as any).getId = vi.fn().mockReturnValue(new Uint8Array([1]))
+  RlpxPeer.prototype.accept = vi.fn((input) => {
+    if (JSON.stringify(input[0]) === JSON.stringify(rlpxPeer) && input[1] instanceof RlpxPeer) {
+      return
+    } else {
+      throw new Error('expected input check has failed')
+    }
+  })
+  ;(server as any).initRlpx().catch((error: Error) => {
+    throw error
+  })
+  config.events.on(Event.PEER_CONNECTED, (peer) =>
+    it('should connect', async () => {
       assert.ok(peer instanceof RlpxPeer, 'connected')
-    )
-    config.events.on(Event.PEER_DISCONNECTED, async (peer) =>
+    })
+  )
+  config.events.on(Event.PEER_DISCONNECTED, (peer) =>
+    it('should disconnect', async () => {
       assert.equal(peer.id, '01', 'disconnected')
-    )
-    config.events.on(Event.SERVER_ERROR, (err) => assert.equal(err.message, 'err0', 'got error'))
-    config.events.on(Event.SERVER_LISTENING, (info) =>
+    })
+  )
+  config.events.on(Event.SERVER_ERROR, (err) =>
+    it('should throw error', async () => {
+      assert.equal(err.message, 'err0', 'got error')
+    })
+  )
+  config.events.on(Event.SERVER_LISTENING, (info) =>
+    it('should listen', async () => {
       assert.deepEqual(info, { transport: 'rlpx', url: 'enode://ff@0.0.0.0:30303' }, 'listening')
-    )
-    server.rlpx!.events.emit('peer:added', rlpxPeer)
-    ;(server as any).peers.set('01', { id: '01' } as any)
-    server.rlpx!.events.emit('peer:removed', rlpxPeer)
-    server.rlpx!.events.emit('peer:error', rlpxPeer, new Error('err0'))
-
-    // @ts-ignore
-    server.rlpx!.id = hexToBytes('0xff')
-    server.rlpx!.events.emit('listening')
-    await server.stop()
-  })
-
-  it('should handles errors from id-less peers', async () => {
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const server = new RlpxServer({ config })
-    const rlpxPeer = new RlpxPeer()
-    ;(rlpxPeer as any).getId = vi.fn().mockReturnValue(utf8ToBytes('test'))
-    RlpxPeer.prototype.accept = vi.fn((input) => {
-      if (JSON.stringify(input[0]) === JSON.stringify(rlpxPeer) && input[1] instanceof RlpxPeer) {
-        return
-      } else {
-        throw new Error('expected input check has failed')
-      }
     })
-    ;(server as any).initRlpx().catch((error: Error) => {
-      throw error
-    })
-    config.events.on(Event.SERVER_ERROR, (err) => assert.equal(err.message, 'err0', 'got error'))
-    server.rlpx!.events.emit('peer:error', rlpxPeer, new Error('err0'))
+  )
+  server.rlpx!.events.emit('peer:added', rlpxPeer)
+  ;(server as any).peers.set('01', { id: '01' } as any)
+  server.rlpx!.events.emit('peer:removed', rlpxPeer)
+  server.rlpx!.events.emit('peer:error', rlpxPeer, new Error('err0'))
+
+  // @ts-ignore
+  server.rlpx!.id = hexToBytes('0xff')
+  server.rlpx!.events.emit('listening')
+})
+
+describe('should handles errors from id-less peers', async () => {
+  const { RlpxServer } = await import('../../../src/net/server/rlpxserver.js')
+
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const server = new RlpxServer({ config })
+  const rlpxPeer = new RlpxPeer()
+  ;(rlpxPeer as any).getId = vi.fn().mockReturnValue(utf8ToBytes('test'))
+  RlpxPeer.prototype.accept = vi.fn((input) => {
+    if (JSON.stringify(input[0]) === JSON.stringify(rlpxPeer) && input[1] instanceof RlpxPeer) {
+      return
+    } else {
+      throw new Error('expected input check has failed')
+    }
   })
+  ;(server as any).initRlpx().catch((error: Error) => {
+    throw error
+  })
+  config.events.on(Event.SERVER_ERROR, (err) =>
+    it('should throw', async () => {
+      assert.equal(err.message, 'err0', 'got error')
+    })
+  )
+  server.rlpx!.events.emit('peer:error', rlpxPeer, new Error('err0'))
 })
