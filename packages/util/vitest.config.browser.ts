9ef38d1fef4c0b8fb3a652789c53f0d8d1b0f10c
ScottyPoi <scott.simpson@ethereum.org>
2024-01-12 02:13:58 -0700

    util: add node polyfill plugin to vitest browser config

--- packages/util/vitest.config.browser.ts
+++ packages/util/vitest.config.browser.ts
@@ -1,7 +1,9 @@
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'test/provider.spec.ts', 'test/asyncEventEmitter.spec.ts'],
   },
+  plugins: [nodePolyfills()],
 })
