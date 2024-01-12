7e21a21584bedca0f065102c5465aa3a5ec64f66
ScottyPoi <scott.simpson@ethereum.org>
2024-01-12 14:45:46 -0700

    statemanager: fix for browser mode tests

--- packages/statemanager/vitest.config.browser.ts
+++ packages/statemanager/vitest.config.browser.ts
@@ -1,18 +1,22 @@
 import topLevelAwait from 'vite-plugin-top-level-await'
 import wasm from 'vite-plugin-wasm'
-import { configDefaults, defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
+import baseConfig from '../../config/vitest.config.browser'
 
-export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
-  test: {
-    exclude: [
-      ...configDefaults.exclude,
-      // Importing pedersenHash through wasm failed.
-      'test/statelessVerkleStateManager.spec.ts',
-      // Importing a module script failed.
-      'test/rpcStateManager.spec.ts',
-      // undefined is not an object (evaluating 'state.reading')
-      'test/stateManager.storage.spec.ts',
-    ],
-  },
-})
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    plugins: [wasm(), topLevelAwait()],
+    test: {
+      exclude: [
+        ...configDefaults.exclude,
+        // Importing pedersenHash through wasm failed.
+        'test/statelessVerkleStateManager.spec.ts',
+        // Importing a module script failed.
+        'test/rpcStateManager.spec.ts',
+        // undefined is not an object (evaluating 'state.reading')
+        'test/stateManager.storage.spec.ts',
+      ],
+    },
+  })
+)
