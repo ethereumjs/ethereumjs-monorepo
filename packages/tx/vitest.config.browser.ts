d89052b8c00c3b4ab406fb67575c63924ce59019
ScottyPoi <scott.simpson@ethereum.org>
2024-01-12 14:30:28 -0700

    tx: fix for browser mode tests

--- packages/tx/vitest.config.browser.ts
+++ packages/tx/vitest.config.browser.ts
@@ -1,14 +1,17 @@
-import { configDefaults, defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import baseConfig from '../../config/vitest.config.browser'
 
-export default defineConfig({
-  test: {
-    exclude: [
-      ...configDefaults.exclude,
-      // c-kzg dependency
-      'test/eip4844.spec.ts',
-      // default export for minimist
-      // wrong ethereum-tests path reference (../ is stripped)
-      'test/transactionRunner.spec.ts',
-    ],
-  },
-})
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      exclude: [
+        // c-kzg dependency
+        'test/eip4844.spec.ts',
+        // default export for minimist
+        // wrong ethereum-tests path reference (../ is stripped)
+        'test/transactionRunner.spec.ts',
+      ],
+    },
+  })
+)
