c7b081b5a2beeaca7834de1f5a94655a8ac35645
ScottyPoi <scott.simpson@ethereum.org>
2024-01-12 14:29:01 -0700

    trie: fix browser mode tests

--- packages/trie/vitest.config.browser.ts
+++ packages/trie/vitest.config.browser.ts
@@ -1,11 +1,14 @@
-import { configDefaults, defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import baseConfig from '../../config/vitest.config.browser'
 
-export default defineConfig({
-  test: {
-    exclude: [
-      ...configDefaults.exclude,
-      // 1 test failing, undefined is not an object (evaluating 'state.reading')
-      'test/stream.spec.ts',
-    ],
-  },
-})
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      exclude: [
+        // 1 test failing, undefined is not an object (evaluating 'state.reading')
+        'test/stream.spec.ts',
+      ],
+    },
+  })
+)
