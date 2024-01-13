7bdbab57178d0091fa43c40afc082e839f98f09e
ScottyPoi <scott.simpson@ethereum.org>
2024-01-12 17:52:50 -0700

    util: remove skips from from asyncEventEmitter tests

--- packages/util/test/asyncEventEmitter.spec.ts
+++ packages/util/test/asyncEventEmitter.spec.ts
@@ -1,43 +1,42 @@
 import { assert, describe, it } from 'vitest'
 
+import { AsyncEventEmitter } from '../src/asyncEventEmitter.js'
+
 import type { EventMap } from '../src/asyncEventEmitter.js'
 
 const isBrowser = new Function('try {return this===window;}catch(e){ return false;}')
 
-describe.skipIf(isBrowser)('async event emit/on test', async () => {
-  const { AsyncEventEmitter } = await import('../src/index.js')
+describe('async event emit/on test', async () => {
+  const emitter = new AsyncEventEmitter()
+  it('should receive event', () => {
+    emitter.on('event', async ([data, startTime]) => {
+      assert.equal(data, 'eventData', 'Received data from an event')
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(undefined)
+        }, 1000)
+      })
+      assert.ok(Date.now() > startTime, 'some time passed before event resolved')
+    })
+    const startTime = Date.now()
+    emitter.emit('event', ['eventData', startTime], () => {})
+  })
+})
+
+describe('async event emit/once test', async () => {
   it('should receive event', () => {
     const emitter = new AsyncEventEmitter()
-    emitter.on('event', async (data, next) => {
-      const startTime = Date.now()
-      assert.equal(data, 'eventData', 'Received data from an event')
+    emitter.once('event', async () => {
       setTimeout(() => {
-        assert.ok(Date.now() > startTime, 'some time passed before event resolved')
-        next?.()
+        assert.equal(emitter.listenerCount('event'), 0, 'listener removed after one event emitted')
       }, 1000)
     })
-    emitter.emit('event', 'eventData')
-  })
-})
-
-describe.skipIf(isBrowser)('async event emit/once test', async () => {
-  const { AsyncEventEmitter } = await import('../src/index.js')
-
-  it('should receive event', () => {
-    const emitter = new AsyncEventEmitter()
-    emitter.once('event', async (data, next) => {
-      setTimeout(next!, 1000)
-    })
     assert.equal(emitter.listenerCount('event'), 1, 'emitter has one event listener')
-    emitter.emit('event', 'eventData', () => {
-      assert.equal(emitter.listenerCount('event'), 0, 'listener removed after one event emitted')
-    })
+    emitter.emit('event', 'eventData', () => {})
   })
 })
 
-describe.skipIf(isBrowser)('AsyncEventEmitter', async () => {
-  const { AsyncEventEmitter } = await import('../src/index.js')
-
+describe('AsyncEventEmitter', async () => {
   it('should add listener using addListener()', () => {
     const emitter = new AsyncEventEmitter<EventMap>()
     const listener = () => {}
@@ -48,7 +47,7 @@ describe.skipIf(isBrowser)('AsyncEventEmitter', async () => {
     assert.deepStrictEqual(emitter.listeners(event), [listener])
   })
 
-  it('should prepend listener using prependListener()', () => {
+  it.skipIf(isBrowser())('should prepend listener using prependListener()', () => {
     const emitter = new AsyncEventEmitter<EventMap>()
     const listener1 = () => {}
     const listener2 = () => {}
@@ -60,7 +59,7 @@ describe.skipIf(isBrowser)('AsyncEventEmitter', async () => {
     assert.deepStrictEqual(emitter.listeners(event), [listener2, listener1])
   })
 
-  it('should prepend once listener using prependOnceListener()', () => {
+  it.skipIf(isBrowser())('should prepend once listener using prependOnceListener()', () => {
     const emitter = new AsyncEventEmitter<EventMap>()
     const listener1 = () => {}
     const listener2 = () => {}
@@ -95,22 +94,28 @@ describe.skipIf(isBrowser)('AsyncEventEmitter', async () => {
     assert.deepStrictEqual(emitter.listeners(event), [listener1, listener2])
   })
 
-  it('should return the maximum number of listeners when using getMaxListeners()', () => {
-    const emitter = new AsyncEventEmitter<EventMap>()
+  it.skipIf(isBrowser())(
+    'should return the maximum number of listeners when using getMaxListeners()',
+    () => {
+      const emitter = new AsyncEventEmitter<EventMap>()
 
-    const maxListeners = emitter.getMaxListeners()
+      const maxListeners = emitter.getMaxListeners()
 
-    assert.strictEqual(maxListeners, 10)
-  })
+      assert.strictEqual(maxListeners, 10)
+    }
+  )
 
-  it('should set the maximum number of listeners when using setMaxListeners()', () => {
-    const emitter = new AsyncEventEmitter<EventMap>()
+  it.skipIf(isBrowser())(
+    'should set the maximum number of listeners when using setMaxListeners()',
+    () => {
+      const emitter = new AsyncEventEmitter<EventMap>()
 
-    const newMaxListeners = 10
-    emitter.setMaxListeners(newMaxListeners)
+      const newMaxListeners = 10
+      emitter.setMaxListeners(newMaxListeners)
 
-    const maxListeners = emitter.getMaxListeners()
+      const maxListeners = emitter.getMaxListeners()
 
-    assert.strictEqual(maxListeners, newMaxListeners)
-  })
+      assert.strictEqual(maxListeners, newMaxListeners)
+    }
+  )
 })
