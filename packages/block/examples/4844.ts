d0e7a5f8b9ac8ad376030d9eb0628c5439abb7da
ScottyPoi <scott.simpson@ethereum.org>
2024-01-19 16:04:21 -0700

    block: revert change to 4844 example

--- packages/block/examples/4844.ts
+++ packages/block/examples/4844.ts
@@ -2,14 +2,11 @@ import { Common, Chain, Hardfork } from '@ethereumjs/common'
 import { Block } from '@ethereumjs/block'
 import { BlobEIP4844Transaction } from '@ethereumjs/tx'
 import { Address, initKZG } from '@ethereumjs/util'
-import kzg from 'c-kzg'
+import * as kzg from 'c-kzg'
 import { randomBytes } from 'crypto'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)).slice(0, -14)
 const main = async () => {
-  await initKZG(kzg, __dirname + 'client/src/trustedSetups/devnet6.txt')
+  await initKZG(kzg, __dirname + '/../../client/src/trustedSetups/devnet6.txt')
   const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.Cancun })
 
   const blobTx = BlobEIP4844Transaction.fromTxData(
