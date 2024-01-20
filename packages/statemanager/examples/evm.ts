5e98aa2118a6778629f4ea5bff6b26e686135732
Amir <indigophi@protonmail.com>
2024-01-19 19:12:34 -0700

    Catch and log errors in examples

--- packages/statemanager/examples/evm.ts
+++ packages/statemanager/examples/evm.ts
@@ -2,10 +2,14 @@ import { RPCStateManager, RPCBlockChain } from '@ethereumjs/statemanager'
 import { EVM } from '@ethereumjs/evm'
 
 const main = async () => {
-  const provider = 'https://path.to.my.provider.com'
-  const blockchain = new RPCBlockChain(provider)
-  const blockTag = 1n
-  const state = new RPCStateManager({ provider, blockTag })
-  const evm = new EVM({ blockchain, stateManager: state })
+  try {
+    const provider = 'https://path.to.my.provider.com'
+    const blockchain = new RPCBlockChain(provider)
+    const blockTag = 1n
+    const state = new RPCStateManager({ provider, blockTag })
+    const evm = new EVM({ blockchain, stateManager: state })
+  } catch (e) {
+    console.log(e)
+  }
 }
 main()
