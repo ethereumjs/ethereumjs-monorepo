4c37cccd7cb42805e49d421a9277072620ced89d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-01-23 02:37:51 -0500

    Update vm/wallet examples (#3250)
    * add vm examples
    
    * Update vm examples
    
    * Update vm examples
    
    * Update readme
    
    * update wallet readme ts references
    
    * Update wallet examples
    
    * fix example imports
    
    * Add more complete wallet examples

--- packages/vm/examples/run-blockchain.ts
+++ packages/vm/examples/run-blockchain.ts
@@ -6,14 +6,7 @@
 // 4. Puts the blocks from ../utils/blockchain-mock-data "blocks" attribute into the Blockchain
 // 5. Runs the Blockchain on the VM.
 
-import {
-  Account,
-  Address,
-  toBytes,
-  setLengthLeft,
-  bytesToHex,
-  hexToBytes
-} from '@ethereumjs/util'
+import { Account, Address, toBytes, setLengthLeft, bytesToHex, hexToBytes } from '@ethereumjs/util'
 import { Block } from '@ethereumjs/block'
 import { Blockchain } from '@ethereumjs/blockchain'
 import { Common, ConsensusType } from '@ethereumjs/common'
