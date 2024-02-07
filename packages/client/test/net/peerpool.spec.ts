9e5798089cf5d15d991d1e9f44f444cea1c12d27
Scotty <66335769+ScottyPoi@users.noreply.github.com>
2024-02-07 06:06:29 -0700

    Client: fix test nesting and other vitest issues (#3266)
    * client: fix rlpxserver.spec.ts nesting
    
    * client: fix event based test
    
    * client: fix test nesting / events in rlpxsender.spec.ts
    
    * client: fix test nesting in CLConnectionManager test
    
    * client: fix test nesting / event issues in integration/beaconsync.spec.ts
    
    * client: fix nesting / events issues in client.spec.ts
    
    * client: fix test nesting / events in fullethereumservice.spec.ts
    
    * client: fix test nesting / events in fullysync.spec.ts
    
    * client: fix test nesting / events in lightsync.spec.ts
    
    * client: fix merge.spec.ts
    
    * client: fix miner.spec.ts
    
    * client: fix peerpool.spec.ts
    
    * client: refactor and fix pow.spec.ts
    
    * client: fix for miner/miner.spec.ts
    
    * client: fix for peerpool.spec.ts
    
    * client: fix nexting in fullethereumservice.spec.ts
    
    * client: fix for lightethereumservice.spec.ts
    
    * client: fix for sim/beacsonsync.spec.ts
    
    * client: apply fixes to blockfetcher.spec.ts
    
    * client: apply fixes to fetcher.spec.ts
    
    * client: apply fixes to headerfetcher.spec.ts
    
    * client: apply fixes to reverseblockfetcher.spec.ts
    
    * client: apply fixes to lightsync.spec.ts
    
    * client: apply fixes to sync.spec.ts
    
    * fetcher.spec: remove as any declarations

--- packages/client/test/net/peerpool.spec.ts
+++ packages/client/test/net/peerpool.spec.ts
@@ -5,28 +5,14 @@ import { Config } from '../../src/config'
 import { Event } from '../../src/types'
 import { MockPeer } from '../integration/mocks/mockpeer'
 
-describe('[PeerPool]', async () => {
-  const Peer = function (this: any, id: string) {
-    this.id = id // eslint-disable-line no-invalid-this
-  }
-  vi.doMock('../../src/net/peer/peer', () => Peer)
-  const { PeerPool } = await import('../../src/net/peerpool')
+const { PeerPool } = await import('../../src/net/peerpool')
 
-  it('should initialize', () => {
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const pool = new PeerPool({ config })
+describe('should initialize', () => {
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const pool = new PeerPool({ config })
+  it('should open/close', async () => {
     assert.notOk((pool as any).pool.size, 'empty pool')
     assert.notOk((pool as any).opened, 'not open')
-  })
-
-  it('should open/close', async () => {
-    const server = {} as any
-    const config = new Config({
-      server,
-      accountCache: 10000,
-      storageCache: 1000,
-    })
-    const pool = new PeerPool({ config })
     const peer = new MockPeer({
       id: 'peer',
       location: 'abc',
@@ -47,36 +33,48 @@ describe('[PeerPool]', async () => {
     await pool.close()
     assert.notOk((pool as any).opened, 'closed')
   })
+})
 
-  it('should connect/disconnect peer', () => {
-    const peer = new EventEmitter() as any
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const pool = new PeerPool({ config })
-    ;(peer as any).id = 'abc'
-    ;(peer as any).handleMessageQueue = vi.fn()
-    ;(pool as any).connected(peer)
-    pool.config.events.on(Event.PROTOCOL_MESSAGE, (msg: any, proto: any, p: any) => {
+describe('should connect/disconnect peer', () => {
+  const peer = new EventEmitter() as any
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const pool = new PeerPool({ config })
+  ;(peer as any).id = 'abc'
+  ;(peer as any).handleMessageQueue = vi.fn()
+  ;(pool as any).connected(peer)
+  pool.config.events.on(Event.PROTOCOL_MESSAGE, (msg: any, proto: any, p: any) => {
+    it('should get message', () => {
       assert.ok(msg === 'msg0' && proto === 'proto0' && p === peer, 'got message')
     })
-    config.events.emit(Event.PROTOCOL_MESSAGE, 'msg0', 'proto0', peer)
-    pool.config.events.emit(Event.PEER_ERROR, new Error('err0'), peer)
-    ;(pool as any).disconnected(peer)
-    assert.notOk((pool as any).pool.get('abc'), 'peer removed')
   })
+  config.events.emit(Event.PROTOCOL_MESSAGE, 'msg0', 'proto0', peer)
+  pool.config.events.emit(Event.PEER_ERROR, new Error('err0'), peer)
+  ;(pool as any).disconnected(peer)
+  assert.notOk((pool as any).pool.get('abc'), 'peer removed')
+})
 
-  it('should check contains', () => {
-    const peer = new Peer('abc')
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const pool = new PeerPool({ config })
+const Peer = function (this: any, id: string) {
+  this.id = id // eslint-disable-line no-invalid-this
+}
+vi.doMock('../../src/net/peer/peer', () => Peer)
+describe('should check contains', () => {
+  // @ts-ignore
+  const peer = new Peer('abc')
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const pool = new PeerPool({ config })
+  it('should add peer', () => {
     pool.add(peer)
     assert.ok(pool.contains(peer.id), 'found peer')
   })
+})
 
-  it('should get idle peers', () => {
-    const peers = [new Peer(1), new Peer(2), new Peer(3)]
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const pool = new PeerPool({ config })
-    peers[1].idle = true
+it('should get idle peers', () => {
+  // @ts-ignore
+  const peers = [new Peer(1), new Peer(2), new Peer(3)]
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const pool = new PeerPool({ config })
+  peers[1].idle = true
+  it('should add peers', () => {
     for (const p of peers) {
       pool.add(p)
     }
@@ -87,21 +85,25 @@ describe('[PeerPool]', async () => {
       'correct idle peer with filter'
     )
   })
+})
 
-  it('should ban peer', () => {
-    const peers = [{ id: 1 }, { id: 2, server: { ban: vi.fn() } }]
-    const config = new Config({ accountCache: 10000, storageCache: 1000 })
-    const pool = new PeerPool({ config })
-    for (const p of peers as any) {
-      pool.add(p)
-      pool.ban(p, 1000)
-    }
-    pool.config.events.on(Event.POOL_PEER_BANNED, (peer) =>
+describe('should ban peer', () => {
+  const peers = [{ id: 1 }, { id: 2, server: { ban: vi.fn() } }]
+  const config = new Config({ accountCache: 10000, storageCache: 1000 })
+  const pool = new PeerPool({ config })
+  pool.config.events.on(Event.POOL_PEER_BANNED, (peer) => {
+    it('should ban peer', () => {
       assert.equal(peer, peers[1] as any, 'banned peer')
-    )
-    pool.config.events.on(Event.POOL_PEER_REMOVED, (peer) =>
-      assert.equal(peer, peers[1] as any, 'removed peer')
-    )
-    assert.equal(pool.peers[0], peers[0] as any, 'outbound peer not banned')
+    })
   })
+  pool.config.events.on(Event.POOL_PEER_REMOVED, (peer) => {
+    it('should remove peer', () => {
+      assert.equal(peer, peers[1] as any, 'removed peer')
+      assert.equal(pool.peers[0], peers[0] as any, 'outbound peer not banned')
+    })
+  })
+  for (const p of peers as any) {
+    pool.add(p)
+    pool.ban(p, 1000)
+  }
 })
