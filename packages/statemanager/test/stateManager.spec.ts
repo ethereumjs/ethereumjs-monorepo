3483cd0c4ab72f702f3ce413bf316e4cc203800c
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-02-15 10:42:38 -0500

    Add second proof

--- packages/statemanager/test/stateManager.spec.ts
+++ packages/statemanager/test/stateManager.spec.ts
@@ -4,9 +4,11 @@ import {
   Address,
   KECCAK256_RLP,
   bigIntToBytes,
+  bytesToHex,
   equalsBytes,
   hexToBytes,
   intToBytes,
+  randomBytes,
   setLengthLeft,
   utf8ToBytes,
 } from '@ethereumjs/util'
@@ -290,9 +292,13 @@ describe('StateManager -> General', () => {
       const trie = await Trie.create({ useKeyHashing: false })
       const sm = new DefaultStateManager({ trie })
       const pk = hexToBytes('0x9f12aab647a25a81f821a5a0beec3330cd057b2346af4fb09d7a807e896701ea')
+      const pk2 = hexToBytes('0x8724f27e2ce3714af01af3220478849db68a03c0f84edf1721d73d9a6139ad1c')
       const address = Address.fromPrivateKey(pk)
+      const address2 = Address.fromPrivateKey(pk2)
       const account = new Account()
+      const account2 = new Account(undefined, 100n)
       await sm.putAccount(address, account)
+      await sm.putAccount(address2, account2)
       await sm.putContractStorage(address, setLengthLeft(intToBytes(0), 32), intToBytes(32))
       const storage = await sm.dumpStorage(address)
       const keys = Object.keys(storage)
@@ -300,11 +306,14 @@ describe('StateManager -> General', () => {
         address,
         keys.map((key) => hexToBytes(key))
       )
+      const proof2 = await sm.getProof(address2)
       const newTrie = await Trie.createFromProof(
         proof.accountProof.map((e) => hexToBytes(e)),
         { useKeyHashing: false }
       )
-      const partialSM = await DefaultStateManager.fromProof(proof, true, { trie: newTrie })
+      const partialSM = await DefaultStateManager.fromProof([proof, proof2], true, {
+        trie: newTrie,
+      })
       assert.equal(
         partialSM['_trie']['_opts'].useKeyHashing,
         false,
@@ -314,6 +323,7 @@ describe('StateManager -> General', () => {
         intToBytes(32),
         await partialSM.getContractStorage(address, hexToBytes(keys[0]))
       )
+      assert.equal((await partialSM.getAccount(address2))?.balance, 100n)
     }
   )
 })
