f8530b530169a46cd507a0a13fc594b1a08da1b2
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-02-21 15:32:52 -0500

    Add more 4844 tests to browser run

--- packages/tx/vitest.config.browser.ts
+++ packages/tx/vitest.config.browser.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   test: {
     exclude: [
       ...configDefaults.exclude,
-      // c-kzg dependency
-      // 'test/eip4844.spec.ts',
       // default export for minimist
       // wrong ethereum-tests path reference (../ is stripped)
       'test/transactionRunner.spec.ts',
