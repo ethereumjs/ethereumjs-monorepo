4097f5c5f979a6c50ebccf2fd728c11588876f9a
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-02-22 11:37:23 -0500

    Initial integration of kzg-wasm on git

--- packages/common/examples/initKzg.ts
+++ packages/common/examples/initKzg.ts
@@ -1,11 +1,16 @@
-import * as kzg from 'c-kzg'
+import { initKzg } from 'kzg-wasm'
 import { Common, Chain, Hardfork } from '@ethereumjs/common'
 import { initKZG } from '@ethereumjs/util'
 
-initKZG(kzg, __dirname + '/../../client/src/trustedSetups/official.txt')
-const common = new Common({
-  chain: Chain.Mainnet,
-  hardfork: Hardfork.Cancun,
-  customCrypto: { kzg: kzg },
-})
-console.log(common.customCrypto.kzg) // Should print the initialized KZG interface
+const main = async () => {
+  const kzg = await initKzg()
+  initKZG(kzg, '')
+  const common = new Common({
+    chain: Chain.Mainnet,
+    hardfork: Hardfork.Cancun,
+    customCrypto: { kzg: kzg },
+  })
+  console.log(common.customCrypto.kzg) // Should print the initialized KZG interface
+}
+
+main()
