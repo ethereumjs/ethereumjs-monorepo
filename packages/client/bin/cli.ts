b8f5b6df2a5e454f315d9b30b4f04e89fd92fdf8
Holger Drewes <Holger.Drewes@gmail.com>
2024-02-29 13:07:30 +0100

    Make trustedSetupPath in Util kzg module optional (#3296)

--- packages/client/bin/cli.ts
+++ packages/client/bin/cli.ts
@@ -807,7 +807,7 @@ async function run() {
   const chain = args.networkId ?? args.network ?? Chain.Mainnet
   const cryptoFunctions: CustomCrypto = {}
   const kzg = await createKZG()
-  initKZG(kzg, '')
+  initKZG(kzg)
 
   // Initialize WASM crypto if JS crypto is not specified
   if (args.useJsCrypto === false) {
--- packages/client/devnets/4844-interop/tools/txGenerator.ts
+++ packages/client/devnets/4844-interop/tools/txGenerator.ts
@@ -29,7 +29,7 @@ async function getNonce(client: Client, account: string) {
 
 async function run(data: any) {
   const kzg = await createKZG()
-  initKZG(kzg, '')
+  initKZG(kzg)
 
   const common = Common.fromGethGenesis(genesisJson, {
     chain: genesisJson.ChainName ?? 'devnet',
