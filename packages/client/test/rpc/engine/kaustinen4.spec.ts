d5705dfef11608aea5d1e0cbebbf8018d84dbe96
harkamal <gajinder@g11.in>
2024-03-02 19:26:07 +0530

    update test to test more than one block

--- packages/client/test/rpc/engine/kaustinen4.spec.ts
+++ packages/client/test/rpc/engine/kaustinen4.spec.ts
@@ -48,10 +48,10 @@ describe(`valid verkle network setup`, async () => {
   // to not do clean init of the statemanager. this isn't a problem in sequential
   // execution, but need to be fixed up in the stateless random execution
   const testCases = [
-    // "block353",
-    // "block368",
-    // 'block374',
-    'block479',
+    "block353",
+    "block368",
+    'block374',
+    // 'block479',
   ] as const
 
   for (const testCase of testCases) {
