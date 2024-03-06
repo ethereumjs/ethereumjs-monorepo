90d162b9c7603d5ac057de07be7074d7ef133caa
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-03-05 20:59:30 -0500

    Update examples

--- packages/evm/examples/eips.ts
+++ packages/evm/examples/eips.ts
@@ -1,6 +1,10 @@
 import { Chain, Common } from '@ethereumjs/common'
 import { EVM } from '@ethereumjs/evm'
 
-const common = new Common({ chain: Chain.Mainnet, eips: [3074] })
-const evm = new EVM({ common })
-console.log(`EIP 3074 is active - ${evm.common.isActivatedEIP(3074)}`)
+const main = async () => {
+  const common = new Common({ chain: Chain.Mainnet, eips: [3074] })
+  const evm = await EVM.create({ common })
+  console.log(`EIP 3074 is active - ${evm.common.isActivatedEIP(3074)}`)
+}
+
+main()
