ae95039ef5be19a2992d9c0aa489551e4809db0f
ScottyPoi <scott.simpson@ethereum.org>
2024-03-09 19:56:54 -0700

    trie: helper function to sort batch keys

--- packages/trie/src/node/batch.ts
+++ packages/trie/src/node/batch.ts
@@ -12,3 +12,14 @@ import { bytesToNibbles, nibblesCompare } from '../util/nibbles.js'
 import { BranchNode } from './branch.js'
 
 import type { Trie } from '../trie.js'
+
+export function orderBatch(ops: BatchDBOp[]): BatchDBOp[] {
+  const keyNibbles: [number, number[]][] = ops.map((o, i) => {
+    const nibbles: number[] = bytesToNibbles(o.key)
+    return [i, nibbles]
+  })
+  keyNibbles.sort(([_, a], [__, b]) => {
+    return nibblesCompare(a, b)
+  })
+  return keyNibbles.map(([i, _]) => ops[i])
+}
