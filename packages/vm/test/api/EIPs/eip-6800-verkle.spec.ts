76489363a96cdc75c6e6f69e34f4ec907909c912
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-04-07 12:35:45 -0700

    vm: fix eip6800 test

--- packages/vm/test/api/EIPs/eip-6800-verkle.spec.ts
+++ packages/vm/test/api/EIPs/eip-6800-verkle.spec.ts
@@ -2,14 +2,19 @@ import { Block } from '@ethereumjs/block'
 import { Common, Hardfork } from '@ethereumjs/common'
 import { EVM } from '@ethereumjs/evm'
 import { StatelessVerkleStateManager } from '@ethereumjs/statemanager'
+import { TransactionFactory } from '@ethereumjs/tx'
+import { hexToBytes } from '@ethereumjs/util'
 import { describe, it } from 'vitest'
 
 import * as verkleBlockJSON from '../../../../statemanager/test/testdata/verkleKaustinenBlock.json'
 import { VM } from '../../../src'
 
 const customChainParams = { name: 'custom', chainId: 69420, networkId: 678 }
-const common = Common.custom(customChainParams, { hardfork: Hardfork.Cancun, eips: [6800] })
-const block = Block.fromBlockData(verkleBlockJSON, { common })
+const common = Common.custom(customChainParams, { hardfork: Hardfork.Cancun, eips: [4895, 6800] })
+const decodedTxs = verkleBlockJSON.transactions.map((tx) =>
+  TransactionFactory.fromSerializedData(hexToBytes(tx))
+)
+const block = Block.fromBlockData({ ...verkleBlockJSON, transactions: decodedTxs }, { common })
 
 describe('EIP 6800 tests', () => {
   it('successfully run transactions statelessly using the block witness', async () => {
