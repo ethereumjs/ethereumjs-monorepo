6fd8b88e0b010f02e58acb2a964d94a4f2ba4395
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-04-24 18:26:26 -0400

    Add evm vitest config

---
+++ packages/evm/vitest.config.browser.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import wasm from 'vite-plugin-wasm'
+
+const config = defineConfig({
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      isolate: true,
+      name: 'chrome',
+    },
+  },
+  plugins: [
+    wasm(),
+    nodePolyfills({
+      include: ['util', 'fs', 'buffer', 'path'],
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ['kzg-wasm'],
+  },
+})
+
+export default config
