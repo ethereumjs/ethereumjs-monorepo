04a0cb5fd5f6537edfba39b53b46ce48eb38393f
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-05-14 15:48:47 -0400

    verkle: verkle proof test

--- packages/verkle/test/crypto.spec.ts
+++ packages/verkle/test/crypto.spec.ts
@@ -1,10 +1,12 @@
-import { Address, bytesToHex } from '@ethereumjs/util'
+import { Address, bytesToHex, hexToBytes } from '@ethereumjs/util'
 import { loadVerkleCrypto } from 'verkle-cryptography-wasm'
 import { assert, beforeAll, describe, it } from 'vitest'
 
-import { getStem } from '../src/index.js'
+import * as verkleBlockJSON from '../../statemanager/test/testdata/verkleKaustinen6Block72.json'
+import { getStem, verifyProof } from '../src/index.js'
 
 import type { VerkleCrypto } from '../src/index.js'
+import type { VerkleExecutionWitness } from '@ethereumjs/block'
 
 describe('Verkle cryptographic helpers', () => {
   let verkle: VerkleCrypto
@@ -24,5 +26,14 @@ describe('Verkle cryptographic helpers', () => {
       bytesToHex(getStem(verkle, Address.fromString('0x71562b71999873DB5b286dF957af199Ec94617f7'))),
       '0x1540dfad7755b40be0768c6aa0a5096fbf0215e0e8cf354dd928a178346466'
     )
+  })
+
+  it('verifyProof(): should verify verkle proofs', () => {
+    // Src: Kaustinen6 testnet, block 71 state root (parent of block 72)
+    const prestateRoot = hexToBytes(
+      '0x64e1a647f42e5c2e3c434531ccf529e1b3e93363a40db9fc8eec81f492123510'
+    )
+    const executionWitness = verkleBlockJSON.executionWitness as VerkleExecutionWitness
+    assert.isTrue(verifyProof(verkle, prestateRoot, executionWitness))
   })
 })
