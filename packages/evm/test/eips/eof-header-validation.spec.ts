84fd3c2732cd94408a8765f2506d7d0e2e96c807
Jochem Brouwer <jochembrouwer96@gmail.com>
2024-05-29 23:21:51 +0200

    evm: add general eof container validation tests

---
+++ packages/evm/test/eips/eof-header-validation.spec.ts
@@ -0,0 +1,78 @@
+import { hexToBytes } from '@ethereumjs/util'
+import * as dir from 'node-dir'
+import path from 'path'
+import { assert, describe, it } from 'vitest'
+
+import { validateEOF } from '../../src/eof/container.js'
+import { EVM } from '../../src/index.js'
+
+import { getCommon } from './eof-utils.js'
+
+const testDir = path.resolve('./test/eips/eoftests')
+
+async function getEVM() {
+  const common = getCommon()
+  const evm = EVM.create({
+    common,
+  })
+  return evm
+}
+
+await new Promise<void>((resolve, reject) => {
+  const finishedCallback = (err: Error | undefined) => {
+    if (err) {
+      reject(err)
+      return
+    }
+    resolve()
+  }
+  const fileCallback = async (
+    err: Error | undefined,
+    content: string | Uint8Array,
+    fileName: string,
+    next: Function
+  ) => {
+    if (err) {
+      reject(err)
+      return
+    }
+    const name = path.parse(fileName).name
+    describe(`EOF Header valdiation tests - ${name}`, async () => {
+      const testData = JSON.parse(<string>content)
+      //console.log(testData)
+      const evm = await getEVM()
+      for (const key in testData) {
+        it(`Container validation tests ${key}`, () => {
+          //@ts-ignore
+          const input = testData[key]
+          for (const testKey in input.vectors) {
+            const test = input.vectors[testKey]
+            const code = hexToBytes(test.code)
+
+            const expected = test.results.Prague.result
+            const _exception = test.results.Prague.exception
+
+            if (expected === true) {
+              validateEOF(code, evm)
+            } else {
+              assert.throws(() => {
+                // TODO verify that the correct error is thrown
+                validateEOF(code, evm)
+              })
+            }
+          }
+        })
+      }
+    })
+
+    next()
+  }
+  dir.readFiles(
+    testDir,
+    {
+      match: /.json$/,
+    },
+    fileCallback,
+    finishedCallback
+  )
+})
