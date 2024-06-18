10acaf48998dbc47b5110ef090022b6880cabdc5
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-06-18 07:30:04 -0700

    feat: Add bundle visualizer

--- packages/evm/vite.config.ts
+++ packages/evm/vite.config.ts
@@ -1,7 +1,21 @@
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import { defineConfig } from 'vitest/config'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [wasm(), topLevelAwait(), visualizer({ open: true, gzipSize: true })],
+  build: {
+    rollupOptions: {
+      // We choose safest to get worst case values
+      treeshake: 'safest',
+    },
+    lib: {
+      entry: 'src/index.ts',
+      name: '@ethereumjs/evm',
+      fileName: (format) => `my-library.${format}.js`,
+      // only build for es
+      formats: ['es'],
+    },
+  },
 })
