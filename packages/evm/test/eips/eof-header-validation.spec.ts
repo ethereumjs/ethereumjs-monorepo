1e97ba19f17f33cd0f4efd6f442b08e93c53d0c9
Jochem Brouwer <jochembrouwer96@gmail.com>
2024-07-04 13:26:25 +0200

    evm: updat eeof header validation for "initcode" containers

--- packages/evm/test/eips/eof-header-validation.spec.ts
+++ packages/evm/test/eips/eof-header-validation.spec.ts
@@ -3,12 +3,13 @@ import * as dir from 'node-dir'
 import path from 'path'
 import { assert, describe, it } from 'vitest'
 
-import { validateEOF } from '../../src/eof/container.js'
+import { EOFContainerMode, validateEOF } from '../../src/eof/container.js'
+import { ContainerSectionType } from '../../src/eof/verify.js'
 import { EVM } from '../../src/index.js'
 
 import { getCommon } from './eof-utils.js'
 
-const testDir = path.resolve('./test/eips/eoftests')
+const testDir = path.resolve('../ethereum-tests/eof/fixtures/eof_tests')
 
 async function getEVM() {
   const common = getCommon()
@@ -51,12 +52,24 @@ await new Promise<void>((resolve, reject) => {
             const expected = test.results.Prague.result
             const _exception = test.results.Prague.exception
 
+            let containerSectionType = ContainerSectionType.RuntimeCode
+            let eofContainerMode = EOFContainerMode.Default
+
+            if (test.containerKind !== undefined) {
+              if (test.containerKind === 'INITCODE') {
+                containerSectionType = ContainerSectionType.InitCode
+                eofContainerMode = EOFContainerMode.Initmode
+              } else {
+                throw new Error('unknown container kind: ' + test.containerKind)
+              }
+            }
+
             if (expected === true) {
-              validateEOF(code, evm)
+              validateEOF(code, evm, containerSectionType, eofContainerMode)
             } else {
               assert.throws(() => {
                 // TODO verify that the correct error is thrown
-                validateEOF(code, evm)
+                validateEOF(code, evm, containerSectionType, eofContainerMode)
               })
             }
           }
