f033ce702d77541908c5c54b9b5cde292e75c73c
Holger Drewes <Holger.Drewes@gmail.com>
2024-07-15 12:34:36 +0200

    Remove misplaced (non consensus checkand useless (already checked in block header) difficulty equals 0 check from CasperConsensus

--- packages/blockchain/src/consensus/casper.ts
+++ packages/blockchain/src/consensus/casper.ts
@@ -1,8 +1,6 @@
 import { ConsensusAlgorithm } from '@ethereumjs/common'
-import { BIGINT_0 } from '@ethereumjs/util'
 
 import type { Consensus } from '../types.js'
-import type { BlockHeader } from '@ethereumjs/block'
 
 /**
  * This class encapsulates Casper-related consensus functionality when used with the Blockchain class.
@@ -20,11 +18,6 @@ export class CasperConsensus implements Consensus {
 
   public async validateConsensus(): Promise<void> {}
 
-  public async validateDifficulty(header: BlockHeader): Promise<void> {
-    if (header.difficulty !== BIGINT_0) {
-      const msg = 'invalid difficulty.  PoS blocks must have difficulty 0'
-      throw new Error(`${msg} ${header.errorStr()}`)
-    }
-  }
+  public async validateDifficulty(): Promise<void> {}
   public async newBlock(): Promise<void> {}
 }
