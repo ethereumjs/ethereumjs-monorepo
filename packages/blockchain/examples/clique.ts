9052933cec85ef240057b2ab3bb86043b68189f6
Holger Drewes <Holger.Drewes@gmail.com>
2024-07-16 11:59:04 +0200

    Add clique example

---
+++ packages/blockchain/examples/clique.ts
@@ -0,0 +1,14 @@
+import { createBlockchain, CliqueConsensus, ConsensusDict } from '@ethereumjs/blockchain'
+import { Chain, Common, ConsensusAlgorithm, Hardfork } from '@ethereumjs/common'
+
+const common = new Common({ chain: Chain.Goerli, hardfork: Hardfork.London })
+
+const consensusDict: ConsensusDict = {}
+consensusDict[ConsensusAlgorithm.Clique] = new CliqueConsensus()
+const blockchain = await createBlockchain({
+  consensusDict: {
+    clique: new CliqueConsensus(),
+  },
+  common,
+})
+console.log(`Created blockchain with ${blockchain.consensus.algorithm} consensus algorithm`)
