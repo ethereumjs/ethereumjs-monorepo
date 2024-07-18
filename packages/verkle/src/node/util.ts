d7c26198e7967c6303b5b65988979a5461a6223b
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-07-18 15:37:36 -0400

    Apply leaf marker on all touched values (#3520)
    * Apply leaf marker to touched values
    
    * Add test for leaf marker

--- packages/verkle/src/node/util.ts
+++ packages/verkle/src/node/util.ts
@@ -88,9 +88,9 @@ export const createCValues = (values: (Uint8Array | VerkleLeafNodeValue)[]) => {
         break
     }
     // We add 16 trailing zeros to each value since all commitments are padded to an array of 32 byte values
-    // TODO: Determine whether we need to apply the leaf marker (i.e. set 129th bit) for all written values
-    // regardless of whether the value stored is zero or not
     expandedValues[x * 2] = setLengthRight(val.slice(0, 16), 32)
+    // Apply leaf marker to all touched values (i.e. flip 129th bit)
+    if (retrievedValue !== VerkleLeafNodeValue.Untouched) expandedValues[x * 2][16] = 0x80
     expandedValues[x * 2 + 1] = setLengthRight(val.slice(16), 32)
   }
   return expandedValues
