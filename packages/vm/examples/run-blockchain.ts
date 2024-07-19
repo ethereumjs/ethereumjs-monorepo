783643ae2d450490a1731dfb4d6048a2c3f17b04
ScottyPoi <scott.simpson@ethereum.org>
2024-07-19 13:30:14 -0600

    update examples

--- packages/vm/examples/run-blockchain.ts
+++ packages/vm/examples/run-blockchain.ts
@@ -6,7 +6,14 @@
 // 4. Puts the blocks from ../utils/blockchain-mock-data "blocks" attribute into the Blockchain
 // 5. Runs the Blockchain on the VM.
 
-import { Account, Address, toBytes, setLengthLeft, bytesToHex, hexToBytes } from '@ethereumjs/util'
+import {
+  Address,
+  toBytes,
+  setLengthLeft,
+  bytesToHex,
+  hexToBytes,
+  accountFromAccountData,
+} from '@ethereumjs/util'
 import {
   Block,
   createBlockFromBlockData,
