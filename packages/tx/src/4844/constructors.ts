402b4f72cafe05fa068acb5ffe4966226f7f22ca
Jochem Brouwer <jochembrouwer96@gmail.com>
2024-07-23 21:16:58 +0200

    add 4844 docs
    Co-authored-by: acolytec3 <17355484+acolytec3@users.noreply.github.com>

--- packages/tx/src/4844/constructors.ts
+++ packages/tx/src/4844/constructors.ts
@@ -58,6 +58,18 @@ const validateBlobTransactionNetworkWrapper = (
   }
 }
 
+/**
+ * Instantiate a transaction from a data dictionary.
+ *
+ * Format: { chainId, nonce, gasPrice, gasLimit, to, value, data, accessList,
+ * v, r, s, blobs, kzgCommitments, blobVersionedHashes, kzgProofs }
+ *
+ * Notes:
+ * - `chainId` will be set automatically if not provided
+ * - All parameters are optional and have some basic default values
+ * - `blobs` cannot be supplied as well as `kzgCommittments`, `blobVersionedHashes`, `kzgProofs`
+ * - If `blobs` is passed in,  `kzgCommittments`, `blobVersionedHashes`, `kzgProofs` will be derived by the constructor
+ */
 export function create4844BlobTx(txData: TxData, opts?: TxOptions) {
   if (opts?.common?.customCrypto?.kzg === undefined) {
     throw new Error(
