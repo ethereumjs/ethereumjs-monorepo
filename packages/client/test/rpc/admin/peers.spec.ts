f9788a1c1ee703de2b9bbd3b930ad80f5aeea834
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-08-08 04:36:35 -0400

    Add `admin_peer` RPC endpoint (#3570)
    * add initial implementation of admin_peers
    
    * add more fields
    
    * fix versions
    
    * add test

---
+++ packages/client/test/rpc/admin/peers.spec.ts
@@ -0,0 +1,38 @@
+import { randomBytes } from 'crypto'
+import { assert, describe, it } from 'vitest'
+
+import { createClient, createManager, getRpcClient, startRPC } from '../helpers.js'
+
+const method = 'admin_peers'
+
+describe(method, () => {
+  it('works', async () => {
+    const manager = createManager(await createClient({ opened: true, noPeers: true }))
+    const rpc = getRpcClient(startRPC(manager.getMethods()))
+
+    console.log(manager['_client'].services[0].pool)
+    //@ts-ignore
+    manager['_client'].services[0].pool.peers = [
+      {
+        id: 'abcd',
+        eth: {
+          versions: ['68'],
+          status: {
+            td: 1n,
+            bestHash: randomBytes(32),
+          },
+        },
+        rlpxPeer: {
+          _hello: {
+            clientId: 'fakeClient',
+          },
+        },
+        address: '127.0.0.1:8545',
+      },
+    ]
+    const res = await rpc.request(method, [])
+    const { result } = res
+    console.log(res)
+    assert.notEqual(result, undefined, 'admin_peers returns a value')
+  })
+})
