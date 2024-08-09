814b9f7814f434bba71b5ca1cd83a71a510a56ca
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-08-09 07:16:16 -0400

    statemanager: fix tests

--- packages/statemanager/test/stateManager.account.spec.ts
+++ packages/statemanager/test/stateManager.account.spec.ts
@@ -12,7 +12,7 @@ describe('StateManager -> General/Account', () => {
     { deactivate: false, size: 0 },
   ]) {
     it(`should set the state root to empty`, async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       assert.ok(equalsBytes(stateManager['_trie'].root(), KECCAK256_RLP), 'it has default root')
 
       // commit some data to the trie
@@ -32,7 +32,7 @@ describe('StateManager -> General/Account', () => {
     })
 
     it(`should clear the cache when the state root is set`, async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
       const account = createAccountWithDefaults()
 
@@ -75,7 +75,7 @@ describe('StateManager -> General/Account', () => {
     })
 
     it('should put and get account, and add to the underlying cache if the account is not found', async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       const account = createAccountWithDefaults()
       const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
 
@@ -86,7 +86,7 @@ describe('StateManager -> General/Account', () => {
       assert.equal(res1!.balance, BigInt(0xfff384))
 
       await stateManager.flush()
-      stateManager['_accountCache']?.clear()
+      stateManager['_caches'].account?.clear()
 
       const res2 = await stateManager.getAccount(address)
 
@@ -94,7 +94,7 @@ describe('StateManager -> General/Account', () => {
     })
 
     it(`should return undefined for a non-existent account`, async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
 
       const res = (await stateManager.getAccount(address)) === undefined
@@ -103,7 +103,7 @@ describe('StateManager -> General/Account', () => {
     })
 
     it(`should return undefined for an existent account`, async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       const account = createAccountWithDefaults(BigInt(0x1), BigInt(0x1))
       const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
 
@@ -115,7 +115,7 @@ describe('StateManager -> General/Account', () => {
     })
 
     it(`should modify account fields correctly`, async () => {
-      const stateManager = new DefaultStateManager({ accountCacheOpts })
+      const stateManager = new DefaultStateManager({ cachesOpts: { accountCacheOpts } })
       const account = createAccountWithDefaults()
       const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
       await stateManager.putAccount(address, account)
