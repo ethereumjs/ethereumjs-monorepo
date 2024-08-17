bef29538004161a0f897ace5ea136b284c0ca061
Holger Drewes <Holger.Drewes@gmail.com>
2024-08-17 15:11:19 +0200

    Do not initialize caches for default VM state manager

--- packages/vm/src/vm.ts
+++ packages/vm/src/vm.ts
@@ -1,7 +1,7 @@
 import { createBlockchain } from '@ethereumjs/blockchain'
 import { Common, Mainnet } from '@ethereumjs/common'
 import { createEVM, getActivePrecompiles } from '@ethereumjs/evm'
-import { Caches, DefaultStateManager } from '@ethereumjs/statemanager'
+import { DefaultStateManager } from '@ethereumjs/statemanager'
 import {
   Account,
   Address,
@@ -83,7 +83,6 @@ export class VM {
 
     if (opts.stateManager === undefined) {
       opts.stateManager = new DefaultStateManager({
-        caches: new Caches(),
         common: opts.common,
       })
     }
