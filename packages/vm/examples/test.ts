79f76724b116f715847a7a8bc82d9ceefefad9bf
Holger Drewes <Holger.Drewes@gmail.com>
2024-08-19 15:07:43 +0200

    Remove accidentally committed examples/test.ts file

--- packages/vm/examples/test.ts
+++
@@ -1,6 +0,0 @@
-import { StatelessVerkleStateManager } from '@ethereumjs/statemanager'
-import { loadVerkleCrypto } from 'verkle-cryptography-wasm'
-
-const verkleCrypto = await loadVerkleCrypto()
-const sm = new StatelessVerkleStateManager({ verkleCrypto })
-console.log(sm)
