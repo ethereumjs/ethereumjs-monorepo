7906bf741cb4a8107f1b98bcf81172a1be126843
Jochem Brouwer <jochembrouwer96@gmail.com>
2024-09-02 17:17:01 +0200

    vm: correctly exit early on system contracts, un-bumping system account nonce

--- packages/vm/src/requests.ts
+++ packages/vm/src/requests.ts
@@ -1,14 +1,14 @@
 import { Mainnet } from '@ethereumjs/common'
 import {
-  ConsolidationRequest,
-  DepositRequest,
-  WithdrawalRequest,
   bigIntToAddressBytes,
   bigIntToBytes,
   bytesToBigInt,
   bytesToHex,
   bytesToInt,
   createAddressFromString,
+  createConsolidationRequest,
+  createDepositRequest,
+  createWithdrawalRequest,
   setLengthLeft,
   unpadBytes,
 } from '@ethereumjs/util'
@@ -66,16 +66,9 @@ const accumulateEIP7002Requests = async (
   )
   const withdrawalsAddress = createAddressFromString(bytesToHex(addressBytes))
 
-  const code = await vm.stateManager.getCode(withdrawalsAddress)
-
-  if (code.length === 0) {
-    throw new Error(
-      'Attempt to accumulate EIP-7002 requests failed: the contract does not exist. Ensure the deployment tx has been run, or that the required contract code is stored',
-    )
-  }
-
   const systemAddressBytes = bigIntToAddressBytes(vm.common.param('systemAddress'))
   const systemAddress = createAddressFromString(bytesToHex(systemAddressBytes))
+  const systemAccount = await vm.stateManager.getAccount(systemAddress)
 
   const originalAccount = await vm.stateManager.getAccount(withdrawalsAddress)
 
@@ -89,6 +82,12 @@ const accumulateEIP7002Requests = async (
     to: withdrawalsAddress,
   })
 
+  if (systemAccount === undefined) {
+    await vm.stateManager.deleteAccount(systemAddress)
+  } else {
+    await vm.stateManager.putAccount(systemAddress, systemAccount)
+  }
+
   const resultsBytes = results.execResult.returnValue
   if (resultsBytes.length > 0) {
     // Each request is 76 bytes
@@ -97,7 +96,7 @@ const accumulateEIP7002Requests = async (
       const sourceAddress = slicedBytes.slice(0, 20) // 20 Bytes
       const validatorPubkey = slicedBytes.slice(20, 68) // 48 Bytes
       const amount = bytesToBigInt(unpadBytes(slicedBytes.slice(68, 76))) // 8 Bytes / Uint64
-      requests.push(WithdrawalRequest.fromRequestData({ sourceAddress, validatorPubkey, amount }))
+      requests.push(createWithdrawalRequest({ sourceAddress, validatorPubkey, amount }))
     }
   }
 }
@@ -113,16 +112,9 @@ const accumulateEIP7251Requests = async (
   )
   const consolidationsAddress = createAddressFromString(bytesToHex(addressBytes))
 
-  const code = await vm.stateManager.getCode(consolidationsAddress)
-
-  if (code.length === 0) {
-    throw new Error(
-      'Attempt to accumulate EIP-7251 requests failed: the contract does not exist. Ensure the deployment tx has been run, or that the required contract code is stored',
-    )
-  }
-
   const systemAddressBytes = bigIntToAddressBytes(vm.common.param('systemAddress'))
   const systemAddress = createAddressFromString(bytesToHex(systemAddressBytes))
+  const systemAccount = await vm.stateManager.getAccount(systemAddress)
 
   const originalAccount = await vm.stateManager.getAccount(consolidationsAddress)
 
@@ -136,6 +128,12 @@ const accumulateEIP7251Requests = async (
     to: consolidationsAddress,
   })
 
+  if (systemAccount === undefined) {
+    await vm.stateManager.deleteAccount(systemAddress)
+  } else {
+    await vm.stateManager.putAccount(systemAddress, systemAccount)
+  }
+
   const resultsBytes = results.execResult.returnValue
   if (resultsBytes.length > 0) {
     // Each request is 116 bytes
@@ -144,9 +142,7 @@ const accumulateEIP7251Requests = async (
       const sourceAddress = slicedBytes.slice(0, 20) // 20 Bytes
       const sourcePubkey = slicedBytes.slice(20, 68) // 48 Bytes
       const targetPubkey = slicedBytes.slice(68, 116) // 48 bytes
-      requests.push(
-        ConsolidationRequest.fromRequestData({ sourceAddress, sourcePubkey, targetPubkey }),
-      )
+      requests.push(createConsolidationRequest({ sourceAddress, sourcePubkey, targetPubkey }))
     }
   }
 }
@@ -215,7 +211,7 @@ const accumulateDeposits = async (
         ])
         const index = bytesToBigInt(indexBytesBigEndian)
         requests.push(
-          DepositRequest.fromRequestData({
+          createDepositRequest({
             pubkey,
             withdrawalCredentials,
             amount,
