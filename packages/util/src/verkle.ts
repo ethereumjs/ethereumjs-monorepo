54c30c53d7a8ea30145c75dc811fe932ad57dd33
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-09-06 15:24:02 -0400

    Update types and add test

--- packages/util/src/verkle.ts
+++ packages/util/src/verkle.ts
@@ -34,6 +34,8 @@ export interface VerkleCrypto {
   verifyExecutionWitnessPreState: (prestateRoot: string, execution_witness_json: string) => boolean
   hashCommitment: (commitment: Uint8Array) => Uint8Array
   serializeCommitment: (commitment: Uint8Array) => Uint8Array
+  createProof: (bytes: Uint8Array) => Uint8Array
+  verifyProof: (proof: Uint8Array) => boolean
 }
 
 /**
