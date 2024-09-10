cec4f9218f32d23a459fdbe505a9e0355bbbfbb2
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-09-09 23:47:52 -0400

    Update packages/util/src/verkle.ts

--- packages/util/src/verkle.ts
+++ packages/util/src/verkle.ts
@@ -291,7 +291,7 @@ export function decodeVerkleLeafBasicData(encodedBasicData: Uint8Array): VerkleL
  * in EIP-6800.
  * @param {VerkleLeafBasicData} basicData - An object containing the version, nonce,
  *   code size, and balance to be encoded.
- * @returns {Uint8Array} - A compact, bytes representation of the account header basic data.
+ * @returns {Uint8Array} - A compact bytes representation of the account header basic data.
  */
 export function encodeVerkleLeafBasicData(basicData: VerkleLeafBasicData): Uint8Array {
   const encodedVersion = setLengthLeft(int32ToBytes(basicData.version), VERKLE_VERSION_BYTES_LENGTH)
