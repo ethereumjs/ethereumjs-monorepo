3bc8e6090af9810536d78fa38fb9597faa6082b0
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-09-20 12:45:57 -0400

    ignore missing types for trusted-setups

--- packages/client/bin/cli.ts
+++ packages/client/bin/cli.ts
@@ -28,7 +28,8 @@ import {
   setLengthLeft,
   short,
 } from '@ethereumjs/util'
-import trustedSetup from '@paulmillr/trusted-setups/fast.js'
+//@ts-ignore trusted-setups doesn't include types
+import { trustedSetup as fast } from '@paulmillr/trusted-setups/fast.js'
 import {
   keccak256 as keccak256WASM,
   secp256k1Expand,
@@ -934,7 +935,7 @@ async function run() {
   const chain = getPresetChainConfig(chainName)
   const cryptoFunctions: CustomCrypto = {}
 
-  const kzg = new microEthKZG(trustedSetup.trustedSetup)
+  const kzg = new microEthKZG(fast)
   // Initialize WASM crypto if JS crypto is not specified
   if (args.useJsCrypto === false) {
     await waitReadyPolkadotSha256()
