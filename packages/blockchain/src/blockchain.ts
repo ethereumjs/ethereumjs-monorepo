58e68b9a93de234027c6516f782b46b2c5f04e66
Scorbajio <indigophi@protonmail.com>
2024-09-25 12:39:00 -0700

    Debug logger namespace standardization (#3692)
    * trie: Make debug logger tags use lower case naming
    
    * verkle: Make debug logger tags use lower case naming
    
    * trie: Update debug namespaces in README
    
    * trie: Update debug examples to use lower case naming
    
    * verkle: Update debug namespaces in README
    
    * verkle: Update debug examples in README to use lower case naming
    
    * trie: Print hex instead of bytes to console
    
    * Change root debug namespaces to include a collon-hash to be included in wild-card namespaces
    
    * Make base namespace end with colon-hash in docs and READMEs
    
    * Do not calculate canoncial header if DEBUG is not set
    
    * Fix spelling issue
    
    * Fix spelling mistakes
    
    * Make fetcher namespace use collon-pound
    
    * Remove unnecessary check
    
    * Add verkle core namespace option to README
    
    * Update debug option description
    
    * Fix spelling mistake in example
    
    * Update trie README debug namespaces
    
    * Add proof symbols to debug options list
    
    * Make proof-related debug tags lowercase
    
    * Put branch index in debug message and out of namespace
    
    * Update verkle README debug namespaces
    
    * Convert bytes before logging them
    
    * Expand debug string logic and add branch index to it
    
    * Do not convert bytes to hex
    
    ---------
    
    Co-authored-by: Holger Drewes <Holger.Drewes@gmail.com>
    Co-authored-by: acolytec3 <17355484+acolytec3@users.noreply.github.com>

--- packages/blockchain/src/blockchain.ts
+++ packages/blockchain/src/blockchain.ts
@@ -107,7 +107,7 @@ export class Blockchain implements BlockchainInterface {
   constructor(opts: BlockchainOptions = {}) {
     this.DEBUG =
       typeof window === 'undefined' ? (process?.env?.DEBUG?.includes('ethjs') ?? false) : false
-    this._debug = debugDefault('blockchain')
+    this._debug = debugDefault('blockchain:#')
 
     if (opts.common) {
       this.common = opts.common
@@ -327,7 +327,10 @@ export class Blockchain implements BlockchainInterface {
 
   async resetCanonicalHead(canonicalHead: bigint) {
     let hash: Uint8Array | undefined
-    const canonicalHeadHash = (await this.getCanonicalHeadHeader()).hash()
+    let canonicalHeadHash: Uint8Array | undefined
+    if (this.DEBUG) {
+      canonicalHeadHash = (await this.getCanonicalHeadHeader()).hash()
+    }
     await this.runWithLock<void>(async () => {
       hash = await this.dbManager.numberToHash(canonicalHead)
       if (hash === undefined) {
@@ -352,7 +355,7 @@ export class Blockchain implements BlockchainInterface {
 
       this.DEBUG &&
         this._debug(
-          `Canonical head set from ${bytesToHex(canonicalHeadHash)} to ${bytesToHex(hash!)} (number ${bigIntToHex(canonicalHead)})`,
+          `Canonical head set from ${bytesToHex(canonicalHeadHash!)} to ${bytesToHex(hash!)} (number ${bigIntToHex(canonicalHead)})`,
         )
 
       this._deletedBlocks = []
