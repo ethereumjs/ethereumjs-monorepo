f9b86646e4561c07f18f5252ab45cae82f67ba53
Amir <indigophi@protonmail.com>
2024-12-02 16:30:43 -0700

    Update test messages

--- packages/client/test/rpc/debug/verbosity.spec.ts
+++ packages/client/test/rpc/debug/verbosity.spec.ts
@@ -4,16 +4,16 @@ import { createClient, createManager, getRPCClient, startRPC } from '../helpers.
 
 const method = 'debug_verbosity'
 
-const logLevels: {[key: number]: string} = {
+const logLevels: { [key: number]: string } = {
   0: 'error',
   1: 'warn',
   2: 'info',
-  3: 'debug'
+  3: 'debug',
 }
 
 describe(method, () => {
   it('works', async () => {
-    const manager = createManager(await createClient({ opened: true, noPeers: true, }))
+    const manager = createManager(await createClient({ opened: true, noPeers: true }))
     const rpc = getRPCClient(startRPC(manager.getMethods()))
     const client = manager['_client']
 
@@ -22,13 +22,13 @@ describe(method, () => {
     // lowest level; e.g. only show errors
     const levelError = 0
     res = await rpc.request(method, [levelError])
-    assert.equal(res.result, 'level: error', 'verbosity level successfully changed')
+    assert.equal(res.result, 'level: error', 'verbosity level successfully lowered')
     assert.equal(client.config.logger.level, logLevels[levelError])
 
     // highest level; e.g. be very verbose and show even debug logs
     const levelDebug = 3
     res = await rpc.request(method, [levelDebug])
-    assert.equal(res.result, 'level: debug', 'verbosity level successfully changed')
+    assert.equal(res.result, 'level: debug', 'verbosity level successfully highered')
     assert.equal(client.config.logger.level, logLevels[levelDebug])
   })
 })
