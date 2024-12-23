216650badf76c98dc5ad83f0e955daf9b8db5dee
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-12-22 21:27:41 -0700

    vm: don't include access witness if miner value is 0

--- packages/vm/src/runTx.ts
+++ packages/vm/src/runTx.ts
@@ -676,7 +676,14 @@ async function _runTx(vm: VM, opts: RunTxOpts): Promise<RunTxResult> {
     : results.amountSpent
   minerAccount.balance += results.minerValue
 
-  if (vm.common.isActivatedEIP(6800)) {
+  // If the miner value is zero, revert the access witness as the proof of absence was not necessary.
+  if (results.minerValue === BIGINT_0) {
+    vm.evm.verkleAccessWitness?.revert()
+  } else {
+    vm.evm.verkleAccessWitness?.commit()
+  }
+
+  if (vm.common.isActivatedEIP(6800) && results.minerValue !== BIGINT_0) {
     if (vm.evm.verkleAccessWitness === undefined) {
       throw Error(`verkleAccessWitness required if verkle (EIP-6800) is activated`)
     }
