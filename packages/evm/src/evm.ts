e15b73ba13f7985c9025b32f45c84adad1247d8f
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-01-08 10:57:16 -0700

    evm: simplify verkle access witness methods (#3830)

--- packages/evm/src/evm.ts
+++ packages/evm/src/evm.ts
@@ -269,18 +269,22 @@ export class EVM implements EVMInterface {
       }
       const sendsValue = message.value !== BIGINT_0
       if (message.depth === 0) {
-        const originAccessGas = message.accessWitness.touchTxOriginAndComputeGas(fromAddress)
+        const originAccessGas = message.accessWitness.readAccountHeader(fromAddress)
         debugGas(`originAccessGas=${originAccessGas} waived off for origin at depth=0`)
 
-        const destAccessGas = message.accessWitness.touchTxTargetAndComputeGas(message.to, {
-          sendsValue,
-        })
+        let destAccessGas = message.accessWitness.readAccountCodeHash(message.to)
+        if (sendsValue) {
+          destAccessGas += message.accessWitness.writeAccountBasicData(message.to)
+        } else {
+          destAccessGas += message.accessWitness.readAccountBasicData(message.to)
+        }
+
         debugGas(`destAccessGas=${destAccessGas} waived off for target at depth=0`)
       }
 
-      let callAccessGas = message.accessWitness.touchAndChargeMessageCall(message.to)
+      let callAccessGas = message.accessWitness.readAccountBasicData(message.to)
       if (sendsValue) {
-        callAccessGas += message.accessWitness.touchAndChargeValueTransfer(message.to)
+        callAccessGas += message.accessWitness.writeAccountBasicData(message.to)
       }
       gasLimit -= callAccessGas
       if (gasLimit < BIGINT_0) {
@@ -315,9 +319,7 @@ export class EVM implements EVMInterface {
     let toAccount = await this.stateManager.getAccount(message.to)
     if (!toAccount) {
       if (this.common.isActivatedEIP(6800)) {
-        const absenceProofAccessGas = message.accessWitness!.touchAndChargeProofOfAbsence(
-          message.to,
-        )
+        const absenceProofAccessGas = message.accessWitness!.readAccountHeader(message.to)
         gasLimit -= absenceProofAccessGas
         if (gasLimit < BIGINT_0) {
           if (this.DEBUG) {
@@ -418,7 +420,7 @@ export class EVM implements EVMInterface {
 
     if (this.common.isActivatedEIP(6800)) {
       if (message.depth === 0) {
-        const originAccessGas = message.accessWitness!.touchTxOriginAndComputeGas(fromAddress)
+        const originAccessGas = message.accessWitness!.readAccountHeader(fromAddress)
         debugGas(`originAccessGas=${originAccessGas} waived off for origin at depth=0`)
       }
     }
@@ -464,9 +466,7 @@ export class EVM implements EVMInterface {
     }
 
     if (this.common.isActivatedEIP(6800)) {
-      const contractCreateAccessGas = message.accessWitness!.touchAndChargeContractCreateInit(
-        message.to,
-      )
+      const contractCreateAccessGas = message.accessWitness!.writeAccountBasicData(message.to)
       gasLimit -= contractCreateAccessGas
       if (gasLimit < BIGINT_0) {
         if (this.DEBUG) {
@@ -550,8 +550,7 @@ export class EVM implements EVMInterface {
 
     if (exit) {
       if (this.common.isActivatedEIP(6800)) {
-        const createCompleteAccessGas =
-          message.accessWitness!.touchAndChargeContractCreateCompleted(message.to)
+        const createCompleteAccessGas = message.accessWitness!.writeAccountHeader(message.to)
         gasLimit -= createCompleteAccessGas
         if (gasLimit < BIGINT_0) {
           if (this.DEBUG) {
@@ -671,9 +670,7 @@ export class EVM implements EVMInterface {
     // get the fresh gas limit for the rest of the ops
     gasLimit = message.gasLimit - result.executionGasUsed
     if (!result.exceptionError && this.common.isActivatedEIP(6800)) {
-      const createCompleteAccessGas = message.accessWitness!.touchAndChargeContractCreateCompleted(
-        message.to,
-      )
+      const createCompleteAccessGas = message.accessWitness!.writeAccountHeader(message.to)
       gasLimit -= createCompleteAccessGas
       if (gasLimit < BIGINT_0) {
         if (this.DEBUG) {
@@ -699,12 +696,11 @@ export class EVM implements EVMInterface {
     ) {
       // Add access charges for writing this code to the state
       if (this.common.isActivatedEIP(6800)) {
-        const byteCodeWriteAccessfee =
-          message.accessWitness!.touchCodeChunksRangeOnWriteAndComputeGas(
-            message.to,
-            0,
-            result.returnValue.length - 1,
-          )
+        const byteCodeWriteAccessfee = message.accessWitness!.writeAccountCodeChunks(
+          message.to,
+          0,
+          result.returnValue.length - 1,
+        )
         gasLimit -= byteCodeWriteAccessfee
         if (gasLimit < BIGINT_0) {
           if (this.DEBUG) {
