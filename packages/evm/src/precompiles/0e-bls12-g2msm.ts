a46beab850069d7fb420515065d5acd773a29f9b
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-01-22 11:53:37 +0100

    Remove unused params from common (#3836)
    * params: remove unused params
    
    * evm: unpeg BLS costs to EIP2537

--- packages/evm/src/precompiles/0e-bls12-g2msm.ts
+++ packages/evm/src/precompiles/0e-bls12-g2msm.ts
@@ -27,7 +27,7 @@ export async function precompile0e(opts: PrecompileInput): Promise<ExecResult> {
   }
 
   const numPairs = Math.floor(opts.data.length / 288)
-  const gasUsedPerPair = opts.common.paramByEIP('bls12381G2MulGas', 2537) ?? BigInt(0)
+  const gasUsedPerPair = opts.common.param('bls12381G2MulGas') ?? BigInt(0)
   const gasUsed = msmGasUsed(numPairs, gasUsedPerPair, BLS_GAS_DISCOUNT_PAIRS_G2)
 
   if (!gasLimitCheck(opts, gasUsed, pName)) {
