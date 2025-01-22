16bbd5530799ada02915ea37ff3b9f1efb20f61b
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-22 15:39:40 -0500

    switch c8 to `@vitest/coverage-v8`

---
+++ packages/client/vitest.config.coverage.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    silent: true,
+    exclude: ['test/integration', 'test/sim', 'test/cli'],
+    testTimeout: 300000,
+    alias: { '@polkadot/util': 'false' },
+    typecheck: {
+      tsconfig: './tsconfig.prod.esm.json',
+    },
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      all: true,
+    },
+  },
+})
