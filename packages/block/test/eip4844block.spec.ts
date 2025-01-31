dfd9446e83c72813181bdd0507ec895da76a349c
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-31 06:32:49 -0500

    Address feedback

--- packages/block/test/eip4844block.spec.ts
+++ packages/block/test/eip4844block.spec.ts
@@ -27,7 +27,6 @@ describe('EIP4844 header tests', () => {
     customCrypto: { kzg },
   })
 
-  console.log(common.hardforks())
   it('should work', () => {
     const earlyCommon = new Common({ chain: Mainnet, hardfork: Hardfork.Istanbul })
 
