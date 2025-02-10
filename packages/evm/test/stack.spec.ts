a1f5de03da88ef59fc7ba9d80b42857986f57ce5
Lxt3h <44911576+Lexterl33t@users.noreply.github.com>
2025-02-10 19:58:11 +0100

    Fix the vulnerability found in issue #3861 (#3863)
    * Fix PUSHN Opcode Non-Compliance
    
    Co-authored-by: Lexterl33t <bryton@fuzzinglabs.com>
    
    * Test for PUSHn non compliance
    
    Co-authored-by: mhoste51 <mathieu@fuzzinglabs.com>
    
    * Remove console.log from test
    
    * Comment moved above the code
    
    * Comment moved above the code (updated)
    
    * Update the user balance to 0
    
    * Refacto after the review
    
    Co-authored-by: mhoste51 <mathieu@fuzzinglabs.com>
    
    * evm: update push logic
    
    * evm: test all push opcodes
    
    * evm: add stack test for code with jumps, truncated push opcodes
    
    * evm: fix cache item for truncated pushs
    
    * evm/interpreter: use subarray instead of slice for push bytes cache
    
    ---------
    
    Co-authored-by: Mhoste <mathieu@fuzzinglabs.com>
    Co-authored-by: Scorbajio <indigophi@protonmail.com>
    Co-authored-by: Jochem Brouwer <jochembrouwer96@gmail.com>

--- packages/evm/test/stack.spec.ts
+++ packages/evm/test/stack.spec.ts
@@ -1,4 +1,12 @@
-import { Account, Address, bigIntToBytes, hexToBytes, setLengthLeft } from '@ethereumjs/util'
+import {
+  Account,
+  Address,
+  bigIntToBytes,
+  bytesToBigInt,
+  hexToBytes,
+  setLengthLeft,
+  setLengthRight,
+} from '@ethereumjs/util'
 import { assert, describe, it } from 'vitest'
 
 import { createEVM } from '../src/index.js'
@@ -147,5 +155,31 @@ describe('Stack', () => {
     s.pop()
     const reportedStack = s.getStack()
     assert.deepEqual(reportedStack, [BigInt(4), BigInt(6)])
+  })
+
+  it('stack should return the padded value', async () => {
+    const evm = await createEVM()
+
+    for (let pushN = 0x60; pushN <= 0x7f; pushN++) {
+      // PUSHx 01
+      const code = `0x${pushN.toString(16)}01`
+      // PUSH 0x03 JUMP JUMPDEST < PUSHx 01 >
+      const codeWithJumps = `0x6003565B${pushN.toString(16)}01`
+
+      const expectedStack = new Stack(1024)
+      expectedStack.push(bytesToBigInt(setLengthRight(new Uint8Array([0x01]), pushN - 0x5f)))
+
+      const resWithoutJumps = await evm.runCall({
+        data: hexToBytes(code),
+      })
+      const executionStack = resWithoutJumps.execResult.runState?.stack
+      assert.deepEqual(executionStack, expectedStack, 'code without jumps ok')
+
+      const resWithJumps = await evm.runCall({
+        data: hexToBytes(codeWithJumps),
+      })
+      const executionStackWithJumps = resWithJumps.execResult.runState?.stack
+      assert.deepEqual(executionStackWithJumps, expectedStack, 'code with jumps ok')
+    }
   })
 })
