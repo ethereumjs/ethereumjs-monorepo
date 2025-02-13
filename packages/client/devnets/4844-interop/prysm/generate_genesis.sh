a94c860ac2df38f124367cd580540acfc99e034f
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-13 12:35:27 +0100

    Update generate_genesis.sh

--- packages/client/devnets/4844-interop/prysm/generate_genesis.sh
+++ packages/client/devnets/4844-interop/prysm/generate_genesis.sh
@@ -3,7 +3,7 @@ CONFIGDIR=$(pwd)
 PRYSMDIR=$1
 GENESIS=$(($(date +%s) + 30)) # 120s until genesis, feel free to increase this to give you more time to everything
 
-# The following are configureable too but you have to make sure they align.
+# The following are configurable too but you have to make sure they align.
 # Take SECONDS_PER_SLOT * SLOTS_PER_EPOCH * CAPELLA_FORK_EPOCH for SHANGHAI
 # Take SECONDS_PER_SLOT * SLOTS_PER_EPOCH * EIP4844_FORK_EPOCH for CANCUN
 SHANGHAI=$(($GENESIS + 144))
