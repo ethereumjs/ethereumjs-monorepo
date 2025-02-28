988b6f39fc99744c721a627cefb1d2707749f75f
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-02-28 14:13:51 +0100

    vm: unpad r/s values for authority items

--- packages/vm/test/api/runBlock.spec.ts
+++ packages/vm/test/api/runBlock.spec.ts
@@ -627,7 +627,14 @@ describe('runBlock() -> tx types', async () => {
 
       const yParity = signed.v === BigInt(27) ? new Uint8Array() : new Uint8Array([1])
 
-      return [chainIdBytes, addressBytes, nonceBytes, yParity, signed.r, signed.s]
+      return [
+        chainIdBytes,
+        addressBytes,
+        nonceBytes,
+        yParity,
+        unpadBytes(signed.r),
+        unpadBytes(signed.s),
+      ]
     }
 
     const common = new Common({
