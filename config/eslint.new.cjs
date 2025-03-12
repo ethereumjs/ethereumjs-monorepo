7793663cb99ca95c10c2bead1f5ec360cfe78490
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-03-11 21:58:17 -0700

    monorepo: wip eslint updates

---
+++ config/eslint.new.cjs
@@ -0,0 +1,207 @@
+const jsPlugin = require('@eslint/js');
+const typeScriptPlugin = require('@typescript-eslint/eslint-plugin');
+const ethereumjsPlugin = require('eslint-plugin-ethereumjs');
+const githubPlugin = require('eslint-plugin-github');
+const implicitDependenciesPlugin = require('eslint-plugin-implicit-dependencies');
+const importPlugin = require('eslint-plugin-import');
+const simpleImportSortPlugin = require('eslint-plugin-simple-import-sort');
+
+module.exports = [
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  jsPlugin.configs.recommended,
+  {
+    languageOptions: {
+    globals: {
+      es2020: true,
+      node: true
+    },
+    parser: require('@typescript-eslint/parser'),
+    parserOptions: {
+      project: './config/tsconfig.lint.json',
+      extraFileExtensions: ['.json'],
+      sourceType: 'module',
+
+    },
+  },
+  plugins: {
+    '@typescript-eslint': typeScriptPlugin,
+    'github': githubPlugin,
+    'implicit-dependencies': implicitDependenciesPlugin,
+    'simple-import-sort': simpleImportSortPlugin,
+    'ethereumjs': ethereumjsPlugin
+  },
+  ignores: [
+    '.eslintrc.cjs',
+    '.eslintrc.js',
+    'benchmarks',
+    'coverage',
+    'dist',
+    'node_modules',
+    'recipes',
+    'rlp.cjs',
+    'scripts',
+    'typedoc.js',
+    'vitest.config.ts',
+    'vitest.config.browser.ts',
+    'vitest.config.unit.ts',
+    'vitest.config.coverage.ts',
+    'vite.*.ts',
+    'ethereum-tests',
+    'archive',
+    'devnets',
+    'eslint',
+    'lint-staged.config.js',
+    'tsconfig.lint.json',
+    'package.json',
+  ],
+  rules: {
+    'no-restricted-imports': ['error', 'ethereum-cryptography/utils'],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase', 'camelCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-redeclare': ['error'],
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/return-await': 'error',
+    '@typescript-eslint/strict-boolean-expressions': ['error'],
+    '@typescript-eslint/no-use-before-define': 'warn', // TODO: decide if we care
+    eqeqeq: 'error',
+    'github/array-foreach': 'error',
+    'implicit-dependencies/no-implicit': ['error', { peer: true, dev: true, optional: true }],
+    'import/default': 'off',
+    'import/export': 'error',
+    'import/exports-last': 'off', // TODO: set to `warn` for fixing and then `error`
+    'import/extensions': ['error','ignorePackages'],
+    'import/first': 'error',
+    'import/group-exports': 'off',
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'import/no-absolute-path': 'error',
+    'import/no-anonymous-default-export': 'error',
+    'import/no-cycle': 'error',
+    'import/no-default-export': ['error'],
+    'import/no-deprecated': 'off', // TODO: set to `warn` for fixing and then `error`
+    'import/no-duplicates': 'error',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-namespace': 'off',
+    'import/no-self-import': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-unused-modules': 'error',
+    'import/no-useless-path-segments': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: ['object', ['builtin', 'external'], 'parent', 'sibling', 'index', 'type'],
+        'newlines-between': 'always',
+      },
+    ],
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-dupe-class-members': 'off',
+    'no-extra-semi': 'off',
+    'no-redeclare': 'off',
+    'no-unused-vars': 'off',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-const': 'error',
+    'simple-import-sort/exports': 'error',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'ethereumjs/noBuffer': 'error',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "ThrowStatement > NewExpression[callee.name='Error']",
+        message: "Throwing default JS Errors is not allowed. Only throw `EthereumJSError` (see the util package)",
+      }
+    ]
+  }},
+  // Override for test files
+  {
+    files: ['**/test/**/*.ts'],
+    rules: {
+      'implicit-dependencies/no-implicit': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'no-restricted-syntax': 'off',
+    },
+  },
+  // Override for examples and benchmarks
+  {
+    files: ['**/examples/**/*.ts', '**/examples/**/*.js', '**/benchmarks/*.ts'],
+    rules: {
+      'implicit-dependencies/no-implicit': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-restricted-syntax': 'off',
+    },
+  },
+  // Override for packages/statemanager/src and packages/vm/src
+  {
+    files: ['packages/statemanager/src/**', 'packages/vm/src/**'],
+    rules: {
+      '@typescript-eslint/no-use-before-define': 'off',
+      'no-invalid-this': 'off',
+    },
+  },
+  // Override for packages/devp2p
+  {
+    files: ['packages/devp2p/**'],
+    rules: {
+      'no-redeclare': 'off',
+      'no-undef': 'off', // temporary until fixed: 'NodeJS' is not defined
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  // Override for packages/devp2p/src/ext
+  {
+    files: ['packages/devp2p/src/ext/**'],
+    rules: {
+      'no-restricted-syntax': 'off',
+    },
+  },
+  // Override for packages/client/src/ext
+  {
+    files: ['packages/client/src/ext/**'],
+    rules: {
+      'no-restricted-syntax': 'off',
+    },
+  },
+  // Override for packages/wallet
+  {
+    files: ['packages/wallet/**'],
+    rules: {
+      'github/array-foreach': 'warn',
+      'no-prototype-builtins': 'warn',
+      'no-restricted-syntax': 'off',
+    },
+  },
+  // Override for packages/rlp
+  {
+    files: ['packages/rlp/**'],
+    rules: {
+      'no-restricted-syntax': 'off',
+    },
+}]
