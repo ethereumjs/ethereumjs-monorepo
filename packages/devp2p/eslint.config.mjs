d4accaa16c0cad8fdeb67e43135fe098baacc1ac
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-03-16 00:18:12 -0700

    devp2p: fix lint issues

--- packages/devp2p/eslint.config.mjs
+++ packages/devp2p/eslint.config.mjs
@@ -3,6 +3,13 @@ import rootConfig from '../../config/eslint.config.mjs'
 export default [
   ...rootConfig,
   {
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.lint.json'],
+      },
+    },
+  },
+  {
     rules: {
       'no-redeclare': 'off',
       'no-undef': 'off', // temporary until fixed: 'NodeJS' is not defined
