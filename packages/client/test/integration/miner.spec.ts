fb37e05bc5973e146b90cd31f58d4a283aeae387
Amir <indigophi@protonmail.com>
2025-03-20 20:07:46 -0700

    Fix imports

--- packages/client/test/integration/miner.spec.ts
+++ packages/client/test/integration/miner.spec.ts
@@ -8,8 +8,8 @@ import {
 } from '@ethereumjs/util'
 import { assert, describe, it } from 'vitest'
 
+import { getLogger } from '../../bin/logging/winstonlogger.ts'
 import { Config } from '../../src/config.ts'
-import { getLogger } from '../../src/logging/winstonlogger.ts'
 import { Event } from '../../src/types.ts'
 import { createInlineClient } from '../../src/util/index.ts'
 import { parseMultiaddrs } from '../../src/util/parse.ts'
--- packages/client/test/sim/beaconsync.spec.ts
+++ packages/client/test/sim/beaconsync.spec.ts
@@ -4,8 +4,8 @@ import debug from 'debug'
 import { Client } from 'jayson/promise/index.js'
 import { assert, describe, it } from 'vitest'
 
+import { getLogger } from '../../bin/logging/winstonlogger.ts'
 import { Config } from '../../src/config.ts'
-import { getLogger } from '../../src/logging/winstonlogger.ts'
 import { Event } from '../../src/types.ts'
 import { createInlineClient } from '../../src/util/index.ts'
 
