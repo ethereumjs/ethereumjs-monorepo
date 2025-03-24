d3fe18ed212131c96120a45c176a771ec26fdbd0
Amir <indigophi@protonmail.com>
2025-03-24 11:51:41 -0700

    Group code better

--- packages/evm/src/opcodes/codes.ts
+++ packages/evm/src/opcodes/codes.ts
@@ -343,6 +343,19 @@ const eipOpcodes: { eip: number; opcodes: OpcodeEntry }[] = [
     },
   },
   {
+    eip: 6690,
+    opcodes: {
+      // control & i/o
+      0xc0: asyncAndDynamicGasOp('SETMODX'),
+      0xc1: asyncAndDynamicGasOp('LOADX'),
+      0xc2: asyncAndDynamicGasOp('STOREX'),
+      // arithmetic
+      0xc3: asyncAndDynamicGasOp('ADDMODX'),
+      0xc4: asyncAndDynamicGasOp('SUBMODX'),
+      0xc5: asyncAndDynamicGasOp('MULMODX'),
+    },
+  },
+  {
     eip: 7069,
     opcodes: {
       0xf7: defaultOp('RETURNDATALOAD'),
@@ -371,19 +384,6 @@ const eipOpcodes: { eip: number; opcodes: OpcodeEntry }[] = [
     opcodes: {
       0xec: asyncAndDynamicGasOp('EOFCREATE'),
       0xee: asyncAndDynamicGasOp('RETURNCONTRACT'),
-    },
-  },
-  {
-    eip: 6690,
-    opcodes: {
-      // control & i/o
-      0xc0: asyncAndDynamicGasOp('SETMODX'),
-      0xc1: asyncAndDynamicGasOp('LOADX'),
-      0xc2: asyncAndDynamicGasOp('STOREX'),
-      // arithmetic
-      0xc3: asyncAndDynamicGasOp('ADDMODX'),
-      0xc4: asyncAndDynamicGasOp('SUBMODX'),
-      0xc5: asyncAndDynamicGasOp('MULMODX'),
     },
   },
 ]
