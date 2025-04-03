9d870115d057169f3dea578787fd1f62e28fd9e4
Amir <indigophi@protonmail.com>
2025-04-03 10:46:52 -0700

    Update constants

--- packages/evm/src/evmmax/constants.ts
+++ packages/evm/src/evmmax/constants.ts
@@ -1,4 +1,6 @@
 export const STOREX_BASE_COST = 1
 export const LOADX_BASE_COST = 1
-export const SETMODX_ODD_MODULUS_COST = [23, 26, 29, 32, 36, 39, 42, 45, 48, 51, 54, 58]
 export const MAX_ALLOC_SIZE = 96 * 256
+export const SETMODX_ODD_MODULUS_COST = [23, 26, 29, 32, 36, 39, 42, 45, 48, 51, 54, 58]
+export const MULMODX_COST = [1, 1, 1, 2, 2, 3, 4, 5, 7, 8, 10, 12]
+export const ADD_OR_SUB_COST = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
