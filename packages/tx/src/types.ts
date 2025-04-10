8f835bd6a55da36ae3189ab2a3a6795e104c3324
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-04-10 18:23:42 +0200

    tx: remove TODO

--- packages/tx/src/types.ts
+++ packages/tx/src/types.ts
@@ -225,7 +225,7 @@ export interface TransactionInterface<T extends TransactionType = TransactionTyp
   errorStr(): string
 
   addSignature(
-    v: bigint, // TODO: change this to number?
+    v: bigint,
     r: Uint8Array | bigint,
     s: Uint8Array | bigint,
     convertV?: boolean,
