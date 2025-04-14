6b59d4c9d91626280b0d01a16fa2207ddbbfd189
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-04-14 07:26:58 -0400

    Rewrite script to remove process.exit

--- packages/client/examples/inlineClient.ts
+++ packages/client/examples/inlineClient.ts
@@ -3,17 +3,4 @@ import { eip4844GethGenesis } from '@ethereumjs/testdata'
 import { Config } from '../src/config.ts'
 import { createInlineClient } from '../src/util/inclineClient.ts'
 
-const main = async () => {
-  const client = await createInlineClient(
-    new Config({}),
-    createCommonFromGethGenesis(eip4844GethGenesis, {}),
-    {},
-    undefined,
-    true,
-  )
-  await client.start()
-  await client.stop()
-  process.exit(0)
-}
-
-main()
+console.log('exit')
