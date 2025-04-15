804f72ea95a4f5f60c18801b484dd169c75e569e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-04-14 20:26:20 -0400

    move noCompile to scripts dir

--- packages/client/scripts/inlineClient.ts
+++ packages/client/scripts/inlineClient.ts
@@ -3,4 +3,6 @@ import { eip4844GethGenesis } from '@ethereumjs/testdata'
 import { Config } from '../src/config.ts'
 import { createInlineClient } from '../src/util/inclineClient.ts'
 
+// This script imports `createInlineClient` which in turn loads all of our cour libraries and will trigger the UNSUPPORTED_TYPESCRIPT_SYNTAX error when
+// we run `node --conditions=typescript --experimental-strip-types examples/inlineClient.ts`
 console.log('exit')
