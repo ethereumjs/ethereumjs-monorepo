06f55dea723cf03c62eadd6a0d38d413b62c9877
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-04-16 05:53:51 -0400

    Move typecheck to separate run. Add invocation to noCompile

--- packages/client/scripts/inlineClient.ts
+++ packages/client/scripts/inlineClient.ts
@@ -3,6 +3,16 @@ import { eip4844GethGenesis } from '@ethereumjs/testdata'
 import { Config } from '../src/config.ts'
 import { createInlineClient } from '../src/util/inclineClient.ts'
 
-// This script imports `createInlineClient` which in turn loads all of our cour libraries and will trigger the UNSUPPORTED_TYPESCRIPT_SYNTAX error when
-// we run `node --conditions=typescript --experimental-strip-types examples/inlineClient.ts`
-console.log('exit')
+// This script imports `createInlineClient` which in turn loads all of our our libraries and will trigger the UNSUPPORTED_TYPESCRIPT_SYNTAX error when
+// we run `node --conditions=typescript --experimental-strip-types examples/inlineClient.ts` if there is unerasable syntax (e.g. <SomeType>someVar)
+
+const main = async () => {
+  const common = createCommonFromGethGenesis(eip4844GethGenesis, {})
+  const config = new Config({ common })
+  const client = await createInlineClient(config, common, {}, undefined, true)
+
+  console.log(`client is started: ${client.started}`)
+  await client.stop()
+}
+
+void main()
