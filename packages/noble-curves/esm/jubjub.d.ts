546251c1d7b8b031894619af11550a8fc2b543f0
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-04-22 00:33:36 +0200

    delete noble curves clone [no ci]

--- packages/noble-curves/esm/jubjub.d.ts
+++
@@ -1,5 +0,0 @@
-import { type CurveFn, type ExtPointType } from './abstract/edwards.js'
-export declare const jubjub: CurveFn
-export declare function groupHash(tag: Uint8Array, personalization: Uint8Array): ExtPointType
-export declare function findGroupHash(m: Uint8Array, personalization: Uint8Array): ExtPointType
-//# sourceMappingURL=jubjub.d.ts.map
--- packages/noble-curves/pasta.d.ts
+++
@@ -1,8 +0,0 @@
-import { type CurveFn } from './abstract/weierstrass.js'
-export declare const p: bigint
-export declare const q: bigint
-/** https://neuromancer.sk/std/other/Pallas */
-export declare const pallas: CurveFn
-/** https://neuromancer.sk/std/other/Vesta */
-export declare const vesta: CurveFn
-//# sourceMappingURL=pasta.d.ts.map
