de05e6620b49eb74676c8bdf9f761123c6172fe4
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-04-22 16:35:33 +0200

    update noble [no ci]

--- packages/evm/src/precompiles/bls12_381/noble.ts
+++ packages/evm/src/precompiles/bls12_381/noble.ts
@@ -20,9 +20,9 @@ import {
   BLS_ZERO_BUFFER,
 } from './constants.ts'
 
-import type { Fp2 } from '@noble/curves/abstract/tower'
-import type { AffinePoint } from '@noble/curves/abstract/weierstrass'
 import type { EVMBLSInterface } from '../../types.ts'
+import type { Fp2 } from './nbl/src/abstract/tower.ts'
+import type { AffinePoint } from './nbl/src/abstract/weierstrass.ts'
 
 const G1_ZERO = bls12_381.G1.ProjectivePoint.ZERO
 const G2_ZERO = bls12_381.G2.ProjectivePoint.ZERO
