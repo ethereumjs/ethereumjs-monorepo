36b2f313aaa15ebb76f1fdd55c25e984b65f301d
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-04-23 22:18:11 -0400

    client: more hexToBytes

--- packages/client/src/rpc/modules/web3.ts
+++ packages/client/src/rpc/modules/web3.ts
@@ -45,7 +45,7 @@ export class Web3 {
    * @param params The data to convert into a SHA3 hash
    */
   sha3(params: PrefixedHexString[]): PrefixedHexString {
-    const hexEncodedDigest = bytesToHex(keccak256(toBytes(hexToBytes(params[0]))))
+    const hexEncodedDigest = bytesToHex(keccak256(hexToBytes(params[0])))
     return hexEncodedDigest
   }
 }
