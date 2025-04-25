fd754876b25b9bcd7911e683404d5697a8adab67
Dargon@789 <apichanmittare@gmail.com>
2025-04-25 12:01:07 +0700

    nx build & test project vitest

---
+++ vitest-example/HelloWorld.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest'
+import '@testing-library/jest-dom'
+import { getByText } from '@testing-library/dom'
+import HelloWorld from './HelloWorld.js'
+
+test('renders name', () => {
+  const parent = HelloWorld({ name: 'Vitest' })
+  document.body.appendChild(parent)
+
+  const element = getByText(parent, 'Hello Vitest!')
+  expect(element).toBeInTheDocument()
+})
