a9a709e47fb41c6c46605c82bf84be7bc7e59238
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-04-27 10:03:42 -0400

    tx: small adjustments to 7702 tx example2

--- packages/tx/examples/EOACodeTx.ts
+++ packages/tx/examples/EOACodeTx.ts
@@ -1,22 +1,26 @@
 import { Common, Hardfork, Mainnet } from '@ethereumjs/common'
-import { createEOACode7702Tx } from '@ethereumjs/tx'
-import { type PrefixedHexString, createAddressFromPrivateKey, randomBytes } from '@ethereumjs/util'
+import { Capability, createEOACode7702Tx } from '@ethereumjs/tx'
+import {
+  EOACode7702AuthorizationListItem,
+  type PrefixedHexString,
+  createAddressFromPrivateKey,
+  randomBytes,
+} from '@ethereumjs/util'
 
-const ones32 = `0x${'01'.repeat(32)}` as PrefixedHexString
+const ones32: PrefixedHexString = `0x${'01'.repeat(32)}`
+const authorizationListItem: EOACode7702AuthorizationListItem = {
+  chainId: '0x2',
+  address: `0x${'20'.repeat(20)}`,
+  nonce: '0x1',
+  yParity: '0x1',
+  r: ones32,
+  s: ones32,
+}
 
-const common = new Common({ chain: Mainnet, hardfork: Hardfork.Cancun, eips: [7702] })
+const common = new Common({ chain: Mainnet, hardfork: Hardfork.Prague })
 const tx = createEOACode7702Tx(
   {
-    authorizationList: [
-      {
-        chainId: '0x2',
-        address: `0x${'20'.repeat(20)}`,
-        nonce: '0x1',
-        yParity: '0x1',
-        r: ones32,
-        s: ones32,
-      },
-    ],
+    authorizationList: [authorizationListItem],
     to: createAddressFromPrivateKey(randomBytes(32)),
   },
   { common },
@@ -25,3 +29,4 @@ const tx = createEOACode7702Tx(
 console.log(
   `EIP-7702 EOA code tx created with ${tx.authorizationList.length} authorization list item(s).`,
 )
+console.log('Tx supports EIP-7702? ', tx.supports(Capability.EIP7702EOACode))
