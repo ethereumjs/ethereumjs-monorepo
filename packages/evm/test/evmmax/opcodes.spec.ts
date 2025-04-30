9c978c45c6d3d056ab85ea5f09a84913224f0532
Amir <indigophi@protonmail.com>
2025-04-29 21:08:54 -0700

    Debug evmmax opcode tests

--- packages/evm/test/evmmax/opcodes.spec.ts
+++ packages/evm/test/evmmax/opcodes.spec.ts
@@ -1,4 +1,4 @@
-import { Common, Hardfork, Mainnet } from '@ethereumjs/common'
+import { Common, Mainnet } from '@ethereumjs/common'
 import { assert, describe, it } from 'vitest'
 
 import { type PrefixedHexString, createAddressFromString, hexToBytes } from '@ethereumjs/util'
@@ -51,8 +51,10 @@ function storex(dst: number, source: number, count: number) {
   )
 }
 
-function loadx(num_vals: number, val_idx: number, mem_offset: number) {
-  return numToOpcode(num_vals) + numToOpcode(val_idx) + numToOpcode(mem_offset) + LOADX
+function loadx(dest: number, source: number, count: number) {
+  return (
+    PUSH1 + numToOpcode(count) + PUSH1 + numToOpcode(source) + PUSH1 + numToOpcode(dest) + LOADX
+  )
 }
 
 function addmodx(
@@ -76,12 +78,46 @@ function addmodx(
   )
 }
 
-function submodx(result_slot_idx: number, x_slot_idx: number, y_slot_idx: number) {
-  return SUBMODX + numToOpcode(result_slot_idx) + numToOpcode(x_slot_idx) + numToOpcode(y_slot_idx)
+function submodx(
+  result_slot_idx: number,
+  result_stride: number,
+  x_slot_idx: number,
+  x_stride: number,
+  y_slot_idx: number,
+  y_stride: number,
+  count: number,
+) {
+  return (
+    SUBMODX +
+    numToOpcode(result_slot_idx) +
+    numToOpcode(result_stride) +
+    numToOpcode(x_slot_idx) +
+    numToOpcode(x_stride) +
+    numToOpcode(y_slot_idx) +
+    numToOpcode(y_stride) +
+    numToOpcode(count)
+  )
 }
 
-function mulmodx(result_slot_idx: number, x_slot_idx: number, y_slot_idx: number) {
-  return MULMODX + numToOpcode(result_slot_idx) + numToOpcode(x_slot_idx) + numToOpcode(y_slot_idx)
+function mulmodx(
+  result_slot_idx: number,
+  result_stride: number,
+  x_slot_idx: number,
+  x_stride: number,
+  y_slot_idx: number,
+  y_stride: number,
+  count: number,
+) {
+  return (
+    MULMODX +
+    numToOpcode(result_slot_idx) +
+    numToOpcode(result_stride) +
+    numToOpcode(x_slot_idx) +
+    numToOpcode(x_stride) +
+    numToOpcode(y_slot_idx) +
+    numToOpcode(y_stride) +
+    numToOpcode(count)
+  )
 }
 
 describe('EVM -> getActiveOpcodes()', () => {
@@ -101,22 +137,25 @@ describe('EVM -> getActiveOpcodes()', () => {
       },
     })
     const evm = await createEVM({ common })
+    evm.events.on('step', (e) => {
+      console.log(e.opcode.name)
+    })
 
     console.log('dbg100')
     console.log(addmodx(0, 1, 1, 1, 2, 1, 1))
     // create bytecode
     const bytecode =
       '0x' +
-      mstore8(0, 8) + // store value 0x07 at index 0 in memory
+      mstore8(0, 8) + // store value 0x08 at index 0 in memory
       setupx(0, 1, 0, 3) +
       mstore8(1, 3) +
       mstore8(2, 6) +
       storex(1, 1, 1) +
       storex(2, 2, 1) +
-      addmodx(0, 1, 1, 1, 2, 1, 1)
-    // + mulmodx(2, 2, 1)
-    // + submodx(2, 2, 1)
-    // + loadx(1, 2, 96)
+      addmodx(0, 1, 1, 1, 2, 1, 1) +
+      mulmodx(0, 1, 1, 1, 2, 1, 1) +
+      submodx(0, 1, 1, 1, 2, 1, 1) +
+      loadx(0, 0, 1)
     // + ret(96, 32)
 
     const ADDR_TO_CALL = createAddressFromString('0x' + '20'.repeat(20))
