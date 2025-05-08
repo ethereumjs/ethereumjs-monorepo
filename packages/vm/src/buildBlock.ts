c7be5e521980f478d374ad004fb2e94140d1ef11
harkamal <develop@g11tech.io>
2025-05-08 16:49:30 +0530

    more debug log

--- packages/vm/src/buildBlock.ts
+++ packages/vm/src/buildBlock.ts
@@ -8,7 +8,11 @@ import {
 import { ConsensusType, Hardfork } from '@ethereumjs/common'
 import { MerklePatriciaTrie } from '@ethereumjs/mpt'
 import { RLP } from '@ethereumjs/rlp'
-import { Blob4844Tx, createMinimal4844TxFromNetworkWrapper } from '@ethereumjs/tx'
+import {
+  Blob4844Tx,
+  NetworkWrapperType,
+  createMinimal4844TxFromNetworkWrapper,
+} from '@ethereumjs/tx'
 import {
   Address,
   BIGINT_0,
@@ -243,7 +247,14 @@ export class BlockBuilder {
       )
     }
     let blobGasUsed = undefined
+    let network4844tx = 0
+    let network7596tx = 0
     if (tx instanceof Blob4844Tx) {
+      if (tx.networkWrapperVersion === NetworkWrapperType.EIP4844) {
+        network4844tx++
+      } else {
+        network7596tx++
+      }
       if (this.blockOpts.common?.isActivatedEIP(4844) === false) {
         throw Error('eip4844 not activated yet for adding a blob transaction')
       }
@@ -264,6 +275,7 @@ export class BlockBuilder {
 
       blobGasUsed = this.blobGasUsed
     }
+    console.log({ network4844tx, network7596tx })
     const header = {
       ...this.headerData,
       gasUsed: this.gasUsed,
