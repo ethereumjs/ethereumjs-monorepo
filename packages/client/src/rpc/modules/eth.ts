b112d813a73889ebbfbf7d2f7420df5c4c9c63ce
harkamal <develop@g11tech.io>
2025-05-08 18:47:30 +0530

    add debug log

--- packages/client/src/rpc/modules/eth.ts
+++ packages/client/src/rpc/modules/eth.ts
@@ -1166,6 +1166,7 @@ export class Eth {
           common.isActivatedEIP(7594) &&
           tx.networkWrapperVersion !== NetworkWrapperType.EIP7594
         ) {
+          console.log('received old format tx', { hash: bytesToHex(tx.hash()) })
           throw Error(
             `tx with networkWrapperVersion=${tx.networkWrapperVersion} sent for EIP-7594 activated hardfork=${common.hardfork()}`,
           )
