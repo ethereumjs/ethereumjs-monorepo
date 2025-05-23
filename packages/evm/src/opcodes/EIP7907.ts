6e73e7074c5eb1b28d837e932b10da03be5ef579
jgresham <johnsgresham@gmail.com>
2025-05-23 15:31:57 -0700

    minor cleanup EIP7907.ts and add param to tx/src/params

--- packages/evm/src/opcodes/EIP7907.ts
+++ packages/evm/src/opcodes/EIP7907.ts
@@ -5,14 +5,13 @@ import type { RunState } from '../interpreter.ts'
 import { ceil32 } from './util.ts'
 
 /**
- * Adds address to accessAddressCode set if not already included.
+ * Adds address to warmedCodeAddress set if not already included.
  * Adjusts cost incurred for executing opcode based on whether address code
  * is warm/cold and whether the address is a large contract. (EIP 7907)
  * @param {RunState} runState
  * @param {Address}  address
  * @param {Common}   common
  * @param {Boolean}  chargeGas (default: true)
- * @param {Boolean}  isSelfdestruct (default: false)
  */
 export function accessAddressCodeEIP7907(
   runState: RunState,
@@ -26,18 +25,16 @@ export function accessAddressCodeEIP7907(
   if (!runState.interpreter.journal.isWarmedCodeAddress(address)) {
     runState.interpreter.journal.addWarmedCodeAddress(address)
 
-    // CREATE, CREATE2 opcodes have the address code warmed for free.
     if (chargeGas) {
       // EIP 7907:
       // get the large contract cost gas
-      // initCodeWordGas (2) * every byte (rounded up to the next 32 bytes) over the excessCodeSizeThreshold
       const excessContractSize = BigInt(
         Math.max(
           0,
           runState.env.contract.codeSize - Number(common.param('excessCodeSizeThreshold')),
         ),
       )
-      const largeContractCost = ceil32(excessContractSize) * common.param('initCodeWordGas')
+      const largeContractCost = ceil32(excessContractSize) * common.param('initCodeWordGas') // 2
       return largeContractCost
     }
   }
--- packages/tx/src/params.ts
+++ packages/tx/src/params.ts
@@ -70,6 +70,8 @@ export const paramsTx: ParamsDict = {
    * Meter Contract Code Size And Increase Limit
    */
   7907: {
+    // The threshold for the excess contract code size (prev. maxCodeSize EIP-607)
+    excessCodeSizeThreshold: 24576, // 24 x 1024 = 24576 bytes (0x6000)
     // Maximum length of contract code
     maxCodeSize: 262144, // 256 × 1024 = 262,144 bytes (0x40000)
     // Maximum length of initialization code when creating a contract
