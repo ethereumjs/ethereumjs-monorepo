5d25bf5e9ed31b913684294df7d57e9612851155
Holger Drewes <Holger.Drewes@gmail.com>
2025-05-28 10:17:50 +0200

    Add basic chain creation framing

--- packages/client/test/rpc/eth/getMaxPriorityFeePerGas.spec.ts
+++ packages/client/test/rpc/eth/getMaxPriorityFeePerGas.spec.ts
@@ -1,24 +1,45 @@
 import { assert, describe, it } from 'vitest'
 
 import { createClient, createManager, getRPCClient, startRPC } from '../helpers.ts'
+//import { createFeeMarket1559Tx } from '@ethereumjs/tx'
 
 const method = 'eth_getMaxPriorityFeePerGas'
 
-function createChain() {
-  const block = {
+interface Block {
+  transactions: any[]
+  hash: () => Uint8Array
+}
+
+function createBlock(_blockMPF: bigint[]): Block {
+  return {
     transactions: [],
     hash: () => new Uint8Array([1]),
   }
+}
+
+function createChain(blocksMPF: bigint[][] = [[]]) {
+  /*const tx = createFeeMarket1559Tx({
+    maxPriorityFeePerGas: 123456789n,
+  })*/
+  if (blocksMPF.length === 0) {
+    throw new Error('call with minimum 1 block')
+  }
+
+  const blocks: Block[] = []
+
+  for (const blockMPF of blocksMPF) {
+    const block = createBlock(blockMPF)
+    blocks.push(block)
+  }
+  const latest = blocks[blocks.length - 1]
   return {
-    blocks: { latest: block },
-    getBlock: () => block,
-    getCanonicalHeadBlock: () => block,
-    getBlocks: () => [block],
+    getCanonicalHeadBlock: () => latest,
+    getBlocks: () => blocks,
   }
 }
 
 describe(method, () => {
-  it('calls', async () => {
+  it('should return 0 for a simple block with no transactions', async () => {
     const client = await createClient({ chain: createChain() })
     const manager = createManager(client)
     const rpcServer = startRPC(manager.getMethods())
@@ -27,7 +48,6 @@ describe(method, () => {
     client.config.synchronized = true
 
     const res = await rpc.request(method, [])
-    console.log(res)
-    assert.strictEqual(1, 1)
+    assert.strictEqual(res.result, '0x0')
   })
 })
