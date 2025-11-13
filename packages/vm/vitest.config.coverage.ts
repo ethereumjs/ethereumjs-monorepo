08158a073f21386d2867420ddd2ab3d93a0271ff
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-11-13 15:15:29 -0500

    chore: undo defualt

--- packages/vm/vitest.config.coverage.ts
+++ packages/vm/vitest.config.coverage.ts
@@ -2,7 +2,7 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [topLevelAwait.default()],
+  plugins: [topLevelAwait],
   optimizeDeps: {
     exclude: ['kzg-wasm'],
   },
