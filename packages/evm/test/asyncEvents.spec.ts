3a4e78d80d2f2a767a2b7c1e72c0f0710bd4d310
Jochem Brouwer <jochembrouwer96@gmail.com>
2025-11-17 18:43:31 -0300

    evm: harden asyncEvent test

--- packages/evm/test/asyncEvents.spec.ts
+++ packages/evm/test/asyncEvents.spec.ts
@@ -1,5 +1,5 @@
 import { Common, Hardfork, Mainnet } from '@ethereumjs/common'
-import { Address, hexToBytes } from '@ethereumjs/util'
+import { Address, createAddressFromBigInt, hexToBytes } from '@ethereumjs/util'
 import { assert, describe, it } from 'vitest'
 
 import { createEVM } from '../src/index.ts'
@@ -11,10 +11,14 @@ describe('async events', () => {
     const evm = await createEVM({
       common,
     })
+    const targ = createAddressFromBigInt(BigInt(123456))
+    await evm.stateManager.putCode(targ, hexToBytes('0x6001'))
+    let didTimeOut = false
     evm.events.on('step', async (event, next) => {
       const startTime = Date.now()
       setTimeout(() => {
         assert.isTrue(Date.now() > startTime + 999, 'evm paused on step function for one second')
+        didTimeOut = true
         next?.()
       }, 1000)
     })
@@ -22,7 +26,9 @@ describe('async events', () => {
       caller, // call address
       gasLimit: BigInt(0xffffffffff),
       data: hexToBytes('0x600000'),
+      to: targ,
     }
     await evm.runCall(runCallArgs)
+    assert.isTrue(didTimeOut)
   })
 })
