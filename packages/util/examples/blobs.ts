ab12d49c18ac6c34e5aed7f6cda9f0c998555ea1
Holger Drewes <Holger.Drewes@gmail.com>
2025-11-18 13:52:55 +0100

    Expand util blob example

--- packages/util/examples/blobs.ts
+++ packages/util/examples/blobs.ts
@@ -1,12 +1,44 @@
-import { bytesToHex, computeVersionedHash, getBlobs } from '@ethereumjs/util'
+import * as fs from 'fs'
+import {
+  type PrefixedHexString,
+  blobsToCellProofs,
+  blobsToProofs,
+  computeVersionedHash,
+  getBlob,
+  getBlobs,
+  hexToBytes,
+} from '@ethereumjs/util'
+import { trustedSetup } from '@paulmillr/trusted-setups/fast-peerdas.js'
+import { KZG as microEthKZG } from 'micro-eth-signer/kzg.js'
 
-const blobs = getBlobs('test input')
+const kzg = new microEthKZG(trustedSetup)
+
+// Use with node ./examples/blobs.ts <file path>
+const filePath = process.argv[2]
+let blobData: string = fs.readFileSync(filePath, 'ascii')
+console.log(blobData)
+console.log(blobData.length)
+blobData = blobData.substring(0, 100)
+
+const blobs = [getBlob(hexToBytes(`0x${blobData}`))]
+
+//const blobData = 'hello'
+//const blobs = getBlobs(blobData)
 
 console.log('Created the following blobs:')
-console.log(blobs)
+//console.log(blobs)
 
-const commitment = bytesToHex(new Uint8Array([1, 2, 3]))
+const commitment = kzg.blobToKzgCommitment(blobs[0])
+
 const blobCommitmentVersion = 0x01
-const versionedHash = computeVersionedHash(commitment, blobCommitmentVersion)
+const versionedHash = computeVersionedHash(commitment as PrefixedHexString, blobCommitmentVersion)
 
-console.log(`Versioned hash ${versionedHash} computed`)
+// EIP-4844 only
+const blobProof = blobsToProofs(kzg, blobs, [commitment as PrefixedHexString])
+const cellProofs = blobsToCellProofs(kzg, blobs)
+
+console.log(`Commitment                  : ${commitment}`)
+console.log(`Versioned hash              : ${versionedHash}`)
+console.log(`Blob proof (EIP-4844)       : ${blobProof}`)
+console.log(`First cell proof (EIP-7594) : ${cellProofs[0]}`)
+console.log(`Num cell proofs (EIP-7594)  : ${cellProofs.length}`)
