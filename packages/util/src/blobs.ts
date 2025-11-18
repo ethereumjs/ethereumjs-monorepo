ee631465ac6e76cc0df009f9b1f709d777f30f7e
Holger Drewes <Holger.Drewes@gmail.com>
2025-11-18 13:52:55 +0100

    Expand util blob example

--- packages/util/src/blobs.ts
+++ packages/util/src/blobs.ts
@@ -36,7 +36,7 @@ function getPadded(data: Uint8Array, blobs_len: number): Uint8Array {
  * @param data Input data (must be exactly BLOB_SIZE bytes)
  * @returns Hex-prefixed blob string
  */
-function getBlob(data: Uint8Array): PrefixedHexString {
+export function getBlob(data: Uint8Array): PrefixedHexString {
   const blob = new Uint8Array(BLOB_SIZE)
   for (let i = 0; i < FIELD_ELEMENTS_PER_BLOB; i++) {
     const chunk = new Uint8Array(32)
