759e0893dbfe4d4a45c5f2101b3425058df6ae05
ScottyPoi <scott.simpson@ethereum.org>
2026-01-20 22:53:51 -0700

    define types for BAL

--- packages/util/src/bal.ts
+++ packages/util/src/bal.ts
@@ -1,5 +1,41 @@
-import { RLP } from '@ethereumjs/rlp'
+import { type Input, RLP } from '@ethereumjs/rlp'
 import { keccak_256 } from '@noble/hashes/sha3.js'
+
+type Address = string // bytes20
+type StorageKey = string // uint256
+type StorageValue = Uint8Array // uint256
+type ByteCode = Uint8Array // bytes
+type BlockAccessIndex = number // uint16
+type Balance = bigint // uint256
+type Nonce = bigint // uint64
+
+type StorageChange = [BlockAccessIndex, StorageValue]
+type BalanceChange = [BlockAccessIndex, Balance]
+type NonceChange = [BlockAccessIndex, Nonce]
+type CodeChange = [BlockAccessIndex, ByteCode]
+type SlotChanges = [StorageKey, StorageChange[]]
+
+type AccountChanges = [
+  Address,
+  SlotChanges[],
+  StorageKey[],
+  BalanceChange[],
+  NonceChange[],
+  CodeChange[],
+]
+
+type BlockAccessList = AccountChanges[]
+
+type Accesses = Record<
+  Address,
+  {
+    storageWrites: Record<StorageKey, [BlockAccessIndex, StorageValue][]>
+    storageReads: Set<StorageKey>
+    balanceChanges: [BlockAccessIndex, Balance][]
+    nonceChanges: [BlockAccessIndex, Nonce][]
+    codeChanges: [BlockAccessIndex, ByteCode][]
+  }
+>
 
 /**
  * Structural helper class for block level access lists
