ca93f37c3831f3e88531c47a5cd7269b00d030f2
Holger Drewes <Holger.Drewes@gmail.com>
2026-01-27 12:07:07 +0100

    Convert testdata to internal format (AI supported), integrate into tests

--- packages/util/test/bal.spec.ts
+++ packages/util/test/bal.spec.ts
@@ -2,17 +2,34 @@ import { assert, describe, it } from 'vitest'
 
 import {
   type BALJSONBlockAccessList,
+  BlockLevelAccessList,
   createBlockLevelAccessList,
   createBlockLevelAccessListFromJSON,
 } from '../src/bal.ts'
 import bal_all_transaction_types from './testdata/bal/bal_all_transaction_types.json' with {
   type: 'json',
 }
+import { balSimple } from './testdata/bal/bal_simple.ts'
 
 describe('Basic initialization', () => {
   it('should create an empty access list', () => {
     const bal = createBlockLevelAccessList()
     assert.isNotNull(bal)
+  })
+
+  it('should create an access list from Accesses data', () => {
+    const bal = new BlockLevelAccessList(balSimple)
+    assert.isNotNull(bal)
+    assert.deepEqual(bal.accesses, balSimple)
+
+    // Verify some specific data was loaded correctly
+    const address1 = '0x00000961ef480eb55e80d19ad83579a64c007002'
+    assert.equal(bal.accesses[address1].storageReads.size, 4)
+    assert.isTrue(bal.accesses[address1].storageReads.has('0x00'))
+
+    const addressWithBalanceChanges = '0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba'
+    assert.equal(bal.accesses[addressWithBalanceChanges].balanceChanges.length, 2)
+    assert.deepEqual(bal.accesses[addressWithBalanceChanges].balanceChanges[0], [0x01, 0xf618n])
   })
 })
 
