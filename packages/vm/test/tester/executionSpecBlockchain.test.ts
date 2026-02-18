aed7cd8a3e5edf153b6333ec539bea2ede20ef20
ScottyPoi <scott.simpson@ethereum.org>
2026-02-18 15:08:19 -0700

    Add error message to test

--- packages/vm/test/tester/executionSpecBlockchain.test.ts
+++ packages/vm/test/tester/executionSpecBlockchain.test.ts
@@ -215,6 +215,7 @@ const exceptionMessages: Record<string, RegExp> = {
   'TransactionException.TYPE_4_TX_PRE_FORK': /^EIP-7702 not enabled on Common$/,
 
   // BlockException entries
+  'BlockException.GAS_USED_OVERFLOW': /tx has a higher gas limit than the block/,
   'BlockException.INCORRECT_BLOB_GAS_USED': /invalid blobGasUsed/,
   'BlockException.INCORRECT_BLOCK_FORMAT':
     /(?:blob gas used can only be provided with EIP4844 activated|^invalid header.*)/,
