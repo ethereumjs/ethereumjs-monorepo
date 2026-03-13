da981173119bc7403f83746915bdf3cb23c3b60f
ScottyPoi <scott.simpson@ethereum.org>
2026-03-13 16:07:42 -0600

    vm: create consumeBal function

---
+++ packages/vm/src/consumeBal.ts
@@ -0,0 +1,57 @@
+import type { MerkleStateManager } from '@ethereumjs/statemanager'
+import {
+  type BALJSONBlockAccessList,
+  bytesToHex,
+  createAddressFromString,
+  equalsBytes,
+  hexToBigInt,
+  hexToBytes,
+  setLengthLeft,
+} from '@ethereumjs/util'
+import type { VM } from './vm.ts'
+
+export async function consumeBal(
+  vm: VM,
+  bal: BALJSONBlockAccessList,
+  expectedStateRoot?: Uint8Array,
+) {
+  for (const acc of bal) {
+    if (
+      acc.balanceChanges.length === 0 &&
+      acc.nonceChanges.length === 0 &&
+      acc.codeChanges.length === 0 &&
+      acc.storageChanges.length === 0
+    ) {
+      continue
+    }
+    const balance =
+      acc.balanceChanges.slice(-1)[0]?.postBalance &&
+      hexToBigInt(acc.balanceChanges.slice(-1)[0]?.postBalance)
+    const nonce =
+      acc.nonceChanges.slice(-1)[0]?.postNonce &&
+      hexToBigInt(acc.nonceChanges.slice(-1)[0]?.postNonce)
+    const code = acc.codeChanges.slice(-1)[0]?.newCode ?? undefined
+    if (code !== undefined) {
+      await vm.stateManager.putCode(createAddressFromString(acc.address), hexToBytes(code))
+    }
+    await vm.stateManager.modifyAccountFields(createAddressFromString(acc.address), {
+      balance,
+      nonce,
+    })
+    for (const storage of acc.storageChanges) {
+      const value = storage.slotChanges.slice(-1)[0].postValue
+      await vm.stateManager.putStorage(
+        createAddressFromString(acc.address),
+        setLengthLeft(hexToBytes(storage.slot), 32),
+        setLengthLeft(hexToBytes(value), 32),
+      )
+    }
+    await (vm.stateManager as MerkleStateManager).flush()
+  }
+  const stateRoot = await vm.stateManager.getStateRoot()
+  if (expectedStateRoot && !equalsBytes(expectedStateRoot, stateRoot)) {
+    throw new Error(
+      `Expected state root ${bytesToHex(expectedStateRoot)} but got ${bytesToHex(stateRoot)}`,
+    )
+  }
+}
